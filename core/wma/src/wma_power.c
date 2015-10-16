67bc5525855c1041d1666d9572246919fe2b64ab
Peng Xu <pxu@qca.qualcomm.com>
2015-10-16 16:02:26 -0700

    qcacld-3.0: Vendor command to scale TX power
    This is for FR23278: Runtime configuration of softAP Tx power.
    The value that send by the vendor command will set the power levels
    as below:
    	0 --- no scaling, 100%
    	1 --- 50% of max power
    	2 --- 25% of max power
    	3 --- 12% of max power
    	4 --- minimum power
    Another vendor command is to decrease the power by actual db value.
    
    CRs-fixed: 941772
    Change-Id: I61d48299b4af361c214e2ab8bb1fa848161431de

--- core/wma/src/wma_power.c
+++ core/wma/src/wma_power.c
@@ -2252,3 +2252,62 @@ void wma_set_resume_dtim(tp_wma_handle wma)
 	}
 }
 
+/**
+ * wma_set_tx_power_scale() - set tx power scale
+ * @vdev_id: vdev id
+ * @value: value
+ *
+ * Return: CDF_STATUS_SUCCESS for success or error code.
+ */
+CDF_STATUS wma_set_tx_power_scale(uint8_t vdev_id, int value)
+{
+	int ret = CDF_STATUS_SUCCESS;
+	tp_wma_handle wma_handle =
+			(tp_wma_handle)cds_get_context(CDF_MODULE_ID_WMA);
+	if (NULL == wma_handle) {
+		WMA_LOGE("%s: wma_handle is NULL", __func__);
+		return CDF_STATUS_E_FAILURE;
+	}
+
+	if (!(wma_handle->interfaces[vdev_id].vdev_up)) {
+		WMA_LOGE("%s: vdev id %d is not up", __func__, vdev_id);
+		return CDF_STATUS_E_FAILURE;
+	}
+
+	ret = wmi_unified_vdev_set_param_send(wma_handle->wmi_handle, vdev_id,
+				WMI_VDEV_PARAM_TXPOWER_SCALE, value);
+	if (ret != 0)
+		WMA_LOGE("Set tx power scale failed");
+
+	return ret;
+}
+
+/**
+ * wma_set_tx_power_scale_decr_db() - decrease power by DB value
+ * @vdev_id: vdev id
+ * @value: value
+ *
+ * Return: CDF_STATUS_SUCCESS for success or error code.
+ */
+CDF_STATUS wma_set_tx_power_scale_decr_db(uint8_t vdev_id, int value)
+{
+	int ret = CDF_STATUS_SUCCESS;
+	tp_wma_handle wma_handle =
+			(tp_wma_handle)cds_get_context(CDF_MODULE_ID_WMA);
+	if (NULL == wma_handle) {
+		WMA_LOGE("%s: wma_handle is NULL", __func__);
+		return CDF_STATUS_E_FAILURE;
+	}
+
+	if (!(wma_handle->interfaces[vdev_id].vdev_up)) {
+		WMA_LOGE("%s: vdev id %d is not up", __func__, vdev_id);
+		return CDF_STATUS_E_FAILURE;
+	}
+
+	ret = wmi_unified_vdev_set_param_send(wma_handle->wmi_handle, vdev_id,
+				WMI_VDEV_PARAM_TXPOWER_SCALE_DECR_DB, value);
+	if (ret != 0)
+		WMA_LOGE("Decrease tx power value failed");
+
+	return ret;
+}
