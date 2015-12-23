ce7212f41425818d14b23bf7690f1195c0790d52
Satish Singh <c_ssing@qca.qualcomm.com>
2015-12-23 15:40:13 -0800

    Release 5.0.0.153
    Release 5.0.0.153
    
    Change-Id: I30b27c1d77de35cacb94e0b1ac3f384e87dd7ece
    CRs-Fixed: 688141

--- core/mac/inc/qwlan_version.h
+++ core/mac/inc/qwlan_version.h
@@ -42,8 +42,8 @@
 #define QWLAN_VERSION_MINOR            0
 #define QWLAN_VERSION_PATCH            0
 #define QWLAN_VERSION_EXTRA            ""
-#define QWLAN_VERSION_BUILD            152
+#define QWLAN_VERSION_BUILD            153
 
-#define QWLAN_VERSIONSTR               "5.0.0.152"
+#define QWLAN_VERSIONSTR               "5.0.0.153"
 
 #endif /* QWLAN_VERSION_H */
