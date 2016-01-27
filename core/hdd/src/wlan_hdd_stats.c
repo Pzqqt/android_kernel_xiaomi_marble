abfad4f2011348995777ba6d7f71850fb38a0a41
Edhar, Mahesh Kumar <c_medhar@qti.qualcomm.com>
2016-01-27 16:33:05 +0530

    qcacld-3.0: Dump TX/RX count on get_station cfg80211 function call
    qcacld-2.0 to qcacld-3.0 propagation
    
    print TX/RX packet count values during get_station
    function call to help in debugging data path issues.
    
    Change-Id: I0798123f96111bbadc4014f5f04193d88977a38b
    CRs-Fixed: 956473

--- core/hdd/src/wlan_hdd_stats.c
+++ core/hdd/src/wlan_hdd_stats.c
@@ -2118,12 +2118,6 @@ static int __wlan_hdd_cfg80211_get_station(struct wiphy *wiphy,
 		}
 	}
 
-	if (rate_flags & eHAL_TX_RATE_LEGACY)
-		hdd_info("Reporting legacy rate %d", sinfo->txrate.legacy);
-	else
-		hdd_info("Reporting MCS rate %d flags 0x%x",
-			 sinfo->txrate.mcs, sinfo->txrate.flags);
-
 	sinfo->filled |= STATION_INFO_TX_BITRATE;
 
 	sinfo->tx_bytes = pAdapter->stats.tx_bytes;
@@ -2156,6 +2150,16 @@ static int __wlan_hdd_cfg80211_get_station(struct wiphy *wiphy,
 
 	sinfo->rx_packets = pAdapter->stats.rx_packets;
 	sinfo->filled |= STATION_INFO_RX_PACKETS;
+
+	if (rate_flags & eHAL_TX_RATE_LEGACY)
+		hddLog(LOG1, FL("Reporting legacy rate %d pkt cnt tx %d rx %d"),
+			sinfo->txrate.legacy, sinfo->tx_packets,
+			sinfo->rx_packets);
+	else
+		hddLog(LOG1,
+			FL("Reporting MCS rate %d flags 0x%x pkt cnt tx %d rx %d"),
+			sinfo->txrate.mcs, sinfo->txrate.flags,
+			sinfo->tx_packets, sinfo->rx_packets);
 
 	MTRACE(cdf_trace(CDF_MODULE_ID_HDD,
 			 TRACE_CODE_HDD_CFG80211_GET_STA,
