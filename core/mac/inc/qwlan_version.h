107e6a36c1d1f9ca32ec5cf3b90582173786ff4f
Satish Singh <ssing@codeaurora.org>
2016-02-10 15:58:59 -0800

    Release 5.1.0.3G
    Rebase of 5.0.0.163 onto 5.1.0.2G
    
    Change-Id: If244ee2a2bc26a8b4387c89d7bce965c00497630
    CRs-Fixed: 688141

--- core/mac/inc/qwlan_version.h
+++ core/mac/inc/qwlan_version.h
@@ -42,8 +42,8 @@
 #define QWLAN_VERSION_MINOR            1
 #define QWLAN_VERSION_PATCH            0
 #define QWLAN_VERSION_EXTRA            "G"
-#define QWLAN_VERSION_BUILD            2
+#define QWLAN_VERSION_BUILD            3
 
-#define QWLAN_VERSIONSTR               "5.1.0.2G"
+#define QWLAN_VERSIONSTR               "5.1.0.3G"
 
 #endif /* QWLAN_VERSION_H */
