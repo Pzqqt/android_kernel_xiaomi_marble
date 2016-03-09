d7468a5cd3465616a263909bface1f738d2d02cd
Govind Singh <govinds@codeaurora.org>
2016-03-09 14:32:57 +0530

    qcacmn: Adpat common wmi layer with QDF os abstraction
    Adapt common wmi layer with converged os abstraction layer.
    Remove references to non-converged header files from
    wmi layer.
    
    Change-Id: I3671a40604fa1a5f10a5a67106db33f382e0f335
    CRs-Fixed: 983619

--- wmi_unified_non_tlv.h
+++ wmi_unified_non_tlv.h
@@ -31,99 +31,99 @@
 
 #include "ol_defines.h" /* Fix Me: wmi_unified_t structure definition */
 
-CDF_STATUS send_vdev_create_cmd_non_tlv(wmi_unified_t wmi_handle,
+QDF_STATUS send_vdev_create_cmd_non_tlv(wmi_unified_t wmi_handle,
 				 uint8_t macaddr[IEEE80211_ADDR_LEN],
 				 struct vdev_create_params *param);
 
-CDF_STATUS send_vdev_delete_cmd_non_tlv(wmi_unified_t wmi_handle,
+QDF_STATUS send_vdev_delete_cmd_non_tlv(wmi_unified_t wmi_handle,
 					  uint8_t if_id);
 
-CDF_STATUS send_vdev_stop_cmd_non_tlv(wmi_unified_t wmi,
+QDF_STATUS send_vdev_stop_cmd_non_tlv(wmi_unified_t wmi,
 					uint8_t vdev_id);
 
-CDF_STATUS send_vdev_down_cmd_non_tlv(wmi_unified_t wmi,
+QDF_STATUS send_vdev_down_cmd_non_tlv(wmi_unified_t wmi,
 			uint8_t vdev_id);
 
-CDF_STATUS send_peer_flush_tids_cmd_non_tlv(wmi_unified_t wmi,
+QDF_STATUS send_peer_flush_tids_cmd_non_tlv(wmi_unified_t wmi,
 					 uint8_t peer_addr[IEEE80211_ADDR_LEN],
 					 struct peer_flush_params *param);
 
-CDF_STATUS send_peer_delete_cmd_non_tlv(wmi_unified_t wmi,
+QDF_STATUS send_peer_delete_cmd_non_tlv(wmi_unified_t wmi,
 				    uint8_t peer_addr[IEEE80211_ADDR_LEN],
 				    uint8_t vdev_id);
 
-CDF_STATUS send_peer_param_cmd_non_tlv(wmi_unified_t wmi,
+QDF_STATUS send_peer_param_cmd_non_tlv(wmi_unified_t wmi,
 				uint8_t peer_addr[IEEE80211_ADDR_LEN],
 				struct peer_set_params *param);
 
-CDF_STATUS send_vdev_up_cmd_non_tlv(wmi_unified_t wmi,
+QDF_STATUS send_vdev_up_cmd_non_tlv(wmi_unified_t wmi,
 			     uint8_t bssid[IEEE80211_ADDR_LEN],
 				 struct vdev_up_params *params);
 
-CDF_STATUS send_peer_create_cmd_non_tlv(wmi_unified_t wmi,
+QDF_STATUS send_peer_create_cmd_non_tlv(wmi_unified_t wmi,
 					struct peer_create_params *param);
 
-CDF_STATUS send_green_ap_ps_cmd_non_tlv(wmi_unified_t wmi_handle,
+QDF_STATUS send_green_ap_ps_cmd_non_tlv(wmi_unified_t wmi_handle,
 						uint32_t value, uint8_t mac_id);
 
-CDF_STATUS
+QDF_STATUS
 send_pdev_utf_cmd_non_tlv(wmi_unified_t wmi_handle,
 				struct pdev_utf_params *param,
 				uint8_t mac_id);
 
-CDF_STATUS
+QDF_STATUS
 send_pdev_param_cmd_non_tlv(wmi_unified_t wmi_handle,
 			   struct pdev_params *param,
 				uint8_t mac_id);
 
-CDF_STATUS send_suspend_cmd_non_tlv(wmi_unified_t wmi_handle,
+QDF_STATUS send_suspend_cmd_non_tlv(wmi_unified_t wmi_handle,
 				struct suspend_params *param,
 				uint8_t mac_id);
 
-CDF_STATUS send_resume_cmd_non_tlv(wmi_unified_t wmi_handle,
+QDF_STATUS send_resume_cmd_non_tlv(wmi_unified_t wmi_handle,
 				uint8_t mac_id);
 
-CDF_STATUS send_wow_enable_cmd_non_tlv(wmi_unified_t wmi_handle,
+QDF_STATUS send_wow_enable_cmd_non_tlv(wmi_unified_t wmi_handle,
 				struct wow_cmd_params *param,
 				uint8_t mac_id);
 
-CDF_STATUS send_set_ap_ps_param_cmd_non_tlv(wmi_unified_t wmi_handle,
+QDF_STATUS send_set_ap_ps_param_cmd_non_tlv(wmi_unified_t wmi_handle,
 					   uint8_t *peer_addr,
 					   struct ap_ps_params *param);
 
-CDF_STATUS send_set_sta_ps_param_cmd_non_tlv(wmi_unified_t wmi_handle,
+QDF_STATUS send_set_sta_ps_param_cmd_non_tlv(wmi_unified_t wmi_handle,
 					   struct sta_ps_params *param);
 
-CDF_STATUS send_crash_inject_cmd_non_tlv(wmi_unified_t wmi_handle,
+QDF_STATUS send_crash_inject_cmd_non_tlv(wmi_unified_t wmi_handle,
 			 struct crash_inject *param);
 
-CDF_STATUS
+QDF_STATUS
 send_dbglog_cmd_non_tlv(wmi_unified_t wmi_handle,
 				struct dbglog_params *dbglog_param);
 
-CDF_STATUS send_vdev_set_param_cmd_non_tlv(wmi_unified_t wmi_handle,
+QDF_STATUS send_vdev_set_param_cmd_non_tlv(wmi_unified_t wmi_handle,
 				struct vdev_set_params *param);
 
-CDF_STATUS send_stats_request_cmd_non_tlv(wmi_unified_t wmi_handle,
+QDF_STATUS send_stats_request_cmd_non_tlv(wmi_unified_t wmi_handle,
 				uint8_t macaddr[IEEE80211_ADDR_LEN],
 				struct stats_request_params *param);
 
-CDF_STATUS send_packet_log_enable_cmd_non_tlv(wmi_unified_t wmi_handle,
+QDF_STATUS send_packet_log_enable_cmd_non_tlv(wmi_unified_t wmi_handle,
 				uint8_t macaddr[IEEE80211_ADDR_LEN],
 				struct packet_enable_params *param);
 
-CDF_STATUS send_beacon_send_cmd_non_tlv(wmi_unified_t wmi_handle,
+QDF_STATUS send_beacon_send_cmd_non_tlv(wmi_unified_t wmi_handle,
 				struct beacon_params *param);
 
-CDF_STATUS send_peer_assoc_cmd_non_tlv(wmi_unified_t wmi_handle,
+QDF_STATUS send_peer_assoc_cmd_non_tlv(wmi_unified_t wmi_handle,
 				struct peer_assoc_params *param);
 
-CDF_STATUS send_scan_start_cmd_non_tlv(wmi_unified_t wmi_handle,
+QDF_STATUS send_scan_start_cmd_non_tlv(wmi_unified_t wmi_handle,
 				struct scan_start_params *param);
 
-CDF_STATUS send_scan_stop_cmd_non_tlv(wmi_unified_t wmi_handle,
+QDF_STATUS send_scan_stop_cmd_non_tlv(wmi_unified_t wmi_handle,
 				struct scan_stop_params *param);
 
-CDF_STATUS send_scan_chan_list_cmd_non_tlv(wmi_unified_t wmi_handle,
+QDF_STATUS send_scan_chan_list_cmd_non_tlv(wmi_unified_t wmi_handle,
 				struct scan_chan_list_params *param);
 
