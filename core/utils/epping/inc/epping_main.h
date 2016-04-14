fd95385a9341d53d1f5534176f1291e8f34fc017
Rajeev Kumar <rajekuma@codeaurora.org>
2016-04-14 16:29:11 -0700

    qcacld-3.0: Use new EPPING con_mode enum
    EPPING con_mode is now a single enum.
    
    Change-Id: I403a2ab33ea85dc69aa98fc50d9ece6cf8e1ee19
    CRs-Fixed: 989695

--- core/utils/epping/inc/epping_main.h
+++ core/utils/epping/inc/epping_main.h
@@ -39,12 +39,13 @@
    Include files
    -------------------------------------------------------------------------*/
 #include <qdf_lock.h>
+#include <qdf_types.h>
 
 #define WLAN_EPPING_ENABLE_BIT          (1 << 8)
 #define WLAN_EPPING_IRQ_BIT             (1 << 9)
 #define WLAN_EPPING_FW_UART_BIT         (1 << 10)
-#define WLAN_IS_EPPING_ENABLED(x)       (x & WLAN_EPPING_ENABLE_BIT)
-#define WLAN_IS_EPPING_IRQ(x)           (x & WLAN_EPPING_IRQ_BIT)
+#define WLAN_IS_EPPING_ENABLED(x)       (x == QDF_GLOBAL_EPPING_MODE)
+#define WLAN_IS_EPPING_IRQ(x)            1
 #define WLAN_IS_EPPING_FW_UART(x)       (x & WLAN_EPPING_FW_UART_BIT)
 
 /* epping_main signatures */
