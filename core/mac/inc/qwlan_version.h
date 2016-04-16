2253beab04e94497e60b9aa6399a3cbb56054409
Rajeev Kumar <rajekuma@codeaurora.org>
2016-04-15 18:14:47 -0700

    Release 5.1.0.10G
    rebase of release 5.0.0.170 to release 5.1.0.10G
    
    Change-Id: I635cd6aeff453515934e7be6ca510b0af05a27af
    CRs-Fixed: 688141

--- core/mac/inc/qwlan_version.h
+++ core/mac/inc/qwlan_version.h
@@ -41,9 +41,10 @@
 #define QWLAN_VERSION_MAJOR            5
 #define QWLAN_VERSION_MINOR            1
 #define QWLAN_VERSION_PATCH            0
-#define QWLAN_VERSION_EXTRA            ""
+#define QWLAN_VERSION_EXTRA            "A"
 #define QWLAN_VERSION_BUILD            2
 
-#define QWLAN_VERSIONSTR               "5.1.0.2"
+#define QWLAN_VERSIONSTR               "5.1.0.2A"
+/* 170 */
 
 #endif /* QWLAN_VERSION_H */
