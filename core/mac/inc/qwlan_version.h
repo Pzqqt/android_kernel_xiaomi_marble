0eb0b3d600d54af2b457dca87fce61c9239d212a
Vishwajith Upendra <vishwaji@codeaurora.org>
2016-04-20 15:03:14 -0700

    Release 5.1.0.3
    Corresponds to SU 172 qcacld-3.0 unconverged
    
    Change-Id: I3a99de1f9c1edae1a4f69c1f8c9f62c56d135636
    CRs-Fixed: 688141

--- core/mac/inc/qwlan_version.h
+++ core/mac/inc/qwlan_version.h
@@ -41,10 +41,10 @@
 #define QWLAN_VERSION_MAJOR            5
 #define QWLAN_VERSION_MINOR            1
 #define QWLAN_VERSION_PATCH            0
-#define QWLAN_VERSION_EXTRA            "C"
-#define QWLAN_VERSION_BUILD            2
+#define QWLAN_VERSION_EXTRA            ""
+#define QWLAN_VERSION_BUILD            3
 
-#define QWLAN_VERSIONSTR               "5.1.0.2C"
+#define QWLAN_VERSIONSTR               "5.1.0.3"
 /* 172 */
 
 #endif /* QWLAN_VERSION_H */
