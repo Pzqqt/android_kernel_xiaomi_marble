209c1959275e94cc1d734e75b97ecb8ef0b6bb28
Padma, Santhosh Kumar <skpadma@codeaurora.org>
2016-08-10 17:30:45 +0530

    qcacld-3.0: Validate and Rectify RSN IE in probe response frame
    qcacld-2.0 to qcacld-3.0 propagation
    
    There are some Access points that have not included the
    capability field in the RSN ie's though the length for the
    RSN ie's indicate for the presence of this field. This shall
    result in the next byte after this RSN ie as the capability
    field, thus resulting in the improper interpretation of this
    field, the end result being a failure to connect to such AP's.
    This commit introduces a work around to interop with such AP's
    by appending the capability field with 0 value to the obtained ie.
    It updates MPDU length of received RxPacket based on addition of
    RSN Capability if it is missing in Probe response.
    
    Change-Id: Ic599c8bdb19e368fefb13293499451e7ab38d517
    CRs-Fixed: 667983

--- core/mac/src/include/parser_api.h
+++ core/mac/src/include/parser_api.h
@@ -59,6 +59,13 @@
 #define NSS_2x2_MODE 2
 #define MBO_IE_ASSOC_DISALLOWED_SUBATTR_ID 0x04
 
+#define SIZE_OF_FIXED_PARAM 12
+#define SIZE_OF_TAG_PARAM_NUM 1
+#define SIZE_OF_TAG_PARAM_LEN 1
+#define RSNIEID 0x30
+#define RSNIE_CAPABILITY_LEN 2
+#define DEFAULT_RSNIE_CAP_VAL 0x00
+
 #ifdef FEATURE_AP_MCC_CH_AVOIDANCE
 #define QCOM_VENDOR_IE_MCC_AVOID_CH 0x01
 
@@ -950,5 +957,11 @@ tSirRetStatus populate_dot11f_timing_advert_frame(tpAniSirGlobal pMac,
 	tDot11fTimingAdvertisementFrame *frame);
 void populate_dot11_supp_operating_classes(tpAniSirGlobal mac_ptr,
 	tDot11fIESuppOperatingClasses *dot_11_ptr, tpPESession session_entry);
+
+tSirRetStatus
+sir_validate_and_rectify_ies(tpAniSirGlobal mac_ctx,
+				uint8_t *mgmt_frame,
+				uint32_t frame_bytes,
+				uint32_t *missing_rsn_bytes);
 
 #endif /* __PARSE_H__ */
