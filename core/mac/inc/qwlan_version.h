7d06dd55c95d5e5f3b00d95958ed1d7b859e6783
qcabuildsw <qcabuildsw@localhost>
2016-09-06 15:24:24 -0700

    Release 5.1.0.25U
    Release 5.1.0.25U
    
    Change-Id: Ie1ea4337b4dc365fac9ffc29ca16d80382ab8c32
    CRs-Fixed: 688141

--- core/mac/inc/qwlan_version.h
+++ core/mac/inc/qwlan_version.h
@@ -41,9 +41,9 @@
 #define QWLAN_VERSION_MAJOR            5
 #define QWLAN_VERSION_MINOR            1
 #define QWLAN_VERSION_PATCH            0
-#define QWLAN_VERSION_EXTRA            "T"
+#define QWLAN_VERSION_EXTRA            "U"
 #define QWLAN_VERSION_BUILD            25
 
-#define QWLAN_VERSIONSTR               "5.1.0.25T"
+#define QWLAN_VERSIONSTR               "5.1.0.25U"
 
 #endif /* QWLAN_VERSION_H */
