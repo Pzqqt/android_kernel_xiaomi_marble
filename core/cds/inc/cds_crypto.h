cc1753d2884798ddc84583740a3e56f6178984da
Dustin Brown <dustinb@codeaurora.org>
2016-10-19 16:25:45 -0700

    qcacld-3.0: 4.8 Kernel Migration - Remove ablkcipher
    As of version 4.8 of the Linux kernel, the ablkcipher Api is removed.
    Remove all obsolete crypto functions that reference this Api.
    
    Change-Id: Ia82743d048834a67dfce23b91f660beb59b1e94d
    CRs-Fixed: 1079951
    (cherry picked from commit bf349575d765f53f207c7cb2ad6c66a198cf07de)

--- core/cds/inc/cds_crypto.h
+++ core/cds/inc/cds_crypto.h
@@ -34,28 +34,7 @@
  *
  */
 
-#include <qdf_types.h>
-#include <qdf_status.h>
-#include <qdf_mem.h>
-#include <qdf_list.h>
-#include <qdf_trace.h>
-#include <qdf_event.h>
-#include <qdf_lock.h>
-#include <cds_reg_service.h>
-#include <cds_mq.h>
-#include <cds_packet.h>
-#include <cds_sched.h>
-#include <qdf_threads.h>
-#include <qdf_mc_timer.h>
-#include <cds_pack_align.h>
-#include <crypto/aes.h>
-#include <crypto/hash.h>
-
-static inline struct crypto_ahash *cds_crypto_alloc_ahash(const char *alg_name,
-	u32 type, u32 mask)
-{
-	return crypto_alloc_ahash(alg_name, type, mask);
-}
+#include <linux/crypto.h>
 
 static inline struct crypto_cipher *
 cds_crypto_alloc_cipher(const char *alg_name, u32 type, u32 mask)
@@ -66,38 +45,6 @@ cds_crypto_alloc_cipher(const char *alg_name, u32 type, u32 mask)
 static inline void cds_crypto_free_cipher(struct crypto_cipher *tfm)
 {
 	crypto_free_cipher(tfm);
-}
-
-static inline void cds_crypto_free_ahash(struct crypto_ahash *tfm)
-{
-	crypto_free_ahash(tfm);
-}
-
-static inline int cds_crypto_ahash_setkey(struct crypto_ahash *tfm,
-	const u8 *key, unsigned int keylen)
-{
-	return crypto_ahash_setkey(tfm, key, keylen);
-}
-
-static inline int cds_crypto_ahash_digest(struct ahash_request *req)
-{
-	return crypto_ahash_digest(req);
-}
-
-static inline struct crypto_ablkcipher *
-cds_crypto_alloc_ablkcipher(const char *alg_name, u32 type, u32 mask)
-{
-	return crypto_alloc_ablkcipher(alg_name, type, mask);
-}
-
-static inline void cds_ablkcipher_request_free(struct ablkcipher_request *req)
-{
-	ablkcipher_request_free(req);
-}
-
-static inline void cds_crypto_free_ablkcipher(struct crypto_ablkcipher *tfm)
-{
-	crypto_free_ablkcipher(tfm);
 }
 
 #endif /* if !defined __CDS_CRYPTO_H */
