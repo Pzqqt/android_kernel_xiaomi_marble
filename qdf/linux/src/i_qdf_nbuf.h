62e4d70de35879070412d8c75ea1d79ce2d916ea
Kai Liu <kaliu@codeaurora.org>
2016-10-24 16:27:16 +0800

    qcacmn: Store tx desc id in sk_buff instead of skb head
    qcacld-2.0 to qcacmn propagation
    
    Store tx desc id in skb_buff.
    
    Change-Id: I2186a06ad3ec929683292c4c052904a18427cc64
    CRs-Fixed: 951208

--- qdf/linux/src/i_qdf_nbuf.h
+++ qdf/linux/src/i_qdf_nbuf.h
@@ -124,6 +124,7 @@ typedef union {
  * @tx.trace.vdev_id     : vdev (for protocol trace)
  * @tx.ipa.owned   : packet owned by IPA
  * @tx.ipa.priv    : private data, used by IPA
+ * @tx.desc_id     : tx desc id, used to sync between host and fw
  */
 struct qdf_nbuf_cb {
 	/* common */
@@ -197,7 +198,8 @@ struct qdf_nbuf_cb {
 						uint32_t owned:1,
 							priv:31;
 					} ipa; /* 4 */
-				} mcl;/* 12 bytes*/
+					uint16_t desc_id; /* 2 bytes */
+				} mcl;/* 14 bytes*/
 			} dev;
 		} tx; /* 40 bytes */
 	} u;
@@ -300,6 +302,8 @@ struct qdf_nbuf_cb {
 	(((struct qdf_nbuf_cb *)((skb)->cb))->u.tx.dev.mcl.ipa.owned)
 #define QDF_NBUF_CB_TX_IPA_PRIV(skb) \
 	(((struct qdf_nbuf_cb *)((skb)->cb))->u.tx.dev.mcl.ipa.priv)
+#define QDF_NBUF_CB_TX_DESC_ID(skb) \
+	(((struct qdf_nbuf_cb *)((skb)->cb))->u.tx.dev.mcl.desc_id)
 #define QDF_NBUF_CB_TX_FTYPE(skb) \
 	(((struct qdf_nbuf_cb *)((skb)->cb))->u.tx.dev.win.ftype)
 #define QDF_NBUF_CB_TX_SUBMIT_TS(skb) \
