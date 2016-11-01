f0c537b25261d1b5fcbfdb2da25268a9d0312803
Prakash Dhavali <pdhavali@codeaurora.org>
2016-10-31 17:25:07 -0700

    Release 5.2.0.1A
    Release 5.2.0.1A
    
    Change-Id: I1e952bd9f42ae149631cbc173a66f220debbbba0
    CRs-Fixed: 688141

--- core/mac/inc/qwlan_version.h
+++ core/mac/inc/qwlan_version.h
@@ -39,11 +39,11 @@
    ===========================================================================*/
 
 #define QWLAN_VERSION_MAJOR            5
-#define QWLAN_VERSION_MINOR            1
+#define QWLAN_VERSION_MINOR            2
 #define QWLAN_VERSION_PATCH            0
-#define QWLAN_VERSION_EXTRA            "D"
-#define QWLAN_VERSION_BUILD            34
+#define QWLAN_VERSION_EXTRA            "A"
+#define QWLAN_VERSION_BUILD            1
 
-#define QWLAN_VERSIONSTR               "5.1.0.34D"
+#define QWLAN_VERSIONSTR               "5.2.0.1A"
 
 #endif /* QWLAN_VERSION_H */
