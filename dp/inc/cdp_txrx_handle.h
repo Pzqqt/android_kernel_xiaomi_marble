703be6d6c46844d4177be39ac8628253b4b11d30
Venkata Sharath Chandra Manchala <vmanchal@codeaurora.org>
2017-01-10 17:12:58 -0800

    qcacmn: New file to hold abstract structure handlers
    Introduce new file cdp_txrx_handle.h to hold the
    abstract structure handles for pdev and vdev
    
    Change-Id: Ia8c0a5b1591c630390e7b367e1eceee2bd0517ba
    CRs-Fixed: 1109835

---
+++ dp/inc/cdp_txrx_handle.h
@@ -0,0 +1,33 @@
+
+/*
+ * Copyright (c) 2017 The Linux Foundation. All rights reserved.
+ *
+ * Permission to use, copy, modify, and/or distribute this software for
+ * any purpose with or without fee is hereby granted, provided that the
+ * above copyright notice and this permission notice appear in all
+ * copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL
+ * WARRANTIES WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE
+ * AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL
+ * DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR
+ * PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER
+ * TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
+ * PERFORMANCE OF THIS SOFTWARE.
+ */
+
+ /**
+ * @file cdp_txrx_handle.h
+ * @brief Holds the forward structure declarations for handles
+ * passed from the upper layers
+ */
+
+#ifndef CDP_TXRX_HANDLE_H
+#define CDP_TXRX_HANDLE_H
+
+struct cdp_cfg;
+struct cdp_pdev;
+struct cdp_vdev;
+
+#endif
