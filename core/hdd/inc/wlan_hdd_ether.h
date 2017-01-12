330c6b0ce1e9df4d64a800d9434f143445a563b9
Jeff Johnson <jjohnson@codeaurora.org>
2017-01-12 10:42:31 -0800

    qcacld-3.0: Fix block comments in wlan_hdd_ether.h
    A checkpatch run on the HDD component has produced numerous
    warnings about improper block comments:
    WARNING: Block comments use * on subsequent lines
    WARNING: Block comments should align the * on each line
    WARNING: Block comments use a trailing */ on a separate line
    
    Fix all such warnings in wlan_hdd_ether.h.
    
    Change-Id: Ib33cbcf1425105ecc4dcfd938589deac8d35de3c
    CRs-Fixed: 1110861

--- core/hdd/inc/wlan_hdd_ether.h
+++ core/hdd/inc/wlan_hdd_ether.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2014 The Linux Foundation. All rights reserved.
+ * Copyright (c) 2014, 2017 The Linux Foundation. All rights reserved.
  *
  * Previously licensed under the ISC license by Qualcomm Atheros, Inc.
  *
@@ -27,33 +27,32 @@
 
 #ifndef _WLAN_HDD_ETHER_H
 #define _WLAN_HDD_ETHER_H
-/*============================================================================
-   @file wlan_hdd_ether.h
+/**
+ * DOC: wlan_hdd_ether.h
+ *
+ * This module describes Ethernet packet formats for processing by HDD.
+ */
 
-   This module describes Ethernet packet formats for processing by HDD.
-   ============================================================================*/
-/* $Header$ */
-
-/*----------------------------------------------------------------------------
+/*
  * Include Files
- * -------------------------------------------------------------------------*/
+ */
 #include <linux/version.h>
 #include <linux/byteorder/generic.h>
 #include <linux/if_ether.h>
 #include <linux/if_vlan.h>
 
-/*----------------------------------------------------------------------------
+/*
  * Preprocessor Definitions and Constants
- * -------------------------------------------------------------------------*/
+ */
 #define WLAN_SNAP_OUI_LEN 3
 #define WLAN_SNAP_DSAP 0xAAU
 #define WLAN_SNAP_SSAP 0xAAU
 #define WLAN_SNAP_CTRL 0x03
 #define WLAN_MIN_PROTO 0x0600
 
-/*----------------------------------------------------------------------------
+/*
  * Type Declarations
- * -------------------------------------------------------------------------*/
+ */
 struct wlan_snap_hdr {
 	unsigned char dsap;
 	unsigned char ssap;
