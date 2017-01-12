23980b63f3c5ba5b21f49d3b5a3578dfe52c69e2
Jeff Johnson <jjohnson@codeaurora.org>
2017-01-12 10:41:36 -0800

    qcacld-3.0: Fix block comments in wlan_hdd_includes.h
    A checkpatch run on the HDD component has produced numerous
    warnings about improper block comments:
    WARNING: Block comments use * on subsequent lines
    WARNING: Block comments should align the * on each line
    WARNING: Block comments use a trailing */ on a separate line
    
    Fix all such warnings in wlan_hdd_includes.h.
    
    Change-Id: I64688d67d58b7e62566831b5e33de76a0a99bcd0
    CRs-Fixed: 1110861

--- core/hdd/inc/wlan_hdd_includes.h
+++ core/hdd/inc/wlan_hdd_includes.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2014, 2016 The Linux Foundation. All rights reserved.
+ * Copyright (c) 2014, 2016-2017 The Linux Foundation. All rights reserved.
  *
  * Previously licensed under the ISC license by Qualcomm Atheros, Inc.
  *
@@ -28,21 +28,17 @@
 #if !defined(HDD_INCLUDES_H__)
 #define HDD_INCLUDES_H__
 
-/**===========================================================================
+/**
+ * DOC: wlan_hdd_includes.h
+ *
+ * Internal includes for the Linux HDD
+ */
 
-   \file  wlan_hdd_includes.h
-
-   \brief Internal includes for the Linux HDD
-
-   ==========================================================================*/
-
-/* $HEADER$ */
-
-/*---------------------------------------------------------------------------
-   Include files
-   -------------------------------------------------------------------------*/
-
-/* throw all the includes in here f to get the .c files  in the HDD to compile. */
+/*
+ * Include files
+ *
+ * throw all the includes in here to get the .c files in the HDD to compile.
+ */
 
 #include <linux/version.h>
 #include <linux/module.h>
