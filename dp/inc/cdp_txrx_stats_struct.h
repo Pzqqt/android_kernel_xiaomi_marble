b2f5b6506db45c11f329096ea91ac81c53ca2fd9
Sathish Kumar <ksathis@codeaurora.org>
2017-01-17 22:45:35 +0530

    qcacmn: Add the iwpriv command parameter for runtime target dump
    Change-Id: Ia923abc7681ddbc4344c0a689de59a6e78a3cdcb
    Acked-off-by: yilinl <rlu@codeaurora.org>
    CRs-Fixed: 1107807

--- dp/inc/cdp_txrx_stats_struct.h
+++ dp/inc/cdp_txrx_stats_struct.h
@@ -473,6 +473,7 @@ typedef enum _ol_ath_param_t {
 	OL_ATH_PARAM_PRECAC_ENABLE = 345,
 	OL_ATH_PARAM_PRECAC_TIMEOUT = 346,
 	OL_ATH_COEX_VER_CFG = 347,
+	OL_ATH_PARAM_DUMP_TARGET = 348,
 } ol_ath_param_t;
 
 /*
