0b2c9f4d027ac365192e673635284a6660f85503
SaidiReddy Yenuga <saidir@codeaurora.org>
2017-02-03 12:26:38 +0530

    qcacld-3.0: Add bounday check for multicastAddr array
    qcacld-2.0 to qcacld-3.0 propagation.
    
    In hdd_set_rx_filter API multicastAddr array being accessed beyond
    its size.
    
    Add boundary check for multicastAddr.
    
    CRs-Fixed: 1104565
    Change-Id: I8e1543a8f42ac40c04d2c6a17e69718d13cbd706

--- core/hdd/src/wlan_hdd_ioctl.c
+++ core/hdd/src/wlan_hdd_ioctl.c
@@ -6080,6 +6080,8 @@ static int hdd_set_rx_filter(hdd_adapter_t *adapter, bool action,
 				    MAC_ADDR_ARRAY(filter->multicastAddr[j].bytes));
 				j++;
 			}
+			if (j == SIR_MAX_NUM_MULTICAST_ADDRESS)
+				break;
 		}
 		filter->ulMulticastAddrCnt = j;
 		/* Set rx filter */
