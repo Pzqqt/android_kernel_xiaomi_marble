a65b3b5a494fe39d82a54eac05d0f31b62329f7f
Sandeep Puligilla <spuligil@codeaurora.org>
2017-02-07 10:59:58 -0800

    fw-api: CL 2842375 - update fw common interface files
    Add Country string in LPI
    
    Change-Id: I1c6d7401e675247bb4afbcb877c587ce42d22c79
    CRs-Fixed: 1107600

--- fw/wmi_unified.h
+++ fw/wmi_unified.h
@@ -11466,6 +11466,7 @@ typedef struct
 #define LPI_IE_BITMAP_CHRE_ESS               0x010000000    /* ESS capability info for CHRE */
 #define LPI_IE_BITMAP_CHRE_SEC_MODE          0x020000000    /* Security capability info for CHRE */
 #define LPI_IE_BITMAP_CHRE_SUPPORTED_RATE    0x040000000    /* Hightest MCS corresponding NCC for TX and RX */
+#define LPI_IE_BITMAP_COUNTRY_STRING         0x080000000    /* send country string inside Country IE to LOWI LP */
 #define LPI_IE_BITMAP_ALL                    0xFFFFFFFF
 
 typedef struct {
