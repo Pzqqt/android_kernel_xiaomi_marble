996a2639e6a87e9b5224fcc4cd561b848b32c7f3
Yu Wang <yyuwang@codeaurora.org>
2017-02-10 12:02:07 +0800

    qcacld-3.0: remove redundant definition for HDD_MOD_EXIT_SSR_MAX_RETRIES
    If CONFIG_HL_SUPPORT is enabled, HDD_MOD_EXIT_SSR_MAX_RETRIES
    has 2 definitions in one file, one of the value is 75 and the
    other is 200, which has conflict and cause compilation error.
    
    Remove the redundant definition of HDD_MOD_EXIT_SSR_MAX_RETRIES
    
    Change-Id: Ib707d8b338c394a173ce7dfcefe9040d630a6d5b
    CRs-Fixed: 2004355

--- core/hdd/inc/wlan_hdd_main.h
+++ core/hdd/inc/wlan_hdd_main.h
@@ -164,9 +164,6 @@
 /* Maximum time(ms) to wait for external acs response */
 #define WLAN_VENDOR_ACS_WAIT_TIME 1000
 
-/* SSR Retry Count */
-#define HDD_MOD_EXIT_SSR_MAX_RETRIES 75
-
 #define MAC_ADDR_ARRAY(a) (a)[0], (a)[1], (a)[2], (a)[3], (a)[4], (a)[5]
 /** Mac Address string **/
 #define MAC_ADDRESS_STR "%02x:%02x:%02x:%02x:%02x:%02x"
