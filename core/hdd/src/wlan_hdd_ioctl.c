0161e1d97a343aa633cb8d65d6680406c73ab993
Hanumanth Reddy Pothula <c_hpothu@codeaurora.org>
2017-02-14 17:36:13 +0530

    qcacld-3.0: Return success for STOP DRIVER command
    Presently, STOP Driver command is not handled in driver
    as interface down will takes care to close the modules
    if there are no more interfaces up. But when framework
    issues this command, it's going to bad state as driver
    returns failure.
    
    To mitigate this issue return SUCCESS for STOP DRIVER command.
    
    Change-Id: I47e6898109a35e3194c18645c932c5b2cf5a73c8
    CRs-Fixed: 2006563

--- core/hdd/src/wlan_hdd_ioctl.c
+++ core/hdd/src/wlan_hdd_ioctl.c
@@ -6785,6 +6785,7 @@ static const struct hdd_drv_cmd hdd_drv_cmds[] = {
 	{"CHANNEL_SWITCH",            drv_cmd_set_channel_switch},
 	{"SETANTENNAMODE",            drv_cmd_set_antenna_mode},
 	{"GETANTENNAMODE",            drv_cmd_get_antenna_mode},
+	{"STOP",                      drv_cmd_dummy},
 };
 
 /**
