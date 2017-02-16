eb01e1311b0bd593057a9e0daed112151a6f26fa
Mukul Sharma <mukul@codeaurora.org>
2017-02-16 13:01:03 +0530

    qcacld-3.0: Set IBSS peer state to associate state after connection
    Currently in IBSSS mode, host do not change the peer state to
    associate state after successful connection. Hence other
    components always find peer state as not associate.
    As a part of this fix, move IBSS peer state to associate state.
    
    Change-Id: I21970ae723a3cecd797182c260692810625f2ace
    CRs-Fixed: 2007648

--- core/hdd/src/wlan_hdd_assoc.c
+++ core/hdd/src/wlan_hdd_assoc.c
@@ -1324,6 +1324,12 @@ static void hdd_send_association_event(struct net_device *dev,
 		if (ret)
 			hdd_err("Peer object "MAC_ADDRESS_STR" add fails!",
 				MAC_ADDR_ARRAY(pCsrRoamInfo->bssid.bytes));
+		ret = hdd_set_peer_mlme_state(pAdapter->hdd_vdev,
+				WLAN_ASSOC_STATE);
+		if (ret)
+			hdd_err("Peer object %pM fail to set associated state",
+					peerMacAddr.bytes);
+
 	} else {                /* Not Associated */
 		hdd_err("wlan: disconnected");
 		memset(wrqu.ap_addr.sa_data, '\0', ETH_ALEN);
