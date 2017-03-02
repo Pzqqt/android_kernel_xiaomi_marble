17ff2037e5c7b476f2d732eaf68d4efed1b97810
Arif Hussain <arifhussain@codeaurora.org>
2017-03-02 11:56:04 -0800

    qcacmn: Add support for delay_start_time in scheduled scan request
    Add support to pass delay_start_time to firmware, this delay will be
    used before starting the first scan cycle.
    
    Change-Id: I7488fc8da4e22b170e52fb38c8a1d5b49dbf7e2f
    CRs-Fixed: 2014142

--- wmi_unified_tlv.c
+++ wmi_unified_tlv.c
@@ -6628,6 +6628,7 @@ static QDF_STATUS send_pno_start_cmd_tlv(wmi_unified_t wmi_handle,
 	/* Copy scan interval */
 	cmd->fast_scan_period = pno->fast_scan_period;
 	cmd->slow_scan_period = pno->slow_scan_period;
+	cmd->delay_start_time = WMI_SEC_TO_MSEC(pno->delay_start_time);
 	cmd->fast_scan_max_cycles = pno->fast_scan_max_cycles;
 	WMI_LOGD("fast_scan_period: %d msec slow_scan_period: %d msec",
 			cmd->fast_scan_period, cmd->slow_scan_period);
