4a888841486bd68646b49bc32fd17544880b76d8
Nishank Aggarwal <naggar@codeaurora.org>
2017-03-16 14:30:18 +0530

    qcacld-3.0: Fix ASSERT while loading driver
    Currently in csr_purge_old_scan_results() mutex_lock
    did't acquire by current thread but same thread is
    releasing the lock due to which there is a ASSERT while
    releasing mutex_lock.
    
    Fix is to acuire the mutex_lock properly before releasing.
    
    Change-Id: I1023dca73ffaa3d3b3a123d474c3ac5b0e1c1af2
    CRs-Fixed: 2020452

--- core/sme/src/csr/csr_api_scan.c
+++ core/sme/src/csr/csr_api_scan.c
@@ -3060,7 +3060,7 @@ static void csr_purge_old_scan_results(tpAniSirGlobal mac_ctx)
 	uint64_t oldest_entry = 0;
 	uint64_t curr_time = (uint64_t)qdf_mc_timer_get_system_time();
 
-	csr_ll_unlock(&mac_ctx->scan.scanResultList);
+	csr_ll_lock(&mac_ctx->scan.scanResultList);
 	pentry = csr_ll_peek_head(&mac_ctx->scan.scanResultList,
 					LL_ACCESS_NOLOCK);
 	while (pentry) {
