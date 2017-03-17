6796d8df5e4bb3f6d6511effbe4d87d85702bda3
Tushnim Bhattacharyya <tushnimb@codeaurora.org>
2017-03-17 11:06:03 -0700

    qcacld-3.0: Enable Policy Manager logging
    Enable logging from new policy manager component.
    
    Change-Id: I538b70d7261573a1673386a3984f6a5a821c70f2
    CRs-Fixed: 2009818

--- core/hdd/src/wlan_hdd_main.c
+++ core/hdd/src/wlan_hdd_main.c
@@ -7069,7 +7069,8 @@ static hdd_context_t *hdd_context_create(struct device *dev)
 		[QDF_MODULE_ID_SCHEDULER] = {QDF_TRACE_LEVEL_ALL},
 		[QDF_MODULE_ID_MGMT_TXRX] = {QDF_TRACE_LEVEL_ALL},
 		[QDF_MODULE_ID_PMO] = {QDF_TRACE_LEVEL_ALL},
-		[QDF_MODULE_ID_SCAN] = {QDF_TRACE_LEVEL_ALL}
+		[QDF_MODULE_ID_SCAN] = {QDF_TRACE_LEVEL_ALL},
+		[QDF_MODULE_ID_POLICY_MGR] = {QDF_TRACE_LEVEL_ALL},
 		};
 
 	status = qdf_print_setup();
