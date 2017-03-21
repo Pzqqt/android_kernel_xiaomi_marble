5b3602a617326f05ba92d18cdcca03958eaf2c74
Naveen Rawat <naveenrawat@codeaurora.org>
2017-03-21 15:12:22 -0700

    qcacmn: Define NDP structures and enums and private obj
    1) Define NDP structures and enum.
    2) Define PSOC and VDEV private object
    
    Change-Id: I3cb3c0e357f8b4598a0e4ec8abbd5b69e5de5180
    CRs-Fixed: 2014795

--- umac/nan/core/src/nan_main_i.h
+++ umac/nan/core/src/nan_main_i.h
@@ -24,4 +24,68 @@
 #ifndef _WLAN_NAN_MAIN_I_H_
 #define _WLAN_NAN_MAIN_I_H_
 
+#include "qdf_types.h"
+#include "qdf_status.h"
+#include "nan_public_structs.h"
+#include "wlan_objmgr_cmn.h"
+
+struct wlan_objmgr_vdev;
+struct wlan_objmgr_psoc;
+struct scheduler_msg;
+
+#define nan_log(level, args...) \
+	QDF_TRACE(QDF_MODULE_ID_NAN, level, ## args)
+#define nan_logfl(level, format, args...) \
+	nan_log(level, FL(format), ## args)
+
+#define nan_alert(format, args...) \
+	nan_logfl(QDF_TRACE_LEVEL_FATAL, format, ## args)
+#define nan_err(format, args...) \
+	nan_logfl(QDF_TRACE_LEVEL_ERROR, format, ## args)
+#define nan_warn(format, args...) \
+	nan_logfl(QDF_TRACE_LEVEL_WARN, format, ## args)
+#define nan_notice(format, args...) \
+	nan_logfl(QDF_TRACE_LEVEL_INFO, format, ## args)
+#define nan_info(format, args...) \
+	nan_logfl(QDF_TRACE_LEVEL_INFO_HIGH, format, ## args)
+#define nan_debug(format, args...) \
+	nan_logfl(QDF_TRACE_LEVEL_DEBUG, format, ## args)
+
+#ifndef MAX_PEERS
+#define MAX_PEERS 32
+#endif
+
+/**
+ * struct nan_psoc_priv_obj - nan private psoc obj
+ * @lock: lock to be acquired before reading or writing to object
+ * @cb_obj: struct contaning callback pointers
+ */
+struct nan_psoc_priv_obj {
+	qdf_spinlock_t lock;
+	struct nan_callbacks cb_obj;
+};
+
+/**
+ * struct nan_vdev_priv_obj - nan private vdev obj
+ * @lock: lock to be acquired before reading or writing to object
+ * @state: Current state of NDP
+ * @active_ndp_sessions: active ndp sessions per adapter
+ * @active_ndp_peers: number of active ndp peers
+ * @ndp_create_transaction_id: transaction id for create req
+ * @ndp_delete_transaction_id: transaction id for delete req
+ * @ndi_delete_rsp_reason: reason code for ndi_delete rsp
+ * @ndi_delete_rsp_status: status for ndi_delete rsp
+ */
+struct nan_vdev_priv_obj {
+	qdf_spinlock_t lock;
+	enum nan_datapath_state state;
+	/* idx in following array should follow conn_info.peerMacAddress */
+	uint32_t active_ndp_sessions[MAX_PEERS];
+	uint32_t active_ndp_peers;
+	uint16_t ndp_create_transaction_id;
+	uint16_t ndp_delete_transaction_id;
+	uint32_t ndi_delete_rsp_reason;
+	uint32_t ndi_delete_rsp_status;
+};
+
 #endif
