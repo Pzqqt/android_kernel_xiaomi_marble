cdb624f81dd722f1a88e98548dcc2f98d1a7efce
Naveen Rawat <naveenrawat@codeaurora.org>
2017-03-22 16:19:57 -0700

    qcacmn: Add interface with LMAC for NAN Component
    Add interface with LMAC for NAN Component.
    
    Change-Id: Idbcdaa19a7a2b83d5a2d125419a7bc6cbe72033d
    CRs-Fixed: 2014795

--- src/target_if_nan.c
+++ src/target_if_nan.c
@@ -19,3 +19,57 @@
 /**
  * DOC: contains nan target if functions
  */
+
+#include "wlan_nan_api.h"
+#include "nan_public_structs.h"
+#include "target_if_nan.h"
+
+static QDF_STATUS target_if_nan_req(void *in_req, uint32_t req_type)
+{
+	return QDF_STATUS_SUCCESS;
+}
+
+void target_if_nan_register_tx_ops(struct wlan_lmac_if_tx_ops *tx_ops)
+{
+	tx_ops->nan_tx_ops.nan_req_tx = target_if_nan_req;
+}
+
+void target_if_nan_register_rx_ops(struct wlan_lmac_if_rx_ops *rx_ops)
+{
+	/* TBD */
+	rx_ops->nan_rx_ops.nan_event_rx = NULL;
+}
+
+inline struct wlan_lmac_if_nan_tx_ops *target_if_nan_get_tx_ops(
+				struct wlan_objmgr_psoc *psoc)
+{
+	if (!psoc) {
+		target_if_err("psoc is null");
+		return NULL;
+	}
+
+	return &psoc->soc_cb.tx_ops.nan_tx_ops;
+}
+
+inline struct wlan_lmac_if_nan_rx_ops *target_if_nan_get_rx_ops(
+				struct wlan_objmgr_psoc *psoc)
+{
+	if (!psoc) {
+		target_if_err("psoc is null");
+		return NULL;
+	}
+
+	return &psoc->soc_cb.rx_ops.nan_rx_ops;
+}
+
+QDF_STATUS target_if_nan_register_events(struct wlan_objmgr_psoc *psoc)
+{
+	/* TBD */
+	return QDF_STATUS_SUCCESS;
+}
+
+QDF_STATUS target_if_nan_deregister_events(struct wlan_objmgr_psoc *psoc)
+{
+	/* TBD */
+	return QDF_STATUS_SUCCESS;
+}
