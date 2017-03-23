15a3345ef32e83bdb146c3a016b02b82c9cb7de2
Nishank Aggarwal <naggar@codeaurora.org>
2017-03-23 16:01:38 +0530

    qcacld-3.0: Replace lim_log() with pe_* log levels in lim_process_beacon_frame.c
    Replace lim_log() with pe_* appropriate log levels in lim_process_beacon_frame.c
    
    Change-Id: I99539fc78b927c0448d18b15d18de433d8da6133
    CRs-Fixed: 1093093

--- core/mac/src/pe/lim/lim_process_beacon_frame.c
+++ core/mac/src/pe/lim/lim_process_beacon_frame.c
@@ -77,16 +77,14 @@ lim_process_beacon_frame(tpAniSirGlobal mac_ctx, uint8_t *rx_pkt_info,
 	 * beacon counter
 	 */
 	mac_hdr = WMA_GET_RX_MAC_HEADER(rx_pkt_info);
-	lim_log(mac_ctx, LOG2,
-		FL("Received Beacon frame with length=%d from "),
+	pe_debug("Received Beacon frame with length: %d from",
 		WMA_GET_RX_MPDU_LEN(rx_pkt_info));
 		lim_print_mac_addr(mac_ctx, mac_hdr->sa, LOG2);
 
 	/* Expect Beacon in any state as Scan is independent of LIM state */
 	bcn_ptr = qdf_mem_malloc(sizeof(*bcn_ptr));
 	if (NULL == bcn_ptr) {
-		lim_log(mac_ctx, LOGE,
-			FL("Unable to allocate memory"));
+		pe_err("Unable to allocate memory");
 		return;
 	}
 	/* Parse received Beacon */
@@ -97,8 +95,7 @@ lim_process_beacon_frame(tpAniSirGlobal mac_ctx, uint8_t *rx_pkt_info,
 		 * Received wrongly formatted/invalid Beacon.
 		 * Ignore it and move on.
 		 */
-		lim_log(mac_ctx, LOGW,
-			FL("Received invalid Beacon in state %X"),
+		pe_warn("Received invalid Beacon in state: %X",
 			session->limMlmState);
 		lim_print_mlm_state(mac_ctx, LOGW,
 			session->limMlmState);
@@ -146,8 +143,7 @@ lim_process_beacon_frame(tpAniSirGlobal mac_ctx, uint8_t *rx_pkt_info,
 		session->bcnLen = WMA_GET_RX_PAYLOAD_LEN(rx_pkt_info);
 		session->beacon = qdf_mem_malloc(session->bcnLen);
 		if (NULL == session->beacon) {
-			lim_log(mac_ctx, LOGE,
-				FL("fail to alloc mem to store bcn"));
+			pe_err("fail to alloc mem to store bcn");
 		} else {
 			/*
 			 * Store the Beacon/ProbeRsp. This is sent to
