5c9dfe4656f7520dbafed050424ff22a9e2d7ff1
Srinivas Girigowda <sgirigow@codeaurora.org>
2017-03-25 10:49:14 -0700

    qcacld-3.0: Fix kernel checkpatch warnings in wlan_hdd_lro.h
    Fix kernel checkpatch warnings in wlan_hdd_lro.h.
    
    Change-Id: Idaec566829ed61c0e349c970f4b34f38a825f7d5
    CRs-Fixed: 2024274

--- core/hdd/inc/wlan_hdd_lro.h
+++ core/hdd/inc/wlan_hdd_lro.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2015-2016 The Linux Foundation. All rights reserved.
+ * Copyright (c) 2015-2017 The Linux Foundation. All rights reserved.
  *
  * Previously licensed under the ISC license by Qualcomm Atheros, Inc.
  *
@@ -188,12 +188,10 @@ static inline int hdd_lro_init(hdd_context_t *hdd_ctx)
 static inline void hdd_lro_disable(hdd_context_t *hdd_ctx,
 	 hdd_adapter_t *adapter)
 {
-	return;
 }
 
 static inline void hdd_lro_display_stats(hdd_context_t *hdd_ctx)
 {
-	return;
 }
 #endif /* FEATURE_LRO */
 #endif /* __WLAN_HDD_LRO_H__ */
