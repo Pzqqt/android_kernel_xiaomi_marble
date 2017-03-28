32fa740c8580206521a1ff31ba1057e37146c27f
SaidiReddy Yenuga <saidir@codeaurora.org>
2017-03-28 16:03:08 +0530

    qcacld-3.0: validate NLA attr in wlan_hdd_cfg80211_set_ext_roam_params
    NLA attr QCA_WLAN_VENDOR_ATTR_ROAMING_PARAM_SET_BSSID_PREFS
    used without validation.
    
    validate NLA attribute.
    
    CRs-Fixed: 2025636
    Change-Id: I810f6a15b425638330211919c606e52de562cf9c

--- core/hdd/src/wlan_hdd_cfg80211.c
+++ core/hdd/src/wlan_hdd_cfg80211.c
@@ -3398,6 +3398,10 @@ __wlan_hdd_cfg80211_set_ext_roam_params(struct wiphy *wiphy,
 			goto fail;
 		}
 		hdd_debug("Num of Preferred BSSID (%d)", count);
+		if (!tb[QCA_WLAN_VENDOR_ATTR_ROAMING_PARAM_SET_BSSID_PREFS]) {
+			hdd_err("attr Preferred BSSID failed");
+			goto fail;
+		}
 		i = 0;
 		nla_for_each_nested(curr_attr,
 			tb[QCA_WLAN_VENDOR_ATTR_ROAMING_PARAM_SET_BSSID_PREFS],
