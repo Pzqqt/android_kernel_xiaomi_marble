139de79c9e5b06e302e78e7eb0292f470a308b52
Yun Park <yunp@codeaurora.org>
2017-04-03 08:45:10 -0700

    qcacmn: Fix Datapath kernel checkpatch warnings in cdp_txrx_tx_throttle.h
    Fix Datapath kernel checkpatch warnings in cdp_txrx_tx_throttle.h
    
    Change-Id: I75f5d4408d2616fd14078fff24a6a0954c73e62f
    CRs-Fixed: 2033691

--- dp/inc/cdp_txrx_tx_throttle.h
+++ dp/inc/cdp_txrx_tx_throttle.h
@@ -24,7 +24,7 @@
  * under proprietary terms before Copyright ownership was assigned
  * to the Linux Foundation.
  */
- /**
+/**
  * @file cdp_txrx_tx_throttle.h
  * @brief Define the host data path transmit throttle API
  * functions called by the host control SW and the OS interface
