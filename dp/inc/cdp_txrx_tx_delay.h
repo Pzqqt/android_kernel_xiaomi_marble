1bd62b76d9bd04184605ce98fe1dd2f8ac403130
Yun Park <yunp@codeaurora.org>
2017-04-03 08:21:50 -0700

    qcacmn: Fix Datapath kernel checkpatch warnings in cdp_txrx_tx_delay.h
    Fix Datapath kernel checkpatch warnings in cdp_txrx_tx_delay.h
    
    Change-Id: I4c738b201637981ddefaf4719730440299870643
    CRs-Fixed: 2033691

--- dp/inc/cdp_txrx_tx_delay.h
+++ dp/inc/cdp_txrx_tx_delay.h
@@ -24,7 +24,7 @@
  * under proprietary terms before Copyright ownership was assigned
  * to the Linux Foundation.
  */
- /**
+/**
  * @file cdp_txrx_tx_delay.h
  * @brief Define the host data path histogram API functions
  * called by the host control SW and the OS interface module
