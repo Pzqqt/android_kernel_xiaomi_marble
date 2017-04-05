7abd54674ead7e680349d6846468505390276880
Tushnim Bhattacharyya <tushnimb@codeaurora.org>
2017-04-04 17:23:26 -0700

    qcacmn: Prune PCL to active channels only for P2P GO
    Prune the PCL to have only active channels for P2P GO mode.
    Change-Id: Ifade00b3ccfcecff16a49e9e9aa5e077d5f9629b
    CRs-Fixed: 2029007

--- umac/cmn_services/policy_mgr/src/wlan_policy_mgr_pcl.c
+++ umac/cmn_services/policy_mgr/src/wlan_policy_mgr_pcl.c
@@ -139,6 +139,31 @@ void policy_mgr_update_with_safe_channel_list(uint8_t *pcl_channels,
 	return;
 }
 
+static QDF_STATUS policy_mgr_modify_pcl_based_on_enabled_channels(
+						uint8_t *pcl_list_org,
+						uint8_t *weight_list_org,
+						uint32_t *pcl_len_org)
+{
+	uint32_t i, pcl_len = 0;
+	uint8_t pcl_list[QDF_MAX_NUM_CHAN];
+	uint8_t weight_list[QDF_MAX_NUM_CHAN];
+
+	for (i = 0; i < *pcl_len_org; i++) {
+		if (!wlan_reg_is_passive_or_disable_ch(pcl_list_org[i])) {
+			pcl_list[pcl_len] = pcl_list_org[i];
+			weight_list[pcl_len++] = weight_list_org[i];
+		}
+	}
+
+	qdf_mem_zero(pcl_list_org, QDF_ARRAY_SIZE(pcl_list_org));
+	qdf_mem_zero(weight_list_org, QDF_ARRAY_SIZE(weight_list_org));
+	qdf_mem_copy(pcl_list_org, pcl_list, pcl_len);
+	qdf_mem_copy(weight_list_org, weight_list, pcl_len);
+	*pcl_len_org = pcl_len;
+
+	return QDF_STATUS_SUCCESS;
+}
+
 uint8_t policy_mgr_get_channel(struct wlan_objmgr_psoc *psoc,
 			enum policy_mgr_con_mode mode, uint32_t *vdev_id)
 {
@@ -291,6 +316,19 @@ QDF_STATUS policy_mgr_get_pcl(struct wlan_objmgr_psoc *psoc,
 			policy_mgr_debug("chan:%d weight:%d",
 					pcl_channels[i], pcl_weight[i]);
 
+	}
+
+	if (mode == PM_P2P_GO_MODE) {
+		status = policy_mgr_modify_pcl_based_on_enabled_channels(
+		pcl_channels, pcl_weight, len);
+		if (QDF_IS_STATUS_ERROR(status)) {
+			policy_mgr_err("failed to get modified pcl for GO");
+			return status;
+		}
+		policy_mgr_debug("modified pcl len:%d", *len);
+		for (i = 0; i < *len; i++)
+			policy_mgr_debug("chan:%d weight:%d",
+			pcl_channels[i], pcl_weight[i]);
 	}
 
 	return QDF_STATUS_SUCCESS;
