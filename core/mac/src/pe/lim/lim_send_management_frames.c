0c81692edfe3fa9c7f043273396e0eaebfe32244
Arif Hussain <arifhussain@codeaurora.org>
2017-04-06 15:04:44 -0700

    qcacld-3.0: Fix null pointer access in lim_send_assoc_rsp_mgmt_frame
    Make sure sta is not null before dereferencing in
    lim_send_assoc_rsp_mgmt_frame function.
    
    Change-Id: Ifb7c4a3b1e02297906bfa1b7908c27929682cf28
    CRs-Fixed: 2030304

--- core/mac/src/pe/lim/lim_send_management_frames.c
+++ core/mac/src/pe/lim/lim_send_management_frames.c
@@ -1296,6 +1296,9 @@ lim_send_assoc_rsp_mgmt_frame(tpAniSirGlobal mac_ctx,
 						* retry_int);
 		}
 #endif
+
+		if (LIM_IS_AP_ROLE(pe_session)  && sta->non_ecsa_capable)
+			pe_session->lim_non_ecsa_cap_num++;
 	}
 
 	qdf_mem_set((uint8_t *) &beacon_params, sizeof(beacon_params), 0);
@@ -1305,9 +1308,6 @@ lim_send_assoc_rsp_mgmt_frame(tpAniSirGlobal mac_ctx,
 		    WNI_CFG_FORCE_POLICY_PROTECTION_DISABLE))
 			lim_decide_ap_protection(mac_ctx, peer_addr,
 				&beacon_params, pe_session);
-
-	if (LIM_IS_AP_ROLE(pe_session) && sta->non_ecsa_capable)
-		pe_session->lim_non_ecsa_cap_num++;
 
 	lim_update_short_preamble(mac_ctx, peer_addr, &beacon_params,
 		pe_session);
