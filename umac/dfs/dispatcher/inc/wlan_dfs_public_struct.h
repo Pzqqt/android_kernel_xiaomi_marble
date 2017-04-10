a1e18b6b7dd9bafa187402a7d4837657e24131aa
Arif Hussain <arifhussain@codeaurora.org>
2017-04-10 00:44:06 -0700

    qcacmn: Add dfs public structure file
    Add dfs public structure file.
    
    Change-Id: Id34164da42d2d7b593fcb86ebd585140cd263f6e
    CRs-Fixed: 2017481

---
+++ umac/dfs/dispatcher/inc/wlan_dfs_public_struct.h
@@ -0,0 +1,48 @@
+/*
+ * Copyright (c) 2017 The Linux Foundation. All rights reserved.
+ *
+ * Permission to use, copy, modify, and/or distribute this software for
+ * any purpose with or without fee is hereby granted, provided that the
+ * above copyright notice and this permission notice appear in all
+ * copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL
+ * WARRANTIES WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE
+ * AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL
+ * DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR
+ * PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER
+ * TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
+ * PERFORMANCE OF THIS SOFTWARE.
+ */
+
+/**
+ * DOC: wlan_dfs_public_struct.h
+ * This file contains DFS data structures
+ */
+
+#ifndef __WLAN_DFS_PUBLIC_STRUCT_H_
+#define __WLAN_DFS_PUBLIC_STRUCT_H_
+
+/**
+ * struct radar_found_info - radar found info
+ * @pdev_id: pdev id.
+ * @detection_mode: In-service mode or O-CAC mod.
+ * @freq_offset: frequency offset.
+ * @chan_width: channel width.
+ * @detector_id: detector id.
+ * @segment_id: segment id.
+ * @timestamp: timestamp.
+ * @is_chirp: is chirp or not.
+ */
+struct radar_found_info {
+	uint32_t pdev_id;
+	uint32_t detection_mode;
+	int32_t freq_offset;
+	uint32_t chan_width;
+	uint32_t detector_id;
+	uint32_t segment_id;
+	uint32_t timestamp;
+	uint32_t is_chirp;
+};
+#endif
