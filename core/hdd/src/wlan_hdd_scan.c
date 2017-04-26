479e4c0e4b528ac8152b92b66cbd55e187a81b8c
yeshwanth sriram guntuka <ysriramg@codeaurora.org>
2017-04-26 13:12:44 +0530

    qcacld-3.0: Reject scan on IBSS vdev
    Firmware crash due to scan received on
    IBSS vdev and non DBS mode.
    
    Fix is to reject scan when in ibss mode.
    
    Change-Id: I2ca126ded013beb658b0bb82d854b0a3acccba5a
    CRs-Fixed: 2038271

--- core/hdd/src/wlan_hdd_scan.c
+++ core/hdd/src/wlan_hdd_scan.c
@@ -1538,16 +1538,13 @@ static int __wlan_hdd_cfg80211_scan(struct wiphy *wiphy,
 		pAdapter->device_mode);
 
 	/*
-	 * IBSS vdev does not have peers on other macs,
-	 * so it does not support scan on other band,
-	 * and IBSS vdev does not need to scan to establish
+	 * IBSS vdev does not need to scan to establish
 	 * IBSS connection. If IBSS vdev need to support scan,
 	 * Firmware need to make the change to add self peer
 	 * per mac for IBSS vdev.
 	 */
-	if (policy_mgr_is_hw_dbs_capable(pHddCtx->hdd_psoc) &&
-	   (QDF_IBSS_MODE == pAdapter->device_mode)) {
-		hdd_err("Scan not supported for IBSS in if HW support DBS");
+	if (QDF_IBSS_MODE == pAdapter->device_mode) {
+		hdd_err("Scan not supported for IBSS");
 		return -EINVAL;
 	}
 
