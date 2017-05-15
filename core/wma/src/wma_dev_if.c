32f63aae849cf108dbc4e0f825fceaeeb4c67e53
Ashish Kumar Dhanotiya <adhanoti@codeaurora.org>
2017-05-15 20:31:33 +0530

    qcacld-3.0: Fix SAP beaconing failure after SSR/PDR
    Remove the check for recovery in progress in wma_fill_hold_req
    and wma_fill_vdev_req.
    
    Currently Re-initialization of the SAP after SSR happens with
    the recovery_in_progress flag set, so in wma_fill_hold_req and
    wma_fill_vdev_req APIs we need to remove the check for this flag.
    
    Change-Id: Id6c875936a9f0dddeacc89c24203eaa3c7f72d67
    CRs-Fixed: 2047286

--- core/wma/src/wma_dev_if.c
+++ core/wma/src/wma_dev_if.c
@@ -2612,9 +2612,6 @@ struct wma_target_req *wma_fill_hold_req(tp_wma_handle wma,
 	struct wma_target_req *req;
 	QDF_STATUS status;
 
-	if (cds_is_driver_recovering())
-		return NULL;
-
 	req = qdf_mem_malloc(sizeof(*req));
 	if (!req) {
 		WMA_LOGE(FL("Failed to allocate memory for msg %d vdev %d"),
@@ -2914,9 +2911,6 @@ struct wma_target_req *wma_fill_vdev_req(tp_wma_handle wma,
 {
 	struct wma_target_req *req;
 	QDF_STATUS status;
-
-	if (cds_is_driver_recovering())
-		return NULL;
 
 	req = qdf_mem_malloc(sizeof(*req));
 	if (!req) {
