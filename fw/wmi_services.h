af7ff0a1af7b82dbc4e107bfe07033c13336a5cf
Sandeep Puligilla <spuligil@codeaurora.org>
2017-05-23 13:19:59 -0700

    fw-api: CL 3110231 - update fw common interface files
    Define WMI_SERVICE_CHAN_LOAD_INFO to show whether WMI chan info counters report incremental delta values or cumulative totals values.
    
    Change-Id: I32ecd5442f597fbfcd15e326781e40b698f5dec3
    CRs-Fixed: 1107600

--- fw/wmi_services.h
+++ fw/wmi_services.h
@@ -195,9 +195,6 @@ typedef  enum  {
     WMI_SERVICE_ACK_TIMEOUT=126, /* Support TX ack timeout configurations */
     WMI_SERVICE_PDEV_BSS_CHANNEL_INFO_64 = 127, /* BSS channel info (freq, noise floor, rx clear, cycles 64-bit counters) event support */
 
-
-    /******* ADD NEW SERVICES HERE UNTIL ALL VALUES UP TO 128 ARE USED *******/
-
     WMI_MAX_SERVICE=128, /* max service */
 
     /* NOTE:
@@ -210,8 +207,9 @@ typedef  enum  {
      * WMI_SERVICE_READY_EVENT message.
      */
 
-    /*PUT 1ST EXT SERVICE HERE:*//*WMI_SERVICE_xxxxxxxx=128,*/
-    /*PUT 2ND EXT SERVICE HERE:*//*WMI_SERVICE_yyyyyyyy=129,*/
+    WMI_SERVICE_CHAN_LOAD_INFO=128, /* The values in WMI_CHAN_INFO_EVENTID is the difference in cycle counters */
+
+    /******* ADD NEW SERVICES HERE *******/
 
     WMI_MAX_EXT_SERVICE
 
