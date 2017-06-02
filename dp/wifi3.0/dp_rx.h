a3088d9a346443c8909e576d12f85ea921b8365e
Tallapragada Kalyan <ktallapr@codeaurora.org>
2017-06-03 00:16:22 +0530

    qcacmn: WDS changes to address multi-radio scenario
    Because of mult-radio changes where we have soc and scn
    structures modified we need to address the same for WDS
    
    Change-Id: I63f95d93ac15cdbcdd2b05cb80ce364115e9c16e

--- dp/wifi3.0/dp_rx.h
+++ dp/wifi3.0/dp_rx.h
@@ -343,13 +343,13 @@ dp_rx_wds_srcport_learn(struct dp_soc *soc,
 
 	if (!hal_rx_msdu_end_sa_is_valid_get(rx_tlv_hdr)) {
 		ret = soc->cdp_soc.ol_ops->peer_add_wds_entry(
-						soc->osif_soc,
+						ta_peer->vdev->pdev->osif_pdev,
 						wds_src_mac,
 						ta_peer->mac_addr.raw,
 						flags);
 	} else if (sa_sw_peer_id != ta_peer->peer_ids[0]) {
 		ret = soc->cdp_soc.ol_ops->peer_update_wds_entry(
-						soc->osif_soc,
+						ta_peer->vdev->pdev->osif_pdev,
 						wds_src_mac,
 						ta_peer->mac_addr.raw,
 						flags);
