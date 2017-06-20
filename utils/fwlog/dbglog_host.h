5c1cc50c22ba456694337a4f91949984ce336569
Sandeep Puligilla <spuligil@codeaurora.org>
2017-06-20 16:17:49 -0700

    qcacmn: Fix compilation errors
    Fix compilation errors after updating
    latest dbglog.h file from fw.
    
    Change-Id: I3defd4242d922364849041b35bddb3cca4f9a39a
    CRs-Fixed: 2066361

--- utils/fwlog/dbglog_host.h
+++ utils/fwlog/dbglog_host.h
@@ -128,7 +128,7 @@ dbglog_report_enable(wmi_unified_t wmi_handle, A_BOOL isenable);
  * @brief DBGLOG_ERROR - default log level
  */
 int
-dbglog_set_log_lvl(wmi_unified_t wmi_handle, enum DBGLOG_LOG_LVL log_lvl);
+dbglog_set_log_lvl(wmi_unified_t wmi_handle, DBGLOG_LOG_LVL log_lvl);
 #endif
 
 /*
