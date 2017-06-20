6153382faec1a7fca1271482e25f43e0bf782576
hqu <hqu@codeaurora.org>
2017-06-20 15:04:05 +0800

    qcacld-3.0: Distinguish wlan target defaut config in wlan_hdd_main.h
    Use macro CONFIG_HL_SUPPORT to do conditional compilation in
    wlan_hdd_main.h, it can distinguish HL target from other target
    so that it will use different wlan target defaut config.
    
    Change-Id: Ia33cd336de34c17b73909b671c83e304def3d74b
    CRs-Fixed: 2063356

--- core/hdd/inc/wlan_hdd_main.h
+++ core/hdd/inc/wlan_hdd_main.h
@@ -61,7 +61,11 @@
 #include "cdp_txrx_flow_ctrl_legacy.h"
 #include <cdp_txrx_peer_ops.h>
 #include "wlan_hdd_nan_datapath.h"
+#if defined(CONFIG_HL_SUPPORT)
+#include "wlan_tgt_def_config_hl.h"
+#else
 #include "wlan_tgt_def_config.h"
+#endif
 #include <wlan_objmgr_cmn.h>
 #include <wlan_objmgr_global_obj.h>
 #include <wlan_objmgr_psoc_obj.h>
