19d322b90726ced6335b53f54a12fe61ed3a8e9b
Will Huang <wilhuang@codeaurora.org>
2017-07-03 17:48:26 +0800

    Revert "qcacld-3.0: Reject AssocReq without cipher IE to security SAP"
    This reverts commit e21734860079b0a8bf9f3158a2bf1fbcc09d0855, because
    it fails WPS_PIN certification test. Some ref-STA connect to SAP
    without WPS IEs and any security IEs when test this case, but this case
    can still pass if accept this Association Request.
    
    Change-Id: I037752a78a72730f88569c2cf222da7e0cf6e085
    CRs-Fixed: 2070525

--- core/mac/src/pe/lim/lim_process_assoc_req_frame.c
+++ core/mac/src/pe/lim/lim_process_assoc_req_frame.c
@@ -847,7 +847,8 @@ static bool lim_chk_n_process_wpa_rsn_ie(tpAniSirGlobal mac_ctx,
 						session);
 					return false;
 				}
-			} else if (assoc_req->wpaPresent) {
+			} /* end - if(assoc_req->rsnPresent) */
+			if ((!assoc_req->rsnPresent) && assoc_req->wpaPresent) {
 				/* Unpack the WPA IE */
 				if (assoc_req->wpa.length) {
 					/* OUI is not taken care */
@@ -896,31 +897,7 @@ static bool lim_chk_n_process_wpa_rsn_ie(tpAniSirGlobal mac_ctx,
 						session);
 					return false;
 				} /* end - if(assoc_req->wpa.length) */
-			} else if (assoc_req->wapiPresent) {
-				pe_debug("Assoc req wapiPresent");
-			} else {
-				/*
-				 * When client send AssocReq without any
-				 * cipher IE,We must reject it here,
-				 * otherwise hostapd will not trigger
-				 * eapol and will not delete it, remains
-				 * connected/not-authenticated state
-				 * and block sta scan as checking in
-				 * cds_is_connection_in_progress.
-				 */
-				pe_warn("Re/Assoc rejected from: "
-					MAC_ADDRESS_STR
-					" without cipher suite IE",
-					MAC_ADDR_ARRAY(hdr->sa));
-				lim_send_assoc_rsp_mgmt_frame(mac_ctx,
-					eSIR_MAC_CIPHER_SUITE_REJECTED_STATUS,
-					1,
-					hdr->sa,
-					sub_type,
-					0,
-					session);
-				return false;
-			}
+			} /* end - if(assoc_req->wpaPresent) */
 		}
 		/*
 		 * end of if(session->pLimStartBssReq->privacy
