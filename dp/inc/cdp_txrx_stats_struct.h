1b5ceb75a2c3cbdb3f1e8116939b7a72db6ac441
Gurumoorthi Gnanasambandhan <gguru@codeaurora.org>
2017-08-10 22:12:36 +0530

    qcacmn: Initialize pdev_id for PDEV STATS request and event
    Initialize pdev_id for PDEV stats request and response event.
    Add param to set pdev stats request timer.
    
    Change-Id: I11f648070cd39f026129d1cf3aa13bf296d25e6e

--- dp/inc/cdp_txrx_stats_struct.h
+++ dp/inc/cdp_txrx_stats_struct.h
@@ -496,6 +496,7 @@ enum _ol_ath_param_t {
 	OL_ATH_PARAM_ACS_SRLOADVAR = 352,
 	OL_ATH_PARAM_MGMT_RSSI_THRESHOLD = 353,
 	OL_ATH_PARAM_EXT_NSS_CAPABLE = 354,
+	OL_ATH_PARAM_MGMT_PDEV_STATS_TIMER = 355,
 };
 
 /* Enumeration of PDEV Configuration parameter */
