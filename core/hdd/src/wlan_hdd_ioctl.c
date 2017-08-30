248697d555d0f106c883e202d4c9479c79dda759
Rajeev Kumar Sirasanagandla <rsirasan@codeaurora.org>
2017-08-30 12:27:45 +0530

    qcacld-3.0: Fix incorrect argument in SETROAMMODE IOCTL handler
    qcacld-2.0 to qcacld-3.0 propagation
    
    While processing SETROAMMODE IOCTL, invalid argument is passed to
    kstrtou8() due to which error is returned for valid command.
    
    To fix this, invoke kstrtou8 with base 10.
    
    Change-Id: I78b10b6c13338c3afa420792e2ddbc7c945a7fec
    CRs-Fixed: 2101398

--- core/hdd/src/wlan_hdd_ioctl.c
+++ core/hdd/src/wlan_hdd_ioctl.c
@@ -3154,7 +3154,7 @@ static int drv_cmd_set_roam_mode(hdd_adapter_t *adapter,
 	value = value + SIZE_OF_SETROAMMODE + 1;
 
 	/* Convert the value from ascii to integer */
-	ret = kstrtou8(value, SIZE_OF_SETROAMMODE, &roamMode);
+	ret = kstrtou8(value, 10, &roamMode);
 	if (ret < 0) {
 		/*
 		 * If the input value is greater than max value of datatype,
