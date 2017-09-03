6c4bf942b0a8bdc60842e7be3947a6331d484b08
Jeff Johnson <jjohnson@codeaurora.org>
2017-09-02 18:20:51 -0700

    qcacld-3.0: Remove obsolete struct wlan_txrx_ind_req
    struct wlan_txrx_ind_req is no longer used, so remove it.
    
    Change-Id: I28ebf1bba8a95df8270d3f682971afd371936683
    CRs-Fixed: 2103631

--- core/dp/txrx/txrx.h
+++ core/dp/txrx/txrx.h
@@ -119,18 +119,6 @@ typedef QDF_STATUS (*wlan_txrx_rssi_cross_thresh)(void *adapter, u8 rssi,
 						  void *usr_ctx,
 						  int8_t avg_rssi);
 
-struct wlan_txrx_ind_req {
-	u16 msgType;            /* message type is same as the request type */
-	u16 msgLen;             /* length of the entire request */
-	u8 sessionId;           /* sme Session Id */
-	u8 rssiNotification;
-	u8 avgRssi;
-	void *tlCallback;
-	void *pAdapter;
-	void *pUserCtxt;
-};
-
-
 /* Rx callback registered with txrx */
 typedef int (*wlan_txrx_cb_type)(void *g_cdsctx, qdf_nbuf_t buf, u8 sta_id,
 				 struct txrx_rx_metainfo *rx_meta_info);
