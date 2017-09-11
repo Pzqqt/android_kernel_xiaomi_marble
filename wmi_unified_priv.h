c170542a3856d5cc7dffe4c8b293d849cb2adeec
Anurag Chouhan <achouhan@codeaurora.org>
2017-09-11 14:56:30 +0530

    qcacmn: Add wmi interface command to support debug stats
    Add wmi interface command to support set/get NUD debug stats.
    
    Change-Id: I2c5e9e2f6b0bb2e7b7b0a6404e939b33bffa3f18
    CRs-Fixed: 2011463

--- wmi_unified_priv.h
+++ wmi_unified_priv.h
@@ -750,6 +750,12 @@ QDF_STATUS (*send_roam_scan_offload_rssi_change_cmd)(wmi_unified_t wmi_handle,
 QDF_STATUS (*send_per_roam_config_cmd)(wmi_unified_t wmi_handle,
 		struct wmi_per_roam_config_req *req_buf);
 
+QDF_STATUS (*send_set_arp_stats_req_cmd)(wmi_unified_t wmi_handle,
+					 struct set_arp_stats *req_buf);
+
+QDF_STATUS (*send_get_arp_stats_req_cmd)(wmi_unified_t wmi_handle,
+					 struct get_arp_stats *req_buf);
+
 QDF_STATUS (*send_get_buf_extscan_hotlist_cmd)(wmi_unified_t wmi_handle,
 				   struct ext_scan_setbssi_hotlist_params *
 				   photlist, int *buf_len);
