861a8fa60c95fb9f7fa1784c00173754ddb45a11
Tanya Dixit <tdixit@codeaurora.org>
2017-09-12 15:38:21 +0530

    dsp: fix no recovery after SSR/PDR
    Add apr_reset for q6core service to enable glink channel
    to close and open properly after SSR.
    
    CRs-Fixed: 2099312
    Change-Id: Ia023cdb19a6a9dc7fae3e5c28ab51ddd92d37527
    Signed-off-by: Tanya Dixit <tdixit@codeaurora.org>

--- dsp/q6core.c
+++ dsp/q6core.c
@@ -210,7 +210,11 @@ static int32_t aprv2_core_fn_q(struct apr_client_data *data, void *priv)
 	case RESET_EVENTS:{
 		pr_debug("%s: Reset event received in Core service\n",
 			__func__);
-		/* no reset done as the data will not change after SSR*/
+		/*
+		 * no reset for q6core_avcs_ver_info done as
+		 * the data will not change after SSR
+		 */
+		apr_reset(q6core_lcl.core_handle_q);
 		q6core_lcl.core_handle_q = NULL;
 		break;
 	}
