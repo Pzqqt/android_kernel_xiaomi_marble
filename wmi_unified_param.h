9ecfb55fccc8aa95a0b471dfe31b8cfb2494cbe4
Gyanranjan Hazarika <gyanranj@codeaurora.org>
2017-09-18 15:59:34 -0700

    qcacmn: Add support for 'he_ltf' command
    Currently, we do not have support for 'he_ltf' command. But we need
    this command for certain IOP testcases. Add support for 'he_ltf'
    command.
    
    Change-Id: I296b18762aaa52da80a5755c5bcca8dbd12d9b13
    CRs-fixed: 2118948

--- wmi_unified_param.h
+++ wmi_unified_param.h
@@ -5400,6 +5400,7 @@ typedef enum {
 	wmi_vdev_param_he_bss_color,
 	wmi_vdev_param_he_range_ext_enable,
 	wmi_vdev_param_set_hemu_mode,
+	wmi_vdev_param_set_he_ltf,
 
 	wmi_vdev_param_max,
 } wmi_conv_vdev_param_id;
