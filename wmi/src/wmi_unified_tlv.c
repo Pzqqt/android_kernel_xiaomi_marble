41ac5cf9a71f63e62892e12c809a60d0bc1c034b
Edayilliam Jayadev <ejayadev@codeaurora.org>
2017-09-22 13:21:03 +0530

    qcacmn: Add fields to wmi channel info event object
    Populate tx frame count and mac clock frequency to the
    wmi channel info event object.
    
    CRs-Fixed: 2114541
    Change-Id: I6180e377d576409705e57d7aa9172141965f79ad

--- wmi/src/wmi_unified_tlv.c
+++ wmi/src/wmi_unified_tlv.c
@@ -17516,6 +17516,8 @@ static QDF_STATUS extract_chan_info_event_tlv(wmi_unified_t wmi_handle,
 	chan_info->noise_floor = ev->noise_floor;
 	chan_info->rx_clear_count = ev->rx_clear_count;
 	chan_info->cycle_count = ev->cycle_count;
+	chan_info->tx_frame_cnt = ev->tx_frame_cnt;
+	chan_info->mac_clk_mhz = ev->mac_clk_mhz;
 	chan_info->pdev_id = wlan_get_pdev_id_from_vdev_id(
 			(struct wlan_objmgr_psoc *)wmi_handle->soc->wmi_psoc,
 			ev->vdev_id, WLAN_SCAN_ID);
