ab131773f7d44b7dcaa1d52f8a1bb265ffa61a26
Yun Park <yunp@codeaurora.org>
2017-09-26 16:39:17 -0700

    qcacmn: Enable RPS dynamically for SAP mode
    SAP Rx TPUT is bounded by host network stack, which is running in the
    context of IPA work-queue, when IPA offload data path enabled.
    However RPS shouldn't enabled for legacy Rx data path, where NAPI is
    enabled. So, we dynamically enable RPS for SAP interface when the vdev
    comes up and IPA was enabled only.
    Send RPS enable indication to CNSS daemon to enable RPS only when SAP
    interface comes up and disable RPS when SAP interface is closed.
    
    Change-Id: Ia669b2c3ae814d0577f5226d8acbf026740ad760
    CRs-Fixed: 2129520

--- src/wlan_policy_mgr_init_deinit.c
+++ src/wlan_policy_mgr_init_deinit.c
@@ -632,6 +632,8 @@ QDF_STATUS policy_mgr_register_dp_cb(struct wlan_objmgr_psoc *psoc,
 
 	pm_ctx->dp_cbacks.hdd_disable_lro_in_concurrency =
 		dp_cbacks->hdd_disable_lro_in_concurrency;
+	pm_ctx->dp_cbacks.hdd_set_rx_mode_rps_cb =
+		dp_cbacks->hdd_set_rx_mode_rps_cb;
 
 	return QDF_STATUS_SUCCESS;
 }
