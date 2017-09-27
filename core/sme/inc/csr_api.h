0a6ce08e04e10bad81ad8c0c00308c7a9169d1e2
hqu <hqu@codeaurora.org>
2017-09-27 22:00:35 +0800

    qcacld-3.0: Move chan_switch_hostapd_rate_enabled field for alignment
    Current structure is expected to be 4 bytes align which needs to be
    fixed by moving chan_switch_hostapd_rate_enabled field up in the
    structure.
    
    Change-Id: I234142a135fdeac69601d352beed7eda8ec16b99
    CRs-fixed: 2118364

--- core/sme/inc/csr_api.h
+++ core/sme/inc/csr_api.h
@@ -988,6 +988,7 @@ typedef struct tagCsrRoamProfile {
 	bool ApUapsdEnable;
 	bool protEnabled;
 	bool obssProtEnabled;
+	bool chan_switch_hostapd_rate_enabled;
 	uint16_t cfg_protection;
 	uint8_t wps_state;
 	tCsrMobilityDomainInfo MDID;
@@ -1008,7 +1009,6 @@ typedef struct tagCsrRoamProfile {
 	bool fils_connection;
 	struct cds_fils_connection_info *fils_con_info;
 #endif
-	bool chan_switch_hostapd_rate_enabled;
 } tCsrRoamProfile;
 
 #ifdef FEATURE_WLAN_MCC_TO_SCC_SWITCH
