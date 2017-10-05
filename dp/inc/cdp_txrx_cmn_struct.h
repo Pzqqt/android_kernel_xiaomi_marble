51ddfbcbeebe5fd687cc2d6752b0aec96771b72e
Basamma Yakkanahalli <ybasamma@codeaurora.org>
2017-10-05 22:03:30 +0530

    qcacmn: iwpriv txrx_fw_stats support halphy stats
    Added TXRX_FW_HALPHY_STATS to support txrx_fw_stats
    for halphy stats.
    
    Change-Id: I93a9c9e3199c7c163cd6e93d28acb4e757c1e3eb
    CRs-Fixed: 2114152

--- dp/inc/cdp_txrx_cmn_struct.h
+++ dp/inc/cdp_txrx_cmn_struct.h
@@ -585,6 +585,7 @@ enum cdp_vdev_param_type {
 #define TXRX_FW_MAC_PREFETCH_MGR_STATS           23
 #define TXRX_FW_STATS_DURATION_INFO              24
 #define TXRX_FW_STATS_DURATION_INFO_RESET        25
+#define TXRX_FW_HALPHY_STATS                     26
 #define TXRX_FW_STATS_HOST_RX_STATS              27
 
 #define PER_RADIO_FW_STATS_REQUEST 0
