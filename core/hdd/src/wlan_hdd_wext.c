9a5863f4d3a6174db8e09ba443a764655a177560
Jeff Johnson <jjohnson@codeaurora.org>
2017-10-21 13:39:57 -0700

    qcacld-3.0: Rename hdd_station_info tlSTAState field
    Per the Linux coding style "mixed-case names are frowned upon" so
    rename field tlSTAState in struct hdd_station_info.
    
    Change-Id: I8742b42144ed455213eef92426356c54b08e2038
    CRs-Fixed: 2131087

--- core/hdd/src/wlan_hdd_wext.c
+++ core/hdd/src/wlan_hdd_wext.c
@@ -4492,7 +4492,7 @@ int hdd_set_peer_rate(struct hdd_adapter *adapter, int set_value)
 	hdd_get_aid_rc(&aid, &rc, set_value);
 
 	if ((adapter->aStaInfo[aid].in_use) &&
-	    (OL_TXRX_PEER_STATE_CONN == adapter->aStaInfo[aid].tlSTAState)) {
+	    (OL_TXRX_PEER_STATE_CONN == adapter->aStaInfo[aid].peer_state)) {
 		peer_mac =
 		    (uint8_t *)&(adapter->aStaInfo[aid].macAddrSTA.bytes[0]);
 		hdd_info("Peer AID: %d MAC_ADDR: "MAC_ADDRESS_STR,
