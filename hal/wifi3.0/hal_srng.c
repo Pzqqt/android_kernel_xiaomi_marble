ebdb2daf684615d46352a3121a571695d9788d14
Alok Kumar <alokkuma@codeaurora.org>
2017-10-27 16:42:07 +0530

    qcacmn: Replace instances of unadorned %p
    Replace instances of unadorned %p which violates security guidelines.
    
    Change-Id: I0b00fb9f997d3ed6a2824c2bb133c343fc92caef
    CRs-Fixed: 2133330

--- hal/wifi3.0/hal_srng.c
+++ hal/wifi3.0/hal_srng.c
@@ -993,7 +993,7 @@ void hal_srng_dst_init_hp(struct hal_srng *srng,
 	*(srng->u.dst_ring.hp_addr) = srng->u.dst_ring.cached_hp;
 
 	QDF_TRACE(QDF_MODULE_ID_DP, QDF_TRACE_LEVEL_ERROR,
-		"hp_addr=%p, cached_hp=%d, hp=%d\n",
+		"hp_addr=%pK, cached_hp=%d, hp=%d\n",
 		(void *)srng->u.dst_ring.hp_addr, srng->u.dst_ring.cached_hp,
 		*(srng->u.dst_ring.hp_addr));
 }
