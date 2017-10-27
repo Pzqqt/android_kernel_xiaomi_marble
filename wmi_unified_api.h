7f70d66bdd2f31d00ad3dd5959a34d04f7269717
Naveen Rawat <naveenrawat@codeaurora.org>
2017-10-26 18:50:19 -0700

    qcacmn: Add Wake timer pattern API
    Add WOW_TIMER_PATTERN to enable host to set wake timer in FW when required.
    
    Change-Id: Id37420ee8295d11fc8fe0f0fcc344d6ef31c315b
    CRs-Fixed: 2134108

--- wmi_unified_api.h
+++ wmi_unified_api.h
@@ -732,6 +732,9 @@ QDF_STATUS wmi_unified_egap_conf_params_cmd(void *wmi_hdl,
 QDF_STATUS wmi_unified_fw_profiling_data_cmd(void *wmi_hdl,
 			uint32_t cmd, uint32_t value1, uint32_t value2);
 
+QDF_STATUS wmi_unified_wow_timer_pattern_cmd(void *wmi_hdl, uint8_t vdev_id,
+					     uint32_t cookie, uint32_t time);
+
 QDF_STATUS wmi_unified_nat_keepalive_en_cmd(void *wmi_hdl, uint8_t vdev_id);
 
 QDF_STATUS wmi_unified_csa_offload_enable(void *wmi_hdl, uint8_t vdev_id);
