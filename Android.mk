6bb3a96984c19c3007ef54d75c5e5e1e228a1c99
Laxminath Kasam <lkasam@codeaurora.org>
2017-11-09 12:51:13 +0530

    ASoC: Add support for qcs605 for dlkm
    Update make files to extend dlkm support for qcs605
    
    Change-Id: Icf736fb66ca12966f4c6cd72bb906eb32899868a
    Signed-off-by: Laxminath Kasam <lkasam@codeaurora.org>

--- Android.mk
+++ Android.mk
@@ -3,7 +3,7 @@ MY_LOCAL_PATH := $(call my-dir)
 
 UAPI_OUT := $(OUT)/obj/vendor/qcom/opensource/audio-kernel/include
 
-ifeq ($(call is-board-platform-in-list,sdm845 sdm670),true)
+ifeq ($(call is-board-platform-in-list,sdm845 sdm670 qcs605),true)
 $(shell mkdir -p $(UAPI_OUT)/linux;)
 $(shell mkdir -p $(UAPI_OUT)/sound;)
 
@@ -16,7 +16,7 @@ include $(MY_LOCAL_PATH)/asoc/codecs/Android.mk
 include $(MY_LOCAL_PATH)/asoc/codecs/wcd934x/Android.mk
 endif
 
-ifeq ($(call is-board-platform,sdm670),true)
+ifeq ($(call is-board-platform-in-list,sdm670 qcs605),true)
 include $(MY_LOCAL_PATH)/asoc/codecs/sdm660_cdc/Android.mk
 include $(MY_LOCAL_PATH)/asoc/codecs/msm_sdw/Android.mk
 endif
