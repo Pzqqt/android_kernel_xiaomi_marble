arter97/caf_qcacld-3.0

qcacld-3.0: Fix a memory leak in hdd_assoc handler
Function wlan_hdd_cfg80211_update_bss_db will return pointer of
struct cfg80211_bss.
Caller should invoke cfg80211_put_bss to free resource in pointer.
In some case those resource not be free.

Change-Id: I7fa55a09fcc3515361e9568cf87966e4533085e4
CRs-Fixed: 2146717

core/hdd/src/wlan_hdd_assoc.c
@@ -2577,6 +2577,10 @@ static QDF_STATUS hdd_association_completion_handler(hdd_adapter_t *pAdapter,
 					   eCSR_DISCONNECT_REASON_UNSPECIFIED);
 				}
 				return QDF_STATUS_E_FAILURE;
+			} else {
+				cfg80211_put_bss(
+					pHddCtx->wiphy,
+					bss);
 			}
 			if (pRoamInfo->u.pConnectedProfile->AuthType ==
 			    eCSR_AUTH_TYPE_FT_RSN
@@ -2792,10 +2796,6 @@ static QDF_STATUS hdd_association_completion_handler(hdd_adapter_t *pAdapter,
 				}
 			}
 			if (!hddDisconInProgress) {
-				cfg80211_put_bss(
-					pHddCtx->wiphy,
-					bss);
-
 				/*
 				 * Perform any WMM-related association
 				 * processing.

Pzqqt/android_kernel_xiaomi_marble

core/hdd/src/wlan_hdd_assoc.c
@@ -2787,6 +2787,10 @@ hdd_association_completion_handler(struct hdd_adapter *adapter,
 					   eCSR_DISCONNECT_REASON_UNSPECIFIED);
 				}
 				return QDF_STATUS_E_FAILURE;
+			} else {
+				cfg80211_put_bss(
+					hdd_ctx->wiphy,
+					bss);
 			}
 			if (roam_info->u.pConnectedProfile->AuthType ==
 			    eCSR_AUTH_TYPE_FT_RSN
@@ -3003,10 +3007,6 @@ hdd_association_completion_handler(struct hdd_adapter *adapter,
 				}
 			}
 			if (!hddDisconInProgress) {
-				cfg80211_put_bss(
-					hdd_ctx->wiphy,
-					bss);
-
 				/*
 				 * Perform any WMM-related association
 				 * processing.