8925c8f767acf6ad613490ce494a547bf5350f28
hqu <hqu@codeaurora.org>
2017-12-11 19:29:01 +0800

    qcacld-3.0: Move log level to info high if tx hits invalid peer state
    For sns test in some scenarios when tx hits invalid peer state it will
    print massive log so that it will lead to WD bark issue.
    
    Move log level to lower info high from warn if tx hits invalid peer state.
    
    Change-Id: I91d414e7203bf1e00094ca7b2fcebf80f4102082
    CRs-Fixed: 2156472

--- core/hdd/src/wlan_hdd_tx_rx.c
+++ core/hdd/src/wlan_hdd_tx_rx.c
@@ -546,7 +546,7 @@ static inline bool hdd_is_tx_allowed(struct sk_buff *skb, uint8_t peer_id)
 	if (OL_TXRX_PEER_STATE_CONN == peer_state &&
 	    ntohs(skb->protocol) == HDD_ETHERTYPE_802_1_X)
 		return true;
-	QDF_TRACE(QDF_MODULE_ID_HDD_DATA, QDF_TRACE_LEVEL_WARN,
+	QDF_TRACE(QDF_MODULE_ID_HDD_DATA, QDF_TRACE_LEVEL_INFO_HIGH,
 		  FL("Invalid peer state for Tx: %d"), peer_state);
 	return false;
 }
