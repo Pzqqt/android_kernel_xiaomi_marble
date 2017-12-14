067cd3e3ebb554f4e5957f38bcf5af060f6b00d0
Yeshwanth Sriram Guntuka <ysriramg@codeaurora.org>
2017-12-14 17:56:04 +0530

    qcacmn: Set passive dwell time
    Set passive dwell time to 28msecs for active
    scan when bt a2dp is enabled and hw is not dbs
    capable and when sta is connected on 2G band.
    
    Change-Id: I44f2e3d98f2d7ddc52e4902ba989131c256da4ef
    CRs-Fixed: 2160963

--- src/wlan_policy_mgr_get_set_utils.c
+++ src/wlan_policy_mgr_get_set_utils.c
@@ -2740,3 +2740,27 @@ bool policy_mgr_is_sta_sap_scc_allowed_on_dfs_chan(
 
 	return status;
 }
+
+bool policy_mgr_is_sta_connected_2g(struct wlan_objmgr_psoc *psoc)
+{
+	struct policy_mgr_psoc_priv_obj *pm_ctx;
+	uint32_t conn_index;
+	bool ret = false;
+
+	pm_ctx = policy_mgr_get_context(psoc);
+	if (!pm_ctx) {
+		policy_mgr_err("Invalid Context");
+		return ret;
+	}
+	qdf_mutex_acquire(&pm_ctx->qdf_conc_list_lock);
+	for (conn_index = 0; conn_index < MAX_NUMBER_OF_CONC_CONNECTIONS;
+	     conn_index++) {
+		if (pm_conc_connection_list[conn_index].mode == PM_STA_MODE &&
+		    pm_conc_connection_list[conn_index].chan <= 14 &&
+		    pm_conc_connection_list[conn_index].in_use)
+			ret = true;
+	}
+	qdf_mutex_release(&pm_ctx->qdf_conc_list_lock);
+
+	return ret;
+}
