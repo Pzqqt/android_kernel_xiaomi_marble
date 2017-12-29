942d1c6ed175c95f5b2041c2317f35a8af0c5ca3
Laxminath Kasam <lkasam@codeaurora.org>
2017-12-29 11:02:22 +0530

    audio: Enable audio compile for 8953
    Add support for audio compile for 8953.
    
    Change-Id: I37ef20b44aecba8ffc9a7d1d346bf83a9115ad1b
    Signed-off-by: Laxminath Kasam <lkasam@codeaurora.org>

--- asoc/codecs/Android.mk
+++ asoc/codecs/Android.mk
@@ -7,13 +7,13 @@ ifeq ($(call is-board-platform,sdm845),true)
 AUDIO_SELECT  := CONFIG_SND_SOC_SDM845=m
 endif
 
-ifeq ($(call is-board-platform-in-list,sdm670 qcs605),true)
+ifeq ($(call is-board-platform-in-list,msm8953 sdm670 qcs605),true)
 AUDIO_SELECT  := CONFIG_SND_SOC_SDM670=m
 endif
 
 AUDIO_CHIPSET := audio
 # Build/Package only in case of supported target
-ifeq ($(call is-board-platform-in-list,sdm845 sdm670 qcs605),true)
+ifeq ($(call is-board-platform-in-list,msm8953 sdm845 sdm670 qcs605),true)
 
 LOCAL_PATH := $(call my-dir)
 
@@ -74,7 +74,7 @@ LOCAL_MODULE_DEBUG_ENABLE := true
 LOCAL_MODULE_PATH         := $(KERNEL_MODULES_OUT)
 include $(DLKM_DIR)/AndroidKernelModule.mk
 ###########################################################
-ifeq ($(call is-board-platform-in-list,sdm670 qcs605),true)
+ifeq ($(call is-board-platform-in-list,msm8953 sdm670 qcs605),true)
 include $(CLEAR_VARS)
 LOCAL_MODULE              := $(AUDIO_CHIPSET)_wcd9335.ko
 LOCAL_MODULE_KBUILD_NAME  := wcd9335_dlkm.ko
