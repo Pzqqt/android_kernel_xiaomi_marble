0c738b7b020f435cdf5d16aa03d37bafdb926465
Himanshu Agarwal <himanaga@codeaurora.org>
2018-01-10 14:21:53 +0530

    qcacmn: Green AP UMAC componentization
    Add APIs, structures for Green AP component.
    
    Change-Id: I4a39470104c89c20eec5440b7ae251a764151fd5
    CRs-Fixed: 2166428

--- src/wlan_policy_mgr_get_set_utils.c
+++ src/wlan_policy_mgr_get_set_utils.c
@@ -2053,6 +2053,22 @@ enum policy_mgr_con_mode policy_mgr_convert_device_mode_to_qdf_type(
 	return mode;
 }
 
+QDF_STATUS policy_mgr_mode_specific_num_open_sessions(
+		struct wlan_objmgr_psoc *psoc, enum QDF_OPMODE mode,
+		uint8_t *num_sessions)
+{
+	struct policy_mgr_psoc_priv_obj *pm_ctx;
+
+	pm_ctx = policy_mgr_get_context(psoc);
+	if (!pm_ctx) {
+		policy_mgr_err("Invalid context");
+		return QDF_STATUS_E_FAILURE;
+	}
+
+	*num_sessions = pm_ctx->no_of_open_sessions[mode];
+	return QDF_STATUS_SUCCESS;
+}
+
 /**
  * policy_mgr_concurrent_open_sessions_running() - Checks for
  * concurrent open session
