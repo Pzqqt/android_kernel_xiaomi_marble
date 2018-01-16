08ed90571350f5003cb273f268def97a47fef376
Kabilan Kannan <kabilank@codeaurora.org>
2018-01-16 01:52:25 -0800

    qcacmn: Fix TDLS wpa2 teardown failures
    TDLS teardown request from the DUT is dropped
    in the peer, due to invalid MIC in the
    teardown frame.
    TDLS responder value is not set correctly in TDLS
    teardown frame, and it causes MIC failures in the
    peer device.
    Set the correct responder value in TDLS
    teardown frame.
    
    Change-Id: Ibe056c89f644c094a10f7a5eb51b6d90a13a4a01
    CRs-Fixed: 2155555

--- umac/tdls/core/src/wlan_tdls_mgmt.c
+++ umac/tdls/core/src/wlan_tdls_mgmt.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2017 The Linux Foundation. All rights reserved.
+ * Copyright (c) 2017-2018 The Linux Foundation. All rights reserved.
  *
  * Permission to use, copy, modify, and/or distribute this software for
  * any purpose with or without fee is hereby granted, provided that the
@@ -386,7 +386,7 @@ QDF_STATUS tdls_process_mgmt_req(
 	 * after the  cmd validation
 	 */
 	tdls_mgmt_req->tdls_mgmt.responder =
-			tdls_mgmt_req->chk_frame->responder;
+			!tdls_mgmt_req->chk_frame->responder;
 	tdls_mgmt_req->tdls_mgmt.status_code =
 			tdls_mgmt_req->chk_frame->status_code;
 
