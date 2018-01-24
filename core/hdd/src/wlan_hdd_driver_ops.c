104660100ced03a6420cb946c165d5dbf2906d86
Rajeev Kumar <rajekuma@codeaurora.org>
2018-01-24 14:12:20 -0800

    qcacld-3.0: Avoid panic during HDD resume failure if FW is down
    During resume process FW may assert hence to avoid kernel panic
    and let subsystem restart to gracefully recover the system avoid
    kernel panic if FW is down during HDD resume failure.
    
    Change-Id: I8c53b7251e35dd40b9700f15c0c4897ef80209f8
    CRs-Fixed: 2178302

--- core/hdd/src/wlan_hdd_driver_ops.c
+++ core/hdd/src/wlan_hdd_driver_ops.c
@@ -966,7 +966,8 @@ static int __wlan_hdd_bus_resume(void)
 	return 0;
 
 out:
-	if (cds_is_driver_recovering() || cds_is_driver_in_bad_state())
+	if (cds_is_driver_recovering() || cds_is_driver_in_bad_state() ||
+		cds_is_fw_down())
 		return 0;
 
 	QDF_BUG(false);
