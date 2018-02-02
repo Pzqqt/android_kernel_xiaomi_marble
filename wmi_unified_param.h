cc2eea069fe16ae9cf4dbe7325f0dfcf5ca85fe4
Padma, Santhosh Kumar <skpadma@codeaurora.org>
2018-02-02 18:18:34 +0530

    qcacmn: Add support for GMAC offload
    Add support for GMAC offload capability wmi_service_gmac_offload_support
    
    Change-Id: I4a2e5c52aae630f82294dcaea6cc7c23886dcd8a
    CRs-Fixed: 2185825

--- wmi_unified_param.h
+++ wmi_unified_param.h
@@ -5983,6 +5983,7 @@ typedef enum {
 	wmi_service_11k_neighbour_report_support,
 	wmi_service_ap_obss_detection_offload,
 	wmi_service_bss_color_offload,
+	wmi_service_gmac_offload_support,
 
 	wmi_services_max,
 } wmi_conv_service_ids;
