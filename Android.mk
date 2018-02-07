7ba0a822629f4482fcb44fa16e82d490e89291ae
Meng Wang <mwang@codeaurora.org>
2018-02-07 12:29:41 +0800

    ASoC: change OUT_DIR to PRODUCT_OUT for wcd9360
    OUT_DIR folder doesn't point to the right out folder. It would
    not delete the right Module.symvers for wcd9360.
    Change OUT_DIR to PRODUCT_OUT to delete the right wcd9360 module
    symbol.
    
    Change-Id: I6ad376e1cad8416d8eefeefe41e0933813d64997
    Signed-off-by: Meng Wang <mwang@codeaurora.org>

--- Android.mk
+++ Android.mk
@@ -31,6 +31,6 @@ include $(MY_LOCAL_PATH)/asoc/codecs/msm_sdw/Android.mk
 endif
 
 ifeq ($(call is-board-platform-in-list,msmnile),true)
-$(shell rm -rf $(OUT_DIR)/obj/vendor/qcom/opensource/audio-kernel/asoc/codecs/wcd9360/Module.symvers)
+$(shell rm -rf $(PRODUCT_OUT)/obj/vendor/qcom/opensource/audio-kernel/asoc/codecs/wcd9360/Module.symvers)
 include $(MY_LOCAL_PATH)/asoc/codecs/wcd9360/Android.mk
 endif
