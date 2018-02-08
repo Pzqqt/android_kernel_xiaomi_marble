38ff9e3bb1b0813a3e3ce23c748d31297cea4eb0
Manjunathappa Prakash <prakashpm@codeaurora.org>
2018-02-08 14:29:16 -0800

    qcacmn: clear the tx_desc flags in V2 flow_ctrl dp_tx_desc_free
    Make sure to clear the tx_desc->flags field before putting the tx_desc to
    free pool. This was not takencare in V2 flow_control implementation.
    
    Change-Id: I666b9b09de62d52cffc45e2b6db3a6be06d688e8
    CRs-Fixed: 2186884

--- dp/wifi3.0/dp_tx_desc.h
+++ dp/wifi3.0/dp_tx_desc.h
@@ -177,6 +177,7 @@ dp_tx_desc_free(struct dp_soc *soc, struct dp_tx_desc_s *tx_desc,
 	struct dp_tx_desc_pool_s *pool = &soc->tx_desc[desc_pool_id];
 
 	qdf_spin_lock_bh(&pool->flow_pool_lock);
+	tx_desc->flags = 0;
 	dp_tx_put_desc_flow_pool(pool, tx_desc);
 	switch (pool->status) {
 	case FLOW_POOL_ACTIVE_PAUSED:
