368858c54fa43be5821cf33c1c6e1c0935c56f5e
Naveen Rawat <naveenrawat@codeaurora.org>
2018-02-14 11:47:31 -0800

    qcacld-3.0: Switch order of phymode and ch_width
    As part of csa or opmode IE handling program phymode param after
    ch_width since firmware expects channel width to be programmed
    before phymode.
    
    Change-Id: I46e3a5e1ce94fa53e27f821e70c29e209e591865
    CRs-Fixed: 2186030

--- core/wma/src/wma_dev_if.c
+++ core/wma/src/wma_dev_if.c
@@ -1191,22 +1191,20 @@ int wma_vdev_start_resp_handler(void *handle, uint8_t *cmd_param_info,
 			(iface->type == WMI_VDEV_TYPE_STA)) ||
 			((resp_event->resp_type == WMI_VDEV_START_RESP_EVENT) &&
 			 (iface->type == WMI_VDEV_TYPE_MONITOR))) {
-			err = wma_set_peer_param(wma, iface->bssid,
-					WMI_PEER_PHYMODE, iface->chanmode,
-					resp_event->vdev_id);
-
-			WMA_LOGD("%s:vdev_id %d chanmode %d status %d",
-				__func__, resp_event->vdev_id,
-				iface->chanmode, err);
-
 			chanwidth = chanmode_to_chanwidth(iface->chanmode);
 			err = wma_set_peer_param(wma, iface->bssid,
 					WMI_PEER_CHWIDTH, chanwidth,
 					resp_event->vdev_id);
-
 			WMA_LOGD("%s:vdev_id %d chanwidth %d status %d",
 				__func__, resp_event->vdev_id,
 				chanwidth, err);
+
+			err = wma_set_peer_param(wma, iface->bssid,
+					WMI_PEER_PHYMODE, iface->chanmode,
+					resp_event->vdev_id);
+			WMA_LOGD("%s:vdev_id %d chanmode %d status %d",
+				__func__, resp_event->vdev_id,
+				iface->chanmode, err);
 
 			param.vdev_id = resp_event->vdev_id;
 			param.assoc_id = iface->aid;
