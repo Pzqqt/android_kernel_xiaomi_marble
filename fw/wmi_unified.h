5ce0137aa5bf59fa3ed09002d53f70284a8ff630
spuligil <spuligil@codeaurora.org>
2018-03-08 09:03:42 -0800

    fw-api: CL 4260981 - update fw common interface files
    Change-Id: Iafe1e344d92a6c0adbc9371b02a67b32729ffdc9
    WMI: add VDEV_PARAM_AUTORATE_MISC_CFG def
    CRs-Fixed: 1107600

--- fw/wmi_unified.h
+++ fw/wmi_unified.h
@@ -8290,9 +8290,10 @@ typedef enum {
     WMI_VDEV_PARAM_ENABLE_DISABLE_RTT_RESPONDER_ROLE,        /* 0x7d */
 
     /** Parameter to configure BA mode.
-     * Default: Auto mode.
      * Valid values: 0- Auto mode,
      *               1- Manual mode(addba req not sent).
+     *               2- buffer size 64
+     *               3- buffer size 256
      */
     WMI_VDEV_PARAM_BA_MODE,                                 /* 0x7e */
 
@@ -8304,6 +8305,25 @@ typedef enum {
      *               1- Enabled.
      */
     WMI_VDEV_PARAM_FORCED_MODDTIM_ENABLE,                   /* 0x7f */
+
+    /** specify the setting that are valid for auto rate transmissions.
+     * bits 7:0 (LTF): When bitmask is set, then corresponding LTF value is
+     *                 used for auto rate.
+     *     BIT0   = 1 (WMI_HE_LTF_1X)
+     *     BIT1   = 1 (WMI_HE_LTF_2X)
+     *     BIT2   = 1 (WMI_HE_LTF_4X)
+     *     BIT3-7 = Reserved bits.
+     * bits 15:8 (SGI): When bitmask is set, then corresponding SGI value is
+     *                 used for auto rate.
+     *     BIT8     = 1 (400 NS)
+     *     BIT9     = 1 (800 NS)
+     *     BIT10    = 1 (1600 NS)
+     *     BIT11    = 1 (3200 NS)
+     *     BIT12-15 = Reserved bits.
+     * bits 31:16: Reserved bits. should be set to zero.
+     */
+    WMI_VDEV_PARAM_AUTORATE_MISC_CFG,                       /* 0x80 */
+
 
     /*=== ADD NEW VDEV PARAM TYPES ABOVE THIS LINE ===
      * The below vdev param types are used for prototyping, and are
