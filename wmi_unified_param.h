bb433b22bbde8f547a3011cd8460614c214bd3fd
Abhinav Kumar <abhikuma@codeaurora.org>
2018-03-27 17:06:59 +0530

    qcacmn: Add new ini to enable/disable FT open feature
    Add new ini item "enable_ftopen" to enable/disable
    FT open feature. There is no ini currently to
    disable only FT open.
    
    Add sanity check to send 11r params in case of FT open
    to fw only when enable_ftopen is enabled
    
    Change-Id: Ifb27aea8de8c8837f8eda329e1247abfadabb628
    CRs-Fixed: 2214772

--- wmi_unified_param.h
+++ wmi_unified_param.h
@@ -1958,6 +1958,7 @@ struct roam_offload_scan_params {
 	bool fw_pmksa_cache;
 #endif
 	bool is_ese_assoc;
+	bool is_11r_assoc;
 	struct mobility_domain_info mdid;
 #ifdef CONFIG_MCL
 	/* THis is not available in non tlv target.
