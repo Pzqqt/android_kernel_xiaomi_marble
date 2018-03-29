5f78313578ad3a8e00380f2bca0ddb560db57a5d
Amar Singhal <asinghal@codeaurora.org>
2018-03-29 13:58:50 -0700

    qcacld-3.0: Reset is_acs_allowed variable when chan count is 0
    When world mode is set (ACS chan list is empty) and ACS request comes to
    driver, is_acs_allowed variable is not reset. Due to this, in subsequent
    ACS requests, is_acs_allowed variable would become 1, leading to ACS
    failure in a loop. Fix that.
    
    Change-Id: I97689571e147505ea2c21f271c103e3c8cc74b80
    CRs-Fixed: 2215385

--- core/hdd/src/wlan_hdd_cfg80211.c
+++ core/hdd/src/wlan_hdd_cfg80211.c
@@ -2661,6 +2661,8 @@ static int __wlan_hdd_cfg80211_do_acs(struct wiphy *wiphy,
 	}
 
 	if (!sap_config->acs_cfg.ch_list_count) {
+		qdf_atomic_set(&hdd_ctx->is_acs_allowed, 0);
+		hdd_err("acs config chan count 0");
 		ret = -EINVAL;
 		goto out;
 	}
