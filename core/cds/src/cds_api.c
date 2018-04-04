cad91325bfb8750bb1bd12ab5ec78de0c888aaf3
Vignesh Viswanathan <viswanat@codeaurora.org>
2018-04-04 17:44:28 +0530

    qcacld-3.0: Register SCAN queue to scheduler thread
    Register the new low priority QDF_MODULE_ID_SCAN queue to the scheduler
    thread to handler beacon/probe frames and scan events.
    
    Change-Id: I80c9b6bbbce97942d188f8a7941a3937130d7c8e
    CRs-Fixed: 2226222

--- core/cds/src/cds_api.c
+++ core/cds/src/cds_api.c
@@ -1,9 +1,6 @@
 /*
  * Copyright (c) 2012-2018 The Linux Foundation. All rights reserved.
  *
- * Previously licensed under the ISC license by Qualcomm Atheros, Inc.
- *
- *
  * Permission to use, copy, modify, and/or distribute this software for
  * any purpose with or without fee is hereby granted, provided that the
  * above copyright notice and this permission notice appear in all
@@ -17,12 +14,6 @@
  * PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER
  * TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
  * PERFORMANCE OF THIS SOFTWARE.
- */
-
-/*
- * This file was originally distributed by Qualcomm Atheros, Inc.
- * under proprietary terms before Copyright ownership was assigned
- * to the Linux Foundation.
  */
 
 /**
@@ -397,6 +388,8 @@ static QDF_STATUS cds_register_all_modules(void)
 					&sme_mc_process_handler);
 	status = scheduler_register_module(QDF_MODULE_ID_OS_IF,
 					&scheduler_os_if_mq_handler);
+	status = scheduler_register_module(QDF_MODULE_ID_SCAN,
+					&scheduler_scan_mq_handler);
 	return status;
 }
 
