1896fdcb30645ed0e8380d2920858dea7b71b2b9
Jeff Johnson <jjohnson@codeaurora.org>
2018-04-23 14:10:59 -0700

    qcacld-3.0: dp: Remove legacy markings
    Per current guidance remove legacy markings.
    
    Change-Id: Id790fa94c7b6feb91db5f3d91630bf0cd5264192
    CRs-Fixed: 2229655

--- core/dp/txrx/ol_rx_pn.h
+++ core/dp/txrx/ol_rx_pn.h
@@ -1,9 +1,6 @@
 /*
  * Copyright (c) 2011, 2014-2017 The Linux Foundation. All rights reserved.
  *
- * Previously licensed under the ISC license by Qualcomm Atheros, Inc.
- *
- *
  * Permission to use, copy, modify, and/or distribute this software for
  * any purpose with or without fee is hereby granted, provided that the
  * above copyright notice and this permission notice appear in all
@@ -17,12 +14,6 @@
  * PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER
  * TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
  * PERFORMANCE OF THIS SOFTWARE.
- */
-
-/*
- * This file was originally distributed by Qualcomm Atheros, Inc.
- * under proprietary terms before Copyright ownership was assigned
- * to the Linux Foundation.
  */
 
 #ifndef _OL_RX_PN_H_
