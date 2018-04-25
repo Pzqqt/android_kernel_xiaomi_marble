e71b8c4019ea9401903ce8ed47e9dfb6e893b2b3
Jeff Johnson <jjohnson@codeaurora.org>
2018-04-24 20:29:41 -0700

    qcacmn: dp: Remove legacy markings
    Per current guidance remove legacy markings.
    
    Change-Id: Id6b2e4758232355b0c47bf8c2f1a0b2a1eacba05
    CRs-Fixed: 2230684

--- dp/inc/cdp_txrx_tx_delay.h
+++ dp/inc/cdp_txrx_tx_delay.h
@@ -1,9 +1,6 @@
 /*
  * Copyright (c) 2016-2017 The Linux Foundation. All rights reserved.
  *
- * Previously licensed under the ISC license by Qualcomm Atheros, Inc.
- *
- *
  * Permission to use, copy, modify, and/or distribute this software for
  * any purpose with or without fee is hereby granted, provided that the
  * above copyright notice and this permission notice appear in all
@@ -19,11 +16,6 @@
  * PERFORMANCE OF THIS SOFTWARE.
  */
 
-/*
- * This file was originally distributed by Qualcomm Atheros, Inc.
- * under proprietary terms before Copyright ownership was assigned
- * to the Linux Foundation.
- */
 /**
  * @file cdp_txrx_tx_delay.h
  * @brief Define the host data path histogram API functions
