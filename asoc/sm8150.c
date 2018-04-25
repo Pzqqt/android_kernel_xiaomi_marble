502766a828258aa36fe803234d3920cd63cbfb38
Karthikeyan Mani <kmani@codeaurora.org>
2018-04-24 18:35:56 -0700

    asoc: msm: remove machine driver trivial boot up logs
    Change info logs to debug logs for all trivial
    machine driver logs that appear in boot up.
    
    Change-Id: I37fb24fb758a81af9ae4be0e735cd9b467f88e70
    Signed-off-by: Karthikeyan Mani <kmani@codeaurora.org>

--- asoc/sm8150.c
+++ asoc/sm8150.c
@@ -3828,7 +3828,7 @@ static int msm_audrx_init(struct snd_soc_pcm_runtime *rtd)
 					      134, 135, 136, 137, 138, 139,
 					      140, 141, 142, 143};
 
-	pr_info("%s: dev_name:%s\n", __func__, dev_name(cpu_dai->dev));
+	pr_debug("%s: dev_name:%s\n", __func__, dev_name(cpu_dai->dev));
 
 	rtd->pmdown_time = 0;
 
