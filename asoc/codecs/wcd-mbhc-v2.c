4b8884a7ae28440432a25d081ae40f67b90aba1f
Vatsal Bucha <vbucha@codeaurora.org>
2018-05-10 14:20:18 +0530

    ASoC: wcd: Remove non-essential current source votings from adc
    Zdet current source is enabled for 4 pole aux cable and kept
    even after plug removal. This increases voltage for both hph left and
    right which causes error in cross-connection detection for subsequent plug
    detections. Remove current source enablement from unrequired places.
    
    CRs-Fixed: 2234559
    Change-Id: Icffbca88f7a6873c509c91e31b494dd6d5d21cb9
    Signed-off-by: Vatsal Bucha <vbucha@codeaurora.org>

--- asoc/codecs/wcd-mbhc-v2.c
+++ asoc/codecs/wcd-mbhc-v2.c
@@ -982,6 +982,8 @@ static void wcd_mbhc_swch_irq_handler(struct wcd_mbhc *mbhc)
 			jack_type = SND_JACK_HEADSET;
 			break;
 		case MBHC_PLUG_TYPE_HIGH_HPH:
+			if (mbhc->mbhc_detection_logic == WCD_DETECTION_ADC)
+			    WCD_MBHC_REG_UPDATE_BITS(WCD_MBHC_ELECT_ISRC_EN, 0);
 			mbhc->is_extn_cable = false;
 			jack_type = SND_JACK_LINEOUT;
 			break;
