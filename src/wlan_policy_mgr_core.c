aa40f60b9c6ee2a64f8e8acfb7cd39a550ce3abe
Kiran Kumar Lokere <klokere@codeaurora.org>
2018-05-15 15:48:57 -0700

    qcacmn: Add support for SRD channels in ETSI domain
    Add support for Short Range Devices 25 mW max power
    channels in ETSI regulatory domain.
    
    Add Short Range Devices 25 mW max power channels only
    if DSRC feature is disabled.
    Provide service apis to check SRD and DSRC channels.
    
    Change-Id: Ib2a1d7cf191d07319cb29038ad60130f5cbe7f16
    CRs-Fixed: 2264790

--- src/wlan_policy_mgr_core.c
+++ src/wlan_policy_mgr_core.c
@@ -1800,6 +1800,7 @@ QDF_STATUS policy_mgr_get_channel_list(struct wlan_objmgr_psoc *psoc,
 	uint8_t channel_list_5[QDF_MAX_NUM_CHAN] = {0};
 	uint8_t sbs_channel_list[QDF_MAX_NUM_CHAN] = {0};
 	bool skip_dfs_channel = false;
+	bool is_etsi13_srd_chan_allowed_in_mas_mode = true;
 	uint32_t i = 0, j = 0;
 	struct policy_mgr_psoc_priv_obj *pm_ctx;
 	bool sta_sap_scc_on_dfs_chan;
@@ -1840,12 +1841,17 @@ QDF_STATUS policy_mgr_get_channel_list(struct wlan_objmgr_psoc *psoc,
 	sta_sap_scc_on_dfs_chan =
 		policy_mgr_is_sta_sap_scc_allowed_on_dfs_chan(psoc);
 	policy_mgr_debug("sta_sap_scc_on_dfs_chan %u", sta_sap_scc_on_dfs_chan);
-	if ((((mode == PM_SAP_MODE) || (mode == PM_P2P_GO_MODE)) &&
-	    (policy_mgr_mode_specific_connection_count(
-		psoc, PM_STA_MODE, NULL) > 0)) ||
-		!sta_sap_scc_on_dfs_chan) {
-		policy_mgr_debug("STA present, skip DFS channels from pcl for SAP/Go");
-		skip_dfs_channel = true;
+	if ((mode == PM_SAP_MODE) || (mode == PM_P2P_GO_MODE)) {
+		if ((policy_mgr_mode_specific_connection_count(psoc,
+							       PM_STA_MODE,
+							       NULL) > 0) ||
+		    !sta_sap_scc_on_dfs_chan) {
+			policy_mgr_debug("skip DFS ch from pcl for SAP/Go");
+			skip_dfs_channel = true;
+		}
+		is_etsi13_srd_chan_allowed_in_mas_mode =
+			wlan_reg_is_etsi13_srd_chan_allowed_master_mode(pm_ctx->
+									pdev);
 	}
 
 	/* Let's divide the list in 2.4 & 5 Ghz lists */
@@ -1875,6 +1881,12 @@ QDF_STATUS policy_mgr_get_channel_list(struct wlan_objmgr_psoc *psoc,
 		if ((true == skip_dfs_channel) &&
 		    wlan_reg_is_dfs_ch(pm_ctx->pdev,
 				       channel_list[chan_index])) {
+			chan_index++;
+			continue;
+		}
+		if (!is_etsi13_srd_chan_allowed_in_mas_mode &&
+		    wlan_reg_is_etsi13_srd_chan(pm_ctx->pdev,
+						channel_list[chan_index])) {
 			chan_index++;
 			continue;
 		}
