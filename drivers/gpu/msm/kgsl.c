19444c02bab1b04cb1c49f96186952e7c6551bdc
Sultan Alsawaf <sultan@kerneltoast.com>
2018-05-29 17:11:29 -0700

    msm: kgsl: Mark IRQ and worker thread as performance critical
    These items are in the critical path for rendering frames to the display,
    so mark them as performance-critical.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>

--- drivers/gpu/msm/kgsl.c
+++ drivers/gpu/msm/kgsl.c
@@ -5038,8 +5038,8 @@ int kgsl_request_irq(struct platform_device *pdev, const  char *name,
 	if (num < 0)
 		return num;
 
-	ret = devm_request_irq(&pdev->dev, num, handler, IRQF_TRIGGER_HIGH,
-		name, data);
+	ret = devm_request_irq(&pdev->dev, num, handler,
+		IRQF_TRIGGER_HIGH | IRQF_PERF_AFFINE, name, data);
 
 	if (ret) {
 		dev_err(&pdev->dev, "Unable to get interrupt %s: %d\n",
