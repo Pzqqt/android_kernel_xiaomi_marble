bb06d022d68b1042d1d17b54a2e6c5290441501d
Dundi Raviteja <dundi@codeaurora.org>
2018-05-31 15:54:15 +0530

    qcacld-3.0: Change SIFS burst default value to 1
    Change the value of HDD_ENABLE_SIFS_BURST_DEFAULT from 0 to 1.
    
    Change-Id: I38fc4ebfcf507a53ed5ef9885c825d1eb9e90adb
    CRs-Fixed: 2251821

--- core/hdd/inc/wlan_hdd_main.h
+++ core/hdd/inc/wlan_hdd_main.h
@@ -354,8 +354,7 @@ static inline bool in_compat_syscall(void) { return is_compat_task(); }
 #define HDD_MIN_TX_POWER (-100) /* minimum tx power */
 #define HDD_MAX_TX_POWER (+100) /* maximum tx power */
 
-#define HDD_ENABLE_SIFS_BURST_DEFAULT	(0)
-
+#define HDD_ENABLE_SIFS_BURST_DEFAULT	(1)
 /* If IPA UC data path is enabled, target should reserve extra tx descriptors
  * for IPA data path.
  * Then host data path should allow less TX packet pumping in case
