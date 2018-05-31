144eabc3ee8757b49e4b51963fff09d63f9f9c6e
Laxminath Kasam <lkasam@codeaurora.org>
2018-05-31 10:44:53 +0530

    audio: Add support for audio on msmsteppe
    Update config file and make rules for msmsteppe
    compilation.
    
    Change-Id: I374a0725a7dd36edc9e04652fb07dc719310565c
    Signed-off-by: Laxminath Kasam <lkasam@codeaurora.org>

--- Android.mk
+++ Android.mk
@@ -24,9 +24,14 @@ include $(MY_LOCAL_PATH)/asoc/codecs/Android.mk
 include $(MY_LOCAL_PATH)/asoc/codecs/wcd934x/Android.mk
 endif
 
-ifeq ($(call is-board-platform-in-list,sdm670 msmnile $(MSMSTEPPE)),true)
+ifeq ($(call is-board-platform-in-list,sdm670 msmnile),true)
 $(shell rm -rf $(PRODUCT_OUT)/obj/vendor/qcom/opensource/audio-kernel/asoc/codecs/aqt1000/Module.symvers)
 include $(MY_LOCAL_PATH)/asoc/codecs/aqt1000/Android.mk
+endif
+
+ifeq ($(call is-board-platform-in-list,$(MSMSTEPPE)),true)
+$(shell rm -rf $(PRODUCT_OUT)/obj/vendor/qcom/opensource/audio-kernel/asoc/codecs/bolero/Module.symvers)
+include $(MY_LOCAL_PATH)/asoc/codecs/bolero/Android.mk
 endif
 
 ifeq ($(call is-board-platform-in-list,msm8953 sdm670 qcs605),true)
@@ -36,7 +41,7 @@ include $(MY_LOCAL_PATH)/asoc/codecs/sdm660_cdc/Android.mk
 include $(MY_LOCAL_PATH)/asoc/codecs/msm_sdw/Android.mk
 endif
 
-ifeq ($(call is-board-platform-in-list,msmnile $(MSMSTEPPE)),true)
+ifeq ($(call is-board-platform-in-list,msmnile),true)
 $(shell rm -rf $(PRODUCT_OUT)/obj/vendor/qcom/opensource/audio-kernel/asoc/codecs/wcd9360/Module.symvers)
 include $(MY_LOCAL_PATH)/asoc/codecs/wcd9360/Android.mk
 endif
