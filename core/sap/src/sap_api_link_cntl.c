10b0c91904a1d2d442272ab4ff16a62db028d7c3
Ashish Kumar Dhanotiya <adhanoti@codeaurora.org>
2018-06-19 21:57:14 +0530

    qcacld-3.0: Do not select any invalid channel from acs list
    Driver is selecting first channel from the preferred channel list
    if no channel is selected from the sap channel select algorithm.
    This leads to the selection of an invalid channel if the entire
    preferred channel list is invalid.
    
    To resolve this issue, do not select any channel from the preferred
    channel list if the entire list is invalid or if the entire list
    is unsafe.
    
    Change-Id: Ic90416fe62f854e2c1f124884c200876f543caba
    CRs-Fixed: 2234142

--- core/sap/src/sap_api_link_cntl.c
+++ core/sap/src/sap_api_link_cntl.c
@@ -148,16 +148,6 @@ QDF_STATUS wlansap_scan_callback(tHalHandle hal_handle,
 				  __func__, scan_id);
 #endif
 		operChannel = sap_select_channel(hal_handle, sap_ctx, result);
-		if (!operChannel) {
-			QDF_TRACE(QDF_MODULE_ID_SAP, QDF_TRACE_LEVEL_INFO_HIGH,
-				"No channel was selected from preferred channel for Operating channel");
-
-			operChannel = sap_ctx->acs_cfg->start_ch;
-
-			QDF_TRACE(QDF_MODULE_ID_SAP, QDF_TRACE_LEVEL_INFO_HIGH,
-				"Selecting operating channel as starting channel from preferred channel list: %d",
-				operChannel);
-		}
 		sme_scan_result_purge(result);
 		break;
 
@@ -375,16 +365,6 @@ QDF_STATUS wlansap_pre_start_bss_acs_scan_callback(tHalHandle hal_handle,
 		}
 #endif
 		oper_channel = sap_select_channel(hal_handle, sap_ctx, presult);
-		if (!oper_channel) {
-			QDF_TRACE(QDF_MODULE_ID_SAP, QDF_TRACE_LEVEL_INFO_HIGH,
-				"No channel was selected from preferred channel for Operating channel");
-
-			oper_channel = sap_ctx->acs_cfg->start_ch;
-
-			QDF_TRACE(QDF_MODULE_ID_SAP, QDF_TRACE_LEVEL_INFO_HIGH,
-				"Selecting operating channel as starting channel from preferred channel list: %d",
-				oper_channel);
-		}
 		sme_scan_result_purge(presult);
 	}
 
