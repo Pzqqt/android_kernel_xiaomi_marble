0da2ba67a6316c4e6846072f7cfee6fd2a019405
Kiran Kumar Lokere <klokere@codeaurora.org>
2018-06-21 16:59:30 -0700

    qcacld-3.0: Revert sending the reg sync event
    Revert sending the reg sync event to fix stability issue.
    
    Change-Id: Ie8e95ca338602d0eb48fcf7cf8c22cfd6716c0a5
    CRs-Fixed: 2242697

--- core/hdd/src/wlan_hdd_regulatory.c
+++ core/hdd/src/wlan_hdd_regulatory.c
@@ -1270,9 +1270,6 @@ void hdd_send_wiphy_regd_sync_event(struct hdd_context *hdd_ctx)
 			  regd_rules[i].power_rule.max_eirp,
 			  regd_rules[i].flags);
 	}
-	rtnl_lock();
-	regulatory_set_wiphy_regd_sync_rtnl(hdd_ctx->wiphy, regd);
-	rtnl_unlock();
 	hdd_debug("regd sync event sent with reg rules info");
 	qdf_mem_free(regd);
 }
