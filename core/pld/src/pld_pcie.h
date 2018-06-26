74be384d1cba926e09940fabd9bf48db8f521530
Nirav Shah <nnshah@codeaurora.org>
2018-06-26 19:17:25 +0530

    qcacld-3.0: Fix compilation error to enable genoa pcie
    Fix compilation error while enabling QCA_WIFI_3_0_ADRASTEA
    and CONFIG_CNSS2 flag for Genoa.
    
    Change-Id: Ic44da39377f722c19edcd291d1b35fac1fee9462
    CRs-Fixed: 2267927

--- core/pld/src/pld_pcie.h
+++ core/pld/src/pld_pcie.h
@@ -64,17 +64,7 @@ int pld_pcie_wlan_enable(struct device *dev, struct pld_wlan_enable_cfg *config,
 int pld_pcie_wlan_disable(struct device *dev, enum pld_driver_mode mode);
 #endif
 
-#if defined(CONFIG_PLD_PCIE_CNSS) && defined(QCA_WIFI_3_0_ADRASTEA)
-static inline int pld_pcie_set_fw_log_mode(struct device *dev, u8 fw_log_mode)
-{
-	return cnss_set_fw_debug_mode(fw_log_mode);
-}
-
-static inline void pld_pcie_intr_notify_q6(struct device *dev)
-{
-	cnss_intr_notify_q6();
-}
-#elif defined(CONFIG_PLD_PCIE_CNSS)
+#if defined(CONFIG_PLD_PCIE_CNSS)
 static inline int pld_pcie_set_fw_log_mode(struct device *dev, u8 fw_log_mode)
 {
 	return cnss_set_fw_log_mode(dev, fw_log_mode);
