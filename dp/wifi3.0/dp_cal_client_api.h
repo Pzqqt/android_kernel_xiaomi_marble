7d32b0aebb2bc37937e79cde3d890ad08ddb153a
Ruchi, Agrawal <ruchagra@codeaurora.org>
2018-06-28 14:55:47 +0530

    qcacmn: common api for cal client timer
    common api for cal client timer, stats update
    per sec.
    
    Change-Id: Ic508a818a7ad38253f157326c063fdc1d719c425
    CRs-Fixed: 2286826

---
+++ dp/wifi3.0/dp_cal_client_api.h
@@ -0,0 +1,74 @@
+/*
+ * Copyright (c) 2017-2018 The Linux Foundation. All rights reserved.
+ *
+ * Permission to use, copy, modify, and/or distribute this software for
+ * any purpose with or without fee is hereby granted, provided that the
+ * above copyright notice and this permission notice appear in all
+ * copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL
+ * WARRANTIES WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE
+ * AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL
+ * DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR
+ * PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER
+ * TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
+ * PERFORMANCE OF THIS SOFTWARE.
+ */
+
+/**
+ * @file cal_client_api.h
+ * @brief: define timer to update DP stats
+ */
+#ifndef _DP_CAL_CLIENT_H_
+#define _DP_CAL_CLIENT_H_
+
+#include<cdp_txrx_stats_struct.h>
+#include <qdf_timer.h>
+#include <qdf_mem.h>
+
+/*timer will run every 1 sec*/
+#define DP_CAL_CLIENT_TIME 1000
+
+struct cal_client {
+	qdf_timer_t cal_client_timer;
+	void (*iterate_update_peer_list)(void *ctx);
+	void *pdev_hdl;
+};
+
+void dp_cal_client_attach(void **cal_client_ctx, void *pdev, qdf_device_t osdev,
+			  void (*iterate_peer_list)(void *));
+void dp_cal_client_detach(void **cal_client_ctx);
+void dp_cal_client_timer_start(void *ctx);
+void dp_cal_client_timer_stop(void *ctx);
+void dp_cal_client_stats_timer_fn(void *pdev_hdl);
+void dp_cal_client_update_peer_stats(struct cdp_peer_stats *peer_stats);
+
+#ifndef ATH_SUPPORT_EXT_STAT
+void dp_cal_client_attach(void **cal_client_ctx, void *pdev, qdf_device_t osdev,
+			  void (*iterate_peer_list)(void *))
+{
+}
+
+void dp_cal_client_detach(void **cal_client_ctx)
+{
+}
+
+void dp_cal_client_timer_start(void *ctx)
+{
+}
+
+void dp_cal_client_timer_stop(void *ctx)
+{
+}
+
+void dp_cal_client_stats_timer_fn(void *pdev_hdl)
+{
+}
+
+void dp_cal_client_update_peer_stats(struct cdp_peer_stats *peer_stats)
+{
+}
+#endif
+
+#endif /*_DP_CAL_CLIENT_H_*/
