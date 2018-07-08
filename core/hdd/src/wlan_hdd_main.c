b9feee40b60927ec7d3311496d6e493ac3ecea37
Jeff Johnson <jjohnson@codeaurora.org>
2018-07-08 10:34:48 -0700

    qcacld-3.0: Fix indent in hdd_pld_request_bus_bandwidth()
    Checkpatch flagged the following issue, so fix it:
    
    ERROR:CODE_INDENT: code indent should use tabs where possible
    +^I^I          next_rx_level, avg_rx);$
    
    Change-Id: Ib6133df181ec7517bf0b212bd8d677666d99ccb9
    CRs-Fixed: 2275264

--- core/hdd/src/wlan_hdd_main.c
+++ core/hdd/src/wlan_hdd_main.c
@@ -7456,7 +7456,7 @@ static void hdd_pld_request_bus_bandwidth(struct hdd_context *hdd_ctx,
 		struct wlan_rx_tp_data rx_tp_data = {0};
 
 		hdd_debug("TCP DELACK trigger level %d, average_rx: %llu",
-		          next_rx_level, avg_rx);
+			  next_rx_level, avg_rx);
 		hdd_ctx->cur_rx_level = next_rx_level;
 		rx_level_change = true;
 		/* Send throughput indication only if it is enabled.
