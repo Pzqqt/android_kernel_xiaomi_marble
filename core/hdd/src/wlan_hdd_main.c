3cfe0d47d0d39653e368122e0014251d52c2654a
Dustin Brown <dustinb@codeaurora.org>
2018-07-25 14:13:47 -0700

    qcacld-3.0: Check peers in hdd_check_for_leaks
    Before checking for other kinds of resources leaks, check to ensure all
    objmgr peers have been properly freed.
    
    Change-Id: Ifb1016be456c26e140f324fc3c858bd4a5fa6a27
    CRs-Fixed: 2285186

--- core/hdd/src/wlan_hdd_main.c
+++ core/hdd/src/wlan_hdd_main.c
@@ -2719,6 +2719,7 @@ static void hdd_check_for_leaks(struct hdd_context *hdd_ctx, bool is_ssr)
 {
 	/* DO NOT REMOVE these checks; for false positives, read above first */
 
+	wlan_objmgr_psoc_check_for_peer_leaks(hdd_ctx->hdd_psoc);
 	wlan_objmgr_psoc_check_for_vdev_leaks(hdd_ctx->hdd_psoc);
 	wlan_objmgr_psoc_check_for_pdev_leaks(hdd_ctx->hdd_psoc);
 
