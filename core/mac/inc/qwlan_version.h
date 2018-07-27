1449648511cc69cb1bd0b70fc5ff6851d5e5cd22
nshrivas <nshrivas@codeaurora.org>
2018-07-27 09:09:43 -0700

    Release 5.2.0.90G
    Release 5.2.0.90G
    
    Change-Id: I73d219578f50ed8109899b35b0cf0d120273ce77
    CRs-Fixed: 774533

--- core/mac/inc/qwlan_version.h
+++ core/mac/inc/qwlan_version.h
@@ -32,9 +32,9 @@
 #define QWLAN_VERSION_MAJOR            5
 #define QWLAN_VERSION_MINOR            2
 #define QWLAN_VERSION_PATCH            0
-#define QWLAN_VERSION_EXTRA            "F"
+#define QWLAN_VERSION_EXTRA            "G"
 #define QWLAN_VERSION_BUILD            90
 
-#define QWLAN_VERSIONSTR               "5.2.0.90F"
+#define QWLAN_VERSIONSTR               "5.2.0.90G"
 
 #endif /* QWLAN_VERSION_H */
