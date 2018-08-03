5c81249094bb0214dcb3b1d19e26fd533d0f858d
Manikandan Mohan <manikand@codeaurora.org>
2018-08-02 18:04:16 -0700

    qcacmn: Add support for converged NAN config params
    Update NAN module to support INI configuration params based
    on converged CFG component.
    
    Change-Id: I2253daf8cbbce6d2afa28c5834179f21c2b76660
    CRs-fixed: 2290357

--- umac/nan/core/src/nan_api.c
+++ umac/nan/core/src/nan_api.c
@@ -29,6 +29,57 @@
 #include "wlan_objmgr_psoc_obj.h"
 #include "wlan_objmgr_pdev_obj.h"
 #include "wlan_objmgr_vdev_obj.h"
+#include "cfg_nan.h"
+#include "cfg_ucfg_api.h"
+
+/**
+ * nan_cfg_init() - Initialize NAN config params
+ * @psoc: Pointer to PSOC Object
+ * @nan_obj: Pointer to NAN private object
+ *
+ * This function initialize NAN config params
+ */
+#ifdef WLAN_FEATURE_NAN
+static void nan_cfg_init(struct wlan_objmgr_psoc *psoc,
+			 struct nan_psoc_priv_obj *nan_obj)
+{
+	if (!psoc || !nan_obj)
+		return;
+
+	nan_obj->cfg_param.enable = cfg_get(psoc, CFG_NAN_ENABLE);
+}
+#else
+static void nan_cfg_init(struct wlan_objmgr_psoc *psoc,
+			 struct nan_psoc_priv_obj *nan_obj)
+{
+}
+#endif
+
+/**
+ * nan_cfg_dp_init() - Initialize NAN Datapath config params
+ * @psoc: Pointer to PSOC Object
+ * @nan_obj: Pointer to NAN private object
+ *
+ * This function initialize NAN config params
+ */
+#ifdef WLAN_FEATURE_NAN_DATAPATH
+static void nan_cfg_dp_init(struct wlan_objmgr_psoc *psoc,
+			    struct nan_psoc_priv_obj *nan_obj)
+{
+	if (!psoc || !nan_obj)
+		return;
+	nan_obj->cfg_param.dp_enable = cfg_get(psoc,
+					       CFG_NAN_DATAPATH_ENABLE);
+	nan_obj->cfg_param.ndi_ch = cfg_get(psoc, CFG_NAN_NDI_CHANNEL);
+	nan_obj->cfg_param.ndi_mac_randomize =
+				cfg_get(psoc, CFG_NAN_RANDOMIZE_NDI_MAC);
+}
+#else
+static void nan_cfg_dp_init(struct wlan_objmgr_psoc *psoc,
+			    struct nan_psoc_priv_obj *nan_obj)
+{
+}
+#endif
 
 static QDF_STATUS nan_psoc_obj_created_notification(
 		struct wlan_objmgr_psoc *psoc, void *arg_list)
@@ -51,6 +102,9 @@ static QDF_STATUS nan_psoc_obj_created_notification(
 		nan_alert("obj attach with psoc failed");
 		goto nan_psoc_notif_failed;
 	}
+
+	nan_cfg_init(psoc, nan_obj);
+	nan_cfg_dp_init(psoc, nan_obj);
 
 	return QDF_STATUS_SUCCESS;
 
