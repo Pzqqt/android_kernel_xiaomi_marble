ba57f10c4a796c3d0850e3a2cbe24d4e4da7ea01
Arif Hussain <arifhussain@codeaurora.org>
2018-08-14 13:45:51 -0700

    qcacmn: Do not free stats resource in get_peer_rssi_cb()
    Do not free stats resource in callback handler,
    Let caller free the resource.
    
    Change-Id: I0d77e95d81b8bb995492eb0aebaa3ebf0282718e
    CRs-Fixed: 2296924

--- os_if/linux/cp_stats/src/wlan_cfg80211_mc_cp_stats.c
+++ os_if/linux/cp_stats/src/wlan_cfg80211_mc_cp_stats.c
@@ -315,7 +315,6 @@ static void get_peer_rssi_cb(struct stats_event *ev, void *cookie)
 	request = osif_request_get(cookie);
 	if (!request) {
 		cfg80211_err("Obsolete request");
-		ucfg_mc_cp_stats_free_stats_resources(ev);
 		return;
 	}
 
