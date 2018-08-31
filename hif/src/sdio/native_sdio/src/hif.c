598b1dd3ae4be6676eb226deaf6cd160adc64476
Kai Liu <kaliu@codeaurora.org>
2018-08-31 15:53:11 +0800

    qcacmn: claim host only when there are packets to send
    Async task thread will claim sdio bus, this claim action will
    keep sdio clock on.
    
    During system suspend, after mmc controller suspended, wlan
    claim sdio, then it will cause mmc exit suspend as it found clock is on.
    Move this claim action before bus transaction to avoid rebundant claim.
    
    Change-Id: I3bab449d0a93c9107c455ee7dbcc6df99ca28636
    CRs-Fixed: 2299526

--- hif/src/sdio/native_sdio/src/hif.c
+++ hif/src/sdio/native_sdio/src/hif.c
@@ -499,6 +499,7 @@ static int async_task(void *param)
 	struct hif_sdio_dev *device;
 	struct bus_request *request;
 	QDF_STATUS status;
+	bool claimed = false;
 
 	device = (struct hif_sdio_dev *) param;
 	set_current_state(TASK_INTERRUPTIBLE);
@@ -521,7 +522,6 @@ static int async_task(void *param)
 		 * if possible, but holding the host blocks
 		 * card interrupts
 		 */
-		sdio_claim_host(device->func);
 		qdf_spin_lock_irqsave(&device->asynclock);
 		/* pull the request to work on */
 		while (device->asyncreq != NULL) {
@@ -535,6 +535,10 @@ static int async_task(void *param)
 				("%s: async_task processing req: 0x%lX\n",
 				 __func__, (unsigned long)request));
 
+			if (!claimed) {
+				sdio_claim_host(device->func);
+				claimed = true;
+			}
 			if (request->scatter_req != NULL) {
 				A_ASSERT(device->scatter_enabled);
 				/* pass the request to scatter routine which
@@ -581,7 +585,10 @@ static int async_task(void *param)
 			qdf_spin_lock_irqsave(&device->asynclock);
 		}
 		qdf_spin_unlock_irqrestore(&device->asynclock);
-		sdio_release_host(device->func);
+		if (claimed) {
+			sdio_release_host(device->func);
+			claimed = false;
+		}
 	}
 
 	complete_and_exit(&device->async_completion, 0);
