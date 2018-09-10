f60c3457b4c6feb4acbf4d3f0bd583d3a3a6faa4
Rajeev Kumar <rajekuma@codeaurora.org>
2018-09-10 15:44:37 -0700

    qcacmn: Use QDF_TRACE to log under spin lock debug feature
    Use QDF_TRACE log macro to log spin lock abuse when spin lock
    debug feature is defined.
    
    Change-Id: I1e5db79f5e6f5fc343f1be000efff6907c4857e1
    CRs-Fixed: 2312614

--- qdf/inc/qdf_lock.h
+++ qdf/inc/qdf_lock.h
@@ -144,8 +144,9 @@ do {\
 		lock->stats.num_large_holds++; \
 	if (QDF_LOCK_STATS_BUG_ON && max_hold_time && \
 	    held_time > qdf_usecs_to_log_timestamp(max_hold_time)) { \
-		qdf_warn("BEFORE_UNLOCK: lock held too long (%lluus)", \
-		       qdf_log_timestamp_to_usecs(held_time)); \
+		QDF_TRACE(QDF_MODULE_ID_QDF, QDF_TRACE_LEVEL_ERROR, \
+			"BEFORE_UNLOCK: lock held too long (%lluus)", \
+			qdf_log_timestamp_to_usecs(held_time)); \
 		QDF_BUG(0); \
 	} \
 	lock->stats.acquired_by = NULL; \
@@ -158,7 +159,8 @@ void qdf_lock_stats_cookie_create(struct lock_stats *stats,
 static inline void qdf_lock_stats_destroy(struct lock_stats *stats)
 {
 	if (QDF_LOCK_STATS_DESTROY_PRINT) {
-		qdf_debug("%s: lock: %s %d \t"
+		QDF_TRACE(QDF_MODULE_ID_QDF, QDF_TRACE_LEVEL_DEBUG,
+			"%s: lock: %s %d \t"
 			"acquired:\t%d\tcontended:\t%d\t"
 			"contention_time\t%llu\tmax_contention_wait:\t%llu\t"
 			"non_contention_time\t%llu\t"
