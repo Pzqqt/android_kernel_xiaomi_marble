5f45a5aaea057ca9e5c385bd684bb5ae53dab810
Pragaspathi Thilagaraj <tpragasp@codeaurora.org>
2018-09-15 22:58:18 +0530

    qcacld-3.0: Add MLME CFG items of EDCA parameters
    Add the EDCA cfg parameters:
    1. WNI_CFG_EDCA_ANI_ACBK_LOCAL
    2. WNI_CFG_EDCA_ANI_ACBE_LOCAL
    3. WNI_CFG_EDCA_ANI_ACVI_LOCAL
    4. WNI_CFG_EDCA_ANI_ACVO_LOCAL
    5. WNI_CFG_EDCA_ANI_ACBK
    6. WNI_CFG_EDCA_ANI_ACBE
    7. WNI_CFG_EDCA_ANI_ACVI
    8. WNI_CFG_EDCA_ANI_ACVO
    9. WNI_CFG_EDCA_WME_ACBK_LOCAL
    10. WNI_CFG_EDCA_WME_ACBE_LOCAL
    11. WNI_CFG_EDCA_WME_ACVI_LOCAL
    12. WNI_CFG_EDCA_WME_ACVO_LOCAL
    13. WNI_CFG_EDCA_WME_ACBK
    14. WNI_CFG_EDCA_WME_ACBE
    15. WNI_CFG_EDCA_WME_ACVI
    16. WNI_CFG_EDCA_WME_ACVO
    17. WNI_CFG_EDCA_ETSI_ACBE_LOCAL
    18. WNI_CFG_EDCA_ETSI_ACBK_LOCAL
    19. WNI_CFG_EDCA_ETSI_ACVI_LOCAL
    20. WNI_CFG_EDCA_ETSI_ACVO_LOCAL
    21. WNI_CFG_EDCA_ETSI_ACBE
    22. WNI_CFG_EDCA_ETSI_ACBK
    23. WNI_CFG_EDCA_ETSI_ACVI
    24. WNI_CFG_EDCA_ETSI_ACVO
    
    Change-Id: I10ba5c2b8c63bd3fb19540e999c39f3fdf53197b
    CRs-Fixed: 2315482

--- mlme/dispatcher/inc/cfg_mlme.h
+++ mlme/dispatcher/inc/cfg_mlme.h
@@ -24,6 +24,7 @@
 #include "qdf_types.h"
 
 #include "cfg_mlme_chainmask.h"
+#include "cfg_mlme_edca_params.h"
 #include "cfg_mlme_generic.h"
 #include "cfg_mlme_ht_caps.h"
 #include "cfg_mlme_he_caps.h"
@@ -42,6 +43,7 @@
 /* Please Maintain Alphabetic Order here */
 #define CFG_MLME_ALL \
 	CFG_CHAINMASK_ALL \
+	CFG_EDCA_PARAMS_ALL \
 	CFG_GENERIC_ALL \
 	CFG_HT_CAPS_ALL \
 	CFG_HE_CAPS_ALL \
