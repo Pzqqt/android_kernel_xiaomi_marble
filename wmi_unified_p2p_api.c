510b8659cd45ac95866deba0640c7f3dac262cd9
Liangwei Dong <liangwei@codeaurora.org>
2018-09-19 23:35:14 -0400

    qcacmn: Action frame random mac addr tx support
    1. Send add random mac addr rx filter WMI command
    to target
    2. Add/Del the active random mac addr entry
    3. Clear random mac addr from target if not active
    
    Change-Id: I9dcbdc20b76d9865da7a8db6ee013bf5e44e4407
    CRs-Fixed: 2322097

--- wmi_unified_p2p_api.c
+++ wmi_unified_p2p_api.c
@@ -63,6 +63,42 @@ QDF_STATUS wmi_extract_p2p_noa_ev_param(void *wmi_hdl, void *evt_buf,
 	return QDF_STATUS_E_FAILURE;
 }
 
+QDF_STATUS
+wmi_send_set_mac_addr_rx_filter_cmd(void *wmi_hdl,
+				    struct p2p_set_mac_filter *param)
+{
+	wmi_unified_t wmi_handle = (wmi_unified_t)wmi_hdl;
+
+	if (!wmi_handle) {
+		WMI_LOGE("wmi handle is null");
+		return QDF_STATUS_E_INVAL;
+	}
+
+	if (wmi_handle->ops->set_mac_addr_rx_filter)
+		return wmi_handle->ops->set_mac_addr_rx_filter(
+				wmi_handle, param);
+
+	return QDF_STATUS_E_FAILURE;
+}
+
+QDF_STATUS
+wmi_extract_mac_addr_rx_filter_evt_param(void *wmi_hdl, void *evt_buf,
+					 struct p2p_set_mac_filter_evt *param)
+{
+	wmi_unified_t wmi_handle = (wmi_unified_t)wmi_hdl;
+
+	if (!wmi_handle) {
+		WMI_LOGE("wmi handle is null");
+		return QDF_STATUS_E_INVAL;
+	}
+
+	if (wmi_handle->ops->extract_mac_addr_rx_filter_evt_param)
+		return wmi_handle->ops->extract_mac_addr_rx_filter_evt_param(
+				wmi_handle, evt_buf, param);
+
+	return QDF_STATUS_E_FAILURE;
+}
+
 #ifdef FEATURE_P2P_LISTEN_OFFLOAD
 QDF_STATUS wmi_unified_p2p_lo_start_cmd(void *wmi_hdl,
 					struct p2p_lo_start *param)
