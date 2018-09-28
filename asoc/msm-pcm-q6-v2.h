70668fcb8d0b3610042afcbe77b0917e2f02bc38
Dieter Luecking <dieterl@codeaurora.org>
2018-09-28 15:03:01 +0200

    asoc: msm: Add support for 32 ch
    PCM streams do now support up to 32 channels.
    Extend Playback Channel Map mixer control to 32
    channels. Use new DSP 32 channel API if DSP version
    supports it. If not fall back to 8 channels API.
    
    Change-Id: I74c4f91b0c9fab2a963690ba8143ebea36ad23dd
    Signed-off-by: Dieter Luecking <dieterl@codeaurora.org>
    Signed-off-by: Mangesh Kunchamwar <mangeshk@codeaurora.org>

--- asoc/msm-pcm-q6-v2.h
+++ asoc/msm-pcm-q6-v2.h
@@ -105,7 +105,7 @@ struct msm_audio {
 	int mmap_flag;
 	atomic_t pending_buffer;
 	bool set_channel_map;
-	char channel_map[8];
+	char channel_map[PCM_FORMAT_MAX_NUM_CHANNEL_V8];
 	int cmd_interrupt;
 	bool meta_data_mode;
 	uint32_t volume;
