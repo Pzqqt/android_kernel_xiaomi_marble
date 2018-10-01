a19068f6113785f1e2fa6974748cba9cf8f62ec1
Pragaspathi Thilagaraj <tpragasp@codeaurora.org>
2018-10-01 15:44:06 +0530

    qcacld-3.0: Fix default ini value of gAutoBmpsTimerValue
    When BMPS is already enabled and we have entered the WOW mode,
    we start detecting the AP behavior , where the AP
    continuously sets the TIM bit and because of which we don’t go
    to PM1 mode after waking UP.
    
    With the current ini value, we don't go to sleep for 30 secs,
    which should be updated to 90secs.
    
    Change-Id: I3b551011e90761f1dc13197a4795972f495853a6
    CRs-Fixed: 2306266

--- core/hdd/inc/wlan_hdd_cfg.h
+++ core/hdd/inc/wlan_hdd_cfg.h
@@ -8131,7 +8131,7 @@ enum hdd_wext_control {
  * gAutoBmpsTimerValue - Set Auto BMPS Timer value
  * @Min: 0
  * @Max: 120
- * @Default: 30
+ * @Default: 90
  *
  * This ini is used to set Auto BMPS Timer value in seconds
  *
@@ -8146,7 +8146,7 @@ enum hdd_wext_control {
 #define CFG_AUTO_PS_ENABLE_TIMER_NAME          "gAutoBmpsTimerValue"
 #define CFG_AUTO_PS_ENABLE_TIMER_MIN           (0)
 #define CFG_AUTO_PS_ENABLE_TIMER_MAX           (120)
-#define CFG_AUTO_PS_ENABLE_TIMER_DEFAULT       (30)
+#define CFG_AUTO_PS_ENABLE_TIMER_DEFAULT       (90)
 
 #ifdef WLAN_ICMP_DISABLE_PS
 /*
