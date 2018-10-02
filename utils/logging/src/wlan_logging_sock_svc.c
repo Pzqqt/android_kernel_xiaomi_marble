b7dcab9948a930b7f479b20ca1ecb6dffeb3c530
Rajeev Kumar <rajekuma@codeaurora.org>
2018-10-02 11:52:05 -0700

    qcacmn: Use pr_info for wlan driver console logging
    Use pr_info for wlan driver console logging such that its
    backward compatible with qcacld-3.1 and by default info log
    levels are not logged on console unless kernel's printk current
    console log level is changed to print info logs as well on console.
    
    Change-Id: I33ca583743a9b77253390abcece6c8067f38c35d
    CRs-Fixed: 2326304

--- utils/logging/src/wlan_logging_sock_svc.c
+++ utils/logging/src/wlan_logging_sock_svc.c
@@ -307,7 +307,7 @@ static int wlan_add_user_log_time_stamp(char *tbuf, size_t tbuf_sz, uint64_t ts)
 #ifdef CONFIG_MCL
 static inline void print_to_console(char *tbuf, char *to_be_sent)
 {
-	pr_err("%s %s\n", tbuf, to_be_sent);
+	pr_info("%s %s\n", tbuf, to_be_sent);
 }
 #else
 #define print_to_console(str1, str2)
