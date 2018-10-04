2c545818140072ca81822343c5c8445d208ec50e
Gyanranjan Hazarika <gyanranj@codeaurora.org>
2018-10-04 12:11:46 -0700

    qcacmn: Changes for BSS Color Enahancement FR40903
    1. QDF_MODULE_ID_BSSCOLOR added for logging
    2. BSS Color Offload supported trace added
       as target_if_info
    
    Change-Id: I00db87f6619f7857dd83cf1eb5d805780c501805
    CRs-fixed: 2242109

--- target_if/init_deinit/src/init_event_handler.c
+++ target_if/init_deinit/src/init_event_handler.c
@@ -107,6 +107,9 @@ static int init_deinit_service_ready_event_handler(ol_scn_t scn_handle,
 	if (wmi_service_enabled(wmi_handle, wmi_service_twt_responder))
 		wlan_psoc_nif_fw_ext_cap_set(psoc, WLAN_SOC_CEXT_TWT_RESPONDER);
 
+	if (wmi_service_enabled(wmi_handle, wmi_service_bss_color_offload))
+		target_if_debug(" BSS COLOR OFFLOAD supported");
+
 	target_if_debug(" TT support %d, Wide BW Scan %d, SW cal %d",
 		wlan_psoc_nif_fw_ext_cap_get(psoc, WLAN_SOC_CEXT_TT_SUPPORT),
 		wlan_psoc_nif_fw_ext_cap_get(psoc, WLAN_SOC_CEXT_WIDEBAND_SCAN),
