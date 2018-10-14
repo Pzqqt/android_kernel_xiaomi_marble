dcab67c50473c2bc1c53c2d7b55b1c2ec94e15cc
Hari Veerubhotla <vsubrahm@codeaurora.org>
2018-10-14 17:08:35 +0530

    Initial Android target definition for trinket.
    Change-Id: Ib55a9013b0fa5bcda0e5d2d17d1741e607a9c9b5

--- asoc/codecs/bolero/Android.mk
+++ asoc/codecs/bolero/Android.mk
@@ -3,13 +3,13 @@
 # Assume no targets will be supported
 
 # Check if this driver needs be built for current target
-ifeq ($(call is-board-platform,$(MSMSTEPPE)),true)
+ifeq ($(call is-board-platform,$(MSMSTEPPE) $(TRINKET)),true)
 AUDIO_SELECT  := CONFIG_SND_SOC_SM6150=m
 endif
 
 AUDIO_CHIPSET := audio
 # Build/Package only in case of supported target
-ifeq ($(call is-board-platform-in-list,$(MSMSTEPPE)),true)
+ifeq ($(call is-board-platform-in-list,$(MSMSTEPPE) $(TRINKET)),true)
 
 LOCAL_PATH := $(call my-dir)
 
--- ipc/Android.mk
+++ ipc/Android.mk
@@ -15,13 +15,13 @@ ifeq ($(call is-board-platform,msmnile),true)
 AUDIO_SELECT  := CONFIG_SND_SOC_SM8150=m
 endif
 
-ifeq ($(call is-board-platform,$(MSMSTEPPE)),true)
+ifeq ($(call is-board-platform,$(MSMSTEPPE) $(TRINKET)),true)
 AUDIO_SELECT  := CONFIG_SND_SOC_SM6150=m
 endif
 
 AUDIO_CHIPSET := audio
 # Build/Package only in case of supported target
-ifeq ($(call is-board-platform-in-list,msm8953 sdm845 sdm670 qcs605 msmnile $(MSMSTEPPE)),true)
+ifeq ($(call is-board-platform-in-list,msm8953 sdm845 sdm670 qcs605 msmnile $(MSMSTEPPE) $(TRINKET)),true)
 
 LOCAL_PATH := $(call my-dir)
 
