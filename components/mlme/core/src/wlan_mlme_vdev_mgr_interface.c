4aa9ae3296b66318c48a6f367287b458ddae4da7
Rajeev Kumar Sirasanagandla <rsirasan@codeaurora.org>
2018-10-15 15:04:30 +0530

    qcacld-3.0: Send VDEV_UP command to firmware for STA interface
    For STA interface, add support to send VDEV_UP command to firmware when
    VDEV state is transitioning to VDEV_S_UP.
    
    Change-Id: Ic8f4e8355381d915f4c1c7f6650950d7d12d9db3
    CRs-Fixed: 2333297

--- components/mlme/core/src/wlan_mlme_vdev_mgr_interface.c
+++ components/mlme/core/src/wlan_mlme_vdev_mgr_interface.c
@@ -158,7 +158,7 @@ static QDF_STATUS sta_mlme_vdev_up_send(struct vdev_mlme_obj *vdev_mlme,
 					uint16_t event_data_len,
 					void *event_data)
 {
-	return QDF_STATUS_SUCCESS;
+	return wma_sta_vdev_up_send(vdev_mlme, event_data_len, event_data);
 }
 
 /**
