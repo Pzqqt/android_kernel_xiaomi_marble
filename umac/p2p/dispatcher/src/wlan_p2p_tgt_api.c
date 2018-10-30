d4b6e218c61d3bf44c16f233f39b18408e721f4f
Wu Gao <wugao@codeaurora.org>
2018-10-30 18:43:57 +0800

    qcacmn: Fix potential memory leak when post scheduler msg in P2P
    To avoid potential memory leak, add flush callback for some of P2P
    messages, handle post scheduler messages fail case.
    
    Change-Id: If700eba689bb2423ca84fbba08f7434cc75dbd14
    CRs-Fixed: 2341756

--- umac/p2p/dispatcher/src/wlan_p2p_tgt_api.c
+++ umac/p2p/dispatcher/src/wlan_p2p_tgt_api.c
@@ -80,6 +80,7 @@ QDF_STATUS tgt_p2p_lo_event_cb(struct wlan_objmgr_psoc *psoc,
 	struct p2p_lo_stop_event *lo_stop_event;
 	struct scheduler_msg msg = {0};
 	struct p2p_soc_priv_obj *p2p_soc_obj;
+	QDF_STATUS status;
 
 	p2p_debug("soc:%pK, event_info:%pK", psoc, event_info);
 
@@ -116,11 +117,18 @@ QDF_STATUS tgt_p2p_lo_event_cb(struct wlan_objmgr_psoc *psoc,
 	msg.type = P2P_EVENT_LO_STOPPED;
 	msg.bodyptr = lo_stop_event;
 	msg.callback = p2p_process_evt;
+	msg.flush_callback = p2p_event_flush_callback;
+	status = scheduler_post_message(QDF_MODULE_ID_P2P,
+					QDF_MODULE_ID_P2P,
+					QDF_MODULE_ID_TARGET_IF,
+					&msg);
+	if (QDF_IS_STATUS_ERROR(status)) {
+		qdf_mem_free(lo_stop_event->lo_event);
+		qdf_mem_free(lo_stop_event);
+		p2p_err("post msg fail:%d", status);
+	}
 
-        scheduler_post_message(QDF_MODULE_ID_P2P,
-                               QDF_MODULE_ID_P2P,
-                               QDF_MODULE_ID_TARGET_IF, &msg);
-	return QDF_STATUS_SUCCESS;
+	return status;
 }
 #endif /* FEATURE_P2P_LISTEN_OFFLOAD */
 
@@ -163,7 +171,7 @@ tgt_p2p_add_mac_addr_status_event_cb(struct wlan_objmgr_psoc *psoc,
 	msg.bodyptr = mac_filter_rsp;
 	msg.callback = p2p_process_evt;
 	status = scheduler_post_msg(QDF_MODULE_ID_TARGET_IF, &msg);
-	if (status != QDF_STATUS_SUCCESS)
+	if (QDF_IS_STATUS_ERROR(status))
 		qdf_mem_free(mac_filter_rsp);
 
 	return status;
@@ -261,17 +269,18 @@ QDF_STATUS tgt_p2p_mgmt_ota_comp_cb(void *context, qdf_nbuf_t buf,
 	msg.type = P2P_EVENT_MGMT_TX_ACK_CNF;
 	msg.bodyptr = tx_conf_event;
 	msg.callback = p2p_process_evt;
+	msg.flush_callback = p2p_event_flush_callback;
 	ret = scheduler_post_message(QDF_MODULE_ID_P2P,
 				     QDF_MODULE_ID_P2P,
-				     QDF_MODULE_ID_TARGET_IF, &msg);
-	if (ret != QDF_STATUS_SUCCESS) {
+				     QDF_MODULE_ID_TARGET_IF,
+				     &msg);
+	if (QDF_IS_STATUS_ERROR(ret)) {
 		qdf_mem_free(tx_conf_event);
-		qdf_mem_free(buf);
-		p2p_err("failed to post message");
-		return status;
+		qdf_nbuf_free(buf);
+		p2p_err("post msg fail:%d", status);
 	}
 
-	return QDF_STATUS_SUCCESS;
+	return ret;
 }
 
 QDF_STATUS tgt_p2p_mgmt_frame_rx_cb(struct wlan_objmgr_psoc *psoc,
@@ -286,6 +295,7 @@ QDF_STATUS tgt_p2p_mgmt_frame_rx_cb(struct wlan_objmgr_psoc *psoc,
 	struct wlan_objmgr_vdev *vdev;
 	uint32_t vdev_id;
 	uint8_t *pdata;
+	QDF_STATUS status;
 
 	p2p_debug("psoc:%pK, peer:%pK, type:%d", psoc, peer, frm_type);
 
@@ -349,13 +359,19 @@ QDF_STATUS tgt_p2p_mgmt_frame_rx_cb(struct wlan_objmgr_psoc *psoc,
 	msg.type = P2P_EVENT_RX_MGMT;
 	msg.bodyptr = rx_mgmt_event;
 	msg.callback = p2p_process_evt;
-	scheduler_post_message(QDF_MODULE_ID_P2P,
-			       QDF_MODULE_ID_P2P,
-			       QDF_MODULE_ID_TARGET_IF, &msg);
-
+	msg.flush_callback = p2p_event_flush_callback;
+	status = scheduler_post_message(QDF_MODULE_ID_P2P,
+					QDF_MODULE_ID_P2P,
+					QDF_MODULE_ID_TARGET_IF,
+					&msg);
+	if (QDF_IS_STATUS_ERROR(status)) {
+		qdf_mem_free(rx_mgmt_event->rx_mgmt);
+		qdf_mem_free(rx_mgmt_event);
+		p2p_err("post msg fail:%d", status);
+	}
 	qdf_nbuf_free(buf);
 
-	return QDF_STATUS_SUCCESS;
+	return status;
 }
 
 QDF_STATUS  tgt_p2p_noa_event_cb(struct wlan_objmgr_psoc *psoc,
@@ -364,6 +380,7 @@ QDF_STATUS  tgt_p2p_noa_event_cb(struct wlan_objmgr_psoc *psoc,
 	struct p2p_noa_event *noa_event;
 	struct scheduler_msg msg = {0};
 	struct p2p_soc_priv_obj *p2p_soc_obj;
+	QDF_STATUS status;
 
 	p2p_debug("soc:%pK, event_info:%pK", psoc, event_info);
 
@@ -400,9 +417,16 @@ QDF_STATUS  tgt_p2p_noa_event_cb(struct wlan_objmgr_psoc *psoc,
 	msg.type = P2P_EVENT_NOA;
 	msg.bodyptr = noa_event;
 	msg.callback = p2p_process_evt;
-	scheduler_post_message(QDF_MODULE_ID_P2P,
-			       QDF_MODULE_ID_P2P,
-			       QDF_MODULE_ID_TARGET_IF, &msg);
+	msg.flush_callback = p2p_event_flush_callback;
+	status = scheduler_post_message(QDF_MODULE_ID_P2P,
+					QDF_MODULE_ID_P2P,
+					QDF_MODULE_ID_TARGET_IF,
+					&msg);
+	if (QDF_IS_STATUS_ERROR(status)) {
+		qdf_mem_free(noa_event->noa_info);
+		qdf_mem_free(noa_event);
+		p2p_err("post msg fail:%d", status);
+	}
 
-	return QDF_STATUS_SUCCESS;
+	return status;
 }
