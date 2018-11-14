2f026a2ff8936e03b87802defe71fe4f71e52bf5
Manjunathappa Prakash <prakashpm@codeaurora.org>
2018-11-14 12:16:37 -0800

    qcacld-3.0: Rate limit IPA callback API failure logs
    Excessive failure log due driver is in transition state. This
    is coming from stats callback function, rate limiting the logs.
    
    Change-Id: Idcd5304c492fa538614ac45d4c7bb3a601f23bcc
    CRs-Fixed: 2347721

--- ipa/core/src/wlan_ipa_stats.c
+++ ipa/core/src/wlan_ipa_stats.c
@@ -867,13 +867,13 @@ static void __wlan_ipa_wdi_meter_notifier_cb(qdf_ipa_wdi_meter_evt_type_t evt,
 	ipa_debug("event=%d", evt);
 
 	if (qdf_is_module_state_transitioning()) {
-		ipa_err("Module transition in progress");
+		ipa_err_rl("Module transition in progress");
 		return;
 	}
 
 	iface_ctx = wlan_ipa_get_iface(ipa_ctx, QDF_STA_MODE);
 	if (!iface_ctx) {
-		ipa_err("IPA uC share stats failed - no iface");
+		ipa_err_rl("IPA uC share stats failed - no iface");
 		return;
 	}
 
