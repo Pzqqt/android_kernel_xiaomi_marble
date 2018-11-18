1c93e50d6325008ebb6c43485583c1af3f63488a
Jeff Johnson <jjohnson@codeaurora.org>
2018-11-17 20:42:45 -0800

    qcacld-3.0: Remove SIR_LIM_TD_DUMMY_CALLBACK_MSG
    Message id SIR_LIM_TD_DUMMY_CALLBACK_MSG is obsolete, so remove it.
    
    Change-Id: Ia37dabc3b5b1c897a8dc6fb8b9cd61d16052fc06
    CRs-Fixed: 2353423

--- core/mac/src/include/sir_params.h
+++ core/mac/src/include/sir_params.h
@@ -708,8 +708,7 @@ struct sir_mgmt_msg {
 /* UNUSED                              (SIR_LIM_ITC_MSG_TYPES_BEGIN + 7) */
 /* UNUSED                              (SIR_LIM_ITC_MSG_TYPES_BEGIN + 8) */
 /* UNUSED                              (SIR_LIM_ITC_MSG_TYPES_BEGIN + 9) */
-/* Message from HDD after the TD dummy packet is cleaned up */
-#define SIR_LIM_TD_DUMMY_CALLBACK_MSG      (SIR_LIM_ITC_MSG_TYPES_BEGIN + 0xA)
+/* UNUSED                              (SIR_LIM_ITC_MSG_TYPES_BEGIN + 0xA) */
 /* Message from SCH when the STA is ready to be deleted */
 #define SIR_LIM_SCH_CLEAN_MSG              (SIR_LIM_ITC_MSG_TYPES_BEGIN + 0xB)
 /* Message from ISR upon Radar Detection */
