d33c1ce39bae3c05a2545cd25e52321b7cb4abc4
Vignesh Mohan <vignmoha@codeaurora.org>
2018-11-19 14:54:34 +0530

    qcacmn: Add WMI command to set Sub Channel Marking
    Introduce a new wmi_send pdev param to enable/disable
    "Subchannel Marking" in Firmware (only in  Full Offload)
    
    Change-Id: I3cd4f4f13ebca72c4505b6195cc8dc4856d41671
    CRs-Fixed: 2334258

--- wmi_unified_param.h
+++ wmi_unified_param.h
@@ -4934,6 +4934,7 @@ typedef enum {
 	wmi_pdev_param_enable_peer_retry_stats,
 	wmi_pdev_param_ul_trig_int,
 	wmi_pdev_param_max,
+	wmi_pdev_param_sub_channel_marking,
 } wmi_conv_pdev_params_id;
 
 
