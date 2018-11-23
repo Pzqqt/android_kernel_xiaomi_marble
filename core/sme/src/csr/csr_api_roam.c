4e36fbef212f5f9b508990d0ea054dc6f54c8eef
Pragaspathi Thilagaraj <tpragasp@codeaurora.org>
2018-11-23 17:27:33 +0530

    qcacld-3.0: Send roam scan offload config after roam sync complete
    When host driver receives roam sync complete event, the
    disable_hi_rssi to false if band is 2G and the high rssi
    threshold delta needs to be sent to the firmware to take effect.
    But presently the csr_roam_scan_offload exits with failure as
    the driver issues start again.
    
    Call csr_roam_scan_offload with ROAM_SCAN_OFFLOAD_UPDATE_CFG
    command.
    
    Change-Id: Ic3c9a729c290accbe9822948bc2363154db4c83f
    CRs-Fixed: 2355359

--- core/sme/src/csr/csr_api_roam.c
+++ core/sme/src/csr/csr_api_roam.c
@@ -21431,7 +21431,7 @@ static QDF_STATUS csr_process_roam_sync_callback(tpAniSirGlobal mac_ctx,
 
 		policy_mgr_check_concurrent_intf_and_restart_sap(mac_ctx->psoc);
 		csr_roam_offload_scan(mac_ctx, session_id,
-				ROAM_SCAN_OFFLOAD_START,
+				ROAM_SCAN_OFFLOAD_UPDATE_CFG,
 				REASON_CONNECT);
 		return status;
 	default:
