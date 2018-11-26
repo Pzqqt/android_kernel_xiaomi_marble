9c47aafee3c4d3cfd4a8d3e9285a4ab453f60389
hangtian <hangtian@codeaurora.org>
2018-11-26 17:59:39 +0800

    qcacld-3.0: Stop tsf sync before hdd_wlan_exit and hdd_wlan_shutdown
    Stop tsf sync before hdd_wlan_exit and hdd_wlan_shutdown
    
    Change-Id: Icda5ea64eea9957cd878e7999a041d6b12082ce8
    CRs-Fixed: 2354768

--- core/hdd/src/wlan_hdd_main.c
+++ core/hdd/src/wlan_hdd_main.c
@@ -5323,6 +5323,7 @@ QDF_STATUS hdd_stop_adapter_ext(struct hdd_context *hdd_ctx,
 	hdd_nud_ignore_tracking(adapter, true);
 	hdd_nud_reset_tracking(adapter);
 	hdd_nud_flush_work(adapter);
+	hdd_stop_tsf_sync(adapter);
 
 	hdd_debug("Disabling queues");
 	wlan_hdd_netif_queue_control(adapter,
@@ -5732,6 +5733,7 @@ QDF_STATUS hdd_reset_all_adapters(struct hdd_context *hdd_ctx)
 		hdd_nud_reset_tracking(adapter);
 		hdd_nud_flush_work(adapter);
 		hdd_set_disconnect_status(adapter, false);
+		hdd_stop_tsf_sync(adapter);
 
 		hdd_softap_deinit_tx_rx(adapter);
 		hdd_deregister_tx_flow_control(adapter);
