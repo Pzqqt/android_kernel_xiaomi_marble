42583f90e3b01628a29f06888aee24761bfc983e
Jeff Johnson <jjohnson@codeaurora.org>
2018-12-15 14:01:52 -0800

    qcacld-3.0: Remove "nil scan report" remnant
    Change Ia8b85548ff64ad77b5066fe03229e2b0b2f7fae3 ("qcacld-3.0: Remove
    legacy scan complete callback") removed the "nil scan report"
    feature. However it left behind the last_nil_scan_bug_report_timestamp
    field in struct hdd_context. Since this field is now unused, remove
    it.
    
    Change-Id: I7a208121c3957a5ab04df455ad8a8b54932dfd1c
    CRs-Fixed: 2370022

--- core/hdd/inc/wlan_hdd_main.h
+++ core/hdd/inc/wlan_hdd_main.h
@@ -1878,7 +1878,6 @@ struct hdd_context {
 	int radio_index;
 	qdf_work_t sap_pre_cac_work;
 	bool hbw_requested;
-	uint32_t last_nil_scan_bug_report_timestamp;
 	enum RX_OFFLOAD ol_enable;
 #ifdef WLAN_FEATURE_NAN_DATAPATH
 	bool nan_datapath_enabled;
