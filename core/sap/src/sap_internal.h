e0b7effe1eea7477b9078aabe151488f00993129
Jeff Johnson <jjohnson@codeaurora.org>
2018-12-27 17:33:15 -0800

    qcacld-3.0: Remove macro ANI_EAPOL_KEY_RSN_NONCE_SIZE
    Macro ANI_EAPOL_KEY_RSN_NONCE_SIZE is obsolete, so remove it.
    
    Change-Id: Id9107d513f8c4ebe42e7dc6b6ee19d2e09fd7748
    CRs-Fixed: 2374833

--- core/sap/src/sap_internal.h
+++ core/sap/src/sap_internal.h
@@ -51,9 +51,6 @@ extern "C" {
 
 #define SAP_DEBUG
 
-/* MAC Address length */
-#define ANI_EAPOL_KEY_RSN_NONCE_SIZE      32
-
 #define IS_ETSI_WEATHER_CH(_ch)   ((_ch >= 120) && (_ch <= 130))
 #define IS_CH_BONDING_WITH_WEATHER_CH(_ch)   (_ch == 116)
 #define IS_CHAN_JAPAN_INDOOR(_ch) ((_ch >= 36)  && (_ch <= 64))
