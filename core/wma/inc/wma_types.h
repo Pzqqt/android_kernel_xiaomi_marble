103e4fb219444dffdc588da21762283279417da5
Jeff Johnson <jjohnson@codeaurora.org>
2018-12-27 21:49:41 -0800

    qcacld-3.0: Remove WMA_NVDownload_Start()
    Macro WMA_NVDownload_Start() is obsolete, so remove it.
    
    Change-Id: Iabdf9cd343a36dca78c527a45aa80338c1b43057
    CRs-Fixed: 2374847

--- core/wma/inc/wma_types.h
+++ core/wma/inc/wma_types.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2012-2018 The Linux Foundation. All rights reserved.
+ * Copyright (c) 2012-2019 The Linux Foundation. All rights reserved.
  *
  * Permission to use, copy, modify, and/or distribute this software for
  * any purpose with or without fee is hereby granted, provided that the
@@ -36,8 +36,6 @@
 #ifdef FEATURE_WLAN_TDLS
 #define IS_ADVANCE_TDLS_ENABLE 0
 #endif
-
-#define WMA_NVDownload_Start(x)    ({ QDF_STATUS_SUCCESS; })
 
 #define DPU_FEEDBACK_UNPROTECTED_ERROR 0x0F
 
