blankaf/hasty_davinci

ASoC: bolero: Remove unwanted logs at bootup
Correct name of widgets for tx macro from
TX SWR_MIC to TX SWR_DMIC. Also assign codec pointer
to rx_priv before calling rx_macro_init_bcl_pmic_reg.

Change-Id: I268d79413c53bcd2d8e7a391859b71b7a7e3f64e
Signed-off-by: Vatsal Bucha <vbucha@codeaurora.org>

asoc/codecs/bolero/rx-macro.c
@@ -3139,9 +3139,9 @@ static int rx_macro_init(struct snd_soc_codec *codec)
 	snd_soc_update_bits(codec, BOLERO_CDC_RX_RX0_RX_PATH_CFG3, 0x03, 0x02);
 	snd_soc_update_bits(codec, BOLERO_CDC_RX_RX1_RX_PATH_CFG3, 0x03, 0x02);
 	snd_soc_update_bits(codec, BOLERO_CDC_RX_RX2_RX_PATH_CFG3, 0x03, 0x02);
-	rx_macro_init_bcl_pmic_reg(codec);
 
 	rx_priv->codec = codec;
+	rx_macro_init_bcl_pmic_reg(codec);
 
 	return 0;
 }

Pzqqt/android_kernel_xiaomi_marble

asoc/codecs/bolero/rx-macro.c
@@ -3384,9 +3384,8 @@ static int rx_macro_init(struct snd_soc_component *component)
 	snd_soc_component_update_bits(component, BOLERO_CDC_RX_RX2_RX_PATH_CFG3,
 				0x03, 0x02);
 
-	rx_macro_init_bcl_pmic_reg(component);
-
 	rx_priv->component = component;
+	rx_macro_init_bcl_pmic_reg(component);
 
 	return 0;
 }