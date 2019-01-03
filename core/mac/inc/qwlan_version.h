7e3f2978231f5b9c4ddf67ddebbcb3039f215cd3
nshrivas <nshrivas@codeaurora.org>
2019-01-03 06:47:35 -0800

    Release 5.2.0.114O
    Release 5.2.0.114O
    
    Change-Id: I0ce565dd729925d89d69582741fd2ad28ea1ef9d
    CRs-Fixed: 774533

--- core/mac/inc/qwlan_version.h
+++ core/mac/inc/qwlan_version.h
@@ -32,9 +32,9 @@
 #define QWLAN_VERSION_MAJOR            5
 #define QWLAN_VERSION_MINOR            2
 #define QWLAN_VERSION_PATCH            0
-#define QWLAN_VERSION_EXTRA            "N"
+#define QWLAN_VERSION_EXTRA            "O"
 #define QWLAN_VERSION_BUILD            114
 
-#define QWLAN_VERSIONSTR               "5.2.0.114N"
+#define QWLAN_VERSIONSTR               "5.2.0.114O"
 
 #endif /* QWLAN_VERSION_H */
