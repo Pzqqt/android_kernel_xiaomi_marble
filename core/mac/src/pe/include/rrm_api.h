201dfa846c1f5ed551d300da287ee87491264a17
Jeff Johnson <jjohnson@codeaurora.org>
2019-01-06 09:58:41 -0800

    qcacld-3.0: Harmonize rrm_process_beacon_report_xmit()
    Currently the rrm_process_beacon_report_xmit() prototype uses
    different parameter identifiers than the implementation. Since this
    can be confusing for both humans and code analysis tools, update the
    prototype to match the implementation.
    
    Change-Id: Id6d671dcc6e7f0dc3db9f5a07ca3ea74b45e2b41
    CRs-Fixed: 2380111

--- core/mac/src/pe/include/rrm_api.h
+++ core/mac/src/pe/include/rrm_api.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2011-2012, 2014-2018 The Linux Foundation. All rights reserved.
+ * Copyright (c) 2011-2012, 2014-2019 The Linux Foundation. All rights reserved.
  *
  * Permission to use, copy, modify, and/or distribute this software for
  * any purpose with or without fee is hereby granted, provided that the
@@ -79,8 +79,8 @@ rrm_process_neighbor_report_req(struct mac_context *mac,
 				tpSirNeighborReportReqInd pNeighborReq);
 
 QDF_STATUS
-rrm_process_beacon_report_xmit(struct mac_context *mac,
-			       tpSirBeaconReportXmitInd pBcnReport);
+rrm_process_beacon_report_xmit(struct mac_context *mac_ctx,
+			       tpSirBeaconReportXmitInd beacon_xmit_ind);
 
 void lim_update_rrm_capability(struct mac_context *mac_ctx,
 			       tpSirSmeJoinReq join_req);
