631cde9525764aacececc7b42de49e3d105830ce
Dustin Brown <dustinb@codeaurora.org>
2019-01-17 10:02:18 -0800

    qcacmn: Add qdf_must_check function attribute
    Some compilers allow functions to be annotated such that a warning will
    be emitted if the return value of that function is not used. Allow the
    WLAN driver to leverage these annotations by introducing a wrapper in
    QDF, qdf_must_check.
    
    Change-Id: I9c1328ae904857717703aae3748b207967a8dd3d
    CRs-Fixed: 2382837

--- qdf/inc/qdf_types.h
+++ qdf/inc/qdf_types.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2014-2018 The Linux Foundation. All rights reserved.
+ * Copyright (c) 2014-2019 The Linux Foundation. All rights reserved.
  *
  * Permission to use, copy, modify, and/or distribute this software for
  * any purpose with or without fee is hereby granted, provided that the
@@ -30,6 +30,7 @@
 #endif
 #endif
 
+#define qdf_must_check __qdf_must_check
 
 /* Include Files */
 #include <i_qdf_types.h>
