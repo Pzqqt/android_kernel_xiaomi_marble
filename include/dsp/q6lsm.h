16e535969ba0ddb07b93b3fd9e976d38678659d3
Kunlei Zhang <kunleiz@codeaurora.org>
2019-01-23 17:18:19 +0800

    lsm: check payload size validity before using it as array index
    Payload size validity is not checked before using it in array index.
    Check payload size to avoid out-of-boundary memory.
    
    Change-Id: Ic0b06bb331fc1753ff7543bb218ab12d6a4a3ca8
    Signed-off-by: Kunlei Zhang <kunleiz@codeaurora.org>

--- include/dsp/q6lsm.h
+++ include/dsp/q6lsm.h
@@ -21,7 +21,7 @@
 #define LSM_API_VERSION_V3 3
 
 typedef void (*lsm_app_cb)(uint32_t opcode, uint32_t token,
-		       uint32_t *payload, void *priv);
+		       uint32_t *payload, uint16_t client_size, void *priv);
 
 struct lsm_sound_model {
 	dma_addr_t      phys;
