335304e701153bba6ed8b612a2cff970ef54d657
Vikram Panduranga <vpandura@codeaurora.org>
2019-01-25 11:18:42 -0800

    dsp: config: Add configuration for voice mhi driver
    Add and enable configuration for voice mhi driver
    for kona target.
    
    Change-Id: I43c28fbe26a5429f1a30b056ff96a9815acb3aa4
    Signed-off-by: Vikram Panduranga <vpandura@codeaurora.org>

--- config/konaautoconf.h
+++ config/konaautoconf.h
@@ -1,5 +1,5 @@
 /* SPDX-License-Identifier: GPL-2.0-only */
-/* Copyright (c) 2018, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2018-2019, The Linux Foundation. All rights reserved.
  */
 
 #define CONFIG_PINCTRL_WCD 1
@@ -38,3 +38,4 @@
 #define CONFIG_SND_SOC_WCD938X_SLAVE 1
 #define CONFIG_SND_SOC_KONA 1
 #define CONFIG_SND_EVENT 1
+#define CONFIG_VOICE_MHI 1
