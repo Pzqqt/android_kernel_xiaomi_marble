2fdd9ac6ae106b829509f0c840fb7ff506656fcf
spuligil <spuligil@codeaurora.org>
2019-01-29 18:01:27 -0800

    fw-api: CL 6396869 - update fw common interface files
    Wrap A_RATEMASK def in ifndef CONFIG_MOVE_RC_STRUCT_TO_MACCORE
    
    Change-Id: I08cee37f55ce250ac53553b4a82f38243d40d1b9
    CRs-Fixed: 2262693

--- fw/wlan_defs.h
+++ fw/wlan_defs.h
@@ -352,12 +352,13 @@ typedef struct {
  * In host-based implementation of the rate-control feature, this struture is used to
  * create the payload for HTT message/s from target to host.
  */
-
-#if (NUM_SPATIAL_STREAM > 3)
-  #define A_RATEMASK A_UINT64
-#else
-  #define A_RATEMASK A_UINT32
-#endif
+#ifndef CONFIG_MOVE_RC_STRUCT_TO_MACCORE
+  #if (NUM_SPATIAL_STREAM > 3)
+    #define A_RATEMASK A_UINT64
+  #else
+    #define A_RATEMASK A_UINT32
+  #endif
+#endif /* CONFIG_MOVE_RC_STRUCT_TO_MACCORE */
 
 typedef A_UINT8 A_RATE;
 typedef A_UINT8 A_RATECODE;
