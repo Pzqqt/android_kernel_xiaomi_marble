f0fc34b2b17ff90239d6ba72b9fbd945778b3ae5
Hariharan Basuthkar <hbasuthk@codeaurora.org>
2019-02-11 11:45:33 +0530

    qcacmn: Update regdb with regulatory database version 28
    Update regdb with QCA regulatory database version 28 with
    following changes:
    
    1) Change ETSI10, ETSI11, ETSI12 lower 5G channels to "Indoor-Only"
    2) Add 40MHz 5835 channel to India APL19
    3) Remove HT/VHT20 Ch 5825 and Add HT/VHT20 Ch. 5720 (straddle) channel
       to ETSI15 for Saudi Arabia
    
    Change-Id: Id3075d22ec4403685ce3eaf00f0070b6ecab1fdf
    CRs-Fixed: 2396234

--- umac/regulatory/core/src/reg_services_common.c
+++ umac/regulatory/core/src/reg_services_common.c
@@ -49,7 +49,8 @@ static const struct bonded_channel bonded_chan_40mhz_list[] = {
 	{132, 136},
 	{140, 144},
 	{149, 153},
-	{157, 161}
+	{157, 161},
+	{165, 169}
 };
 
 static const struct bonded_channel bonded_chan_80mhz_list[] = {
@@ -121,7 +122,7 @@ static const struct chan_map channel_map_old[NUM_CHANNELS] = {
 	[CHAN_ENUM_161] = {5805, 161, 2, 160},
 	[CHAN_ENUM_165] = {5825, 165, 2, 160},
 #ifndef WLAN_FEATURE_DSRC
-	[CHAN_ENUM_169] = {5845, 169, 2, 20},
+	[CHAN_ENUM_169] = {5845, 169, 2, 40},
 	[CHAN_ENUM_173] = {5865, 173, 2, 20},
 #else
 	[CHAN_ENUM_170] = {5852, 170, 2, 20},
