c2acbee6020655e1c4fe27a9309a51a664ad10f1
Meng Wang <mengw@codeaurora.org>
2019-02-12 08:26:49 +0800

    dsp: update copyright marking for q6afecal-hwdep and q6asm
    Copyright year 2018 for q6afecal-hwdep and third party copyright
    marking for q6asm are removed during updating file headers.
    Add them back.
    
    Change-Id: I0341116ca0cf82be8c3876534b98ad2c376b3531
    Signed-off-by: Meng Wang <mengw@codeaurora.org>

--- dsp/q6afecal-hwdep.c
+++ dsp/q6afecal-hwdep.c
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: GPL-2.0-only
 /*
- * Copyright (c) 2015, 2017, The Linux Foundation. All rights reserved.
+ * Copyright (c) 2015, 2017 - 2018, The Linux Foundation. All rights reserved.
  */
 #include <linux/kernel.h>
 #include <linux/platform_device.h>
