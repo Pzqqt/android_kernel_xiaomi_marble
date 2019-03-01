93463d2d7cb3cce922e536e48b0b9965b5aa9cd8
Abhinav Kumar <abhikuma@codeaurora.org>
2019-03-01 18:16:35 +0530

    qcacld-3.0: Device not roaming if FastRoamingEnabled disabled case
    Allow driver to send RSO command irrespective of value of INI
    FastRoamingEnabled.
    FastRoamEnabled has nothing to do with LFR enable or disable.
    
    Change-Id: Ice5e653b249f988a49d18ed56c9be53d4436bf56
    CRs-Fixed: 2385045

--- core/sme/src/csr/csr_neighbor_roam.c
+++ core/sme/src/csr/csr_neighbor_roam.c
@@ -1000,14 +1000,16 @@ static void csr_neighbor_roam_info_ctx_init(
 			FL("isESEAssoc is = %d ft = %d"),
 			ngbr_roam_info->isESEAssoc, init_ft_flag);
 #endif
-	/* If "Legacy Fast Roaming" is enabled */
+	/* If "FastRoamEnabled" ini is enabled */
 	if (csr_roam_is_fast_roam_enabled(mac, session_id))
 		init_ft_flag = true;
-	if (init_ft_flag == false)
-		return;
-	/* Initialize all the data structures needed for the 11r FT Preauth */
-	ngbr_roam_info->FTRoamInfo.currentNeighborRptRetryNum = 0;
-	csr_neighbor_roam_purge_preauth_failed_list(mac);
+
+	if (init_ft_flag) {
+		/* Initialize all the data needed for the 11r FT Preauth */
+		ngbr_roam_info->FTRoamInfo.currentNeighborRptRetryNum = 0;
+		csr_neighbor_roam_purge_preauth_failed_list(mac);
+	}
+
 	if (csr_roam_is_roam_offload_scan_enabled(mac)) {
 		/*
 		 * If this is not a INFRA type BSS, then do not send the command
