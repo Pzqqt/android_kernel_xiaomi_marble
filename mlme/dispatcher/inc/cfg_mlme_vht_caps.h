b060ffbd7bc3e118f22ad73e88a9cf353bb964bf
wadesong <wadesong@codeaurora.org>
2019-03-12 17:17:27 +0800

    qcacld-3.0: cfg ini documentation refine
    Some malformed comments were discovered when generating
    the official documentation of wlan cfg ini items.
    
    Refine the documentation text of the following items
    in one batch:
    
    gEnableVendorVhtFor24GHzBand
    hostArpOffload
    ssdp
    gEnableDynamicDTIM
    gEnableModulatedDTIM
    
    Change-Id: I3cd617e52e86e2a890e35edb33b37ba3df8f4f6e
    CRs-Fixed: 2414259

--- mlme/dispatcher/inc/cfg_mlme_vht_caps.h
+++ mlme/dispatcher/inc/cfg_mlme_vht_caps.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2012-2018 The Linux Foundation. All rights reserved.
+ * Copyright (c) 2012-2019 The Linux Foundation. All rights reserved.
  *
  * Permission to use, copy, modify, and/or distribute this software for
  * any purpose with or without fee is hereby granted, provided that the
@@ -488,7 +488,7 @@
 		"VHT Enable for 24GHz")
 
 /*
- * gEnableVendorVhtFor24GHzBand:Parameter to control VHT support
+ * gEnableVendorVhtFor24GHzBand - Parameter to control VHT support
  * based on vendor ie in 2.4 GHz band
  * @Min: 0
  * @Max: 1
