b3a341bb6f68ce5a894166bb4122c4542a60cfc3
Om Prakash Tripathi <otripath@codeaurora.org>
2019-03-18 13:46:05 +0530

    qcacmn: Fix possible null pointer dereference
    Fix possible null pointer dereference glagged by static analysis
    
    Change-Id: I100619888318182c19300bcbbcd95e9c68efe8a5
    CRs-Fixed: 2417910

--- umac/scan/core/src/wlan_scan_manager.c
+++ umac/scan/core/src/wlan_scan_manager.c
@@ -149,6 +149,8 @@ static void scm_scan_post_event(struct wlan_objmgr_vdev *vdev,
 	}
 	scan = wlan_vdev_get_scan_obj(vdev);
 	pdev_ev_handler = wlan_vdev_get_pdev_scan_ev_handlers(vdev);
+	if (!pdev_ev_handler)
+		return;
 	cb_handlers = &(pdev_ev_handler->cb_handlers[0]);
 	requesters = scan->requesters;
 
