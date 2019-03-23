aa7b531b2065ca8f906a17bc877a5ce3bde83907
Jeff Johnson <jjohnson@codeaurora.org>
2019-03-23 10:24:45 -0700

    qcacld-3.0: Rename HDD identifier broadcastMacAddr
    The Linux Coding Style frowns upon mixed-case names so rename HDD
    identifier broadcastMacAddr to be compliant.
    
    Change-Id: Ib4edff8bd0b78dc16605ac8bd129021b6923a7d2
    CRs-Fixed: 2422195

--- core/hdd/src/wlan_hdd_softap_tx_rx.c
+++ core/hdd/src/wlan_hdd_softap_tx_rx.c
@@ -1120,7 +1120,7 @@ QDF_STATUS hdd_softap_register_bc_sta(struct hdd_adapter *adapter,
 {
 	QDF_STATUS qdf_status = QDF_STATUS_E_FAILURE;
 	struct hdd_context *hdd_ctx = WLAN_HDD_GET_CTX(adapter);
-	struct qdf_mac_addr broadcastMacAddr = QDF_MAC_ADDR_BCAST_INIT;
+	struct qdf_mac_addr broadcast_macaddr = QDF_MAC_ADDR_BCAST_INIT;
 	struct hdd_ap_ctx *ap_ctx;
 
 	ap_ctx = WLAN_HDD_GET_AP_CTX_PTR(adapter);
@@ -1130,7 +1130,7 @@ QDF_STATUS hdd_softap_register_bc_sta(struct hdd_adapter *adapter,
 	qdf_status =
 		hdd_softap_register_sta(adapter, false, privacy_required,
 					ap_ctx->broadcast_sta_id,
-					&broadcastMacAddr, 0);
+					&broadcast_macaddr, 0);
 
 	return qdf_status;
 }
