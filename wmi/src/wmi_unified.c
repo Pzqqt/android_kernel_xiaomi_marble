6b276f6a8fa35a31646cfb06dfda23c573015582
Rajeev Kumar <rajekuma@codeaurora.org>
2019-04-25 14:50:05 -0700

    qcacmn: Log error and fail WMI send if target is suspended
    Replace qdf_bug by error log becasue when using wow unit test
    framework its very common for higher layer components to send wmi
    commands and in this case WMI should fail the request rather than
    asserting.
    
    Change-Id: Ie09e4b6014468bcd6b5d75e7f64a78d069855f4f
    CRs-Fixed: 2442275

--- wmi/src/wmi_unified.c
+++ wmi/src/wmi_unified.c
@@ -1672,12 +1672,8 @@ QDF_STATUS wmi_unified_cmd_send_fl(wmi_unified_t wmi_handle, wmi_buf_t buf,
 							      cmd_id);
 	} else if (qdf_atomic_read(&wmi_handle->is_target_suspended) &&
 		   !wmi_is_pm_resume_cmd(cmd_id)) {
-		if (!wmi_handle->wmi_stopinprogress)
-			QDF_DEBUG_PANIC("Target is suspended (via %s:%u)",
-					func, line);
-		else
 			wmi_nofl_err("Target is suspended (via %s:%u)",
-				     func, line);
+					func, line);
 		return QDF_STATUS_E_BUSY;
 	}
 
