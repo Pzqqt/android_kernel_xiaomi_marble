5827fd091da94141e9fc86bdb587834985bcdd65
Varsha Mishra <varsham@codeaurora.org>
2019-05-03 16:59:57 +0530

    qcacmn: Set invalid tid in multicast enhancement before enqueueing
    Set tid invalid before enqueuing frame to transmit classifier.
    When not set to invalid, tid is not classified by HW.
    
    Change-Id: I4fb96a010fed1e0178d7d70a83503e1dcf5f755a

--- dp/wifi3.0/dp_tx.c
+++ dp/wifi3.0/dp_tx.c
@@ -4217,6 +4217,7 @@ dp_tx_me_send_convert_ucast(struct cdp_vdev *vdev_handle, qdf_nbuf_t nbuf,
 	msdu_info.num_seg = new_mac_cnt;
 	msdu_info.frm_type = dp_tx_frm_me;
 
+	msdu_info.tid = HTT_INVALID_TID;
 	if (qdf_unlikely(vdev->mcast_enhancement_en > 0) &&
 	    qdf_unlikely(pdev->hmmc_tid_override_en))
 		msdu_info.tid = pdev->hmmc_tid;
