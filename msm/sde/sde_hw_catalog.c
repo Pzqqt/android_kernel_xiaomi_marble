627f4335aa7e657ec0e276c04b0a490818ca3125
Dhaval Patel <pdhaval@codeaurora.org>
2019-05-16 20:31:01 -0700

    disp: msm: sde: update min prefill lines to 35 for kona
    Update minimum prefill ines to 35 lines for kona
    target.
    
    Change-Id: Ib37304024947c581a2a29f820eef881d6db984f9
    Signed-off-by: Dhaval Patel <pdhaval@codeaurora.org>

--- msm/sde/sde_hw_catalog.c
+++ msm/sde/sde_hw_catalog.c
@@ -3850,7 +3850,7 @@ static int _sde_hardware_pre_caps(struct sde_mdss_cfg *sde_cfg, uint32_t hw_rev)
 		sde_cfg->has_cwb_support = true;
 		sde_cfg->has_wb_ubwc = true;
 		sde_cfg->has_qsync = true;
-		sde_cfg->perf.min_prefill_lines = 24;
+		sde_cfg->perf.min_prefill_lines = 35;
 		sde_cfg->vbif_qos_nlvl = 8;
 		sde_cfg->ts_prefill_rev = 2;
 		sde_cfg->ctl_rev = SDE_CTL_CFG_VERSION_1_0_0;
