326536d1d679bebaf00cd0fa9aa36c74868145fa
Karthikeyan Mani <kmani@codeaurora.org>
2019-06-03 13:29:43 -0700

    asoc: codecs: defer probe if soundwire pin is not ready
    Defer codec macro probes if soundwire pins are not
    ready to be accessed yet during boot up.
    
    Change-Id: I20173e49043a076d73cdbb811863afbff87d4a3b
    Signed-off-by: Karthikeyan Mani <kmani@codeaurora.org>

--- include/asoc/msm-cdc-pinctrl.h
+++ include/asoc/msm-cdc-pinctrl.h
@@ -1,5 +1,5 @@
 /* SPDX-License-Identifier: GPL-2.0-only */
-/* Copyright (c) 2016-2018, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2016-2019, The Linux Foundation. All rights reserved.
  */
 
 #ifndef __MFD_CDC_PINCTRL_H_
@@ -11,7 +11,7 @@
 #if IS_ENABLED(CONFIG_MSM_CDC_PINCTRL)
 extern int msm_cdc_pinctrl_select_sleep_state(struct device_node *np);
 extern int msm_cdc_pinctrl_select_active_state(struct device_node *np);
-extern bool msm_cdc_pinctrl_get_state(struct device_node *np);
+extern int msm_cdc_pinctrl_get_state(struct device_node *np);
 extern int msm_cdc_get_gpio_state(struct device_node *np);
 int msm_cdc_pinctrl_drv_init(void);
 void msm_cdc_pinctrl_drv_exit(void);
@@ -36,7 +36,7 @@ int msm_cdc_pinctrl_drv_init(void)
 void msm_cdc_pinctrl_drv_exit(void)
 {
 }
-bool msm_cdc_pinctrl_get_state(struct device_node *np)
+int msm_cdc_pinctrl_get_state(struct device_node *np)
 {
 	return true;
 }
