30a1c046b4aacc3e70063474b7b26f4f0bda8cb8
Vijayakumar Badiger <vbadig@codeaurora.org>
2019-06-20 15:19:12 -0700

    ASoC: Preliminary Android target definition for sdmshrike
    Add target definition support for sdmshrike.
    
    Change-Id: Ic87588d7aa9e14d2893bc1d6172cb35c34f6ede2
    Signed-off-by: Derek Chen <chenche@codeaurora.org>

--- Android.mk
+++ Android.mk
@@ -3,7 +3,7 @@ MY_LOCAL_PATH := $(call my-dir)
 
 UAPI_OUT := $(PRODUCT_OUT)/obj/vendor/qcom/opensource/audio-kernel/include
 
-ifeq ($(call is-board-platform-in-list,msmnile $(MSMSTEPPE) $(TRINKET) kona lito bengal),true)
+ifeq ($(call is-board-platform-in-list,msmnile $(MSMSTEPPE) $(TRINKET) kona lito bengal sdmshrike),true)
 $(shell mkdir -p $(UAPI_OUT)/linux;)
 $(shell mkdir -p $(UAPI_OUT)/sound;)
 $(shell rm -rf $(PRODUCT_OUT)/obj/vendor/qcom/opensource/audio-kernel/ipc/Module.symvers)
@@ -22,7 +22,7 @@ include $(MY_LOCAL_PATH)/asoc/Android.mk
 include $(MY_LOCAL_PATH)/asoc/codecs/Android.mk
 endif
 
-ifeq ($(call is-board-platform-in-list,msmnile $(MSMSTEPPE) $(TRINKET)),true)
+ifeq ($(call is-board-platform-in-list,msmnile $(MSMSTEPPE) $(TRINKET) sdmshrike),true)
 ifneq ($(TARGET_BOARD_AUTO),true)
 $(shell rm -rf $(PRODUCT_OUT)/obj/vendor/qcom/opensource/audio-kernel/asoc/codecs/wcd934x/Module.symvers)
 include $(MY_LOCAL_PATH)/asoc/codecs/wcd934x/Android.mk
@@ -31,7 +31,7 @@ include $(MY_LOCAL_PATH)/soc/Android.mk
 endif
 endif
 
-ifeq ($(call is-board-platform-in-list,msmnile),true)
+ifeq ($(call is-board-platform-in-list,msmnile sdmshrike),true)
 ifneq ($(TARGET_BOARD_AUTO),true)
 $(shell rm -rf $(PRODUCT_OUT)/obj/vendor/qcom/opensource/audio-kernel/asoc/codecs/aqt1000/Module.symvers)
 include $(MY_LOCAL_PATH)/asoc/codecs/aqt1000/Android.mk
