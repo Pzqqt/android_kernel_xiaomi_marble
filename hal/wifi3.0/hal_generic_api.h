49cd7b9173936474182bbd72dbbe9f9c71a4ab0f
Jinwei Chen <jinweic@codeaurora.org>
2019-06-21 17:56:14 +0800

    qcacmn: add correct VHT NSS value for monitor radiotype
    add correct VHT NSS value for monitor radiotype.
    
    Change-Id: I6d2258b58873a1f843020e290b82c32bc88df336
    CRs-Fixed: 2477387

--- hal/wifi3.0/hal_generic_api.h
+++ hal/wifi3.0/hal_generic_api.h
@@ -653,6 +653,9 @@ hal_rx_status_get_tlv_info_generic(void *rx_tlv_hdr, void *ppduinfo,
 		case TARGET_TYPE_QCA8074:
 		case TARGET_TYPE_QCA8074V2:
 		case TARGET_TYPE_QCA6018:
+#ifdef QCA_WIFI_QCA6390
+		case TARGET_TYPE_QCA6390:
+#endif
 			ppdu_info->rx_status.is_stbc =
 				HAL_RX_GET(vht_sig_a_info,
 					   VHT_SIG_A_INFO_0, STBC);
@@ -679,11 +682,6 @@ hal_rx_status_get_tlv_info_generic(void *rx_tlv_hdr, void *ppduinfo,
 			ppdu_info->rx_status.nss = 0;
 #endif
 			break;
-#ifdef QCA_WIFI_QCA6390
-		case TARGET_TYPE_QCA6390:
-			ppdu_info->rx_status.nss = 0;
-			break;
-#endif
 		default:
 			break;
 		}
