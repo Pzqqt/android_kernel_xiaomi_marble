835c8d87c6d7f0b5f4446d0c48352d9287206bf1
arnav_s <arnav_s@codeaurora.org>
2019-07-02 14:46:28 -0700

    Moving IPA and GSI driver code to dataipa techpack
    Change-Id: I196832c62a6109178cb234f000d3db7c481e0ef5
    Signed-off-by: Arnav Sharma <arnav_s@codeaurora.org>

---
+++ config/litodataipaconf.h
@@ -0,0 +1,16 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+/*
+ * Copyright (c) 2019, The Linux Foundation. All rights reserved.
+ */
+
+#define CONFIG_GSI 1
+#define CONFIG_GSI_REGISTER_VERSION_2 1
+#define CONFIG_IPA3 1
+#define CONFIG_IPA_WDI_UNIFIED_API 1
+#define CONFIG_RMNET_IPA3 1
+#define CONFIG_RNDIS_IPA 1
+#define CONFIG_IPA3_MHI_PRIME_MANAGER 1
+#define CONFIG_IPA_UT 1
+#define CONFIG_IPA3_REGDUMP 1
+#define CONFIG_IPA3_REGDUMP_IPA_4_5 1
+#define CONFIG_IPA3_REGDUMP_NUM_EXTRA_ENDP_REGS 0
--- dataipa.c
+++
@@ -1,5 +0,0 @@
-// SPDX-License-Identifier: GPL-2.0-only
-
-static void _dataipa_techpack_stub(void)
-{
-}
