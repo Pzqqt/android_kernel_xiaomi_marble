56b50ec857e1e5d92d9c3462d2b0a3e87909e9e3
Pragaspathi Thilagaraj <tpragasp@codeaurora.org>
2019-07-17 00:16:15 +0530

    qcacld-3.0: Check SAE & OWE target capability to enable RSO
    Currently the host driver blocks roam scan offload command to
    the target if the connected akm is OWE or SAE.
    
    Add changes to read the WMI_SERVICE_WPA3_SAE_ROAM_SUPPORT and
    WMI_SERVICE_WPA3_OWE_ROAM_SUPPORT service capability advertised
    by the target and set it to the mlme fw_akm_bitmap. Read this
    bitmap in csr_roam_offload_scan() to allow RSO command.
    
    Also the connected akm is sent to the target via the
    WMI_AP_PROFILE command. Translate the CSR akm type for OWE and
    SAE akm also.
    
    Change-Id: I68527722ab099e0c8e1681ed64ea416b9a6e9936
    CRs-Fixed: 2491021

--- mlme/dispatcher/inc/wlan_mlme_ucfg_api.h
+++ mlme/dispatcher/inc/wlan_mlme_ucfg_api.h
@@ -1044,18 +1044,6 @@ ucfg_mlme_get_first_scan_bucket_threshold(struct wlan_objmgr_psoc *psoc,
 					  uint8_t *val);
 
 /**
- * ucfg_mlme_set_fw_supported_roaming_akm() - update the supported roaming
- * akm suites advertised by the firmware via wmi service capability
- * @psoc: pointer to psoc object
- * @val:  bitmap value based on firmware capabilities
- *
- * Return: QDF Status
- */
-QDF_STATUS
-ucfg_mlme_set_fw_supported_roaming_akm(struct wlan_objmgr_psoc *psoc,
-				       uint32_t val);
-
-/**
  * ucfg_mlme_is_mawc_enabled() - MAWC enabled or not
  * @psoc: pointer to psoc object
  * @val:  Pointer to the value which will be filled for the caller
