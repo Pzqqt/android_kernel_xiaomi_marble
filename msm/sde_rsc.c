2078f3dd2e9c30258dedeffb5d4a84c5c7fd8879
Satya Rama Aditya Pinapala <psraditya30@codeaurora.org>
2019-07-25 10:56:51 -0700

    disp: msm: sde: add check before buffer copy
    This change adds a check for the length of the buffer
    before copying it to avoid a buffer overflow.
    
    Change-Id: I9af9d422e0b3cf02c8d6662af3310337a9861a7a
    Signed-off-by: Satya Rama Aditya Pinapala <psraditya30@codeaurora.org>

--- msm/sde_rsc.c
+++ msm/sde_rsc.c
@@ -1102,6 +1102,7 @@ end:
 	if (blen <= 0)
 		return 0;
 
+	blen = min_t(size_t, MAX_BUFFER_SIZE, count);
 	if (copy_to_user(buf, buffer, blen))
 		return -EFAULT;
 
@@ -1195,6 +1196,7 @@ end:
 	if (blen <= 0)
 		return 0;
 
+	blen = min_t(size_t, MAX_BUFFER_SIZE, count);
 	if (copy_to_user(buf, buffer, blen))
 		return -EFAULT;
 
