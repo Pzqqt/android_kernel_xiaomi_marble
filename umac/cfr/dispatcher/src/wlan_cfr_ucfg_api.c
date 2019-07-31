ea20fa5e535f00abeb8078ecfee40d84cba892b2
Abhiram Jogadenu <ajogaden@codeaurora.org>
2019-07-31 18:49:59 +0530

    qca-wifi: Add support for CFR capture on unassociated clients
    As part of FR 56301, add support to capture CFR data for
    unassociated clients on its Probe response's ACK
    
    Change-Id: I14a4eead4ceeb3eb21d8f2bdf76007f873db1f4e
    CRs-Fixed: 2501938

--- umac/cfr/dispatcher/src/wlan_cfr_ucfg_api.c
+++ umac/cfr/dispatcher/src/wlan_cfr_ucfg_api.c
@@ -86,6 +86,111 @@ int ucfg_cfr_start_capture(struct wlan_objmgr_pdev *pdev,
 	return status;
 }
 
+int ucfg_cfr_start_capture_probe_req(struct wlan_objmgr_pdev *pdev,
+				     struct qdf_mac_addr *unassoc_mac,
+				     struct cfr_capture_params *params)
+{
+	int idx, idx_to_insert = -1;
+	struct pdev_cfr *pa;
+
+	pa = wlan_objmgr_pdev_get_comp_private_obj(pdev, WLAN_UMAC_COMP_CFR);
+	if (!pa) {
+		cfr_err("Pdev cfr object is null!");
+		return -EINVAL;
+	}
+
+	if (!(pa->is_cfr_capable)) {
+		cfr_err("CFR is not supported on this chip");
+		return -EINVAL;
+	}
+
+	if (pa->cfr_current_sta_count == pa->cfr_max_sta_count) {
+		cfr_err("max cfr cleint reached");
+		return -EINVAL;
+	}
+
+	for (idx = 0; idx < MAX_CFR_ENABLED_CLIENTS; idx++) {
+		/* Store first invalid entry's index, to add mac entry if not
+		 * already present.
+		 */
+		if (idx_to_insert < 0) {
+			if (pa->unassoc_pool[idx].is_valid != true)
+				idx_to_insert = idx;
+		}
+
+		/* Add new mac entry only if it is not present. If already
+		 * present, update the capture parameters
+		 */
+		if (qdf_mem_cmp(&pa->unassoc_pool[idx].mac, unassoc_mac,
+				sizeof(struct qdf_mac_addr)) == 0) {
+			cfr_info("Node already present. Updating params");
+			qdf_mem_copy(&pa->unassoc_pool[idx].cfr_params,
+				     params,
+				     sizeof(struct cfr_capture_params));
+			pa->unassoc_pool[idx].is_valid = true;
+			return 0;
+		}
+	}
+
+	if (idx_to_insert < 0) {
+		/* All the entries in the table are valid. So we have reached
+		 * max client capacity. To add a new client, capture on one of
+		 * the clients in table has to be stopped.
+		 */
+		cfr_err("Maximum client capacity reached");
+		return -EINVAL;
+	}
+
+	/* If control reaches here, we did not find mac in the table
+	 * and we have atleast one free entry in table.
+	 * Add the entry at index = idx_to_insert
+	 */
+	qdf_mem_copy(&pa->unassoc_pool[idx_to_insert].mac,
+		     unassoc_mac, sizeof(struct qdf_mac_addr));
+	qdf_mem_copy(&pa->unassoc_pool[idx_to_insert].cfr_params,
+		     params, sizeof(struct cfr_capture_params));
+	pa->unassoc_pool[idx_to_insert].is_valid = true;
+	pa->cfr_current_sta_count++;
+
+	return 0;
+}
+
+int ucfg_cfr_stop_capture_probe_req(struct wlan_objmgr_pdev *pdev,
+				    struct qdf_mac_addr *unassoc_mac)
+{
+	struct pdev_cfr *pa;
+	int idx;
+
+	pa = wlan_objmgr_pdev_get_comp_private_obj(pdev, WLAN_UMAC_COMP_CFR);
+	if (!pa) {
+		cfr_err("Pdev cfr object is NULL!\n");
+		return -EINVAL;
+	}
+
+	if (!(pa->is_cfr_capable)) {
+		cfr_err("CFR is not supported on this chip\n");
+		return -EINVAL;
+	}
+
+	for (idx = 0; idx < MAX_CFR_ENABLED_CLIENTS; idx++) {
+		/* Remove mac only if it is present */
+		if (qdf_mem_cmp(&pa->unassoc_pool[idx].mac, unassoc_mac,
+				sizeof(struct qdf_mac_addr)) == 0) {
+			qdf_mem_zero(&pa->unassoc_pool[idx],
+				     sizeof(struct unassoc_pool_entry));
+			pa->cfr_current_sta_count--;
+			return 0;
+		}
+	}
+
+	/* If mac was present in pool it would have been deleted in the
+	 * above loop and returned from there.
+	 * If control reached here, mac was not found. So, ignore the request.
+	 */
+	cfr_err("Trying to delete mac not present in pool. Ignoring request.");
+	return 0;
+}
+
 int ucfg_cfr_set_timer(struct wlan_objmgr_pdev *pdev, uint32_t value)
 {
 	struct pdev_cfr *pa;
