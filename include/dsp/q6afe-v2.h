450ee1b712f606e5a876792e8b2cd5bfad7be8b6
Zhou Song <zhous@codeaurora.org>
2019-08-02 11:06:01 +0800

    dsp: q6afe: add tws mono switch for aptx adaptive encoder
    Add support to switch tws channel mode for aptx adaptive
    encoder.
    
    Change-Id: Ife4170be56b152d34758a1cfd20fa4ec203742db
    Signed-off-by: Zhou Song <zhous@codeaurora.org>

--- include/dsp/q6afe-v2.h
+++ include/dsp/q6afe-v2.h
@@ -389,7 +389,7 @@ void afe_set_island_mode_cfg(u16 port_id, u32 enable_flag);
 void afe_get_island_mode_cfg(u16 port_id, u32 *enable_flag);
 int afe_port_start(u16 port_id, union afe_port_config *afe_config,
 	u32 rate);
-int afe_set_tws_channel_mode(u16 port_id, u32 channel_mode);
+int afe_set_tws_channel_mode(u32 foramt, u16 port_id, u32 channel_mode);
 int afe_port_start_v2(u16 port_id, union afe_port_config *afe_config,
 		      u32 rate, u16 afe_in_channels, u16 afe_in_bit_width,
 		      struct afe_enc_config *enc_config,
