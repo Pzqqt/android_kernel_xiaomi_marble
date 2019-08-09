962a56bfd879f5acf0ca325c11b01e5e3a2c88ae
Rajeev Kumar <rajekuma@codeaurora.org>
2019-08-08 23:13:14 -0700

    qcacld-3.0: Enable HW filters in runtime pm mode
    Enable hw filter in runtime pm mode such that target
    CPU wake ups can be avoided in order to achieve max
    power savings.
    
    Change-Id: I97aa26b063e70db5e3023faf6aa40cc53bfac3af
    CRs-Fixed: 2502744

--- pmo/core/src/wlan_pmo_hw_filter.c
+++ pmo/core/src/wlan_pmo_hw_filter.c
@@ -41,6 +41,7 @@ QDF_STATUS pmo_core_enable_hw_filter_in_fwr(struct wlan_objmgr_vdev *vdev)
 
 	if (wlan_vdev_is_up(vdev) != QDF_STATUS_SUCCESS) {
 		status = QDF_STATUS_E_NOSUPPORT;
+		pmo_vdev_put_ref(vdev);
 		goto exit_with_status;
 	}
 
@@ -77,6 +78,7 @@ QDF_STATUS pmo_core_disable_hw_filter_in_fwr(struct wlan_objmgr_vdev *vdev)
 
 	if (wlan_vdev_is_up(vdev) != QDF_STATUS_SUCCESS) {
 		status = QDF_STATUS_E_NOSUPPORT;
+		pmo_vdev_put_ref(vdev);
 		goto exit_with_status;
 	}
 
