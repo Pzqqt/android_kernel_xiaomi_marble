e5ea8befdec60471f0c1e584faa64a992a4b6088
Julian Liu <wlootlxt123@gmail.com>
2019-08-24 01:20:14 +0800

    kernel: fix build with CONFIG_CPUSETS_ASSIST disabled

--- kernel/cgroup/cpuset.c
+++ kernel/cgroup/cpuset.c
@@ -2472,6 +2472,7 @@ out_unlock:
 	return retval ?: nbytes;
 }
 
+#ifdef CONFIG_CPUSETS_ASSIST
 static ssize_t cpuset_write_resmask_assist(struct kernfs_open_file *of,
 					   struct cs_target tgt, size_t nbytes,
 					   loff_t off)
@@ -2479,6 +2480,7 @@ static ssize_t cpuset_write_resmask_assist(struct kernfs_open_file *of,
 	pr_info("cpuset_assist: setting %s to %s\n", tgt.name, tgt.cpus);
 	return cpuset_write_resmask(of, tgt.cpus, nbytes, off);
 }
+#endif
 
 static ssize_t cpuset_write_resmask_wrapper(struct kernfs_open_file *of,
 					 char *buf, size_t nbytes, loff_t off)
