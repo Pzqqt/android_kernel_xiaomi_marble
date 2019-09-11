cd339694c5d9e720d73cecd057837b0a3a497b62
Vatsal Bucha <vbucha@codeaurora.org>
2019-09-11 12:17:11 +0530

    ASoC: wcd-mbhc: Remove max impedance limit for lineout detection
    Max limit of 60k ohms was set for cable to be detected as lineout
    to fix headset detected as lineout issue on legacy targets.
    This was because of large impedance detected for headset.
    This is not applicable for adc based targets as headset is
    detected based on mic adc voltage. Remove max impedance limit for
    lineout detection.
    
    Change-Id: If73bbe2cd78d04d1fcb359a3e5ed5cd69f3ab9f0
    Signed-off-by: Vatsal Bucha <vbucha@codeaurora.org>

--- include/asoc/wcd-mbhc-v2.h
+++ include/asoc/wcd-mbhc-v2.h
@@ -140,7 +140,6 @@ do {                                                    \
 #define FW_READ_ATTEMPTS 15
 #define FW_READ_TIMEOUT 4000000
 #define FAKE_REM_RETRY_ATTEMPTS 3
-#define MAX_IMPED 60000
 
 #define WCD_MBHC_BTN_PRESS_COMPL_TIMEOUT_MS  50
 #define ANC_DETECT_RETRY_CNT 7
