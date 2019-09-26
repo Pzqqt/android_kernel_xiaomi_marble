e6c06ae348412ea6313c0bc629a305ae8aa40275
Laxminath Kasam <lkasam@codeaurora.org>
2019-09-26 15:05:08 +0530

    asoc: codecs: wcd937x: restore clear_ack setting for wcd937x
    For wcd937x interrupts, need to set clear_ack explicitly
    for regmap irq to clear the interrupt.
    
    Change-Id: Ieffc20db0665fa82fe088337fbdf8b518191be3d
    Signed-off-by: Laxminath Kasam <lkasam@codeaurora.org>

--- asoc/codecs/wcd937x/wcd937x.c
+++ asoc/codecs/wcd937x/wcd937x.c
@@ -88,6 +88,7 @@ static struct regmap_irq_chip wcd937x_regmap_irq_chip = {
 	.mask_base = WCD937X_DIGITAL_INTR_MASK_0,
 	.ack_base = WCD937X_DIGITAL_INTR_CLEAR_0,
 	.use_ack = 1,
+	.clear_ack = 1,
 	.type_base = WCD937X_DIGITAL_INTR_LEVEL_0,
 	.runtime_pm = false,
 	.handle_post_irq = wcd937x_handle_post_irq,
