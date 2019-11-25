b7796edd5bc32225e05798f230799c502f0eee77
Chaithanya Garrepalli <cgarre@codeaurora.org>
2019-11-25 11:55:09 +0530

    Revert "qcacmn: corrected the print statement"
    This reverts commit I37a7c98eef6a8ad0b27541584927505ffc447cb2.
    
    Reverting because below change address the compilation error
    I4bb7bb14a2e521645be6e2677a44a78fa79fcb0b
    
    Change-Id: Ib38276ab9974cf22486631f10ec345a629637b82

--- dp/wifi3.0/dp_main.c
+++ dp/wifi3.0/dp_main.c
@@ -1047,7 +1047,8 @@ void dp_print_ast_stats(struct dp_soc *soc)
 					    " ast_idx = %d"
 					    " ast_hash = %d"
 					    " delete_in_progress = %d"
-					    " pdev_id = %d",
+					    " pdev_id = %d"
+					    " vdev_id = %d",
 					    ++num_entries,
 					    ase->mac_addr.raw,
 					    ase->peer->mac_addr.raw,
