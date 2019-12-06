0dce16d5ed6b2988fc0ca2f4d96f65eedaa3fe48
Sourav Mohapatra <mohapatr@codeaurora.org>
2019-12-06 09:40:27 +0530

    qcacld-3.0: Increase the number of supported interfaces
    The max number of VDEV that the FW can support is 4. Due to memory
    limitation, the host cannot send more than 4 vdev create to the firmware.
    But for certain scenarios such as NAN, where there is a need to create two
    extra interfaces apart from wlan0, wlan1 and p2p0 (e.g. aware_dataN
    interfaces), there needs to be 5 vdevs created in the host.
    
    To support 5 vdevs in host and in the same time only send 4 vdev
    create to the firmware, allow an extra 5th interface to be created.
    But do not allow all the 5 vdevs to be "up" simultaneously
    (existing check in objmgr).
    
    Change-Id: I5b677ae21043f7ee54194541576aa5125719d9b6
    CRs-Fixed: 2472869

--- core/hdd/src/wlan_hdd_main.c
+++ core/hdd/src/wlan_hdd_main.c
@@ -5420,7 +5420,7 @@ struct hdd_adapter *hdd_open_adapter(struct hdd_context *hdd_ctx, uint8_t sessio
 	u8 intf_count = 0;
 	QDF_STATUS status = QDF_STATUS_E_FAILURE;
 
-	if (hdd_ctx->current_intf_count >= hdd_ctx->max_intf_count) {
+	if (hdd_ctx->current_intf_count >= WLAN_MAX_VDEVS) {
 		/*
 		 * Max limit reached on the number of vdevs configured by the
 		 * host. Return error
