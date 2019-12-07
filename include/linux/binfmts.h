c6fa917fcddcbf9c2de42c2fc2e0cf24f5a18523
Adam W. Willis <return.of.octobot@gmail.com>
2019-12-07 12:32:26 -0500

    exec: Extend task_is_booster check to cover OxygenOS userspace HALs
    Subsequent to 2e20671 ("exec: Add node tampering blacklist function")
    
    This device utilizes vendor.qti.hardware.perf@2.0-service, and
    android.hardware.power@1.2-service respectively.
    
    Signed-off-by: Adam W. Willis <return.of.octobot@gmail.com>
    Signed-off-by: Julian Liu <wlootlxt123@gmail.com>

--- include/linux/binfmts.h
+++ include/linux/binfmts.h
@@ -144,8 +144,10 @@ static inline bool task_is_booster(struct task_struct *tsk)
 
 	get_task_comm(comm, tsk);
 	return !strcmp(comm, "init") || !strcmp(comm, "NodeLooperThrea") ||
+	       !strcmp(comm, "power@1.2-servi") ||
 	       !strcmp(comm, "power@1.3-servi") ||
 	       !strcmp(comm, "perf@1.0-servic") ||
+	       !strcmp(comm, "perf@2.0-servic") ||
 	       !strcmp(comm, "init.qcom.post_");
 }
 
