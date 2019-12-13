ec6688849be99df7693cfacbf224a537aad2a18c
Krishna Manikandan <mkrishn@codeaurora.org>
2019-12-13 15:36:32 +0530

    disp: msm: sde: avoid CWB connector in determining active crtc
    CWB connector is tied to primary crtc along with primary connector.
    Avoid using CWB connector power state in determining active crtc,
    as it is already done via primary connector.
    
    Change-Id: I35ec95349790990c49b9a63afd6e0f55d23b4887
    Signed-off-by: Kalyan Thota <kalyant@codeaurora.org>
    Signed-off-by: Krishna Manikandan <mkrishn@codeaurora.org>

--- msm/sde/sde_kms.c
+++ msm/sde/sde_kms.c
@@ -2542,6 +2542,9 @@ static void _sde_kms_pm_suspend_idle_helper(struct sde_kms *sde_kms,
 		if (lp != SDE_MODE_DPMS_LP2)
 			continue;
 
+		if (sde_encoder_in_clone_mode(conn->encoder))
+			continue;
+
 		ret = sde_encoder_wait_for_event(conn->encoder,
 						MSM_ENC_TX_COMPLETE);
 		if (ret && ret != -EWOULDBLOCK)
@@ -2628,7 +2631,8 @@ retry:
 		uint64_t lp;
 
 		if (!conn->state || !conn->state->crtc ||
-				conn->dpms != DRM_MODE_DPMS_ON)
+			conn->dpms != DRM_MODE_DPMS_ON ||
+			sde_encoder_in_clone_mode(conn->encoder))
 			continue;
 
 		lp = sde_connector_get_lp(conn);
