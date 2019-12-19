d114234b615f4f92ba6e8ddbe8ae4052ebe05c5f
Meng Wang <mengw@codeaurora.org>
2019-12-18 16:33:23 -0800

    dsp: adm: increase the length of timeout in adm_open to 2 seconds
    Currently, the length of timeout in adm_open for waiting response
    from ADSP is 1 second. To prevent unnecessary timeout that results
    adm_open failure, increase the length of timeout to 2 seconds.
    
    Change-Id: I670f44964538c3176130c4bb5f8b7a3b3eaf8d8b
    Signed-off-by: Xiaoyu Ye <benyxy@codeaurora.org>

--- dsp/q6adm.c
+++ dsp/q6adm.c
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: GPL-2.0-only
 /*
- * Copyright (c) 2012-2019, The Linux Foundation. All rights reserved.
+ * Copyright (c) 2012-2020, The Linux Foundation. All rights reserved.
  */
 #include <linux/module.h>
 #include <linux/slab.h>
@@ -3372,7 +3372,7 @@ int adm_open(int port_id, int path, int rate, int channel_mode, int topology,
 		ret = wait_event_timeout(this_adm.copp.wait[port_idx][copp_idx],
 			atomic_read(&this_adm.copp.stat
 			[port_idx][copp_idx]) >= 0,
-			msecs_to_jiffies(TIMEOUT_MS));
+			msecs_to_jiffies(2 * TIMEOUT_MS));
 		if (!ret) {
 			pr_err("%s: ADM open timedout for port_id: 0x%x for [0x%x]\n",
 						__func__, tmp_port, port_id);
