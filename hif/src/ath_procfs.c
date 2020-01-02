7b4e341e81642bb5b8acf82f2170bc58c2a92d36
guangde <guangde@codeaurora.org>
2020-01-02 11:01:04 +0800

    qcacmn: HIF changes to support Genova
    Add HIF changes for supporting the newly added USB bus type
    for Genova.
    FW team need athdiag tool to operate FW information. Host
    driver need to pass the command and result between athdiag
    tool and FW.
    
    Change-Id: I6158a9f8c723a797d39beca09a913c7cb9e10025
    CRs-Fixed: 2595515

--- hif/src/ath_procfs.c
+++ hif/src/ath_procfs.c
@@ -89,7 +89,9 @@ static ssize_t ath_procfs_diag_read(struct file *file, char __user *buf,
 	     (tgt_info->target_type == TARGET_TYPE_QCN9000) ||
 	     (tgt_info->target_type == TARGET_TYPE_QCA6018))) ||
 	    (scn->bus_type ==  QDF_BUS_TYPE_IPCI &&
-	     (tgt_info->target_type == TARGET_TYPE_QCA6750))) {
+	     (tgt_info->target_type == TARGET_TYPE_QCA6750)) ||
+	    ((scn->bus_type ==  QDF_BUS_TYPE_USB) &&
+	     (tgt_info->target_type == TARGET_TYPE_QCN7605))) {
 		memtype = ((uint32_t)(*pos) & 0xff000000) >> 24;
 		offset = (uint32_t)(*pos) & 0xffffff;
 		HIF_DBG("%s: offset 0x%x memtype 0x%x, datalen %zu\n",
@@ -168,7 +170,9 @@ static ssize_t ath_procfs_diag_write(struct file *file,
 	      (tgt_info->target_type == TARGET_TYPE_QCN9000) ||
 	      (tgt_info->target_type == TARGET_TYPE_QCA6018))) ||
 	    (scn->bus_type ==  QDF_BUS_TYPE_IPCI &&
-	     (tgt_info->target_type == TARGET_TYPE_QCA6750))) {
+	     (tgt_info->target_type == TARGET_TYPE_QCA6750)) ||
+	    ((scn->bus_type ==  QDF_BUS_TYPE_USB) &&
+	     (tgt_info->target_type == TARGET_TYPE_QCN7605))) {
 		memtype = ((uint32_t)(*pos) & 0xff000000) >> 24;
 		offset = (uint32_t)(*pos) & 0xffffff;
 		HIF_DBG("%s: offset 0x%x memtype 0x%x, datalen %zu\n",
