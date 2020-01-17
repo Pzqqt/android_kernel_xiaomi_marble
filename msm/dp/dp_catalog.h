3b1a8807fd495e0d272fa4b7f950441ba036c3ad
Tatenda Chipeperekwa <tatendac@codeaurora.org>
2020-01-17 12:58:25 -0800

    disp: msm: dp: use sde_drm.h shared header from techpack path
    Update sde_drm.h header to use the display techpack path for
    kernel-5.4.
    
    Change-Id: I2b7dcbbde8128eece7a2a8a652f9f7c427b38110
    Signed-off-by: Tatenda Chipeperekwa <tatendac@codeaurora.org>

--- msm/dp/dp_catalog.h
+++ msm/dp/dp_catalog.h
@@ -1,13 +1,13 @@
 /* SPDX-License-Identifier: GPL-2.0-only */
 /*
- * Copyright (c) 2017-2019, The Linux Foundation. All rights reserved.
+ * Copyright (c) 2017-2020, The Linux Foundation. All rights reserved.
  */
 
 #ifndef _DP_CATALOG_H_
 #define _DP_CATALOG_H_
 
 #include <drm/drm_dp_helper.h>
-#include <drm/msm_drm.h>
+#include <drm/sde_drm.h>
 
 #include "dp_parser.h"
 
