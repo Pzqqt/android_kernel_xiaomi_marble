33ff8de7cb821889dfd386ee846bc0550c1cc06b
Ashish Kumar Dhanotiya <adhanoti@codeaurora.org>
2020-01-20 19:29:53 +0530

    qcacmn: Add vendor event to request SAR power limits
    This change introduces the vendor event
    QCA_NL80211_VENDOR_SUBCMD_REQUEST_SAR_LIMITS_EVENT.
    Host drivers can request user space application to set SAR
    power limits with this interface.
    
    Change-ID: I15cf651c722bf828ba9012d81d99922c39a9e1fc
    CRs-Fixed: 2615364

--- os_if/linux/qca_vendor.h
+++ os_if/linux/qca_vendor.h
@@ -370,6 +370,13 @@
  *	coex chain mode from application/service.
  *	The attributes defined in enum qca_vendor_attr_btc_chain_mode are used
  *	to deliver the parameters.
+ * @QCA_NL80211_VENDOR_SUBCMD_REQUEST_SAR_LIMITS_EVENT: This acts as an event.
+ *	Host drivers can request the user space entity to set the SAR power
+ *	limits with this event. Accordingly, the user space entity is expected
+ *	to set the SAR power limits. Host drivers can retry this event to the
+ *	user space for the SAR power limits configuration from user space. If
+ *	the driver does not get the SAR power limits from user space for all
+ *	the retried attempts, it can configure a default SAR power limit.
  */
 
 enum qca_nl80211_vendor_subcmds {
@@ -589,6 +596,7 @@ enum qca_nl80211_vendor_subcmds {
 	QCA_NL80211_VENDOR_SUBCMD_AVOID_FREQUENCY_EXT = 183,
 	QCA_NL80211_VENDOR_SUBCMD_ADD_STA_NODE = 184,
 	QCA_NL80211_VENDOR_SUBCMD_BTC_CHAIN_MODE = 185,
+	QCA_NL80211_VENDOR_SUBCMD_GET_SAR_LIMITS_EVENT = 187,
 };
 
 enum qca_wlan_vendor_tos {
@@ -937,6 +945,7 @@ enum qca_wlan_vendor_attr_get_station_info {
  * @QCA_NL80211_VENDOR_SUBCMD_WLAN_MAC_INFO_INDEX: MAC mode info index
  * @QCA_NL80211_VENDOR_SUBCMD_NAN_EXT_INDEX: NAN Extended index
  * @QCA_NL80211_VENDOR_SUBCMD_BEACON_REPORTING_INDEX: Beacon reporting index
+ * @QCA_NL80211_VENDOR_SUBCMD_REQUEST_SAR_LIMITS_INDEX: Request SAR limit index
  */
 
 enum qca_nl80211_vendor_subcmds_index {
@@ -1024,6 +1033,7 @@ enum qca_nl80211_vendor_subcmds_index {
 	QCA_NL80211_VENDOR_SUBCMD_BEACON_REPORTING_INDEX,
 	QCA_NL80211_VENDOR_SUBCMD_ROAM_INDEX,
 	QCA_NL80211_VENDOR_SUBCMD_OEM_DATA_INDEX,
+	QCA_NL80211_VENDOR_SUBCMD_REQUEST_SAR_LIMITS_INDEX,
 };
 
 /**
