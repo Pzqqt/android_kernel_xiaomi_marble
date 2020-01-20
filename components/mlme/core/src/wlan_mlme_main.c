32a9096941be0136e904e5c55b75437bc4807c53
Arun Kumar Khandavalli <akhandav@codeaurora.org>
2020-01-20 12:03:23 +0530

    qcacld-3.0: selectively reduce the mlme logs
    Seletively reduce the mlme component logs from info to debug.
    
    Change-Id: If31157aef1eba8fcd8bee5c654a9e87f4437cc95
    CRs-Fixed: 2607165

--- components/mlme/core/src/wlan_mlme_main.c
+++ components/mlme/core/src/wlan_mlme_main.c
@@ -2576,9 +2576,9 @@ static void
 mlme_print_roaming_state(uint8_t vdev_id, enum roam_offload_state cur_state,
 			 enum roam_offload_state new_state)
 {
-	mlme_info("ROAM: vdev[%d] ROAM State Changed from [%s] to [%s]",
-		  vdev_id, mlme_roam_state_to_string(cur_state),
-		  mlme_roam_state_to_string(new_state));
+	mlme_debug("ROAM: vdev[%d] ROAM State Changed from [%s] to [%s]",
+		   vdev_id, mlme_roam_state_to_string(cur_state),
+		   mlme_roam_state_to_string(new_state));
 
 	/* TODO: Try to print the state change requestor also */
 }
