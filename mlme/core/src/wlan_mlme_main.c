282732250bcb2a30700acdf6c7a11ba330bed7ac
Abhinav Kumar <abhikuma@codeaurora.org>
2020-01-30 11:35:32 +0530

    qcacld-3.0: Introduce a new compile time flag and INI
    Introduce a compile time flag SAE_SINGLE_PMK and new INI
    "sae_same_pmk_feature_enabled" to enable/disable SAE
    Roaming with same PMK/PMKID feature.
    
    Change-Id: Ifca8ec8bcf6e9a2f1d8409b35390bfee0033bd30
    CRs-Fixed: 2616025

--- mlme/core/src/wlan_mlme_main.c
+++ mlme/core/src/wlan_mlme_main.c
@@ -1432,6 +1432,30 @@ mlme_init_adaptive_11r_cfg(struct wlan_objmgr_psoc *psoc,
 }
 #endif
 
+#ifdef WLAN_SAE_SINGLE_PMK
+/**
+ * mlme_init_sae_single_pmk_cfg() - initialize sae_same_pmk_config
+ * flag
+ * @psoc: Pointer to PSOC
+ * @lfr:  pointer to mlme lfr config
+ *
+ * Return: None
+ */
+static void
+mlme_init_sae_single_pmk_cfg(struct wlan_objmgr_psoc *psoc,
+			     struct wlan_mlme_lfr_cfg *lfr)
+{
+	lfr->sae_same_pmk_feature_enabled = cfg_get(psoc, CFG_SAE_SINGLE_PMK);
+}
+
+#else
+static inline void
+mlme_init_sae_single_pmk_cfg(struct wlan_objmgr_psoc *psoc,
+			     struct wlan_mlme_lfr_cfg *lfr)
+{
+}
+#endif
+
 #ifdef WLAN_FEATURE_ROAM_OFFLOAD
 static void mlme_init_roam_offload_cfg(struct wlan_objmgr_psoc *psoc,
 				       struct wlan_mlme_lfr_cfg *lfr)
@@ -1684,6 +1708,7 @@ static void mlme_init_lfr_cfg(struct wlan_objmgr_psoc *psoc,
 	mlme_init_bss_load_trigger_params(psoc, &lfr->bss_load_trig);
 	mlme_init_adaptive_11r_cfg(psoc, lfr);
 	mlme_init_subnet_detection(psoc, lfr);
+	mlme_init_sae_single_pmk_cfg(psoc, lfr);
 }
 
 static uint32_t
