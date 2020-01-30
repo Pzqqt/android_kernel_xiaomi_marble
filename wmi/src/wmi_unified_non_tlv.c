6cca13cc9d3678c8d15a4f28435de98535b5459e
Shashikala Prabhu <pshashik@codeaurora.org>
2020-01-30 19:15:43 +0530

    qca-wifi: Add WMI support for native 160MHZ
    Set native 160MHZ wmi support as unavailable for pre-lithium  chipsets.
    
    Change-Id: I55650821896e2d59c0fc7d465ab30d6271d8359b
    CRs-Fixed: 2612980

--- wmi/src/wmi_unified_non_tlv.c
+++ wmi/src/wmi_unified_non_tlv.c
@@ -10687,6 +10687,8 @@ static void populate_non_tlv_service(uint32_t *wmi_service)
 	wmi_service[wmi_service_rx_fse_support] = WMI_SERVICE_UNAVAILABLE;
 	wmi_service[wmi_service_bw_165mhz_support] =
 		WMI_SERVICE_UNAVAILABLE;
+	wmi_service[wmi_service_nss_ratio_to_host_support] =
+		WMI_SERVICE_UNAVAILABLE;
 }
 
 /**
