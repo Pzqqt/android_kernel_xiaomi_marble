9c5572c1b753fe421aefb27d374584ca9daf7a98
nshrivas <nshrivas@codeaurora.org>
2020-02-20 01:08:01 -0800

    Release 5.2.0.172
    Release 5.2.0.172
    
    Change-Id: I18832c207f4b5337fa3689ed793ff23d64d65670
    CRs-Fixed: 774533

--- core/mac/inc/qwlan_version.h
+++ core/mac/inc/qwlan_version.h
@@ -32,9 +32,9 @@
 #define QWLAN_VERSION_MAJOR            5
 #define QWLAN_VERSION_MINOR            2
 #define QWLAN_VERSION_PATCH            0
-#define QWLAN_VERSION_EXTRA            "Z"
-#define QWLAN_VERSION_BUILD            171
+#define QWLAN_VERSION_EXTRA            ""
+#define QWLAN_VERSION_BUILD            172
 
-#define QWLAN_VERSIONSTR               "5.2.0.171Z"
+#define QWLAN_VERSIONSTR               "5.2.0.172"
 
 #endif /* QWLAN_VERSION_H */
