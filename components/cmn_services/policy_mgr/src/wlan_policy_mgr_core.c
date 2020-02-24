b4c5783d46534621f14dabb110d6c7a43216adc8
Jianmin Zhu <jianminz@codeaurora.org>
2020-02-24 19:48:48 +0800

    qcacld-3.0: Fix wrong channel freq API issue
    Call channel API with frequency parameters wrongly
    
    Change-Id: Ib810887ddc7eab3e1fa84de50720e2fe2a56bee7
    CRs-Fixed: 2627531

--- components/cmn_services/policy_mgr/src/wlan_policy_mgr_core.c
+++ components/cmn_services/policy_mgr/src/wlan_policy_mgr_core.c
@@ -2766,9 +2766,9 @@ bool policy_mgr_allow_new_home_channel(
 				 * and therefore a 3rd connection with the
 				 * same MAC is possible.
 				 */
-			} else if (wlan_reg_is_same_band_channels(
-				ch_freq, pm_conc_connection_list[0].freq) &&
-				policy_mgr_is_multi_ap_plus_sta_3vif_conc(
+			} else if (wlan_reg_is_same_band_freqs(ch_freq,
+					pm_conc_connection_list[0].freq) &&
+				   policy_mgr_is_multi_ap_plus_sta_3vif_conc(
 					pm_conc_connection_list[0].mode,
 					pm_conc_connection_list[1].mode,
 					mode)) {
