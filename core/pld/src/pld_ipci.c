c16cd55f6579d91abc04bf22e20077b12ea5dbe6
Naman Padhiar <npadhiar@codeaurora.org>
2020-02-28 23:23:51 +0530

    qcacld-3.0: Add V2 shadow config for WCN6750
    Add shadow configuration version 2 for WCN6750.
    
    Change-Id: I7472b22a83ae4d83ac87f3c064de60f1b07d1e4b
    CRs-Fixed: 2633612

--- core/pld/src/pld_ipci.c
+++ core/pld/src/pld_ipci.c
@@ -335,6 +335,16 @@ int pld_ipci_wlan_enable(struct device *dev, struct pld_wlan_enable_cfg *config,
 	cfg.num_shadow_reg_cfg = config->num_shadow_reg_cfg;
 	cfg.shadow_reg_cfg = (struct icnss_shadow_reg_cfg *)
 		config->shadow_reg_cfg;
+	cfg.num_shadow_reg_v2_cfg = config->num_shadow_reg_v2_cfg;
+	cfg.shadow_reg_v2_cfg = (struct icnss_shadow_reg_v2_cfg *)
+		config->shadow_reg_v2_cfg;
+	cfg.rri_over_ddr_cfg_valid = config->rri_over_ddr_cfg_valid;
+	if (config->rri_over_ddr_cfg_valid) {
+		cfg.rri_over_ddr_cfg.base_addr_low =
+			 config->rri_over_ddr_cfg.base_addr_low;
+		cfg.rri_over_ddr_cfg.base_addr_high =
+			 config->rri_over_ddr_cfg.base_addr_high;
+	}
 
 	switch (mode) {
 	case PLD_FTM:
