12fd5e34fd45cc7071873bdcf8b815433ab393a0
Lincoln Tran <linctran@codeaurora.org>
2020-03-02 09:34:53 -0800

    qcacld-3.0: Add ini to pass xLNA bypass config to FW
    Add new ini CFG_SET_SAP_XLNA_BYPASS under fw_offload to allow host
    to pass config to FW. WMI_PDEV_PARAM_SET_SAP_XLNA_BYPASS makes the
    SAP xLNA bypass configureable.
    
    Change-Id: Ie501005f3035f14b3f23cdd260f401a6af81b798
    CRs-fixed: 2632088

--- fw_offload/core/inc/wlan_fw_offload_main.h
+++ fw_offload/core/inc/wlan_fw_offload_main.h
@@ -190,6 +190,7 @@ struct wlan_fwol_neighbor_report_cfg {
  * @enable_fw_log_type: Set the FW log type
  * @enable_fw_module_log_level: enable fw module log level
  * @enable_fw_module_log_level_num: enablefw module log level num
+ * @sap_xlna_bypass: bypass SAP xLNA
  * @is_rate_limit_enabled: Enable/disable RA rate limited
  * @tsf_gpio_pin: TSF GPIO Pin config
  * @tsf_irq_host_gpio_pin: TSF GPIO Pin config
@@ -225,6 +226,7 @@ struct wlan_fwol_cfg {
 	uint16_t enable_fw_log_type;
 	uint8_t enable_fw_module_log_level[FW_MODULE_LOG_LEVEL_STRING_LENGTH];
 	uint8_t enable_fw_module_log_level_num;
+	bool sap_xlna_bypass;
 #ifdef FEATURE_WLAN_RA_FILTERING
 	bool is_rate_limit_enabled;
 #endif
