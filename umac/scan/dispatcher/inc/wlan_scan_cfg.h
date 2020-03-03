c27a6095e940f49c8d6d69c74b695428b56c767a
Vivek <vchettri@codeaurora.org>
2020-03-03 15:18:38 +0530

    qcacmn: Provide valid range for gScanProbeRepeatTime INI
    The default value provided for the INI item
    gScanProbeRepeatTime for WIN is 50 which is
    beyond the specified INI range 0 and 30.
    
    Changing the max value to 50 to accommodate
    the default value used by the INI and avoid
    warning while parsing INI values.
    
    CRs-Fixed: 2633733
    Change-Id: I442d07de55813c930b77e7dfffcdebf5372c5c0e

--- umac/scan/dispatcher/inc/wlan_scan_cfg.h
+++ umac/scan/dispatcher/inc/wlan_scan_cfg.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2012-2019 The Linux Foundation. All rights reserved.
+ * Copyright (c) 2012-2020 The Linux Foundation. All rights reserved.
  *
  * Permission to use, copy, modify, and/or distribute this software for
  * any purpose with or without fee is hereby granted, provided that the
@@ -240,7 +240,7 @@ enum scan_mode_6ghz {
  */
 #define CFG_SCAN_PROBE_REPEAT_TIME CFG_INI_UINT(\
 			"gScanProbeRepeatTime",\
-			0, 30, PLATFORM_VALUE(20, 50),\
+			0, 50, PLATFORM_VALUE(20, 50),\
 			CFG_VALUE_OR_DEFAULT,\
 			"probe repeat time on each channel")
 
