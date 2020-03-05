93c03dad7f16638b3bc798082c574fb20083c085
Liangwei Dong <liangwei@codeaurora.org>
2020-03-05 17:34:05 +0800

    qcacld-3.0: Flush pmksa cache for SAP when SAP stop
    In certain case, the active SAP interface will be deleted by
    __wlan_hdd_del_virtual_intf by upper layer. In that case,
    we need to flush pmksa in SAP PMKID cache.
    
    Change-Id: I9a152df7a4c27aef82664f039fbca18259d6b63a
    CRs-Fixed: 2635424

--- core/hdd/src/wlan_hdd_main.c
+++ core/hdd/src/wlan_hdd_main.c
@@ -6338,6 +6338,10 @@ QDF_STATUS hdd_stop_adapter(struct hdd_context *hdd_ctx,
 		/* Diassociate with all the peers before stop ap post */
 		if (test_bit(SOFTAP_BSS_STARTED, &adapter->event_flags))
 			wlan_hdd_del_station(adapter);
+		status = wlan_hdd_flush_pmksa_cache(adapter);
+		if (QDF_IS_STATUS_ERROR(status))
+			hdd_debug("Cannot flush PMKIDCache");
+
 		sap_config = &adapter->session.ap.sap_config;
 		wlansap_reset_sap_config_add_ie(sap_config, eUPDATE_IE_ALL);
 
