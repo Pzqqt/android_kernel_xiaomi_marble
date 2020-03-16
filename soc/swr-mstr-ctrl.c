0b07cf669f6e3684fb9f76abe1f8001828cd2436
Vatsal Bucha <vbucha@codeaurora.org>
2020-03-16 23:54:29 +0530

    soc: swr-mstr: Add delay between fifo writes to avoid overflow/underflow
    Overflow/Underflow errors are observed sometimes while doing fifo write.
    Add delay between writes to resolve error.
    
    Change-Id: Id460e17753001a0dd52acd01a181d91132c36048
    Signed-off-by: Vatsal Bucha <vbucha@codeaurora.org>

--- soc/swr-mstr-ctrl.c
+++ soc/swr-mstr-ctrl.c
@@ -592,10 +592,10 @@ static int swr_master_bulk_write(struct swr_mstr_ctrl *swrm, u32 *reg_addr,
 		for (i = 0; i < length; i++) {
 		/* wait for FIFO WR command to complete to avoid overflow */
 		/*
-		 * Reduce sleep from 100us to 10us to meet KPIs
+		 * Reduce sleep from 100us to 50us to meet KPIs
 		 * This still meets the hardware spec
 		 */
-			usleep_range(10, 12);
+			usleep_range(50, 55);
 			swr_master_write(swrm, reg_addr[i], val[i]);
 		}
 		mutex_unlock(&swrm->iolock);
