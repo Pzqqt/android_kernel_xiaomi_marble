49dcd5d662afb78406ce8346c3fa5ebb01e220ea
Dundi Raviteja <dundi@codeaurora.org>
2020-03-31 16:50:54 +0530

    qcacld-3.0: Skip QOS NULL frames from monitor interface
    Skip QOS NULL frames from monitor interface in
    packet capture mode.
    
    Change-Id: I132df424c6537fa0cc6f0621affae54e22a7d531
    CRs-Fixed: 2644167

--- components/pkt_capture/core/inc/wlan_pkt_capture_main.h
+++ components/pkt_capture/core/inc/wlan_pkt_capture_main.h
@@ -185,4 +185,15 @@ uint32_t pkt_capture_drop_nbuf_list(qdf_nbuf_t buf_list);
  * Return: None
  */
 void pkt_capture_record_channel(struct wlan_objmgr_vdev *vdev);
+
+/**
+ * pkt_capture_mon() - Wrapper function to invoke mon cb
+ * @cb_ctx: packet capture callback context
+ * @msdu: packet
+ *
+ * Return: None
+ */
+void pkt_capture_mon(struct pkt_capture_cb_context *cb_ctx,
+		     qdf_nbuf_t msdu);
+
 #endif /* end of _WLAN_PKT_CAPTURE_MAIN_H_ */
