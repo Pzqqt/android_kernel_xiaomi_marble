6cddfae45a218e210abd1c2696eec2952c927ded
Bapiraju Alla <balla@codeaurora.org>
2020-04-14 20:07:00 +0530

    qcacld-3.0: Wait for sme_soc_set_dual_mac_config to get complete
    SAP turn on is getting stuck for longer time due to vdev is getting
    deleted logically before response for sme_soc_set_dual_mac_config comes.
    
    To resolve this, add wait in wlan_hdd_update_dbs_scan_and_fw_mode_config
    to confirm sme_soc_set_dual_mac_config completion.
    
    Change-Id: I9408713fcf828d24688ecc45290d8c90a8d54c22
    CRs-Fixed: 2663509

--- core/sme/src/common/sme_api.c
+++ core/sme/src/common/sme_api.c
@@ -1973,7 +1973,7 @@ static QDF_STATUS sme_process_dual_mac_config_resp(struct mac_context *mac,
 			sme_err("Callback failed-Dual mac config is NULL");
 		} else {
 			sme_debug("Calling HDD callback for Dual mac config");
-			callback(param->status,
+			callback(mac->psoc, param->status,
 				command->u.set_dual_mac_cmd.scan_config,
 				command->u.set_dual_mac_cmd.fw_mode_config);
 		}
