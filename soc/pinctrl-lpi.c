204e309fad75c8474a80a01408948de72b4def32
Aditya Bavanari <abavanar@codeaurora.org>
2020-04-16 11:16:12 +0530

    soc: pinctrl-lpi: Add 100msec before resetting hw votes
    Add 100msec delay before resetting hw vote counts in
    SSR up sequence in order to ensure AVS is up when
    votes are requested.
    
    Change-Id: I0f46c60555f6bf833b1cac76f86a54afdcb78ba0
    Signed-off-by: Aditya Bavanari <abavanar@codeaurora.org>

--- soc/pinctrl-lpi.c
+++ soc/pinctrl-lpi.c
@@ -15,6 +15,7 @@
 #include <linux/types.h>
 #include <linux/clk.h>
 #include <linux/bitops.h>
+#include <linux/delay.h>
 #include <soc/snd_event.h>
 #include <dsp/digital-cdc-rsc-mgr.h>
 #include <linux/pm_runtime.h>
@@ -489,6 +490,8 @@ static int lpi_notifier_service_cb(struct notifier_block *this,
 
 		/* Reset HW votes after SSR */
 		if (!lpi_dev_up) {
+			/* Add 100ms sleep to ensure AVS is up after SSR */
+			msleep(100);
 			if (state->lpass_core_hw_vote)
 				digital_cdc_rsc_mgr_hw_vote_reset(
 					state->lpass_core_hw_vote);
