b8396604b4a9a8fd33bfa434c856f603455188a0
Basamma Yakkanahalli <ybasamma@codeaurora.org>
2020-05-05 12:44:30 +0530

    qcacmn: Reduce number of CE to 6 for IPQ5018
    Reduce the number of CEs used in IPQ5018 to 6 since it has only
    one mac and requires only 6 CEs. CE7 and CE8 are reserved for FW
    and not needed to be configured from host.
    
    Change-Id: Ibe262c32d8b3234873391559d939bc198c658c60

--- hif/src/ce/ce_assignment.h
+++ hif/src/ce/ce_assignment.h
@@ -919,7 +919,7 @@ static struct CE_pipe_config target_ce_config_wlan_adrastea[] = {
 	{ /* CE11 */ 11, PIPEDIR_IN,  32, 2048, CE_ATTR_FLAGS, 0,},
 };
 
-#define QCA_5018_CE_COUNT 9
+#define QCA_5018_CE_COUNT 6
 static struct CE_attr host_ce_config_wlan_qca5018[] = {
 	/* host->target HTC control and raw streams */
 	{/*CE0*/ (CE_ATTR_FLAGS), 0, 16, 2048, 0, NULL,},
