e2170a189419cb6007615ca3dd45b58744f219ee
Vignesh Kulothungan <vigneshk@codeaurora.org>
2020-05-06 11:40:11 -0700

    ASoC: create wsa883x codec entry in proc fs
    Create a WSA883x codec entry with variant and version info
    for each of available wsa883x devices in proc file system.
    
    Change-Id: I0656a947a735abc0a00aa1dcbfc57ce00f653e13
    Signed-off-by: Vignesh Kulothungan <vigneshk@codeaurora.org>

--- asoc/lahaina.c
+++ asoc/lahaina.c
@@ -7167,7 +7167,8 @@ static int msm_int_audrx_init(struct snd_soc_pcm_runtime *rtd)
 			snd_soc_dapm_ignore_suspend(dapm, "spkrLeft SPKR");
 		}
 
-		/*TODO: create codec entry for wsa1 */
+		wsa883x_codec_info_create_codec_entry(pdata->codec_root,
+							component);
 	}
 
         /* If current platform has more than one WSA */
@@ -7189,7 +7190,8 @@ static int msm_int_audrx_init(struct snd_soc_pcm_runtime *rtd)
 			snd_soc_dapm_ignore_suspend(dapm, "spkrRight SPKR");
 		}
 
-		/*TODO: create codec entry for wsa2 */
+		wsa883x_codec_info_create_codec_entry(pdata->codec_root,
+							component);
 	}
 
 	component = snd_soc_rtdcom_lookup(rtd, "bolero_codec");
