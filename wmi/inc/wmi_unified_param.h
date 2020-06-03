374d2eee382a7c32346f357e0085cbaca561e071
Abhishek Ambure <aambure@codeaurora.org>
2020-06-03 15:35:29 +0530

    qcacmn: Add support to send max ndi host supports to fw
    Host configures max ndi interfaces support to firmware using
    WMI_INIT_CMD. In resource config of WMI_INIT_CMD, host updates
    max number of ndi interfaces support and sends it to firmware.
    
    Change-Id: I44ccbe726663bc18d3c69b4aae0b0e3e9c1501ee
    CRs-Fixed: 2701559

--- wmi/inc/wmi_unified_param.h
+++ wmi/inc/wmi_unified_param.h
@@ -5367,6 +5367,7 @@ struct wmi_host_fw_abi_ver {
  * @ema_max_vap_cnt: Number of maximum EMA tx-vaps at any instance of time
  * @ema_max_profile_period: Maximum EMA profile periodicity on any pdev
  * @max_ndp_sessions: Max ndp sessions support
+ * @max_ndi: max number of ndi host supports
  */
 typedef struct {
 	uint32_t num_vdevs;
@@ -5469,6 +5470,7 @@ typedef struct {
 	uint32_t ema_max_vap_cnt;
 	uint32_t ema_max_profile_period;
 	uint32_t max_ndp_sessions;
+	uint32_t max_ndi;
 } target_resource_config;
 
 /**
--- wmi/src/wmi_unified_tlv.c
+++ wmi/src/wmi_unified_tlv.c
@@ -6928,6 +6928,7 @@ void wmi_copy_resource_config(wmi_resource_config *resource_cfg,
 	if (tgt_res_cfg->max_ndp_sessions)
 		resource_cfg->max_ndp_sessions =
 				tgt_res_cfg->max_ndp_sessions;
+	resource_cfg->max_ndi_interfaces = tgt_res_cfg->max_ndi;
 
 	if (tgt_res_cfg->atf_config)
 		WMI_RSRC_CFG_FLAG_ATF_CONFIG_ENABLE_SET(resource_cfg->flag1, 1);
