fd40e399fb489966d04dc44db7933731b2405456
Wu Gao <wugao@codeaurora.org>
2020-06-23 19:49:01 +0800

    qcacmn: Add WIFI_POWER_EVENT_WAKELOCK_TDLS
    Add wow support for TDLS, so add WIFI_POWER_EVENT_WAKELOCK_TDLS.
    
    Change-Id: I0a7e4075f4fadde631559555f45696511757b819
    CRs-Fixed: 2716903

--- utils/host_diag_log/inc/host_diag_core_event.h
+++ utils/host_diag_log/inc/host_diag_core_event.h
@@ -899,6 +899,7 @@ enum wifi_connectivity_events {
  * @WIFI_POWER_EVENT_WAKELOCK_MONITOR_MODE: Montitor mode wakelock
  * @WIFI_POWER_EVENT_WAKELOCK_DRIVER_IDLE_RESTART: Wakelock for Idle Restart
  * @WIFI_POWER_EVENT_WAKELOCK_DRIVER_IDLE_SHUTDOWN: Wakelock for Idle Shutdown
+ * @WIFI_POWER_EVENT_WAKELOCK_TDLS: Wakelock for TDLS
  *
  * Indicates the reason for which the wakelock was taken/released
  */
@@ -927,6 +928,7 @@ enum wake_lock_reason {
 	WIFI_POWER_EVENT_WAKELOCK_MONITOR_MODE,
 	WIFI_POWER_EVENT_WAKELOCK_DRIVER_IDLE_RESTART,
 	WIFI_POWER_EVENT_WAKELOCK_DRIVER_IDLE_SHUTDOWN,
+	WIFI_POWER_EVENT_WAKELOCK_TDLS,
 };
 
 /* The length of interface name should >= IFNAMSIZ */
