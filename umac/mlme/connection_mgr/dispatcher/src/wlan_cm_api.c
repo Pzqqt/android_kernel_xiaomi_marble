64476bcd6c86d978f9f9d81981846de90768c079
Ashish Kumar Dhanotiya <adhanoti@codeaurora.org>
2020-07-01 15:03:04 +0530

    qcacmn: Add connect infra for connection manager
    Add connect functions to convert kernel specific connect
    request to connection manager specific connection request
    for connection manager infrastructure.
    
    Along with the above functions, add the file structure for
    connect, disconnect and roam functions for connection
    manager infrastructure.
    
    Change-Id: Id4e389d06cea6e39327faef0c119275741574785
    CRs-Fixed: 2739318

---
+++ umac/mlme/connection_mgr/dispatcher/src/wlan_cm_api.c
@@ -0,0 +1,36 @@
+/*
+ * Copyright (c) 2012-2015, 2020, The Linux Foundation. All rights reserved.
+ *
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
+/**
+ * DOC: wlan_cm_api.c
+ *
+ * This file maintains definitaions public apis.
+ */
+
+#include <wlan_cm_api.h>
+
+QDF_STATUS wlan_cm_start_connect(struct wlan_objmgr_vdev *vdev,
+				 struct wlan_cm_connect_req *req)
+{
+	return QDF_STATUS_SUCCESS;
+}
+
+QDF_STATUS wlan_cm_start_disconnect(struct wlan_objmgr_vdev *vdev,
+				    struct wlan_cm_disconnect_req *req)
+{
+	return QDF_STATUS_SUCCESS;
+}
+
