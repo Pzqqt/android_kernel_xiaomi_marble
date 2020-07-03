c4d6b4d9c509d16ce2fe49bf0ca1fbcf1474d7ae
Surya Prakash Raajen <sraajen@codeaurora.org>
2020-07-03 13:04:54 +0530

    qcacmn: Move support_11ax macro to buildcaps
    Move support_11ax macro to buildcaps to remove dependency
    on legacy FW headers and add it on Buildcaps to fix
    redefine error.
    
    CRs-Fixed: 2719782
    Change-Id: Ic2902663083cda10e0a9e9b2da85ee1b267b6b7c

--- dp/cmn_dp_api/dp_ratetable.h
+++ dp/cmn_dp_api/dp_ratetable.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2016-2019 The Linux Foundation. All rights reserved.
+ * Copyright (c) 2016-2020 The Linux Foundation. All rights reserved.
  *
  * Permission to use, copy, modify, and/or distribute this software for
  * any purpose with or without fee is hereby granted, provided that the
@@ -34,7 +34,6 @@ enum CMN_BW_TYPES {
 };
 
 #define NUM_SPATIAL_STREAMS 8
-#define SUPPORT_11AX 1
 #define MAX_SPATIAL_STREAMS_SUPPORTED_AT_160MHZ 4
 #define VHT_EXTRA_MCS_SUPPORT
 #define CONFIG_160MHZ_SUPPORT 1
