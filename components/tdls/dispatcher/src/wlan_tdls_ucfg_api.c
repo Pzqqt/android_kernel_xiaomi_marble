9f6679e0005affdaf64233fb09940042c1e918c8
Pankaj Singh <pansin@codeaurora.org>
2020-07-10 08:07:29 +0530

    qcacld-3.0: TDLS not releasing the vdev ref taken with WLAN_TDLS_NB_ID
    During NAN+TDLS case, when NAN is present dut is trying
    to add TDLS peer and the ref is not released which results in
    assert due to obj delete timeout.
    
    Fix is to release the ref taken properly in default handing of
    tdls serialization cmd and error handling.
    
    Change-ID: Ie3e785f617d320dc88051bf6f22f8e291f120c14
    CRs-Fixed: 2728865

--- components/tdls/dispatcher/src/wlan_tdls_ucfg_api.c
+++ components/tdls/dispatcher/src/wlan_tdls_ucfg_api.c
@@ -858,7 +858,7 @@ QDF_STATUS ucfg_tdls_notify_sta_disconnect(
 
 	notify = qdf_mem_malloc(sizeof(*notify));
 	if (!notify) {
-		wlan_objmgr_vdev_release_ref(notify->vdev, WLAN_TDLS_NB_ID);
+		wlan_objmgr_vdev_release_ref(notify_info->vdev, WLAN_TDLS_NB_ID);
 		return QDF_STATUS_E_NULL_VALUE;
 	}
 
