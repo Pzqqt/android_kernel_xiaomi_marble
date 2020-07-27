8c59f730e65417156358798f2ad083cffaf4985f
gaurank kathpalia <gkathpal@codeaurora.org>
2020-07-27 22:16:17 +0530

    qcacld-3.0: Enhance BLM infra
    Enhance BLM infra to include source,
    reason for blacklist, original timeout,
    received time for better debug.
    
    Change-Id: Ia705a967a08412824063275f5073c338dabde539
    CRs-Fixed: 2744402

--- blacklist_mgr/dispatcher/inc/wlan_blm_public_struct.h
+++ blacklist_mgr/dispatcher/inc/wlan_blm_public_struct.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2019 The Linux Foundation. All rights reserved.
+ * Copyright (c) 2019-2020 The Linux Foundation. All rights reserved.
  *
  * Permission to use, copy, modify, and/or distribute this software for
  * any purpose with or without fee is hereby granted, provided that the
@@ -28,14 +28,31 @@
 #define MAX_BAD_AP_LIST_SIZE               28
 #define MAX_RSSI_AVOID_BSSID_LIST    10
 #define PDEV_MAX_NUM_BSSID_DISALLOW_LIST  28
+
+/**
+ * enum blm_reject_ap_reason - Rejection reason for adding BSSID to BLM
+ * @ADDED_BY_DRIVER: Source adding this BSSID is driver
+ * @ADDED_BY_TARGET: Source adding this BSSID is target
+ */
+enum blm_reject_ap_source {
+	ADDED_BY_DRIVER = 1,
+	ADDED_BY_TARGET,
+};
+
 /**
  * struct blm_rssi_disallow_params - structure to specify params for RSSI reject
  * @retry_delay: Time before which the AP doesn't expect a connection.
  * @expected_rssi: RSSI less than which only the STA should try association.
+ * @received_time: Time at which the AP was added to blacklist.
+ * @original_timeout: Original timeout which the AP sent while blacklisting.
+ * @source: Source of adding this BSSID to RSSI reject list
  */
 struct blm_rssi_disallow_params {
 	uint32_t retry_delay;
 	int8_t expected_rssi;
+	qdf_time_t received_time;
+	uint32_t original_timeout;
+	enum blm_reject_ap_source source;
 };
 
 /**
@@ -57,6 +74,38 @@ enum blm_reject_ap_type {
 };
 
 /**
+ * enum blm_reject_ap_reason - Rejection reason for adding BSSID to BLM
+ * @REASON_UNKNOWN: Unknown reason
+ * @REASON_NUD_FAILURE: NUD failure happened with this BSSID
+ * @REASON_STA_KICKOUT: STA kickout happened with this BSSID
+ * @REASON_ROAM_HO_FAILURE: HO failure happenend with this BSSID
+ * @REASON_ASSOC_REJECT_POOR_RSSI: assoc rsp with reason 71 received from AP.
+ * @REASON_ASSOC_REJECT_OCE: OCE assoc reject received from the AP.
+ * @REASON_USERSPACE_BL: Userspace wants to blacklist this AP.
+ * @REASON_USERSPACE_AVOID_LIST: Userspace wants to avoid this AP.
+ * @REASON_BTM_DISASSOC_IMMINENT: BTM IE received with disassoc imminent set.
+ * @REASON_BTM_BSS_TERMINATION: BTM IE received with BSS termination set.
+ * @REASON_BTM_MBO_RETRY: BTM IE received from AP with MBO retry set.
+ * @REASON_REASSOC_RSSI_REJECT: Re-Assoc resp received with reason code 34
+ * @REASON_REASSOC_NO_MORE_STAS: Re-assoc reject received with reason code 17
+ */
+enum blm_reject_ap_reason {
+	REASON_UNKNOWN = 0,
+	REASON_NUD_FAILURE,
+	REASON_STA_KICKOUT,
+	REASON_ROAM_HO_FAILURE,
+	REASON_ASSOC_REJECT_POOR_RSSI,
+	REASON_ASSOC_REJECT_OCE,
+	REASON_USERSPACE_BL,
+	REASON_USERSPACE_AVOID_LIST,
+	REASON_BTM_DISASSOC_IMMINENT,
+	REASON_BTM_BSS_TERMINATION,
+	REASON_BTM_MBO_RETRY,
+	REASON_REASSOC_RSSI_REJECT,
+	REASON_REASSOC_NO_MORE_STAS,
+};
+
+/**
  * enum blm_connection_state - State with AP (Connected, Disconnected)
  * @BLM_AP_CONNECTED: Connected with the AP
  * @BLM_AP_DISCONNECTED: Disconnected with the AP
@@ -72,12 +121,20 @@ enum blm_connection_state {
  * @reject_ap_type: Type of the rejection done with the BSSID
  * @reject_duration: time left till the AP is in the reject list.
  * @expected_rssi: expected RSSI when the AP expects the connection to be made.
+ * @reject_reason: reason to add the BSSID to BLM
+ * @source: Source of adding the BSSID to BLM
+ * @received_time: Time at which the AP was added to blacklist.
+ * @original_timeout: Original timeout which the AP sent while blacklisting.
  */
 struct reject_ap_config_params {
 	struct qdf_mac_addr bssid;
 	enum blm_reject_ap_type reject_ap_type;
 	uint32_t reject_duration;
 	int32_t expected_rssi;
+	enum blm_reject_ap_reason reject_reason;
+	enum blm_reject_ap_source source;
+	qdf_time_t received_time;
+	uint32_t original_timeout;
 };
 
 /**
@@ -105,11 +162,15 @@ struct wlan_blm_tx_ops {
  * @bssid: BSSID of the AP.
  * @rssi_reject_params: RSSI reject params of the AP is of type RSSI reject
  * @reject_ap_type: Reject type of AP (eg. avoid, blacklist, rssi reject etc.)
+ * @reject_reason: reason to add the BSSID to BLM
+ * @source: Source of adding the BSSID to BLM
  */
 struct reject_ap_info {
 	struct qdf_mac_addr bssid;
 	struct blm_rssi_disallow_params rssi_reject_params;
 	enum blm_reject_ap_type reject_ap_type;
+	enum blm_reject_ap_reason reject_reason;
+	enum blm_reject_ap_source source;
 };
 
 #endif
