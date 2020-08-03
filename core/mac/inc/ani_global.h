ac71ef1d266ebe83cacd8b3510e2dc7707a34571
Lincoln Tran <linctran@codeaurora.org>
2020-08-03 15:41:06 -0700

    qcacld-3.0: Update parameter of stop_roaming_callback
    The last parameter of stop_roaming_callback is currently an
    unsigned int, which does not match the function that is assigned
    to this pointer, which has enum wlan_cm_rso_control_requestor.
    Update the function pointer to have the correct parameter.
    
    Change-Id: I2761293e6d87e9fcf9586d198a6e60f25ca2b51e
    CRs-fixed: 2745188

--- core/mac/inc/ani_global.h
+++ core/mac/inc/ani_global.h
@@ -646,7 +646,7 @@ typedef struct sAniSirLim {
 		(struct mac_context *mac, struct scheduler_msg *msg);
 	QDF_STATUS(*stop_roaming_callback)
 		(mac_handle_t mac, uint8_t session_id, uint8_t reason,
-		 uint32_t requestor);
+		 enum wlan_cm_rso_control_requestor requestor);
 	uint8_t retry_packet_cnt;
 	uint8_t beacon_probe_rsp_cnt_per_scan;
 	wlan_scan_requester req_id;
