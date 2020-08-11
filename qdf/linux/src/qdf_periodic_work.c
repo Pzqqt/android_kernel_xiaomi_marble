9949f46e0474d1efcc7ad5fc447a44e7348b1955
Alok Kumar <alokkuma@codeaurora.org>
2020-08-11 18:08:25 +0530

    qcacmn: Use DEFERRABLE_WORK instead of DELAYED_WORK for BUS BW
    For static display case, BUS BW work periodically wakes up the
    APPS because bus bw work is created as DELAYED_WORK.
    This has the power impact in case of static display.
    
    Use DEFERRABLE_WORK instead of DELAYED_WORK for bus bw work.
    DEFERRABLE_WORK won't wake up the APPS periodically in static
    display case.
    
    Change-Id: Id812907cc6962ab7a9bb2ce8a7417ca0546363ab
    CRs-Fixed: 2748373

--- qdf/linux/src/qdf_periodic_work.c
+++ qdf/linux/src/qdf_periodic_work.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2019 The Linux Foundation. All rights reserved.
+ * Copyright (c) 2019-2020 The Linux Foundation. All rights reserved.
  *
  * Permission to use, copy, modify, and/or distribute this software for
  * any purpose with or without fee is hereby granted, provided that the
@@ -97,7 +97,7 @@ QDF_STATUS __qdf_periodic_work_create(struct qdf_periodic_work *pwork,
 	if (QDF_IS_STATUS_ERROR(status))
 		return status;
 
-	INIT_DELAYED_WORK(&pwork->dwork, __qdf_periodic_work_handler);
+	INIT_DEFERRABLE_WORK(&pwork->dwork, __qdf_periodic_work_handler);
 	pwork->callback = callback;
 	pwork->context = context;
 	pwork->msec = 0;
