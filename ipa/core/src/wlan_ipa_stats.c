7108c9cc7c77565a7b790cf6e5e9d3b8c0a690e0
Vevek Venkatesan <vevekv@codeaurora.org>
2020-08-12 19:16:28 +0530

    qcacld-3.0: fix IPA stats set_quota failure
    Fixing the failure of set_quota feature for IPA stats,
    currently calling set_quota during resource_loading, but
    it is supposed to be called if resource_loading is false.
    This is causing IPA stats set_quota feature is getting
    failed for Adrestea targets and fixing the same.
    
    Change-Id: I95ad60a8e15f4faaea34093785422ad494ee1fb4
    CRs-Fixed: 2730655

--- ipa/core/src/wlan_ipa_stats.c
+++ ipa/core/src/wlan_ipa_stats.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2013-2019 The Linux Foundation. All rights reserved.
+ * Copyright (c) 2013-2020 The Linux Foundation. All rights reserved.
  *
  * Permission to use, copy, modify, and/or distribute this software for
  * any purpose with or without fee is hereby granted, provided that the
@@ -853,9 +853,9 @@ static void wlan_ipa_uc_set_quota(struct wlan_ipa_priv *ipa_ctx,
 	qdf_mutex_acquire(&ipa_ctx->ipa_lock);
 	if (false == ipa_ctx->resource_loading) {
 		qdf_mutex_release(&ipa_ctx->ipa_lock);
-	} else {
 		cdp_ipa_uc_set_quota(ipa_ctx->dp_soc, ipa_ctx->dp_pdev_id,
 				     quota_bytes);
+	} else {
 		qdf_mutex_release(&ipa_ctx->ipa_lock);
 	}
 }
