697ceba7e1a4b291698e91aba156878477a6f44a
Edayilliam Jayadev <ejayadev@codeaurora.org>
2020-08-12 17:25:21 +0530

    qcacmn: Add pdev flags related to scan radio
    Add pdev flags to indicate
    
    1) scan radio support
    2) DFS disable, This flag is valid only for a scan radio pdev.
    
    CRs-Fixed: 2753799
    Change-Id: I2e875b0233bd1c5bf5421821c35170f3412a582a

--- umac/cmn_services/obj_mgr/inc/wlan_objmgr_pdev_obj.h
+++ umac/cmn_services/obj_mgr/inc/wlan_objmgr_pdev_obj.h
@@ -97,6 +97,10 @@
 #define WLAN_PDEV_FEXT_CFR_EN               0x00000001
 /* EMA AP support enable */
 #define WLAN_PDEV_FEXT_EMA_AP_ENABLE        0x00000002
+/* scan radio support */
+#define WLAN_PDEV_FEXT_SCAN_RADIO           0x00000004
+/* DFS disable, valid only for scan radio supported pdevs */
+#define WLAN_PDEV_FEXT_SCAN_RADIO_DFS_DIS   0x00000008
 
 /* PDEV op flags */
    /* Enable htrate for wep and tkip */
