b23bb8b6f22c20b8226a632697680f9d3bade2a0
Rajeev Kumar <rajekuma@codeaurora.org>
2020-08-13 10:37:21 -0700

    qcacmn: Increase spin lock bh timeout threshold in defconfig builds
    In defconfig build(QDF_LOCK_STATS = 1 for slub debug enabled) increase
    spin lock bh max time allowed to 2 seconds to accommodate for cache thrashing
    and slow execution because of many debug features getting enabled.
    
    Change-Id: I817d63a264079d25fed8a7ba1dbd9db5391abcdd
    CRs-Fixed: 2754993

--- qdf/inc/qdf_lock.h
+++ qdf/inc/qdf_lock.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2014-2019 The Linux Foundation. All rights reserved.
+ * Copyright (c) 2014-2020 The Linux Foundation. All rights reserved.
  *
  * Permission to use, copy, modify, and/or distribute this software for
  * any purpose with or without fee is hereby granted, provided that the
@@ -44,8 +44,13 @@
 
 /* Max hold time in micro seconds, 0 to disable detection*/
 #define QDF_MAX_HOLD_TIME_ALOWED_SPINLOCK_IRQ         10000
-#define QDF_MAX_HOLD_TIME_ALOWED_SPINLOCK_BH        1000000
 #define QDF_MAX_HOLD_TIME_ALOWED_SPINLOCK                 0
+
+#if QDF_LOCK_STATS
+#define QDF_MAX_HOLD_TIME_ALOWED_SPINLOCK_BH        2000000
+#else
+#define QDF_MAX_HOLD_TIME_ALOWED_SPINLOCK_BH        1000000
+#endif
 
 #if !QDF_LOCK_STATS
 struct lock_stats {};
