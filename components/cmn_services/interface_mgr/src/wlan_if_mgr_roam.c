60352aed6261d66a2224b65ef6b071040f332421
Sandeep Puligilla <spuligil@codeaurora.org>
2020-08-14 02:23:03 -0700

    qcacld-3.0: Add interface manager module
    - Add interface manager module files
    - Add start_connect event processing
    - Add stubs for start_connect_complete and start_bss
    event processing API's
    
    Change-Id: I24756336c6e6c9d7f3276e4039c0b7236233d45e
    CRs-Fixed: 2759275

---
+++ components/cmn_services/interface_mgr/src/wlan_if_mgr_roam.c
@@ -0,0 +1,30 @@
+/*
+ * Copyright (c) 2020, The Linux Foundation. All rights reserved.
+ *
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
+/*
+ * DOC: contains interface manager roam public api
+ */
+#include "wlan_objmgr_psoc_obj.h"
+#include "wlan_objmgr_pdev_obj.h"
+#include "wlan_objmgr_vdev_obj.h"
+#include "wlan_policy_mgr_api.h"
+#include "wlan_if_mgr_roam.h"
+
+QDF_STATUS if_mgr_enable_roaming(struct wlan_objmgr_vdev *vdev,
+				struct wlan_objmgr_pdev *pdev)
+{
+	return QDF_STATUS_SUCCESS;
+}
