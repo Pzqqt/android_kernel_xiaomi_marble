2717b7abb18d4eb9bdbd104618fb40be5d242f36
Amruta Kulkarni <amrukulk@codeaurora.org>
2020-08-20 18:42:21 -0700

    qcacld-3.0: Retry sending join_req after timer expiry
    If join fail reason is timeout, retry sending join req
    after timer expiry, timer value can be configured
    via join_failure_retry_interval.
    
    Change-Id: If90071b3f6d7f48936db6f7088c3c1547e48b294
    CRs-Fixed: 2760236

--- mlme/core/src/wlan_mlme_main.c
+++ mlme/core/src/wlan_mlme_main.c
@@ -354,6 +354,8 @@ static void mlme_init_generic_cfg(struct wlan_objmgr_psoc *psoc,
 		cfg_get(psoc, CFG_DFS_CHAN_AGEOUT_TIME);
 	gen->sae_connect_retries =
 		cfg_get(psoc, CFG_SAE_CONNECION_RETRIES);
+	gen->join_failure_retry_interval =
+		cfg_get(psoc, CFG_JOIN_FAILURE_RETRY_INTERVAL);
 }
 
 static void mlme_init_edca_ani_cfg(struct wlan_mlme_edca_params *edca_params)
