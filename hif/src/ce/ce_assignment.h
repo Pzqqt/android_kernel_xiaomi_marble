095453ad08353841824c62ba257c10b1e55d6c96
Nisha Menon <nmenon@codeaurora.org>
2020-08-25 15:54:02 -0700

    qcacmn: Increase num of entries in CE2 dest and status rings
    Increase the num of entries in host side from 32 to 64
    so FW can post more descriptors to host src ring
    without hitting ring full condition. This is to avoid
    back pressure issues in FW. The memory increase is
    by 32*4K allocations.
    This change is applicable to all HSP targets.
    
    Change-Id: Ia973e13b9d9b2b682957ae19fc3f23dd49bef401
    CRs-Fixed: 2761953

--- hif/src/ce/ce_assignment.h
+++ hif/src/ce/ce_assignment.h
@@ -1262,7 +1262,7 @@ static struct CE_attr host_ce_config_wlan_qca6490[] = {
 	/* target->host HTT + HTC control */
 	{ /* CE1 */ CE_ATTR_FLAGS, 0, 0,  2048, 512, NULL,},
 	/* target->host WMI */
-	{ /* CE2 */ CE_ATTR_FLAGS, 0, 0,  4096, 32, NULL,},
+	{ /* CE2 */ CE_ATTR_FLAGS, 0, 0,  4096, 64, NULL,},
 	/* host->target WMI */
 	{ /* CE3 */ CE_ATTR_FLAGS, 0, 32, 2048, 0, NULL,},
 	/* host->target HTT */
