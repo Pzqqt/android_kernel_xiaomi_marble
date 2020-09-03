b34fcc2b42a1e1ac55b81f4a317fea1da80bc01b
Abhinav Kumar <abhikuma@codeaurora.org>
2020-09-03 17:23:24 +0530

    qcacld-3.0: Enable SU Bformee in vendor VHTIE by default
    Set default value of INI enable_subfee_vendor_vhtie to 1
    to enable SU Bformee in vendor VHTIE by default.
    
    Change-Id: I8a916495dd5c6ddfe78840dc6f35976fbdbbf8ba
    CRs-Fixed: 2769257

--- mlme/dispatcher/inc/cfg_mlme_vht_caps.h
+++ mlme/dispatcher/inc/cfg_mlme_vht_caps.h
@@ -594,7 +594,7 @@
  */
 #define CFG_ENABLE_SUBFEE_IN_VENDOR_VHTIE CFG_INI_BOOL( \
 			"enable_subfee_vendor_vhtie", \
-			0, \
+			1, \
 			"Enable subfee in vendor vht ie")
 
 #define CFG_VHT_CAPS_ALL \
