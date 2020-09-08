1b97e38bec1af0a9a9639198c86b9226c8303282
Jyoti Kumari <jyotkuma@codeaurora.org>
2020-09-08 13:54:21 +0530

    qcacld-3.0: Change default value of TDLS parameter
    Change default value of gEnableTDLSSupport, gEnableTDLSScan
    and gEnableTDLSImplicitTrigger 1
    
    Change-Id: Ic6559a5f77b1d46bc48647c283327a12c409d873
    CRs-Fixed: 2783560

--- components/tdls/dispatcher/inc/wlan_tdls_cfg.h
+++ components/tdls/dispatcher/inc/wlan_tdls_cfg.h
@@ -150,7 +150,7 @@
  * gEnableTDLSSupport - Enable support for TDLS.
  * @Min: 0
  * @Max: 1
- * @Default: 0
+ * @Default: 1
  *
  * This ini is used to enable/disable TDLS support.
  *
@@ -164,7 +164,7 @@
  */
 #define CFG_TDLS_SUPPORT_ENABLE CFG_INI_BOOL( \
 	"gEnableTDLSSupport", \
-	0, \
+	1, \
 	"enable/disable TDLS support")
 
 /*
@@ -172,7 +172,7 @@
  * gEnableTDLSImplicitTrigger - Enable Implicit TDLS.
  * @Min: 0
  * @Max: 1
- * @Default: 0
+ * @Default: 1
  *
  * This ini is used to enable/disable implicit TDLS.
  * CLD driver initiates TDLS Discovery towards a peer whenever TDLS Setup
@@ -189,7 +189,7 @@
  */
 #define CFG_TDLS_IMPLICIT_TRIGGER CFG_INI_BOOL( \
 	"gEnableTDLSImplicitTrigger", \
-	0, \
+	1, \
 	"enable/disable implicit TDLS")
 
 /*
@@ -605,7 +605,7 @@
  * gEnableTDLSScan - Allow scan and maintain TDLS link.
  * @Min: 0
  * @Max: 1
- * @Default: 0
+ * @Default: 1
  *
  * This ini is used to enable/disable TDLS scan.
  *  0: If peer is not buffer STA capable and device is not sleep STA
@@ -628,7 +628,7 @@
  */
 #define CFG_TDLS_SCAN_ENABLE CFG_INI_BOOL( \
 	"gEnableTDLSScan", \
-	0, \
+	1, \
 	"Allow scan and maintain TDLS link")
 
 /*
