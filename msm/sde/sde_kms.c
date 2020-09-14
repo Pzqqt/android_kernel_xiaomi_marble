4ec0f2a9e81f00cdce4a99b0a8b58179b7162db6
Yashwanth <yvulapu@codeaurora.org>
2020-09-14 14:39:00 +0530

    disp: msm: sde: remove previous pm qos irq vote during affinity change
    In the current code during affinity change, irq cpu mask is
    being updated without clearing previous pm qos vote. Due to
    this, even after entering idle state, qos vote is still
    present on other cpus. This change clears previous pm qos
    vote before adding another.
    
    Change-Id: Ic0326a09d37d03d5a8b0444362a6f1492993040c
    Signed-off-by: Yashwanth <yvulapu@codeaurora.org>

--- msm/sde/sde_kms.c
+++ msm/sde/sde_kms.c
@@ -3823,6 +3823,7 @@ static void sde_kms_irq_affinity_notify(
 
 	mutex_lock(&priv->phandle.phandle_lock);
 
+	_sde_kms_remove_pm_qos_irq_request(sde_kms);
 	// save irq cpu mask
 	sde_kms->irq_cpu_mask = *mask;
 
