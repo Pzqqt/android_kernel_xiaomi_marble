6fa94ff38257a3560bf477733e747a134e9cd378
Samantha Tran <samtran@codeaurora.org>
2020-09-17 20:55:38 -0700

    disp: msm: rename drm_gem_object_put functions
    Commit b5d250744ccc ("drm/gem: fold drm_gem_object_put_unlocked
    and __drm_gem_object_put()"), commit eecd7fd8bf58 (drm/gem: add
    _locked suffix to drm_gem_object_put", commit be6ee102341b ("drm:
    remove _unlocked suffix in drm_object_put_unlocked") updates
    the drm_gem_object_put call APIs. This change updates the relevant
    changes to the msm driver.
    
    Change-Id: I006b51b5f3d1f8b2e88ed7b4c73b5a98afcaf455
    Signed-off-by: Samantha Tran <samtran@codeaurora.org>
    Signed-off-by: Nilaan Gunabalachandran <ngunabal@codeaurora.org>

--- msm/msm_gem.c
+++ msm/msm_gem.c
@@ -701,7 +701,7 @@ int msm_gem_dumb_map_offset(struct drm_file *file, struct drm_device *dev,
 
 	*offset = msm_gem_mmap_offset(obj);
 
-	drm_gem_object_put_unlocked(obj);
+	drm_gem_object_put(obj);
 
 fail:
 	return ret;
@@ -1067,7 +1067,7 @@ int msm_gem_new_handle(struct drm_device *dev, struct drm_file *file,
 	ret = drm_gem_handle_create(file, obj, handle);
 
 	/* drop reference from allocate - handle holds it now */
-	drm_gem_object_put_unlocked(obj);
+	drm_gem_object_put(obj);
 
 	return ret;
 }
@@ -1191,7 +1191,7 @@ static struct drm_gem_object *_msm_gem_new(struct drm_device *dev,
 	return obj;
 
 fail:
-	drm_gem_object_put_unlocked(obj);
+	drm_gem_object_put(obj);
 	return ERR_PTR(ret);
 }
 
@@ -1309,7 +1309,7 @@ struct drm_gem_object *msm_gem_import(struct drm_device *dev,
 	return obj;
 
 fail:
-	drm_gem_object_put_unlocked(obj);
+	drm_gem_object_put(obj);
 	return ERR_PTR(ret);
 }
 
@@ -1343,9 +1343,9 @@ static void *_msm_gem_kernel_new(struct drm_device *dev, uint32_t size,
 	return vaddr;
 err:
 	if (locked)
-		drm_gem_object_put(obj);
+		drm_gem_object_put_locked(obj);
 	else
-		drm_gem_object_put_unlocked(obj);
+		drm_gem_object_put(obj);
 
 	return ERR_PTR(ret);
 
@@ -1375,9 +1375,9 @@ void msm_gem_kernel_put(struct drm_gem_object *bo,
 	msm_gem_unpin_iova(bo, aspace);
 
 	if (locked)
-		drm_gem_object_put(bo);
+		drm_gem_object_put_locked(bo);
 	else
-		drm_gem_object_put_unlocked(bo);
+		drm_gem_object_put(bo);
 }
 
 void msm_gem_object_set_name(struct drm_gem_object *bo, const char *fmt, ...)
