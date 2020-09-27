11bb969e5daf1d53d48214148de1887e340dea43
Sankeerth Billakanti <sbillaka@codeaurora.org>
2020-09-27 14:55:47 +0530

    disp: msm: dp: reset disconnect flags on cable hotplug
    When a physical DP sink is connected after a simulation
    session, the attention events from the sink are ignored.
    The forced_disconnect flag prevents handling of attention
    events when disconnect is simulated or forced. This flag
    does not get reset when a physical sink is connected.
    Certain sinks which lose link integrity after link
    training sequence fail to display image.
    
    This change will reset the flag so that the attention
    events which occur after the cable hotplug can be processed.
    
    Change-Id: Ie551ead19ecff0e93da48acc2dbb1d761c55fa20
    Signed-off-by: Sankeerth Billakanti <sbillaka@codeaurora.org>

--- msm/dp/dp_usbpd.c
+++ msm/dp/dp_usbpd.c
@@ -403,6 +403,7 @@ static void dp_usbpd_response_cb(struct usbpd_svid_handler *hdlr, u8 cmd,
 	case DP_USBPD_VDM_CONFIGURE:
 		pd->alt_mode |= DP_USBPD_ALT_MODE_CONFIGURE;
 		pd->dp_usbpd.base.alt_mode_cfg_done = true;
+		pd->forced_disconnect = false;
 		dp_usbpd_get_status(pd);
 
 		pd->dp_usbpd.base.orientation =
