5d0d3f212495f62848830ca0b1d39d4be31f8274
snandini <snandini@codeaurora.org>
2020-10-01 14:40:44 -0700

    Release 5.2.0.204S
    Release 5.2.0.204S
    
    Change-Id: Ibd18b009ff2ce06bd644fd3a80d87a2d2a36c3ce
    CRs-Fixed: 774533

--- core/mac/inc/qwlan_version.h
+++ core/mac/inc/qwlan_version.h
@@ -32,9 +32,9 @@
 #define QWLAN_VERSION_MAJOR            5
 #define QWLAN_VERSION_MINOR            2
 #define QWLAN_VERSION_PATCH            0
-#define QWLAN_VERSION_EXTRA            "R"
+#define QWLAN_VERSION_EXTRA            "S"
 #define QWLAN_VERSION_BUILD            204
 
-#define QWLAN_VERSIONSTR               "5.2.0.204R"
+#define QWLAN_VERSIONSTR               "5.2.0.204S"
 
 #endif /* QWLAN_VERSION_H */
