cfdd72122195ccd5902970bc918d9d8b3e9d18e9
spuligil <spuligil@codeaurora.org>
2020-10-06 18:01:18 -0700

    fw-api: CL 11794580 - update fw common interface files
    Add WLAN_MODULE_SHO def
    
    Change-Id: Ia2d430a5f95bbbb91981297649da1bbc84d3bca2
    CRs-Fixed: 2262693

--- fw/wlan_module_ids.h
+++ fw/wlan_module_ids.h
@@ -117,6 +117,8 @@ typedef enum {
   WLAN_MODULE_SHMEM_MGR,                /* 0x53 */
   WLAN_MODULE_CFIR,                     /* 0x54 */ /* Channel Capture */
   WLAN_MODULE_CODE_COVER,               /* 0x55 */ /* code coverage */
+  WLAN_MODULE_SHO,                      /* 0x56 */ /* SAP HW offload */
+
 
   WLAN_MODULE_ID_MAX,
   WLAN_MODULE_ID_INVALID = WLAN_MODULE_ID_MAX,
