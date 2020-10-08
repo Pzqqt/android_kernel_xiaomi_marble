57c40f17c22e5552522b513c29e383214ea85aaa
Samantha Tran <samtran@codeaurora.org>
2020-10-08 13:19:23 -0700

    disp: msm: remove usage of DRM_DEBUG_DRIVER_RATELIMITED
    Commit 7ff6ea0fd384 ("drm/print: Delete a few unused
    shouting macros") removes the usage of
    DRM_DEBUG_DRIVER_RATELIMITED. This changes updates
    the relevant changes to the msm driver, printing through
    DRM_DEBUG instead.
    
    Change-Id: I910f0fe72d045e69dda966249437dcd004790f29
    Signed-off-by: Samantha Tran <samtran@codeaurora.org>
    Signed-off-by: Nilaan Gunabalachandran <ngunabal@codeaurora.org>

--- msm/msm_atomic.c
+++ msm/msm_atomic.c
@@ -614,7 +614,7 @@ static void msm_atomic_commit_dispatch(struct drm_device *dev,
 		if (ret == -EINVAL)
 			DRM_ERROR("failed to dispatch commit to any CRTC\n");
 		else
-			DRM_DEBUG_DRIVER_RATELIMITED("empty crtc state\n");
+			DRM_DEBUG("empty crtc state\n");
 
 		/**
 		 * this is not expected to happen, but at this point the state
