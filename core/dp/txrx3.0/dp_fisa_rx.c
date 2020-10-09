5b0832aae5c9ba583403f3d34e447fe8e342b743
Yeshwanth Sriram Guntuka <ysriramg@codeaurora.org>
2020-10-09 23:06:45 +0530

    qcacld-3.0: Skip fisa for multicast or broadcast pkts
    MC/BC packets are received with lro_eligible set to 1
    in rx_pkt_rlvs. MC/BC packets are routed to firmware
    and then re-injected back to REO. FW might modify
    the reo_destination_indication in the descriptor which
    can cause same flow packets to be received on different
    REO2SW rings.
    
    Fix is to skip fisa for multicast or broadcast packets.
    
    Change-Id: If2ab7e3cbdf7839cfd0a4f1e5bf9da4712debc9b
    CRs-Fixed: 2793877

--- core/dp/txrx3.0/dp_fisa_rx.c
+++ core/dp/txrx3.0/dp_fisa_rx.c
@@ -1599,7 +1599,8 @@ static bool dp_is_nbuf_bypass_fisa(qdf_nbuf_t nbuf)
 {
 	/* RX frame from non-regular path or DHCP packet */
 	if (qdf_nbuf_is_exc_frame(nbuf) ||
-	    qdf_nbuf_is_ipv4_dhcp_pkt(nbuf))
+	    qdf_nbuf_is_ipv4_dhcp_pkt(nbuf) ||
+	    qdf_nbuf_is_da_mcbc(nbuf))
 		return true;
 
 	return false;
