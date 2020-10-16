515ff34742abc0659574599923d2e3d1b9d08fbe
Wu Gao <wugao@codeaurora.org>
2020-10-16 11:39:08 +0800

    qcacld-3.0: Fix implicit-fallthrough compile error about connection mgr
    Compiling connection mgr by gcc 9.3 will cause many implicit-fallthrough
    compile errors.
    Fix is to add keyword fallthrough to resolve such compile error.
    
    Change-Id: I02fb3ff12d8cea81ce160b3aec1f590faf83a031
    CRs-Fixed: 2798881

--- components/umac/mlme/connection_mgr/core/src/wlan_cm_roam_offload.c
+++ components/umac/mlme/connection_mgr/core/src/wlan_cm_roam_offload.c
@@ -1152,6 +1152,7 @@ cm_roam_switch_to_roam_start(struct wlan_objmgr_pdev *pdev,
 					    WLAN_ROAMING_IN_PROG);
 			break;
 		}
+		/* fallthrough */
 	case WLAN_ROAM_INIT:
 	case WLAN_ROAM_DEINIT:
 	case WLAN_ROAM_SYNCH_IN_PROG:
@@ -1213,6 +1214,7 @@ cm_roam_switch_to_roam_sync(struct wlan_objmgr_pdev *pdev,
 		 * transition to WLAN_ROAM_SYNCH_IN_PROG not allowed otherwise
 		 * if we're already RSO stopped, fall through to return failure
 		 */
+		/* fallthrough */
 	case WLAN_ROAM_INIT:
 	case WLAN_ROAM_DEINIT:
 	case WLAN_ROAM_SYNCH_IN_PROG:
