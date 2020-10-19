047a93da86dce42c64b999837cf2639b52f925a1
Ashish Kumar Dhanotiya <adhanoti@codeaurora.org>
2020-10-20 00:25:16 +0530

    qcacmn: Use correct api to get the country info
    currently connection manager is using incorrect api to
    get the country info which gives compilation error for WIN.
    
    Use correct api to get the country to avoid any compilation
    issue.
    
    Change-Id: I647b171ad2ee4a2b70f6803869435074643aa677
    CRs-Fixed: 2801991

--- umac/mlme/connection_mgr/core/src/wlan_cm_connect.c
+++ umac/mlme/connection_mgr/core/src/wlan_cm_connect.c
@@ -1246,7 +1246,7 @@ cm_resume_connect_after_peer_create(struct cnx_mgr *cm_ctx, wlan_cm_id *cm_id)
 	req.scan_ie = cm_req->connect_req.req.scan_ie;
 	req.bss = cm_req->connect_req.cur_candidate;
 
-	wlan_reg_read_current_country(psoc, country_code);
+	wlan_reg_get_cc_and_src(psoc, country_code);
 	mlme_nofl_info(CM_PREFIX_FMT "Connecting to %.*s " QDF_MAC_ADDR_FMT " rssi: %d freq: %d akm 0x%x cipher: uc 0x%x mc 0x%x, CC: %c%c",
 		       CM_PREFIX_REF(req.vdev_id, req.cm_id),
 		       cm_req->connect_req.req.ssid.length,
