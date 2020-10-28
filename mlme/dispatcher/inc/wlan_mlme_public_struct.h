0138fa35da68b524a13f6804841ac94c4479bddd
gaurank kathpalia <gkathpal@codeaurora.org>
2020-10-28 16:02:29 +0530

    qcacld-3.0: Move the logic to get HT and VHT caps in LIM
    Move the logic to get HT and VHT caps in LIM from CSR also store
    VHT and HT caps in vdev mlme instead of csr session.
    
    Change-Id: I29b8e836a79a4a9eda5088c0eb2e0bf48d616de2
    CRs-Fixed: 2807209

--- mlme/dispatcher/inc/wlan_mlme_public_struct.h
+++ mlme/dispatcher/inc/wlan_mlme_public_struct.h
@@ -431,6 +431,18 @@ struct mlme_ht_capabilities_info {
 #endif
 
 /**
+ * struct wlan_ht_config - HT capabilities
+ * @ht_info: ht caps in bitwise
+ * @caps: uint32 caps
+ */
+struct wlan_ht_config {
+	union {
+		struct mlme_ht_capabilities_info ht_caps;
+		uint32_t caps;
+	};
+};
+
+/**
  * struct mlme_ht_param_info - HT AMPDU Parameters Info
  * @reserved: reserved bits
  * @mpdu_density: MPDU Density
@@ -900,6 +912,54 @@ struct mlme_vht_capabilities_info {
  */
 struct wlan_mlme_vht_caps {
 	struct mlme_vht_capabilities_info vht_cap_info;
+};
+
+/**
+ * struct wlan_vht_config - VHT capabilities
+ * @max_mpdu_len: MPDU length
+ * @supported_channel_widthset: channel width set
+ * @ldpc_coding: LDPC coding capability
+ * @shortgi80: short GI 80 support
+ * @shortgi160and80plus80: short Gi 160 & 80+80 support
+ * @tx_stbc; Tx STBC cap
+ * @tx_stbc: Rx STBC cap
+ * @su_beam_former: SU beam former cap
+ * @su_beam_formee: SU beam formee cap
+ * @csnof_beamformer_antSup: Antenna support for beamforming
+ * @num_soundingdim: Sound dimensions
+ * @mu_beam_former: MU beam former cap
+ * @mu_beam_formee: MU beam formee cap
+ * @vht_txops: TXOP power save
+ * @htc_vhtcap: HTC VHT capability
+ * @max_ampdu_lenexp: AMPDU length
+ * @vht_link_adapt: VHT link adapatation capable
+ */
+struct wlan_vht_config {
+	union {
+		struct {
+			uint32_t           max_mpdu_len:2;
+			uint32_t supported_channel_widthset:2;
+			uint32_t        ldpc_coding:1;
+			uint32_t         shortgi80:1;
+			uint32_t shortgi160and80plus80:1;
+			uint32_t               tx_stbc:1;
+			uint32_t               rx_stbc:3;
+			uint32_t      su_beam_former:1;
+			uint32_t      su_beam_formee:1;
+			uint32_t csnof_beamformer_antSup:3;
+			uint32_t       num_soundingdim:3;
+			uint32_t      mu_beam_former:1;
+			uint32_t      mu_beam_formee:1;
+			uint32_t            vht_txops:1;
+			uint32_t            htc_vhtcap:1;
+			uint32_t       max_ampdu_lenexp:3;
+			uint32_t        vht_link_adapt:2;
+			uint32_t         rx_antpattern:1;
+			uint32_t         tx_antpattern:1;
+			uint32_t  extended_nss_bw_supp:2;
+		};
+		uint32_t caps;
+	};
 };
 
 /**
