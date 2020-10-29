b5692dc64cc6369c8996c4486770b0fc394ec7f4
aloksing <aloksing@codeaurora.org>
2020-10-29 12:10:57 +0530

    qcacmn: Replace WMI_LOGD() with wmi_debug()
    Replace WMI_LOGD() with wmi_debug().
    
    Change-Id: Ia5f00e0b54de30e2251e3313b46badbde7a90b40
    CRs-Fixed: 2807905

--- wmi/src/wmi_unified_gpio_tlv.c
+++ wmi/src/wmi_unified_gpio_tlv.c
@@ -158,8 +158,8 @@ send_gpio_config_cmd_tlv(wmi_unified_t wmi_handle,
 	cmd->pull_type = convert_gpio_pull_type(param->pin_pull_type);
 	cmd->intr_mode = convert_gpio_interrupt_mode(param->pin_intr_mode);
 
-	WMI_LOGD("GPIO num %d, input-dir %d, pull_type %d, intr_mode %d",
-		 cmd->gpio_num, cmd->input, cmd->pull_type, cmd->intr_mode);
+	wmi_debug("GPIO num %d, input-dir %d, pull_type %d, intr_mode %d",
+		  cmd->gpio_num, cmd->input, cmd->pull_type, cmd->intr_mode);
 	wmi_mtrace(WMI_GPIO_CONFIG_CMDID, NO_SESSION, 0);
 	ret = wmi_unified_cmd_send(wmi_handle, buf, sizeof(*cmd),
 				   WMI_GPIO_CONFIG_CMDID);
@@ -208,7 +208,7 @@ send_gpio_output_cmd_tlv(wmi_unified_t wmi_handle,
 	cmd->gpio_num = param->pin_num;
 	cmd->set = convert_gpio_output_value(param->pin_set);
 
-	WMI_LOGD("GPIO num %d, set %d", cmd->gpio_num, cmd->set);
+	wmi_debug("GPIO num %d, set %d", cmd->gpio_num, cmd->set);
 	wmi_mtrace(WMI_GPIO_OUTPUT_CMDID, NO_SESSION, 0);
 	ret = wmi_unified_cmd_send(wmi_handle, buf, sizeof(*cmd),
 				   WMI_GPIO_OUTPUT_CMDID);
