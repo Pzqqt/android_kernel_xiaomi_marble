effa6ef7f004deea93cca900864c858c85815452
gaurank kathpalia <gkathpal@codeaurora.org>
2020-10-29 21:13:15 +0530

    qcacld-3.0: Filter out the 6ghz chan freq from valid freq list
    Currently the driver does not have a support to
    filter out the 6ghz frequencies from the valid freq
    list, and hence there is a high chance of selecting
    the 6ghz freq as an operating freq for SAP, which
    the legacy clients won't be able to scan.
    
    Fix is to add a support for filtering out the 6ghz
    frequencies from the valid freq list.
    
    Change-Id: I3c32b4c8c5b93eae04d49d2d5b6bd4ec9852c2a9
    CRs-Fixed: 2808350

--- core/sap/src/sap_fsm.c
+++ core/sap/src/sap_fsm.c
@@ -235,6 +235,16 @@ static qdf_freq_t sap_random_channel_sel(struct sap_context *sap_ctx)
 		 sap_operating_chan_preferred_location == 2)
 		flag |= DFS_RANDOM_CH_FLAG_NO_LOWER_5G_CH;
 
+	/*
+	 * Dont choose 6ghz channel currently as legacy clients wont be able to
+	 * scan them. In future create an ini if any customer wants 6ghz freq
+	 * to be prioritize over 5ghz/2.4ghz.
+	 * Currently for SAP there is a high chance of 6ghz being selected as
+	 * an op frequency as PCL will have only 5, 6ghz freq as preferred for
+	 * standalone SAP, and 6ghz channels being high in number.
+	 */
+	flag |= DFS_RANDOM_CH_FLAG_NO_6GHZ_CH;
+
 	if (QDF_IS_STATUS_ERROR(utils_dfs_get_vdev_random_channel_for_freq(
 					pdev, sap_ctx->vdev, flag, ch_params,
 					&hw_mode, &chan_freq, &acs_info))) {
