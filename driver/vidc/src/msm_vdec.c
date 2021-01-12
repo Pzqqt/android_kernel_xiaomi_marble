0750c2bb020b6546198955b60c9f7ef306baf744
Mihir Ganu <mganu@codeaurora.org>
2021-01-12 13:26:20 -0800

    video: driver: Skip subscription/delivery when not required
    Avoid sending subscribe or delivery comannds to firmware
    when no metadata is enabled by client.
    
    Change-Id: I379aa018b676ebc9b7e327757213371dbf089d53
    Signed-off-by: Mihir Ganu <mganu@codeaurora.org>

--- driver/vidc/src/msm_vdec.c
+++ driver/vidc/src/msm_vdec.c
@@ -1003,6 +1003,9 @@ static int msm_vdec_subscribe_metadata(struct msm_vidc_inst *inst,
 		}
 	};
 
+	if (!count)
+		return 0;
+
 	rc = venus_hfi_session_command(inst,
 			HFI_CMD_SUBSCRIBE_MODE,
 			port,
@@ -1041,6 +1044,9 @@ static int msm_vdec_set_delivery_mode_metadata(struct msm_vidc_inst *inst,
 			count++;
 		}
 	};
+
+	if (!count)
+		return 0;
 
 	rc = venus_hfi_session_command(inst,
 			HFI_CMD_DELIVERY_MODE,
