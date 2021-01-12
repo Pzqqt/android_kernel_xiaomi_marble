2864444839b974fd30dadd7065e2b17eac09aa9a
Ruben Columbus <rcolumbu@codeaurora.org>
2021-01-11 18:00:58 -0800

    qcacmn: copying over mcs for delayed frames
    mcs is copied over to already in used ppdu_desc
    this prevents ppdu_desc to increase lower mcs counts during stats
    process
    
    Change-Id: Iec68438a86359ec791302ecf2eeaec4fbd941632

--- dp/wifi3.0/dp_htt.c
+++ dp/wifi3.0/dp_htt.c
@@ -153,6 +153,7 @@ dp_peer_copy_delay_stats(struct dp_peer *peer,
 
 	peer->delayed_ba_ppdu_stats.user_pos = ppdu->user_pos;
 	peer->delayed_ba_ppdu_stats.mu_group_id = ppdu->mu_group_id;
+	peer->delayed_ba_ppdu_stats.mcs = ppdu->mcs;
 
 	peer->last_delayed_ba = true;
 
@@ -199,6 +200,7 @@ dp_peer_copy_stats_to_bar(struct dp_peer *peer,
 
 	ppdu->user_pos = peer->delayed_ba_ppdu_stats.user_pos;
 	ppdu->mu_group_id = peer->delayed_ba_ppdu_stats.mu_group_id;
+	ppdu->mcs = peer->delayed_ba_ppdu_stats.mcs;
 
 	peer->last_delayed_ba = false;
 
