21b93db1dd7786bfd17907838e72ef362f47e2d4
Conner Huff <chuff@codeaurora.org>
2021-01-14 19:34:22 -0800

    rmnet_shs: Enable rmnet_shs compilation on taro
    Adjust code as needed to enable compilation of
    rmnet_shs.ko for target taro.
    
    Change-Id: I6be471333262e4275333d9a5488186d6b6fa726b
    Signed-off-by: Conner Huff <chuff@codeaurora.org>

--- shs/Android.mk
+++ shs/Android.mk
@@ -1,5 +1,6 @@
 ifneq ($(TARGET_PRODUCT),qssi)
 RMNET_SHS_DLKM_PLATFORMS_LIST := lahaina
+RMNET_SHS_DLKM_PLATFORMS_LIST := taro
 
 ifeq ($(call is-board-platform-in-list, $(RMNET_SHS_DLKM_PLATFORMS_LIST)),true)
 #Make file to create RMNET_SHS DLKM
@@ -11,19 +12,11 @@ LOCAL_CLANG :=true
 
 LOCAL_MODULE_PATH := $(KERNEL_MODULES_OUT)
 LOCAL_MODULE := rmnet_shs.ko
-
-LOCAL_SRC_FILES := \
-	rmnet_shs_main.c \
-	rmnet_shs_config.c \
-	rmnet_shs_common.c \
-	rmnet_shs_modules.c \
-	rmnet_shs_wq.c \
-	rmnet_shs_freq.c \
-	rmnet_shs_wq_mem.c \
-	rmnet_shs_wq_genl.c
+LOCAL_SRC_FILES   := $(wildcard $(LOCAL_PATH)/**/*) $(wildcard $(LOCAL_PATH)/*)
 
 #path from build top to the core directory
-RMNET_CORE_PATH := vendor/qcom/opensource/datarmnet/core
+DATARMNET_CORE_PATH := datarmnet/core
+RMNET_CORE_PATH := vendor/qcom/opensource/$(DATARMNET_CORE_PATH)
 DLKM_DIR := $(TOP)/device/qcom/common/dlkm
 #absolute path to the build directory. Can't use $(TOP) here since
 #that resolves to ., and we pass this to Kbuild, where . is different
@@ -32,14 +25,15 @@ RMNET_CORE_INC_DIR := $(shell pwd)/$(RMNET_CORE_PATH)
 #pass variables down to Kbuild environment
 KBUILD_OPTIONS := RMNET_CORE_INC_DIR=$(RMNET_CORE_INC_DIR)
 KBUILD_OPTIONS += RMNET_CORE_PATH=$(RMNET_CORE_PATH)
+KBUILD_OPTIONS += DATARMNET_CORE_PATH=$(DATARMNET_CORE_PATH)
 KBUILD_OPTIONS_GKI := RMNET_CORE_INC_DIR=$(RMNET_CORE_INC_DIR)
 KBUILD_OPTIONS_GKI += RMNET_CORE_PATH=$(RMNET_CORE_PATH)/gki
 
 #Must be built after the core rmnet module
-LOCAL_ADDITIONAL_DEPENDENCIES := $(TARGET_OUT_INTERMEDIATES)/$(RMNET_CORE_PATH)/rmnet_core.ko
-LOCAL_ADDITIONAL_DEPENDENCIES_GKI := $(TARGET_OUT_INTERMEDIATES)/$(RMNET_CORE_PATH)/gki/rmnet_core.ko
+LOCAL_ADDITIONAL_DEPENDENCIES := $(TARGET_OUT_INTERMEDIATES)/DLKM_OBJ/$(RMNET_CORE_PATH)/rmnet_core.ko
+LOCAL_ADDITIONAL_DEPENDENCIES_GKI := $(TARGET_OUT_INTERMEDIATES)/DLKM_OBJ/$(RMNET_CORE_PATH)/gki/rmnet_core.ko
 
-include $(DLKM_DIR)/AndroidKernelModule.mk
+include $(DLKM_DIR)/Build_external_kernelmodule.mk
 
 endif #End of Check for target
 endif #End of Check for qssi target
