fdd65fd9ea09f45e7e4ebb84896761013ae01583
Abdul Muqtadeer Ahmed <amuqtade@codeaurora.org>
2021-01-19 11:18:10 +0530

    qcacld-3.0: Add a new ini param to enable PCI gen switch
    Add a new ini param to enable/disable PCI gen switch
    
    Change-Id: Ie8c61dbfb9834aac96a95eb935521e1bc4b3b835
    CRs-Fixed: 2865861

--- core/wma/src/wma_main.c
+++ core/wma/src/wma_main.c
@@ -345,6 +345,7 @@ static void wma_set_default_tgt_config(tp_wma_handle wma_handle,
 	tgt_cfg->num_ocb_channels = CFG_TGT_NUM_OCB_CHANNELS;
 	tgt_cfg->num_ocb_schedules = CFG_TGT_NUM_OCB_SCHEDULES;
 	tgt_cfg->twt_ap_sta_count = CFG_TGT_DEFAULT_TWT_AP_STA_COUNT;
+	tgt_cfg->enable_pci_gen = cfg_get(wma_handle->psoc, CFG_ENABLE_PCI_GEN);
 
 	tgt_cfg->mgmt_comp_evt_bundle_support = true;
 	tgt_cfg->tx_msdu_new_partition_id_support = true;
