8c318da2c53ae53fedc16fac5b123dee06ed658a
Sebastian Dang <sebastiandang@codeaurora.org>
2021-01-22 10:14:17 -0800

    mmrm: Enable mmrm
    Enable mmrm for all target platforms.
    
    Change-Id: I31a6eec86d4ff694541872e38e562390e01d0957

--- mmrm_kernel_board.mk
+++ mmrm_kernel_board.mk
@@ -1,3 +1,4 @@
-ifeq ($(call is-board-platform-in-list,waipio),true)
+ifeq ($(call is-board-platform-in-list,$(TARGET_BOARD_PLATFORM)),true)
 BOARD_VENDOR_KERNEL_MODULES += $(KERNEL_MODULES_OUT)/msm-mmrm.ko
+BOARD_VENDOR_KERNEL_MODULES += $(KERNEL_MODULES_OUT)/mmrm_test_module.ko
 endif
