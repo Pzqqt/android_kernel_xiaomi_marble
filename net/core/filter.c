802eeec03db87dd9794d20e424d2e0624fa573c1
Stanislav Fomichev <sdf@google.com>
2021-01-27 15:28:53 -0800

    BACKPORT: bpf: Enable bpf_{g,s}etsockopt in BPF_CGROUP_UDP{4,6}_RECVMSG
    Those hooks run as BPF_CGROUP_RUN_SA_PROG_LOCK and operate on a locked socket.
    
    Note that we could remove the switch for prog->expected_attach_type altogether
    since all current sock_addr attach types are covered. However, it makes sense
    to keep it as a safe-guard in case new sock_addr attach types are added that
    might not operate on a locked socket. Therefore, avoid to let this slip through.
    
    Signed-off-by: Stanislav Fomichev <sdf@google.com>
    Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>
    Link: https://lore.kernel.org/bpf/20210127232853.3753823-5-sdf@google.com
    
    (cherry picked from commit 4c3384d7abe58a68d78fd1b8b3bffbf62e1e29a1)
    Signed-off-by: Fuutao <fuutaoao@gmail.com>
    [zhaoyuenan: Ignore changes to the tools/selftest bpf prog tool]
    Signed-off-by: zhaoyuenan <amktiao030215@gmail.com>
    Change-Id: I777fb688ae6cf613589b98ec8cff2ff087b38d2f

--- net/core/filter.c
+++ net/core/filter.c
@@ -7138,6 +7138,8 @@ sock_addr_func_proto(enum bpf_func_id func_id, const struct bpf_prog *prog)
 		case BPF_CGROUP_INET6_BIND:
 		case BPF_CGROUP_INET4_CONNECT:
 		case BPF_CGROUP_INET6_CONNECT:
+		case BPF_CGROUP_UDP4_RECVMSG:
+		case BPF_CGROUP_UDP6_RECVMSG:
 		case BPF_CGROUP_UDP4_SENDMSG:
 		case BPF_CGROUP_UDP6_SENDMSG:
 		case BPF_CGROUP_INET4_GETPEERNAME:
@@ -7154,6 +7156,8 @@ sock_addr_func_proto(enum bpf_func_id func_id, const struct bpf_prog *prog)
 		case BPF_CGROUP_INET6_BIND:
 		case BPF_CGROUP_INET4_CONNECT:
 		case BPF_CGROUP_INET6_CONNECT:
+		case BPF_CGROUP_UDP4_RECVMSG:
+		case BPF_CGROUP_UDP6_RECVMSG:
 		case BPF_CGROUP_UDP4_SENDMSG:
 		case BPF_CGROUP_UDP6_SENDMSG:
 		case BPF_CGROUP_INET4_GETPEERNAME:
