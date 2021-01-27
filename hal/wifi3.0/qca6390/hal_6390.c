e27c8b63480da4f1a47434f7ab67ec29ef5218b9
Chaoli Zhou <zchaoli@codeaurora.org>
2021-01-27 19:43:47 +0800

    qcacmn: Set max rings number of DIR_BUF_RX_DMA_SRC to 2
    For hastings, it has two dma rx rings, and the first
    one is for spectral scan, and the second one is for CFR.
    So increase the max supported rings number from 1 to
    2.
    
    Change-Id: I85500bba366a4321ebc91e92f72146dd20311d03
    CRs-Fixed:

--- hal/wifi3.0/qca6390/hal_6390.c
+++ hal/wifi3.0/qca6390/hal_6390.c
@@ -1593,7 +1593,11 @@ struct hal_hw_srng_config hw_srng_table_6390[] = {
 	},
 	{ /* DIR_BUF_RX_DMA_SRC */
 		.start_ring_id = HAL_SRNG_DIR_BUF_RX_SRC_DMA_RING,
-		.max_rings = 1,
+		/*
+		 * one ring is for spectral scan
+		 * the other one is for cfr
+		 */
+		.max_rings = 2,
 		.entry_size = 2,
 		.lmac_ring = TRUE,
 		.ring_dir = HAL_SRNG_SRC_RING,
