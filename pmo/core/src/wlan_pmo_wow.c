7373a0d83a6cbde8439359b49433b1c95464b757
Pragaspathi Thilagaraj <tpragasp@codeaurora.org>
2021-02-03 18:32:01 +0530

    qcacld-3.0: Allow TWT events to wake up wlan host
    Currently when TWT commands are sent to firmware, the host driver
    doesn't acquire wake lock. So the wlan can got to wow mode before
    receiving the TWT command response and the response will get
    dropped since host is in wow mode.
    
    To avoid this make the TWT events as wakeable so firmware can
    wake up host if host is suspended.
    
    Change-Id: I2750049f9173de564e24114731d3fdd8ec8d6d86
    CRs-Fixed: 2869865

--- pmo/core/src/wlan_pmo_wow.c
+++ pmo/core/src/wlan_pmo_wow.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2017-2020 The Linux Foundation. All rights reserved.
+ * Copyright (c) 2017-2021 The Linux Foundation. All rights reserved.
  *
  * Permission to use, copy, modify, and/or distribute this software for
  * any purpose with or without fee is hereby granted, provided that the
@@ -412,6 +412,10 @@ void pmo_set_sta_wow_bitmask(uint32_t *bitmask, uint32_t wow_bitmap_size)
 				 wow_bitmap_size,
 				 bitmask);
 	pmo_set_wow_event_bitmap(WOW_VDEV_DISCONNECT_EVENT,
+				 wow_bitmap_size,
+				 bitmask);
+
+	pmo_set_wow_event_bitmap(WOW_TWT_EVENT,
 				 wow_bitmap_size,
 				 bitmask);
 }
