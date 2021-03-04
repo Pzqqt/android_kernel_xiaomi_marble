68200a1b77fc9d0b0316c97ceb43d563658dabb5
Bapiraju Alla <balla@codeaurora.org>
2021-03-04 11:02:56 +0530

    qcacld-3.0: Don't create vdev sync for pre CAC adapter again
    Currently, for every pre CAC request a new vdev sync is getting created
    and it will be destroyed only when SAP is stopped. So, there is a possible
    memory leak if a valid pre CAC request is issued second time without SAP
    stop.
    
    To avoid this, don't create vdev sync for pre CAC request if pre CAC
    adapter already exist.
    
    Change-Id: If3f65648645b6801a1504a2d184dbc6928ff4771
    CRs-Fixed: 2891360

--- core/hdd/src/wlan_hdd_main.c
+++ core/hdd/src/wlan_hdd_main.c
@@ -5920,6 +5920,7 @@ static char *net_dev_ref_debug_string_from_id(wlan_net_dev_ref_dbgid dbgid)
 		"NET_DEV_HOLD_COUNTRY_CHANGE_UPDATE_SAP",
 		"NET_DEV_HOLD_CACHE_STATION_STATS_CB",
 		"NET_DEV_HOLD_DISPLAY_TXRX_STATS",
+		"NET_DEV_HOLD_START_PRE_CAC_TRANS",
 		"NET_DEV_HOLD_ID_MAX"};
 	int32_t num_dbg_strings = QDF_ARRAY_SIZE(strings);
 
