a07ad750b0abb802cd1bd57c16582369b59b3fc0
Jeff Johnson <jjohnson@codeaurora.org>
2021-03-10 11:54:16 -0800

    qcacld-3.0: Use static symbolic links for multi-ko support
    The current multi-ko support is achieved by creating, during the
    actual build process, target-specific .<chip> symbolic links beneath
    the qcacld-3.0 folder. Unfortunately this mechanism violates the
    requirements of the Hermetic Build feature which requires that the
    source directory not be modified during the build process.
    
    To satisfy this requirement remove the dynamic creation of symbolic
    links from Android.mk, and instead staticially instantiate all of the
    required symbolic links in the git project.
    
    Change-Id: I7d75bb6a82fd27d8925d53529b4252b782457bc8
    CRs-Fixed: 2896976

--- Android.mk
+++ Android.mk
@@ -56,13 +56,7 @@ endif
 endif
 
 ifeq ($(LOCAL_MULTI_KO), true)
-LOCAL_ANDROID_ROOT := $(shell pwd)
-LOCAL_WLAN_BLD_DIR := $(LOCAL_ANDROID_ROOT)/$(WLAN_BLD_DIR)
-$(shell find $(LOCAL_WLAN_BLD_DIR)/qcacld-3.0/ -maxdepth 1 \
-	-name '.*' ! -name '.git' -exec rm -rf {} +)
 
-$(foreach chip, $(TARGET_WLAN_CHIP), \
-	$(shell ln -sf . $(LOCAL_WLAN_BLD_DIR)/qcacld-3.0/.$(chip)))
 include $(foreach chip, $(TARGET_WLAN_CHIP), $(LOCAL_PATH)/.$(chip)/Android.mk)
 
 else # Multi-ok check
