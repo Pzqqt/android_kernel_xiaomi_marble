083c1c613ac08df557acf9f9fe25ecac8abee0ff
snandini <snandini@codeaurora.org>
2021-03-15 07:22:38 -0700

    Release 5.2.0.226
    Release 5.2.0.226
    
    Change-Id: I25e522daec73d60f9589e1342545a1693064ea92
    CRs-Fixed: 774533

--- core/mac/inc/qwlan_version.h
+++ core/mac/inc/qwlan_version.h
@@ -32,9 +32,9 @@
 #define QWLAN_VERSION_MAJOR            5
 #define QWLAN_VERSION_MINOR            2
 #define QWLAN_VERSION_PATCH            0
-#define QWLAN_VERSION_EXTRA            "Z"
-#define QWLAN_VERSION_BUILD            225
+#define QWLAN_VERSION_EXTRA            ""
+#define QWLAN_VERSION_BUILD            226
 
-#define QWLAN_VERSIONSTR               "5.2.0.225Z"
+#define QWLAN_VERSIONSTR               "5.2.0.226"
 
 #endif /* QWLAN_VERSION_H */
