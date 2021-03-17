c3c6473b364db9652158d86ba2bdbe4c37aedc4a
Jeevan Kukkalli <jeevank@codeaurora.org>
2021-03-17 20:16:12 +0530

    qcacmn: Set mon delayed replenish entries
    Set monitor delayed replenish entries for target type
    QCA5018/QCN6122
    
    Change-Id: I2042dfd2b1ff975907d829e1f111daf40f0f8e3c
    CRs-Fixed: 2888346

--- dp/wifi3.0/dp_main.c
+++ dp/wifi3.0/dp_main.c
@@ -14127,6 +14127,8 @@ static void dp_soc_cfg_init(struct dp_soc *soc)
 		break;
 	case TARGET_TYPE_QCA5018:
 	case TARGET_TYPE_QCN6122:
+		wlan_cfg_set_mon_delayed_replenish_entries(soc->wlan_cfg_ctx,
+							   MON_BUF_MIN_ENTRIES);
 		wlan_cfg_set_reo_dst_ring_size(soc->wlan_cfg_ctx,
 					       REO_DST_RING_SIZE_QCA8074);
 		soc->ast_override_support = 1;
