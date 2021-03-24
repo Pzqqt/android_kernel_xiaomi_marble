c3959896c3d3ae99938e08c3267f9d89fe76e2d8
Jeykumar Sankaran <jsanka@codeaurora.org>
2021-03-23 22:30:30 -0700

    disp: msm: sde: enable tui flag for waipio hw catalog
    Enable trusted vm flag for waipio HW catalog.
    
    Change-Id: I7cd5ba0354e2ed66a21f5f3b923c1855af1a0f14
    Signed-off-by: Jeykumar Sankaran <jsanka@codeaurora.org>

--- msm/sde/sde_hw_catalog.c
+++ msm/sde/sde_hw_catalog.c
@@ -5123,6 +5123,7 @@ static int _sde_hardware_pre_caps(struct sde_mdss_cfg *sde_cfg, uint32_t hw_rev)
 		sde_cfg->has_fp16 = true;
 		set_bit(SDE_MDP_PERIPH_TOP_0_REMOVED, &sde_cfg->mdp[0].features);
 		sde_cfg->has_precise_vsync_ts = true;
+		sde_cfg->has_trusted_vm_support = true;
 	} else {
 		SDE_ERROR("unsupported chipset id:%X\n", hw_rev);
 		sde_cfg->perf.min_prefill_lines = 0xffff;
