7d9bb1dad058c8217e7953952ab60a5bcd541b6b
Aditya Kodukula <akodukul@codeaurora.org>
2021-04-14 13:07:53 -0700

    qcacld-3.0: Restore the ini parameter etsi13_srd_chan_in_master_mode
    The commit Ic695e36e9b4fc9952cc405d1345b4a6a7d543dad, removed the
    ini parameter etsi13_srd_chan_in_master_mode assuming that the
    ini is obsolete and no longer being used. But, the ini is still
    being used by the OEMs. So, revert the previous changes.
    
    Change-Id: I6b2ba8b8e41ab96981b92e5ff4b2f610f92d7e43
    CRs-Fixed: 2923688

--- mlme/dispatcher/inc/cfg_mlme_reg.h
+++ mlme/dispatcher/inc/cfg_mlme_reg.h
@@ -71,6 +71,39 @@
 
 /*
  * <ini>
+ * etsi_srd_chan_in_master_mode - Enable/disable ETSI SRD channels in
+ * master mode PCL and ACS functionality
+ * @Min: 0
+ * @Max: 0xFF
+ * @Default: 6
+ *
+ * etsi_srd_chan_in_master_mode is to enable/disable ETSI SRD channels in
+ * master mode PCL and ACS functionality
+ * Bit map for enabling the SRD mode in various modes are as follows:-
+ * BIT 0:- Enable/Disable SRD channels for SAP.
+ * BIT 1:- Enable/Disable SRD channels for P2P-GO.
+ * BIT 2:- Enable/Disable SRD channels for NAN.
+ * Rest of the bits are currently reserved for future SRD channel support for
+ * other vdevs.
+ *
+ * Related: None
+ *
+ * Supported Feature: SAP/P2P-GO
+ *
+ * Usage: Internal/External
+ *
+ * </ini>
+ */
+#define CFG_ETSI_SRD_CHAN_IN_MASTER_MODE CFG_INI_UINT( \
+	"etsi13_srd_chan_in_master_mode", \
+	0, \
+	0xff, \
+	6, \
+	CFG_VALUE_OR_DEFAULT, \
+	"enable/disable ETSI SRD channels in master mode")
+
+/*
+ * <ini>
  * fcc_5dot9_ghz_chan_in_master_mode - Enable/disable 5.9 GHz channels in
  * master mode for US
  * @Min: 0
@@ -281,6 +314,7 @@
 	CFG(CFG_SELF_GEN_FRM_PWR) \
 	CFG(CFG_ENABLE_PENDING_CHAN_LIST_REQ) \
 	CFG(CFG_ENABLE_11D_IN_WORLD_MODE) \
+	CFG(CFG_ETSI_SRD_CHAN_IN_MASTER_MODE) \
 	CFG(CFG_FCC_5DOT9_GHZ_CHAN_IN_MASTER_MODE) \
 	CFG(CFG_RESTART_BEACONING_ON_CH_AVOID) \
 	CFG(CFG_INDOOR_CHANNEL_SUPPORT) \
