3c98ddde4dbca254d49754242891f6117eb1a375
Yu Tian <yutian@codeaurora.org>
2021-04-22 11:16:57 +0800

    qcacld-3.0: revert TDLS pkt statistics changes
    TDLS pkts statistics API cost 10% CPU cycle of whole RX
    thread. It's optimized in another change from TDLS team to
    remove excessive lock/unlock. So original change is not needed.
    
    Change-Id: Ica1dd6bf8a94a4bef6045b07228bd547aeda3a00
    CRs-Fixed: 2930306

--- core/hdd/src/wlan_hdd_tx_rx.c
+++ core/hdd/src/wlan_hdd_tx_rx.c
@@ -3416,6 +3416,7 @@ void hdd_reset_tcp_adv_win_scale(struct hdd_context *hdd_ctx)
  *
  * Return: True if vote level is high
  */
+#ifdef RX_PERFORMANCE
 bool hdd_is_current_high_throughput(struct hdd_context *hdd_ctx)
 {
 	if (hdd_ctx->cur_vote_level < PLD_BUS_WIDTH_MEDIUM)
@@ -3423,6 +3424,7 @@ bool hdd_is_current_high_throughput(struct hdd_context *hdd_ctx)
 	else
 		return true;
 }
+#endif
 #endif
 
 #ifdef QCA_LL_LEGACY_TX_FLOW_CONTROL
