b566b0b0913d412d50267dca81de74fe1671e41f
George Shen <sqiao@codeaurora.org>
2021-04-23 11:42:17 -0700

    msm: eva: Remove inappropriate comment
    To ensure compliance with kernel module coding convention.
    
    Change-Id: Ib511ea3df44d83078456d1de5e4651e31e0ff1dc
    Signed-off-by: George Shen <sqiao@codeaurora.org>

--- msm/eva/msm_cvp_dsp.c
+++ msm/eva/msm_cvp_dsp.c
@@ -1255,9 +1255,9 @@ static void __dsp_cvp_power_req(struct cvp_dsp_cmd_msg *cmd)
 
 	rc = msm_cvp_update_power(inst);
 	if (rc) {
-		/* May need to define more error types
-		 * Check UMD implementation here:
-		 * https://opengrok.qualcomm.com/source/xref/LA.UM.9.14/vendor/qcom/proprietary/cv-noship/cvp/cpurev/src/cvpcpuRev_skel_imp_cvp2.cpp#380
+		/*
+		 *May need to define more error types
+		 * Check UMD implementation
 		 */
 		dprintk(CVP_ERR, "%s Failed update power\n", __func__);
 		cmd->ret = -1;
