7e27bb4fc8e7e129e8abb14d0938b9f8471269a9
Divya Sharma <divyash@codeaurora.org>
2021-04-26 07:47:10 -0700

    Revert "msm: ipa: use separate event ring for WAN producer"
    This reverts commit 4f9882194b2e32ba61055308917004eb23c5965f.
    
    Change-Id: I89dcb96fa7bce6521c4de9b34de07f425b2d7072

--- drivers/platform/msm/ipa/ipa_v3/ipa_dp.c
+++ drivers/platform/msm/ipa/ipa_v3/ipa_dp.c
@@ -4088,10 +4088,7 @@ static int ipa3_assign_policy(struct ipa_sys_connect_params *in,
 
 	if (in->client == IPA_CLIENT_APPS_WAN_PROD) {
 		sys->policy = IPA_POLICY_INTR_MODE;
-		if (ipa3_ctx->ipa_hw_type >= IPA_HW_v5_0)
-			sys->use_comm_evt_ring = false;
-		else
-			sys->use_comm_evt_ring = true;
+		sys->use_comm_evt_ring = true;
 		INIT_WORK(&sys->work, ipa3_send_nop_desc);
 		atomic_set(&sys->workqueue_flushed, 0);
 
