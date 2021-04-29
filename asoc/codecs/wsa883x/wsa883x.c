b5bc4627f2021ac48ddc1fd5509e520c46c22df0
Laxminath Kasam <lkasam@codeaurora.org>
2021-04-29 13:49:31 +0530

    asoc: wsa883x: Update wsa8832 DRE gain
    Reduce WSA8832 SKU DRE gain offset by
    1p5dB same as WSA8830
    
    Change-Id: I949bd78e2f4e935e1aba7ce0e2319418562e14ac
    Signed-off-by: Laxminath Kasam <lkasam@codeaurora.org>

--- asoc/codecs/wsa883x/wsa883x.c
+++ asoc/codecs/wsa883x/wsa883x.c
@@ -1188,7 +1188,7 @@ static void wsa883x_codec_init(struct snd_soc_component *component)
 		snd_soc_component_update_bits(component, reg_init[i].reg,
 					reg_init[i].mask, reg_init[i].val);
 
-	if (wsa883x->variant == WSA8830) {
+	if (wsa883x->variant == WSA8830 || wsa883x->variant == WSA8832) {
 		snd_soc_component_update_bits(component, WSA883X_DRE_CTL_0,
 					0x07, 0x03);
 		wsa883x->comp_offset = COMP_OFFSET3;
