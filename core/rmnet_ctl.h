d4342c871d0189134c88f436046fd3b1c0444135
Conner Huff <chuff@codeaurora.org>
2021-05-12 16:13:54 -0700

    rmnet_core: Remove rmnet_ctl from compilation
    Break dependency between rmnet_core and rmnet_ctl
    so that we can compile rmnet_core without
    rmnet_ctl on yocto based targets.
    
    Change-Id: I3bfa3dbcd24f9343d073107f40b6d98c77aba881
    Signed-off-by: Conner Huff <chuff@codeaurora.org>

--- core/rmnet_ctl.h
+++ core/rmnet_ctl.h
@@ -1,5 +1,5 @@
 /* SPDX-License-Identifier: GPL-2.0-only */
-/* Copyright (c) 2019-2020, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2019-2021, The Linux Foundation. All rights reserved.
  *
  * RMNET_CTL header
  *
@@ -29,6 +29,10 @@ struct rmnet_ctl_client_if {
 		       const void *data, unsigned int len);
 };
 
+#ifdef RMNET_LA_PLATFORM
 struct rmnet_ctl_client_if *rmnet_ctl_if(void);
+#else
+inline struct rmnet_ctl_client_if *rmnet_ctl_if(void) {return NULL;};
+#endif /* RMNET_LA_PLATFORM */
 
 #endif /* _RMNET_CTL_H_ */
