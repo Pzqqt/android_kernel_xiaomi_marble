183db750e367f00708919e0bf624af1fce684a7b
Tiger Yu <tfyu@codeaurora.org>
2021-05-17 15:17:03 +0800

    qcacmn: Use qdf_assert_always instead of qdf_assert when rx_desc NULL
    Use the qdf_assert_always instead of qdf_assert when rx_desc is NULL
    getting by dp_rx_cookie_2_va_mon_status to capture real problem here
    since the qdf_assert does not do anything.
    
    Change-Id: I480917ecaf30f9faa4fdcda93c09a59e972a7e1c
    CRs-Fixed: 2944083

--- dp/wifi3.0/dp_rx_mon_status.c
+++ dp/wifi3.0/dp_rx_mon_status.c
@@ -94,7 +94,7 @@ dp_rx_mon_handle_status_buf_done(struct dp_pdev *pdev,
 	rx_buf_cookie = HAL_RX_BUF_COOKIE_GET(ring_entry);
 	rx_desc = dp_rx_cookie_2_va_mon_status(soc, rx_buf_cookie);
 
-	qdf_assert(rx_desc);
+	qdf_assert_always(rx_desc);
 
 	status_nbuf = rx_desc->nbuf;
 
@@ -1977,7 +1977,7 @@ dp_rx_mon_status_srng_process(struct dp_soc *soc, struct dp_intr *int_ctx,
 			rx_desc = dp_rx_cookie_2_va_mon_status(soc,
 				rx_buf_cookie);
 
-			qdf_assert(rx_desc);
+			qdf_assert_always(rx_desc);
 
 			status_nbuf = rx_desc->nbuf;
 
@@ -2500,7 +2500,7 @@ dp_mon_status_srng_drop_for_mac(struct dp_pdev *pdev, uint32_t mac_id,
 			rx_desc = dp_rx_cookie_2_va_mon_status(soc,
 							       rx_buf_cookie);
 
-			qdf_assert(rx_desc);
+			qdf_assert_always(rx_desc);
 
 			status_nbuf = rx_desc->nbuf;
 
