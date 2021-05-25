e0c2cd6326ff78182a0885fcf40e8a165bbf33a5
Yu Wang <yyuwang@codeaurora.org>
2021-05-25 15:01:27 +0800

    qcacld-3.0: enhance oui based iot aggr size processing
    Check the configured IOT OUIs in Beacon/Probe Response
    IEs, but not in Associate Response IEs, because some APs
    won't add its specific OUI in Associate Response frame.
    
    Change-Id: I3aedeef0a5dbecd4bed41eb47a9374e654509746
    CRs-Fixed: 2955964

--- core/mac/src/pe/lim/lim_process_assoc_rsp_frame.c
+++ core/mac/src/pe/lim/lim_process_assoc_rsp_frame.c
@@ -650,6 +650,89 @@ static void lim_set_r0kh(tpSirAssocRsp assoc_rsp, struct pe_session *session)
 static inline
 void lim_set_r0kh(tpSirAssocRsp assoc_rsp, struct pe_session *session) {}
 #endif
+
+/**
+ * lim_get_iot_aggr_sz() - check and get IOT aggr size for configured OUI
+ *
+ * @mac_ctx: Pointer to Global MAC structure
+ * @ie_ptr: Pointer to starting IE in Beacon/Probe Response
+ * @ie_len: Length of all IEs combined
+ * @amsdu_sz: pointer to buffer to store AMSDU size
+ * @ampdu_sz: pointer to buffer to store AMPDU size
+ *
+ * This function is called to find configured vendor specific OUIs
+ * from the IEs in Beacon/Probe Response frames, if one of the OUI is
+ * present, get the configured aggr size for the OUI.
+ *
+ * Return: true if found, false otherwise.
+ */
+static bool
+lim_get_iot_aggr_sz(struct mac_context *mac, uint8_t *ie_ptr, uint32_t ie_len,
+		    uint32_t *amsdu_sz, uint32_t *ampdu_sz)
+{
+	const uint8_t *oui, *vendor_ie;
+	struct wlan_mlme_iot *iot;
+	uint32_t oui_len, aggr_num;
+	int i;
+
+	iot = &mac->mlme_cfg->iot;
+	aggr_num = iot->aggr_num;
+	if (!aggr_num)
+		return false;
+
+	for (i = 0; i < aggr_num; i++) {
+		oui = iot->aggr[i].oui;
+		oui_len = iot->aggr[i].oui_len;
+		vendor_ie = wlan_get_vendor_ie_ptr_from_oui(oui, oui_len,
+							    ie_ptr, ie_len);
+		if (!vendor_ie)
+			continue;
+
+		*amsdu_sz = iot->aggr[i].amsdu_sz;
+		*ampdu_sz = iot->aggr[i].ampdu_sz;
+		return true;
+	}
+
+	return false;
+}
+
+/**
+ * lim_update_iot_aggr_sz() - check and update IOT aggr size
+ *
+ * @mac_ctx: Pointer to Global MAC structure
+ * @ie_ptr: Pointer to starting IE in Beacon/Probe Response
+ * @ie_len: Length of all IEs combined
+ * @session_entry: A pointer to session entry
+ *
+ * This function is called to find configured vendor specific OUIs
+ * from the IEs in Beacon/Probe Response frames, and set the aggr
+ * size accordingly.
+ *
+ * Return: None
+ */
+static void
+lim_update_iot_aggr_sz(struct mac_context *mac_ctx, uint8_t *ie_ptr,
+		       uint32_t ie_len, struct pe_session *session_entry)
+{
+	int ret;
+	uint32_t amsdu_sz, ampdu_sz;
+	bool iot_hit;
+
+	if (!ie_ptr || !ie_len)
+		return;
+
+	iot_hit = lim_get_iot_aggr_sz(mac_ctx, ie_ptr, ie_len,
+				      &amsdu_sz, &ampdu_sz);
+	if (!iot_hit)
+		return;
+
+	pe_debug("Try to set iot amsdu size: %u", amsdu_sz);
+	ret = wma_cli_set_command(session_entry->smeSessionId,
+				  GEN_VDEV_PARAM_AMSDU, amsdu_sz, GEN_CMD);
+	if (ret)
+		pe_err("Failed to set iot amsdu size: %d", ret);
+}
+
 /**
  * lim_process_assoc_rsp_frame() - Processes assoc response
  * @mac_ctx: Pointer to Global MAC structure
@@ -669,7 +752,7 @@ lim_process_assoc_rsp_frame(struct mac_context *mac_ctx, uint8_t *rx_pkt_info,
 			    uint32_t reassoc_frame_len,
 			    uint8_t subtype, struct pe_session *session_entry)
 {
-	uint8_t *body;
+	uint8_t *body, *ie;
 	uint16_t caps, ie_len;
 	uint32_t frame_len;
 	tSirMacAddr current_bssid;
@@ -684,8 +767,6 @@ lim_process_assoc_rsp_frame(struct mac_context *mac_ctx, uint8_t *rx_pkt_info,
 	QDF_STATUS status;
 	enum ani_akm_type auth_type;
 	bool sha384_akm;
-	int ret;
-	uint8_t amsdu_sz;
 
 	assoc_cnf.resultCode = eSIR_SME_SUCCESS;
 	/* Update PE session Id */
@@ -1069,23 +1150,6 @@ lim_process_assoc_rsp_frame(struct mac_context *mac_ctx, uint8_t *rx_pkt_info,
 	pe_debug("Successfully Associated with BSS " QDF_MAC_ADDR_FMT,
 		 QDF_MAC_ADDR_REF(hdr->sa));
 
-	/*
-	 * If fail to get the global max amsdu size, or the value is
-	 * 0 (which means FW automode selection), and it hits 'iot_amsdu_sz'
-	 * when parsing vendor IEs in assoc rsp frame, set this iot amsdu size.
-	 */
-	status = ucfg_mlme_get_max_amsdu_num(mac_ctx->psoc, &amsdu_sz);
-	if ((QDF_IS_STATUS_ERROR(status) || !amsdu_sz) &&
-	    assoc_rsp->iot_amsdu_sz) {
-		pe_debug("Try to set iot amsdu size: %u",
-			 assoc_rsp->iot_amsdu_sz);
-		ret = wma_cli_set_command(session_entry->smeSessionId,
-					  GEN_VDEV_PARAM_AMSDU,
-					  assoc_rsp->iot_amsdu_sz, GEN_CMD);
-		if (ret)
-			pe_err("Failed to set iot amsdu size: %d", ret);
-	}
-
 #ifdef FEATURE_WLAN_ESE
 	if (session_entry->eseContext.tsm.tsmInfo.state)
 		session_entry->eseContext.tsm.tsmMetrics.RoamingCount = 0;
@@ -1126,9 +1190,10 @@ lim_process_assoc_rsp_frame(struct mac_context *mac_ctx, uint8_t *rx_pkt_info,
 	 */
 	ie_len = lim_get_ielen_from_bss_description(
 		&session_entry->lim_join_req->bssDescription);
-	lim_extract_ap_capabilities(mac_ctx,
-		(uint8_t *)session_entry->lim_join_req->bssDescription.ieFields,
-		ie_len, beacon);
+	ie = (uint8_t *)session_entry->lim_join_req->bssDescription.ieFields;
+	lim_update_iot_aggr_sz(mac_ctx, ie, ie_len, session_entry);
+
+	lim_extract_ap_capabilities(mac_ctx, ie, ie_len, beacon);
 	lim_update_assoc_sta_datas(mac_ctx, sta_ds, assoc_rsp,
 				   session_entry, beacon);
 
