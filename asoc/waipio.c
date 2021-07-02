a9873e4b9db3f70dd652bbcecb12c7bf343d0cee
Rohit kumar <rohitkr@codeaurora.org>
2021-07-02 13:09:05 +0530

    asoc: Add virtual node for WSA RX0
    Change-Id: I49a300ab37610c325178517147189d96422297af
    Signed-off-by: Rohit kumar <rohitkr@codeaurora.org>

--- asoc/waipio.c
+++ asoc/waipio.c
@@ -476,6 +476,18 @@ static struct snd_soc_dai_link msm_wsa_cdc_dma_be_dai_links[] = {
 		/* .no_host_mode = SND_SOC_DAI_LINK_NO_HOST, */
 		SND_SOC_DAILINK_REG(vi_feedback),
 	},
+	{
+		.name = LPASS_BE_WSA_CDC_DMA_RX_0_VIRT,
+		.stream_name = LPASS_BE_WSA_CDC_DMA_RX_0_VIRT,
+		.playback_only = 1,
+		.trigger = {SND_SOC_DPCM_TRIGGER_POST,
+			SND_SOC_DPCM_TRIGGER_POST},
+		.ignore_pmdown_time = 1,
+		.ignore_suspend = 1,
+		.ops = &msm_common_be_ops,
+		SND_SOC_DAILINK_REG(wsa_dma_rx0),
+		.init = &msm_int_wsa_init,
+	},
 };
 
 static struct snd_soc_dai_link msm_wsa2_cdc_dma_be_dai_links[] = {
