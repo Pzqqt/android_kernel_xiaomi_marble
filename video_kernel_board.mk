314053e08e294f1c445cd400cfde1d0545d98683
Karthikeyan Periasamy <kperiasa@codeaurora.org>
2021-07-09 14:52:22 -0700

    techpack-video: Variant build support
    Added binary artifacts support for variant build
    
    Change-Id: I3ec23c80bc7d5be2222d44c6e7200136dcab6e42
    Signed-off-by: Karthikeyan Periasamy <kperiasa@codeaurora.org>

--- video_kernel_board.mk
+++ video_kernel_board.mk
@@ -3,5 +3,7 @@
 ifneq ($(TARGET_BOARD_AUTO),true)
 ifeq ($(call is-board-platform-in-list,$(TARGET_BOARD_PLATFORM)),true)
 BOARD_VENDOR_KERNEL_MODULES += $(KERNEL_MODULES_OUT)/msm_video.ko
+
+BUILD_VIDEO_TECHPACK_SOURCE := true
 endif
 endif
