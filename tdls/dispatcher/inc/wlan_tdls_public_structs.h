2728e082a72943825f65618e0ab0143109533d57
Utkarsh Bhatnagar <ubhatnag@codeaurora.org>
2021-07-14 02:48:42 +0530

    qcacld-3.0: Add support for TDLS 11AX for 6Ghz band
    Add support for TDLS 11AX for operations on 6Ghz
    band.
    
    Change-Id: I56e27f1e3560cd4374ba186f161bbc2ad945fe1d
    CRs-Fixed: 2990682

--- tdls/dispatcher/inc/wlan_tdls_public_structs.h
+++ tdls/dispatcher/inc/wlan_tdls_public_structs.h
@@ -787,6 +787,14 @@ struct hecap {
 		uint16_t tx_he_mcs_map_80_80;
 	} he_cap_mcs_info;
 } qdf_packed;
+
+struct hecap_6ghz {
+	/* Minimum MPDU Start Spacing B0..B2
+	 * Maximum A-MPDU Length Exponent B3..B5
+	 * Maximum MPDU Length B6..B7 */
+	uint8_t a_mpdu_params; /* B0..B7 */
+	uint8_t info; /* B8..B15 */
+};
 #endif
 
 struct tdls_update_peer_params {
@@ -804,6 +812,7 @@ struct tdls_update_peer_params {
 #ifdef WLAN_FEATURE_11AX
 	uint8_t he_cap_len;
 	struct hecap he_cap;
+	struct hecap_6ghz he_6ghz_cap;
 #endif
 	uint8_t uapsd_queues;
 	uint8_t max_sp;
@@ -1351,6 +1360,7 @@ struct tdls_add_sta_req {
 #ifdef WLAN_FEATURE_11AX
 	uint8_t he_cap_len;
 	struct hecap he_cap;
+	struct hecap_6ghz he_6ghz_cap;
 #endif
 	uint8_t uapsd_queues;
 	uint8_t max_sp;
