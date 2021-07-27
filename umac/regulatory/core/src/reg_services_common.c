75e0ad73524e2b9b98e26cf6b3283f39e32d6cea
Lincoln Tran <linctran@codeaurora.org>
2021-07-27 11:54:28 -0700

    qcacmn: Print the freq in fail case when getting its enum
    Update the invalid freq print with the actual frequency value for easier
    debug.
    
    Change-Id: I4e5bafaf902e19f4d070a7b140a2317a0988abf6
    CRs-fixed: 3001345

--- umac/regulatory/core/src/reg_services_common.c
+++ umac/regulatory/core/src/reg_services_common.c
@@ -3331,7 +3331,7 @@ static uint32_t reg_get_channel_flags_from_secondary_list_for_freq(
 	chan_enum = reg_get_chan_enum_for_freq(freq);
 
 	if (chan_enum == INVALID_CHANNEL) {
-		reg_err_rl("chan freq is not valid");
+		reg_err_rl("chan freq %u is not valid", freq);
 		return REGULATORY_CHAN_INVALID;
 	}
 
