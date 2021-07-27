a031b0d054fb3f531bf5127d1444da0919b1f992
Yu SI <ysi@codeaurora.org>
2021-07-27 16:07:01 -0700

    msm: eva: add QMAA compliance to eva kernel dlkm
    added check on QMAA related flags to
    decide if compile eva kernel ko.
    
    Change-Id: Iae09c460d75be90afb82b4178555497ebae9e573
    Signed-off-by: Yu SI <ysi@codeaurora.org>

--- eva_kernel_product.mk
+++ eva_kernel_product.mk
@@ -1,1 +1,10 @@
+ENABLE_EVA_KERNEL := true
+ifeq ($(TARGET_USES_QMAA), true)
+ifneq ($(TARGET_USES_QMAA_OVERRIDE_CVP), true)
+ENABLE_EVA_KERNEL := false
+endif
+endif
+
+ifeq ($(ENABLE_EVA_KERNEL), true)
 PRODUCT_PACKAGES += msm-eva.ko
+endif
