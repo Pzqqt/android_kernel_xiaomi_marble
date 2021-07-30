fe21212247a3539f86a22f3bcbacc3ad61f0db6d
bings <bings@codeaurora.org>
2021-07-30 16:50:50 +0800

    qcacmn: API to get rnr IE according to cm id
    API to get rnr IE according to cm id
    
    Change-Id: I6666a86a2402323a64472c79dc876077b622512a
    CRs-Fixed: 3004089

--- umac/mlme/connection_mgr/dispatcher/inc/wlan_cm_api.h
+++ umac/mlme/connection_mgr/dispatcher/inc/wlan_cm_api.h
@@ -449,4 +449,15 @@ void wlan_cm_set_candidate_custom_sort_cb(
 		void (*sort_fun)(struct wlan_objmgr_vdev *vdev,
 				 qdf_list_t *list));
 #endif
+
+/**
+ * wlan_cm_get_rnr() - get rnr
+ * @vdev:vdev
+ * @cm_id: connect mgr id
+ * @rnr: rnr to get
+ *
+ * Return: QDF_STATUS
+ */
+QDF_STATUS wlan_cm_get_rnr(struct wlan_objmgr_vdev *vdev, wlan_cm_id cm_id,
+			   struct reduced_neighbor_report *rnr);
 #endif /* __WLAN_CM_UCFG_API_H */
