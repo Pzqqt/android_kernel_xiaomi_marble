446effff960fb35aa29041def1865f5c545a8726
sheenam monga <shebala@codeaurora.org>
2021-08-04 15:06:24 +0530

    qcacld-3.0: Limit ROC for listen to max 600 if GO present
    Add fixed 300ms extra ROC time instead of multiplying the
    ROC duration by 6 as this causes the ROC to be upto 1.5 secs
    if GO is present. Firmware will advertize NOA of 1.5 secs
    and if supplicant cancels ROC after 200 or 300ms then
    firmware cannot cancel NOA. So when supplicant sends next
    ROC it will be delayed as firmware already is running previous
    NOA which may cause p2p find issues if GO is present.
    
    Fix this by capping the max ROC time to 600ms when GO is present.
    
    Change-Id: I0774b06ff4db1fae7efc387388fdf3059505150e
    CRs-Fixed: 3025870

--- components/p2p/core/src/wlan_p2p_roc.c
+++ components/p2p/core/src/wlan_p2p_roc.c
@@ -127,7 +127,17 @@ static QDF_STATUS p2p_scan_start(struct p2p_roc_context *roc_ctx)
 				p2p_soc_obj->soc, PM_P2P_GO_MODE, NULL);
 		p2p_debug("present go number:%d", go_num);
 		if (go_num)
-			req->scan_req.dwell_time_passive *=
+		/* Add fixed 300ms extra ROC time instead of multiplying the
+		 * ROC duration by const value as this causes the ROC to be
+		 * upto 1.5 secs if GO is present. Firmware will advertize NOA
+		 * of 1.5 secs and if supplicant cancels ROC after 200 or 300ms
+		 * then firmware cannot cancel NOA. So when supplicant sends
+		 * next ROC it will be delayed as firmware already is running
+		 * previous NOA. This causes p2p find issues if GO is present.
+		 * So add fixed duration of 300ms and also cap max ROC to 600ms
+		 * when GO is present
+		 */
+			req->scan_req.dwell_time_passive +=
 					P2P_ROC_DURATION_MULTI_GO_PRESENT;
 		else
 			req->scan_req.dwell_time_passive *=
@@ -135,7 +145,12 @@ static QDF_STATUS p2p_scan_start(struct p2p_roc_context *roc_ctx)
 		/* this is to protect too huge value if some customers
 		 * give a higher value from supplicant
 		 */
-		if (req->scan_req.dwell_time_passive > P2P_MAX_ROC_DURATION)
+		if (go_num && req->scan_req.dwell_time_passive >
+		    P2P_MAX_ROC_DURATION_GO_PRESENT)
+			req->scan_req.dwell_time_passive =
+					P2P_MAX_ROC_DURATION_GO_PRESENT;
+		else if (req->scan_req.dwell_time_passive >
+			 P2P_MAX_ROC_DURATION)
 			req->scan_req.dwell_time_passive = P2P_MAX_ROC_DURATION;
 	}
 	p2p_debug("FW requested roc duration is:%d",
