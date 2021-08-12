bad0b3e49107a60dc80f1e997834c1543dbaebd8
Surya Prakash Sivaraj <suryapra@codeaurora.org>
2021-08-12 10:38:43 +0530

    qcacld-3.0: Add an INI to configure SAE auth failure timeout
    Currently, the SAE authentication request retry happens
    after a fixed timeout of 1 second in a STA DUT.
    
    To make this timeout user configurable, add an INI to
    configure SAE auth failure timeout.
    
    Change-Id: I3a3944ab14564a944e0c83cc31a92847b5e8c5e0
    CRs-Fixed: 3011892

--- core/mac/src/pe/lim/lim_send_management_frames.c
+++ core/mac/src/pe/lim/lim_send_management_frames.c
@@ -5735,8 +5735,6 @@ error_delba:
 	return qdf_status;
 }
 
-#define WLAN_SAE_AUTH_TIMEOUT 1000
-
 /**
  * lim_tx_mgmt_frame() - Transmits Auth mgmt frame
  * @mac_ctx Pointer to Global MAC structure
@@ -5796,6 +5794,7 @@ lim_handle_sae_auth_retry(struct mac_context *mac_ctx, uint8_t vdev_id,
 	struct pe_session *session;
 	struct sae_auth_retry *sae_retry;
 	uint8_t retry_count = 0;
+	uint32_t val = 0;
 
 	session = pe_find_session_by_vdev_id(mac_ctx, vdev_id);
 	if (!session) {
@@ -5832,16 +5831,18 @@ lim_handle_sae_auth_retry(struct mac_context *mac_ctx, uint8_t vdev_id,
 		return;
 
 	pe_debug("SAE auth frame queued vdev_id %d seq_num %d",
-		 vdev_id, mac_ctx->mgmtSeqNum);
+		 vdev_id, mac_ctx->mgmtSeqNum + 1);
 	qdf_mem_copy(sae_retry->sae_auth.ptr, frame, frame_len);
 	mac_ctx->lim.lim_timers.g_lim_periodic_auth_retry_timer.sessionId =
 					session->peSessionId;
 	sae_retry->sae_auth.len = frame_len;
 	sae_retry->sae_auth_max_retry = retry_count;
 
+	val = mac_ctx->mlme_cfg->timeouts.sae_auth_failure_timeout;
+
 	tx_timer_change(
 		&mac_ctx->lim.lim_timers.g_lim_periodic_auth_retry_timer,
-		SYS_MS_TO_TICKS(WLAN_SAE_AUTH_TIMEOUT), 0);
+		SYS_MS_TO_TICKS(val), 0);
 	/* Activate Auth Retry timer */
 	if (tx_timer_activate(
 	    &mac_ctx->lim.lim_timers.g_lim_periodic_auth_retry_timer) !=
