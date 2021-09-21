7712bf610f6d947cccca188682833bf07aadf249
Madan Koyyalamudi <mkoyya@codeaurora.org>
2021-09-21 09:30:12 -0700

    Release 5.2.0.248P
    Release 5.2.0.248P
    
    Change-Id: I5e0029a55d824f6a263316701bcf3bc282f7aab5
    CRs-Fixed: 774533

--- core/mac/inc/qwlan_version.h
+++ core/mac/inc/qwlan_version.h
@@ -32,9 +32,9 @@
 #define QWLAN_VERSION_MAJOR            5
 #define QWLAN_VERSION_MINOR            2
 #define QWLAN_VERSION_PATCH            0
-#define QWLAN_VERSION_EXTRA            "O"
+#define QWLAN_VERSION_EXTRA            "P"
 #define QWLAN_VERSION_BUILD            248
 
-#define QWLAN_VERSIONSTR               "5.2.0.248O"
+#define QWLAN_VERSIONSTR               "5.2.0.248P"
 
 #endif /* QWLAN_VERSION_H */
