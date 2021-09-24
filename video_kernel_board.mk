23796052b7ae11cebe1d3f1699f4202e07619a37
Karthikeyan Periasamy <kperiasa@codeaurora.org>
2021-09-23 23:26:50 -0700

    Kalama-BU: add DLKM compliance to video dlkms
    Change-Id: I833edaa05b48f2493ae9c228a8e8ca3bde7844bc
    Signed-off-by: Karthikeyan Periasamy <kperiasa@codeaurora.org>

--- video_kernel_board.mk
+++ video_kernel_board.mk
@@ -1,6 +1,6 @@
 # SPDX-License-Identifier: GPL-2.0-only
 # Build video kernel driver
-ifneq ($(TARGET_USES_QMAA),true)
+ifneq ($(TARGET_KERNEL_DLKM_DISABLE),true)
 ifneq ($(TARGET_BOARD_AUTO),true)
 ifeq ($(call is-board-platform-in-list,$(TARGET_BOARD_PLATFORM)),true)
 BOARD_VENDOR_KERNEL_MODULES += $(KERNEL_MODULES_OUT)/msm_video.ko
