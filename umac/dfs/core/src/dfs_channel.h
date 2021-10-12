1bc1af8b6c7511d3a56db1e43a420d8a471ce5d8
Vignesh U <uvignesh@codeaurora.org>
2021-10-12 13:06:16 +0530

    qcacmn: Add dfs channel flags and channel check macros
    Add the following DFS channel flags for 11BE:
    1) WLAN_CHAN_320MHZ
    2) WLAN_CHAN_EHTCAP
    3) WLAN_CHAN_EHT20
    4) WLAN_CHAN_EHT40PLUS
    5) WLAN_CHAN_EHT40MINUS
    6) WLAN_CHAN_EHT80
    7) WLAN_CHAN_EHT160
    8) WLAN_CHAN_EHT320
    
    Add the following checks for both 5G channels and 6G channels:
    1) WLAN_IS_CHAN_11BE_EHT20
    2) WLAN_IS_CHAN_11BE_EHT40
    3) WLAN_IS_CHAN_11BE_EHT80
    4) WLAN_IS_CHAN_11BE_EHT160
    5) WLAN_IS_CHAN_11BE_EHT320
    
    Update the WLAN_IS_CHAN_DFS check to include 11BE modes.
    
    Change-Id: I2bd9e877aa0b4fca8f93bf2b72247f8fc29ae833

--- umac/dfs/core/src/dfs_channel.h
+++ umac/dfs/core/src/dfs_channel.h
@@ -39,11 +39,15 @@
 
 #define WLAN_CHAN_80_80MHZ         0x0000000000000600
 
+#define WLAN_CHAN_320MHZ           0x0000000000000700
+
 #define WLAN_CHAN_HTCAP            0x0000000000001000
 
 #define WLAN_CHAN_VHTCAP           0x0000000000002000
 
 #define WLAN_CHAN_HECAP            0x0000000000003000
+
+#define WLAN_CHAN_EHTCAP           0x0000000000004000
 
 /* 2 GHz spectrum channel. */
 #define WLAN_CHAN_2GHZ             0x0000000000000010
@@ -98,6 +102,24 @@
 
 /* HE 80_80 channel */
 #define WLAN_CHAN_HE80_80          (WLAN_CHAN_HECAP | WLAN_CHAN_80_80MHZ)
+
+/* EHT 20 channel */
+#define WLAN_CHAN_EHT20            (WLAN_CHAN_EHTCAP | WLAN_CHAN_20MHZ)
+
+/* EHT 40 with extension channel above */
+#define WLAN_CHAN_EHT40PLUS        (WLAN_CHAN_EHTCAP | WLAN_CHAN_40PLUS)
+
+/* EHT 40 with extension channel below */
+#define WLAN_CHAN_EHT40MINUS       (WLAN_CHAN_EHTCAP | WLAN_CHAN_40MINUS)
+
+/* EHT 80 channel */
+#define WLAN_CHAN_EHT80            (WLAN_CHAN_EHTCAP | WLAN_CHAN_80MHZ)
+
+/* EHT 160 channel */
+#define WLAN_CHAN_EHT160           (WLAN_CHAN_EHTCAP | WLAN_CHAN_160MHZ)
+
+/* EHT 320 channel */
+#define WLAN_CHAN_EHT320           (WLAN_CHAN_EHTCAP | WLAN_CHAN_320MHZ)
 
 /* OFDM channel */
 #define WLAN_CHAN_OFDM             0x0000000000400000
@@ -156,6 +178,20 @@
 
 #define WLAN_IS_CHAN_HE80_80(_c)     WLAN_IS_FLAG_HE80_80((_c)->dfs_ch_flags)
 
+#define WLAN_IS_CHAN_EHT20(_c)       WLAN_IS_FLAG_EHT20((_c)->dfs_ch_flags)
+
+#define WLAN_IS_CHAN_EHT40PLUS(_c)   WLAN_IS_FLAG_EHT40PLUS((_c)->dfs_ch_flags)
+
+#define WLAN_IS_CHAN_EHT40MINUS(_c)  WLAN_IS_FLAG_EHT40MINUS((_c)->dfs_ch_flags)
+
+#define WLAN_IS_CHAN_EHT40(_c)       WLAN_IS_FLAG_EHT40((_c)->dfs_ch_flags)
+
+#define WLAN_IS_CHAN_EHT80(_c)       WLAN_IS_FLAG_EHT80((_c)->dfs_ch_flags)
+
+#define WLAN_IS_CHAN_EHT160(_c)      WLAN_IS_FLAG_EHT160((_c)->dfs_ch_flags)
+
+#define WLAN_IS_CHAN_EHT320(_c)      WLAN_IS_FLAG_EHT320((_c)->dfs_ch_flags)
+
 #define WLAN_IS_CHAN_OFDM(_c)        WLAN_IS_FLAG_OFDM((_c)->dfs_ch_flags)
 
 #define WLAN_IS_CHAN_TURBO(_c)       WLAN_IS_FLAG_TURBO((_c)->dfs_ch_flags)
@@ -213,6 +249,27 @@
 
 #define WLAN_IS_FLAG_HE80_80(_flag) \
 	(((_flag) & WLAN_CHAN_BW_MASK) == WLAN_CHAN_HE80_80)
+
+#define WLAN_IS_FLAG_EHT20(_flag) \
+	(((_flag) & WLAN_CHAN_BW_MASK) == WLAN_CHAN_EHT20)
+
+#define WLAN_IS_FLAG_EHT40PLUS(_flag) \
+	(((_flag) & WLAN_CHAN_BW_MASK) == WLAN_CHAN_EHT40PLUS)
+
+#define WLAN_IS_FLAG_EHT40MINUS(_flag) \
+	(((_flag) & WLAN_CHAN_BW_MASK) == WLAN_CHAN_EHT40MINUS)
+
+#define WLAN_IS_FLAG_EHT40(_flag) \
+	((WLAN_IS_FLAG_EHT40PLUS(flag)) || (WLAN_IS_FLAG_EHT40MINUS(flag)))
+
+#define WLAN_IS_FLAG_EHT80(_flag) \
+	(((_flag) & WLAN_CHAN_BW_MASK) == WLAN_CHAN_EHT80)
+
+#define WLAN_IS_FLAG_EHT160(_flag) \
+	(((_flag) & WLAN_CHAN_BW_MASK) == WLAN_CHAN_EHT160)
+
+#define WLAN_IS_FLAG_EHT320(_flag) \
+	(((_flag) & WLAN_CHAN_BW_MASK) == WLAN_CHAN_EHT320)
 
 #define WLAN_IS_FLAG_OFDM(_flag) \
 	((_flag) & WLAN_CHAN_OFDM)
@@ -282,6 +339,28 @@
 #define WLAN_IS_CHAN_11AXA_HE80_80_5G(_c) \
 	(WLAN_IS_CHAN_5GHZ(_c) && WLAN_IS_CHAN_HE80_80(_c))
 
+#define WLAN_IS_CHAN_11BE_EHT20_5G(_c) \
+	(WLAN_IS_CHAN_5GHZ(_c) && WLAN_IS_CHAN_EHT20(_c))
+
+#define WLAN_IS_CHAN_11BE_EHT40PLUS_5G(_c) \
+	(WLAN_IS_CHAN_5GHZ(_c) && WLAN_IS_CHAN_EHT40PLUS(_c))
+
+#define WLAN_IS_CHAN_11BE_EHT40MINUS_5G(_c) \
+	(WLAN_IS_CHAN_5GHZ(_c) && WLAN_IS_CHAN_EHT40MINUS(_c))
+
+#define WLAN_IS_CHAN_11BE_EHT40_5G(_c) \
+	(WLAN_IS_CHAN_11BE_EHT40MINUS_5G(_c) ||  \
+	 WLAN_IS_CHAN_11BE_EHT40PLUS_5G(_c))
+
+#define WLAN_IS_CHAN_11BE_EHT80_5G(_c) \
+	(WLAN_IS_CHAN_5GHZ(_c) && WLAN_IS_CHAN_EHT80(_c))
+
+#define WLAN_IS_CHAN_11BE_EHT160_5G(_c) \
+	(WLAN_IS_CHAN_5GHZ(_c) && WLAN_IS_CHAN_EHT160(_c))
+
+#define WLAN_IS_CHAN_11BE_EHT320_5G(_c) \
+	(WLAN_IS_CHAN_5GHZ(_c) && WLAN_IS_CHAN_EHT320(_c))
+
 #define WLAN_IS_CHAN_11AXA_HE20_6G(_c) \
 	(WLAN_IS_CHAN_6GHZ(_c) && WLAN_IS_CHAN_HE20(_c))
 
@@ -304,6 +383,28 @@
 #define WLAN_IS_CHAN_11AXA_HE80_80_6G(_c) \
 	(WLAN_IS_CHAN_6GHZ(_c) && WLAN_IS_CHAN_HE80_80(_c))
 
+#define WLAN_IS_CHAN_11BE_EHT20_6G(_c) \
+	(WLAN_IS_CHAN_6GHZ(_c) && WLAN_IS_CHAN_EHT20(_c))
+
+#define WLAN_IS_CHAN_11BE_EHT40PLUS_6G(_c) \
+	(WLAN_IS_CHAN_6GHZ(_c) && WLAN_IS_CHAN_EHT40PLUS(_c))
+
+#define WLAN_IS_CHAN_11BE_EHT40MINUS_6G(_c) \
+	(WLAN_IS_CHAN_6GHZ(_c) && WLAN_IS_CHAN_EHT40MINUS(_c))
+
+#define WLAN_IS_CHAN_11BE_EHT40_6G(_c) \
+	(WLAN_IS_CHAN_11BE_EHT40MINUS_6G(_c) ||  \
+	 WLAN_IS_CHAN_11BE_EHT40PLUS_6G(_c))
+
+#define WLAN_IS_CHAN_11BE_EHT80_6G(_c) \
+	(WLAN_IS_CHAN_6GHZ(_c) && WLAN_IS_CHAN_EHT80(_c))
+
+#define WLAN_IS_CHAN_11BE_EHT160_6G(_c) \
+	(WLAN_IS_CHAN_6GHZ(_c) && WLAN_IS_CHAN_EHT160(_c))
+
+#define WLAN_IS_CHAN_11BE_EHT320_6G(_c) \
+	(WLAN_IS_CHAN_6GHZ(_c) && WLAN_IS_CHAN_EHT320(_c))
+
 #define WLAN_IS_CHAN_11AXA_HE20(_c) \
 	(WLAN_IS_CHAN_11AXA_HE20_5G(_c) || WLAN_IS_CHAN_11AXA_HE20_6G(_c))
 
@@ -324,9 +425,28 @@
 #define WLAN_IS_CHAN_11AXA_HE80_80(_c) \
 	(WLAN_IS_CHAN_11AXA_HE80_80_5G(_c) || WLAN_IS_CHAN_11AXA_HE80_80_6G(_c))
 
-#define WLAN_IS_CHAN_11AXA_HE80_80(_c) \
-	(WLAN_IS_CHAN_11AXA_HE80_80_5G(_c) || WLAN_IS_CHAN_11AXA_HE80_80_6G(_c))
+#define WLAN_IS_CHAN_11BE_EHT20(_c) \
+	(WLAN_IS_CHAN_11BE_EHT20_5G(_c) || WLAN_IS_CHAN_11BE_EHT20_6G(_c))
 
+#define WLAN_IS_CHAN_11BE_EHT40PLUS(_c) \
+	(WLAN_IS_CHAN_11BE_EHT40PLUS_5G(_c) || \
+	 WLAN_IS_CHAN_11BE_EHT40PLUS_6G(_c))
+
+#define WLAN_IS_CHAN_11BE_EHT40MINUS(_c) \
+	(WLAN_IS_CHAN_11BE_EHT40MINUS_5G(_c) || \
+	 WLAN_IS_CHAN_11BE_EHT40MINUS_6G(_c))
+
+#define WLAN_IS_CHAN_11BE_EHT40(_c) \
+	(WLAN_IS_CHAN_11BE_EHT40PLUS(_c) || WLAN_IS_CHAN_11BE_EHT40MINUS(_c))
+
+#define WLAN_IS_CHAN_11BE_EHT80(_c) \
+	(WLAN_IS_CHAN_11BE_EHT80_5G(_c) || WLAN_IS_CHAN_11BE_EHT80_6G(_c))
+
+#define WLAN_IS_CHAN_11BE_EHT160(_c) \
+	(WLAN_IS_CHAN_11BE_EHT160_5G(_c) || WLAN_IS_CHAN_11BE_EHT160_6G(_c))
+
+#define WLAN_IS_CHAN_11BE_EHT320(_c) \
+	(WLAN_IS_CHAN_11BE_EHT320_5G(_c) || WLAN_IS_CHAN_11BE_EHT320_6G(_c))
 #else
 
 /* OFDM channel */
@@ -388,6 +508,24 @@
 
 /* HE 80_80 channel */
 #define WLAN_CHAN_HE80_80          0x0000000800000000
+
+/* EHT 20 channel */
+#define WLAN_CHAN_EHT20            0x0000001000000000
+
+/* EHT 40 with extension channel above */
+#define WLAN_CHAN_EHT40PLUS        0x0000002000000000
+
+/* EHT 40 with extension channel below */
+#define WLAN_CHAN_EHT40MINUS       0x0000004000000000
+
+/* EHT 80 channel */
+#define WLAN_CHAN_EHT80            0x0000008000000000
+
+/* HE 160 channel */
+#define WLAN_CHAN_EHT160           0x0000010000000000
+
+/* HE 320 channel */
+#define WLAN_CHAN_EHT320           0x0000020000000000
 
 /* Turbo Channel */
 #define WLAN_CHAN_TURBO            0x0000000000000010
@@ -523,6 +661,42 @@
 #define WLAN_CHAN_11AXA_HE80_80_6G \
 	(WLAN_CHAN_6GHZ | WLAN_CHAN_HE80_80)
 
+#define WLAN_CHAN_11BE_EHT20 \
+	(WLAN_CHAN_5GHZ | WLAN_CHAN_EHT20)
+
+#define WLAN_CHAN_11BE_EHT20_6G \
+	(WLAN_CHAN_6GHZ | WLAN_CHAN_EHT20)
+
+#define WLAN_CHAN_11BE_EHT40PLUS \
+	(WLAN_CHAN_5GHZ | WLAN_CHAN_EHT40PLUS)
+
+#define WLAN_CHAN_11BE_EHT40PLUS_6G \
+	(WLAN_CHAN_6GHZ | WLAN_CHAN_EHT40PLUS)
+
+#define WLAN_CHAN_11BE_EHT40MINUS \
+	(WLAN_CHAN_5GHZ | WLAN_CHAN_EHT40MINUS)
+
+#define WLAN_CHAN_11BE_EHT40MINUS_6G \
+	(WLAN_CHAN_6GHZ | WLAN_CHAN_EHT40MINUS)
+
+#define WLAN_CHAN_11BE_EHT80 \
+	(WLAN_CHAN_5GHZ | WLAN_CHAN_EHT80)
+
+#define WLAN_CHAN_11BE_EHT80_6G \
+	(WLAN_CHAN_6GHZ | WLAN_CHAN_EHT80)
+
+#define WLAN_CHAN_11BE_EHT160 \
+	(WLAN_CHAN_5GHZ | WLAN_CHAN_EHT160)
+
+#define WLAN_CHAN_11BE_EHT160_6G \
+	(WLAN_CHAN_6GHZ | WLAN_CHAN_EHT160)
+
+#define WLAN_CHAN_11BE_EHT320 \
+	(WLAN_CHAN_5GHZ | WLAN_CHAN_EHT320)
+
+#define WLAN_CHAN_11BE_EHT320_6G \
+	(WLAN_CHAN_6GHZ | WLAN_CHAN_EHT320)
+
 #define WLAN_CHAN_108G \
 	(WLAN_CHAN_2GHZ | WLAN_CHAN_OFDM | WLAN_CHAN_TURBO)
 
@@ -561,6 +735,42 @@
 	 WLAN_CHAN_11AXA_HE80_80) || \
 	 (((_c)->dfs_ch_flags & WLAN_CHAN_11AXA_HE80_80_6G) == \
 	  WLAN_CHAN_11AXA_HE80_80_6G))
+
+#define WLAN_IS_CHAN_11BE_EHT20(_c) \
+	((((_c)->dfs_ch_flags & WLAN_CHAN_11BE_EHT20) == \
+	 WLAN_CHAN_11BE_EHT20) || \
+	 (((_c)->dfs_ch_flags & WLAN_CHAN_11BE_EHT20_6G) == \
+	 WLAN_CHAN_11BE_EHT20_6G))
+
+#define WLAN_IS_CHAN_11BE_EHT40PLUS(_c) \
+	((((_c)->dfs_ch_flags & WLAN_CHAN_11BE_EHT40PLUS) == \
+	 WLAN_CHAN_11BE_EHT40PLUS) || \
+	 (((_c)->dfs_ch_flags & WLAN_CHAN_11BE_EHT40PLUS_6G) == \
+	  WLAN_CHAN_11BE_EHT40PLUS_6G))
+
+#define WLAN_IS_CHAN_11BE_EHT40MINUS(_c) \
+	((((_c)->dfs_ch_flags & WLAN_CHAN_11BE_EHT40MINUS) == \
+	 WLAN_CHAN_11BE_EHT40MINUS) || \
+	 (((_c)->dfs_ch_flags & WLAN_CHAN_11BE_EHT40MINUS_6G) == \
+	 WLAN_CHAN_11BE_EHT40MINUS_6G))
+
+#define WLAN_IS_CHAN_11BE_EHT80(_c) \
+	((((_c)->dfs_ch_flags & WLAN_CHAN_11BE_EHT80) == \
+	 WLAN_CHAN_11BE_EHT80) || \
+	 (((_c)->dfs_ch_flags & WLAN_CHAN_11BE_EHT80_6G) == \
+	  WLAN_CHAN_11BE_EHT80_6G))
+
+#define WLAN_IS_CHAN_11BE_EHT160(_c) \
+	((((_c)->dfs_ch_flags & WLAN_CHAN_11BE_EHT160) == \
+	 WLAN_CHAN_11BE_EHT160) || \
+	 (((_c)->dfs_ch_flags & WLAN_CHAN_11BE_EHT160_6G) == \
+	  WLAN_CHAN_11BE_EHT160_6G))
+
+#define WLAN_IS_CHAN_11BE_EHT320(_c) \
+	((((_c)->dfs_ch_flags & WLAN_CHAN_11BE_EHT320) == \
+	 WLAN_CHAN_11BE_EHT320) || \
+	 (((_c)->dfs_ch_flags & WLAN_CHAN_11BE_EHT320_6G) == \
+	  WLAN_CHAN_11BE_EHT320_6G))
 
 #define WLAN_IS_CHAN_TURBO(_c) \
 	(((_c)->dfs_ch_flags & WLAN_CHAN_TURBO) != 0)
@@ -605,7 +815,9 @@
 	 ((WLAN_IS_CHAN_11AC_VHT160(_c) || \
 	 WLAN_IS_CHAN_11AC_VHT80_80(_c) || \
 	 WLAN_IS_CHAN_11AXA_HE160(_c) || \
-	 WLAN_IS_CHAN_11AXA_HE80_80(_c)) \
+	 WLAN_IS_CHAN_11AXA_HE80_80(_c) || \
+	 WLAN_IS_CHAN_11BE_EHT160(_c) || \
+	 WLAN_IS_CHAN_11BE_EHT320(_c)) \
 	&& WLAN_IS_CHAN_DFS_CFREQ2(_c)))
 
 #ifdef CONFIG_HOST_FIND_CHAN
@@ -634,7 +846,8 @@
 	(WLAN_IS_CHAN_A(_c)        ||    \
 	 WLAN_IS_CHAN_11NA_HT20(_c)  ||  \
 	 WLAN_IS_CHAN_11AC_VHT20(_c) ||  \
-	 WLAN_IS_CHAN_11AXA_HE20(_c))
+	 WLAN_IS_CHAN_11AXA_HE20(_c) ||  \
+	 WLAN_IS_CHAN_11BE_EHT20(_c))
 
 #define WLAN_IS_CHAN_MODE_40(_c)          \
 	(WLAN_IS_CHAN_11AC_VHT40PLUS(_c)  || \
@@ -642,15 +855,19 @@
 	 WLAN_IS_CHAN_11NA_HT40PLUS(_c)   || \
 	 WLAN_IS_CHAN_11NA_HT40MINUS(_c)  || \
 	 WLAN_IS_CHAN_11AXA_HE40PLUS(_c)  || \
-	 WLAN_IS_CHAN_11AXA_HE40MINUS(_c))
+	 WLAN_IS_CHAN_11AXA_HE40MINUS(_c) || \
+	 WLAN_IS_CHAN_11BE_EHT40PLUS(_c)  || \
+	 WLAN_IS_CHAN_11BE_EHT40MINUS(_c))
 
 #define WLAN_IS_CHAN_MODE_80(_c)          \
 	(WLAN_IS_CHAN_11AC_VHT80(_c)      || \
-	 WLAN_IS_CHAN_11AXA_HE80(_c))
+	 WLAN_IS_CHAN_11AXA_HE80(_c)      || \
+	 WLAN_IS_CHAN_11BE_EHT80(_c))
 
 #define WLAN_IS_CHAN_MODE_160(_c)         \
 	(WLAN_IS_CHAN_11AC_VHT160(_c)     || \
-	 WLAN_IS_CHAN_11AXA_HE160(_c))
+	 WLAN_IS_CHAN_11AXA_HE160(_c)     || \
+	 WLAN_IS_CHAN_11BE_EHT160(_c))
 
 #define WLAN_IS_CHAN_MODE_80_80(_c)       \
 	(WLAN_IS_CHAN_11AC_VHT80_80(_c)   || \
@@ -659,5 +876,7 @@
 #define WLAN_IS_CHAN_MODE_165(_dfs, _c) \
 	(dfs_is_restricted_80p80mhz_supported(_dfs) && \
 	WLAN_IS_CHAN_MODE_80_80(_c))
+
+#define WLAN_IS_CHAN_MODE_320(_c) WLAN_IS_CHAN_11BE_EHT320(_c)
 
 #endif /* _DFS_CHANNEL_H_ */
