a33322dbe4b0ce2444877c09bd6d593efc0a3a55
Pragaspathi Thilagaraj <quic_tpragasp@quicinc.com>
2021-10-28 19:41:00 +0530

    qcacld-3.0: Flush the specific channels on ADDROAMSCANFREQUENCIES
    When ADDROAMSCANFREQUENCIES is received, the specific channel
    list is not flushed in the driver. This causes the static
    channel list to be sent to firmware.
    
    Flush the specific channel list when the ADDROAMSCANFREQUENCIES
    command is received.
    
    Change-Id: Id0422f9c5a4b10530fecc309a78cedf9adbc43af
    CRs-Fixed: 3063734

--- umac/mlme/connection_mgr/dispatcher/src/wlan_cm_roam_api.c
+++ umac/mlme/connection_mgr/dispatcher/src/wlan_cm_roam_api.c
@@ -15,6 +15,35 @@
  */
 
 /*
+ * Copyright (c) 2021 Qualcomm Innovation Center, Inc. All rights reserved.
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted (subject to the limitations in the
+ * disclaimer below) provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above
+ *       copyright notice, this list of conditions and the following
+ *       disclaimer in the documentation and/or other materials provided
+ *       with the distribution.
+ *     * Neither the name of Qualcomm Innovation Center, Inc. nor the names of
+ *       its contributors may be used to endorse or promote products derived
+ *       from this software without specific prior written permission.
+ * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE
+ * GRANTED BY THIS LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT
+ * HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+ * GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
+ * IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
+ * IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+/*
  * DOC: wlan_cm_roam_api.c
  *
  * Implementation for the Common Roaming interfaces.
@@ -1284,6 +1313,13 @@ wlan_cm_roam_cfg_set_value(struct wlan_objmgr_psoc *psoc, uint8_t vdev_id,
 					   REASON_ROAM_CONTROL_CONFIG_ENABLED);
 		break;
 	case ROAM_PREFERRED_CHAN:
+		/*
+		 * In RSO update command, the specific channel list is
+		 * given priority. So flush the Specific channel list if
+		 * preferred channel list is received. Else the channel list
+		 * type will be filled as static instead of dynamic.
+		 */
+		cm_flush_roam_channel_list(&dst_cfg->specific_chan_info);
 		status = cm_update_roam_scan_channel_list(psoc, vdev, rso_cfg,
 					vdev_id, &dst_cfg->pref_chan_info,
 					src_config->chan_info.freq_list,
