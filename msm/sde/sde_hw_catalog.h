f5d5133807c8fbe7dfc397090390de7957150aca
Raviteja Tamatam <travitej@codeaurora.org>
2021-11-03 14:25:49 +0530

    disp: msm: sde: add rev checks for cape target
    Add required revision checks from display for
    cape target.
    
    Change-Id: Ieb2b0b23462ff122b0090e7c78d8da41fa78fc07
    Signed-off-by: Raviteja Tamatam <travitej@codeaurora.org>

--- msm/sde/sde_hw_catalog.h
+++ msm/sde/sde_hw_catalog.h
@@ -50,6 +50,7 @@
 #define SDE_HW_VER_720	SDE_HW_VER(7, 2, 0) /* yupik */
 #define SDE_HW_VER_810	SDE_HW_VER(8, 1, 0) /* waipio */
 #define SDE_HW_VER_820	SDE_HW_VER(8, 2, 0) /* diwali */
+#define SDE_HW_VER_850	SDE_HW_VER(8, 5, 0) /* cape */
 
 /* Avoid using below IS_XXX macros outside catalog, use feature bit instead */
 #define IS_SDE_MAJOR_SAME(rev1, rev2)   \
@@ -77,6 +78,7 @@
 #define IS_YUPIK_TARGET(rev) IS_SDE_MAJOR_MINOR_SAME((rev), SDE_HW_VER_720)
 #define IS_WAIPIO_TARGET(rev) IS_SDE_MAJOR_MINOR_SAME((rev), SDE_HW_VER_810)
 #define IS_DIWALI_TARGET(rev) IS_SDE_MAJOR_MINOR_SAME((rev), SDE_HW_VER_820)
+#define IS_CAPE_TARGET(rev) IS_SDE_MAJOR_MINOR_SAME((rev), SDE_HW_VER_850)
 
 #define SDE_HW_BLK_NAME_LEN	16
 
