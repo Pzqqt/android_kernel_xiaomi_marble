3eb4ac22d6a6c54b029c6c43c63b05bf5549d5b4
Madan Koyyalamudi <mkoyya@codeaurora.org>
2021-11-18 04:31:22 -0800

    Release 5.2.1.1J
    Release 5.2.1.1J
    
    Change-Id: Ic256e9252e25db907fd6169f8df8c756112aff28
    CRs-Fixed: 774533

--- core/mac/inc/qwlan_version.h
+++ core/mac/inc/qwlan_version.h
@@ -32,9 +32,9 @@
 #define QWLAN_VERSION_MAJOR            5
 #define QWLAN_VERSION_MINOR            2
 #define QWLAN_VERSION_PATCH            1
-#define QWLAN_VERSION_EXTRA            "I"
+#define QWLAN_VERSION_EXTRA            "J"
 #define QWLAN_VERSION_BUILD            1
 
-#define QWLAN_VERSIONSTR               "5.2.1.1I"
+#define QWLAN_VERSIONSTR               "5.2.1.1J"
 
 #endif /* QWLAN_VERSION_H */
