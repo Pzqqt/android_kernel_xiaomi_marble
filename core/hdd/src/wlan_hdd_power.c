304d128a082fb7a05c99798c3d0f97a0d21bd077
Yeshwanth Sriram Guntuka <quic_ysriramg@quicinc.com>
2021-11-30 15:15:35 +0530

    qcacld-3.0: Disable intrabss fwd on SAP/P2P GO on sys suspend
    REO interrupt is fired after sending WOW_ENABLE command
    to FW. Multicast packets are processed and dp_tx_send is
    invoked as part of intrabss forwarding. This results in
    a FW exception.
    
    Fix is to disable intrabss forwarding on SAP/P2P GO
    on system suspend and enable it back on system resume.
    
    Change-Id: If9f20bf59b776016dac3c1a27200deafa80266b5
    CRs-Fixed: 3084288

--- core/hdd/src/wlan_hdd_power.c
+++ core/hdd/src/wlan_hdd_power.c
@@ -1,5 +1,6 @@
 /*
  * Copyright (c) 2012-2021 The Linux Foundation. All rights reserved.
+ * Copyright (c) 2021 Qualcomm Innovation Center, Inc. All rights reserved.
  *
  * Permission to use, copy, modify, and/or distribute this software for
  * any purpose with or without fee is hereby granted, provided that the
@@ -88,6 +89,8 @@
 #include <linux/igmp.h>
 #include "qdf_types.h"
 #include <linux/cpuidle.h>
+#include <cdp_txrx_ctrl.h>
+
 /* Preprocessor definitions and constants */
 #ifdef QCA_WIFI_EMULATION
 #define HDD_SSR_BRING_UP_TIME 3000000
@@ -1648,6 +1651,8 @@ hdd_suspend_wlan(void)
 	QDF_STATUS status;
 	struct hdd_adapter *adapter = NULL, *next_adapter = NULL;
 	uint32_t conn_state_mask = 0;
+	ol_txrx_soc_handle soc = cds_get_context(QDF_MODULE_ID_SOC);
+	cdp_config_param_type val = {0};
 
 	hdd_info("WLAN being suspended by OS");
 
@@ -1679,6 +1684,11 @@ hdd_suspend_wlan(void)
 		if (adapter->device_mode == QDF_STA_MODE)
 			status = hdd_enable_default_pkt_filters(adapter);
 
+		if (adapter->device_mode == QDF_SAP_MODE ||
+		    adapter->device_mode == QDF_P2P_GO_MODE)
+			cdp_txrx_set_vdev_param(soc, adapter->vdev_id,
+						CDP_ENABLE_AP_BRIDGE, val);
+
 		/* Configure supported OffLoads */
 		hdd_enable_host_offloads(adapter, pmo_apps_suspend);
 		hdd_update_conn_state_mask(adapter, &conn_state_mask);
@@ -1709,6 +1719,8 @@ static int hdd_resume_wlan(void)
 	struct hdd_context *hdd_ctx;
 	struct hdd_adapter *adapter, *next_adapter = NULL;
 	QDF_STATUS status;
+	ol_txrx_soc_handle soc = cds_get_context(QDF_MODULE_ID_SOC);
+	cdp_config_param_type val = {0};
 
 	hdd_info("WLAN being resumed by OS");
 
@@ -1746,6 +1758,13 @@ static int hdd_resume_wlan(void)
 
 		if (adapter->device_mode == QDF_STA_MODE)
 			status = hdd_disable_default_pkt_filters(adapter);
+
+		if (adapter->device_mode == QDF_SAP_MODE ||
+		    adapter->device_mode == QDF_P2P_GO_MODE) {
+			val.cdp_vdev_param_ap_brdg_en = true;
+			cdp_txrx_set_vdev_param(soc, adapter->vdev_id,
+						CDP_ENABLE_AP_BRIDGE, val);
+		}
 
 		hdd_adapter_dev_put_debug(adapter, NET_DEV_HOLD_RESUME_WLAN);
 	}
