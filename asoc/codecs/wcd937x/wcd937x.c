ea8a0670e5ebdca76eec8f42759e9cff3f67471c
Aditya Bavanari <quic_abavanar@quicinc.com>
2021-12-01 20:19:24 +0530

    asoc: codecs: wcd937x: Remove QGKI config flag check
    As clear_ack is supported by default and needs to be
    enabled for wcd937x irq, remove QGKI
    config flag check to fix the headset detection issues in
    wcd937x.
    
    Change-Id: I53bc8013f39e24ce36e3fa45bcb7cf7fb2e0ca25

--- asoc/codecs/wcd937x/wcd937x.c
+++ asoc/codecs/wcd937x/wcd937x.c
@@ -102,9 +102,7 @@ static struct regmap_irq_chip wcd937x_regmap_irq_chip = {
 	.mask_base = WCD937X_DIGITAL_INTR_MASK_0,
 	.ack_base = WCD937X_DIGITAL_INTR_CLEAR_0,
 	.use_ack = 1,
-#if IS_ENABLED(CONFIG_AUDIO_QGKI)
 	.clear_ack = 1,
-#endif
 	.type_base = WCD937X_DIGITAL_INTR_LEVEL_0,
 	.runtime_pm = false,
 	.handle_post_irq = wcd937x_handle_post_irq,
