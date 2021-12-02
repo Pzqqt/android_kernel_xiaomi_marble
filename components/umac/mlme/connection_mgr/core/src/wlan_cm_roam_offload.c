0245d58cddcb2e2f0ad5ea6dd0a137b8c4b6b733
Jyoti Kumari <quic_jyotkuma@quicinc.com>
2021-12-02 14:38:35 +0530

    qcacld-3.0: Use proper api to parse vendor specific IE
    Use wlan_get_vendor_ie_ptr_from_oui() instead of
    wlan_get_ext_ie_ptr_from_ext_id() to parse vendor specific
    WFA Hotspot-2.0 IE
    
    Change-Id: Ie9ee6c1f16d0eb83f52b7d9afa97f362f8400ff3
    CRs-Fixed: 3080931

--- components/umac/mlme/connection_mgr/core/src/wlan_cm_roam_offload.c
+++ components/umac/mlme/connection_mgr/core/src/wlan_cm_roam_offload.c
@@ -2481,12 +2481,18 @@ cm_update_btm_offload_config(struct wlan_objmgr_psoc *psoc,
 	/* Return if INI is disabled */
 	if (!(*btm_offload_config))
 		return;
+
 	vdev_id = wlan_vdev_get_id(vdev);
 	wlan_cm_roam_cfg_get_value(psoc, vdev_id, HS_20_AP, &temp);
 	is_hs_20_ap = temp.bool_value;
-	/* For RSO Stop Disable BTM offload to firmware */
+
+	/*
+	 * For RSO Stop/Passpoint R2 cert test case 5.11(when STA is connected
+	 * to Hotspot-2.0 AP), disable BTM offload to firmware
+	 */
 	if (command == ROAM_SCAN_OFFLOAD_STOP || is_hs_20_ap) {
-		mlme_debug("RSO cmd: %d", command);
+		mlme_debug("RSO cmd: %d is_hs_20_ap:%d", command,
+			   is_hs_20_ap);
 		*btm_offload_config = 0;
 		return;
 	}
