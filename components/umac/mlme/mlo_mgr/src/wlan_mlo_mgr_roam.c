a32d1bfd8a0e3c083202399f9f3fe2246116eee7
Amruta Kulkarni <quic_amrukulk@quicinc.com>
2021-12-08 16:09:10 -0800

    qcacld-3.0: Add new files for mlo roaming
    Add new files for mlo roaming and update Kbuild file.
    
    Change-Id: I3e672a5e068648fd836ecccb0edbac084850f11d
    CRs-Fixed: 3090925

---
+++ components/umac/mlme/mlo_mgr/src/wlan_mlo_mgr_roam.c
@@ -0,0 +1,71 @@
+/*
+ * Copyright (c) 2021 Qualcomm Innovation Center, Inc. All rights reserved.
+ *
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
+/*
+ * DOC: contains MLO manager roaming related functionality
+ */
+#include <wlan_cmn.h>
+#include <wlan_cm_public_struct.h>
+#include <wlan_cm_roam_public_struct.h>
+#include "wlan_mlo_mgr_cmn.h"
+#include "wlan_mlo_mgr_main.h"
+#include "wlan_mlo_mgr_roam.h"
+#include "wlan_mlo_mgr_public_structs.h"
+#include "wlan_mlo_mgr_sta.h"
+#include <../../core/src/wlan_cm_roam_i.h>
+#include "wlan_cm_roam_api.h"
+
+QDF_STATUS mlo_fw_roam_sync_req(struct wlan_objmgr_psoc *psoc, uint8_t vdev_id,
+				void *event, uint32_t event_data_len)
+{
+	struct roam_offload_synch_ind *sync_ind;
+
+	sync_ind = (struct roam_offload_synch_ind *)event;
+	if (!sync_ind)
+		return QDF_STATUS_E_FAILURE;
+
+	return QDF_STATUS_SUCCESS;
+}
+
+void
+mlo_fw_ho_fail_req(struct wlan_objmgr_psoc *psoc,
+		   uint8_t vdev_id, struct qdf_mac_addr bssid)
+{
+}
+
+QDF_STATUS
+mlo_get_sta_link_mac_addr(uint8_t vdev_id,
+			  struct roam_offload_synch_ind *sync_ind,
+			  struct qdf_mac_addr *link_mac_addr)
+{
+	QDF_STATUS status = QDF_STATUS_SUCCESS;
+
+	if (!sync_ind || !sync_ind->num_setup_links)
+		return QDF_STATUS_E_FAILURE;
+
+	return status;
+}
+
+bool is_multi_link_roam(struct roam_offload_synch_ind *sync_ind)
+{
+	if (!sync_ind)
+		return false;
+
+	if (sync_ind->num_setup_links)
+		return true;
+
+	return false;
+}
