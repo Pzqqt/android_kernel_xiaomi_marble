46b05743d85f7ae38e5ce8aed2e32f9c57ed7da3
Jianmin Zhu <quic_jianminz@quicinc.com>
2021-12-31 17:08:44 +0800

    qcacld-3.0: sta roam failed after sap stopped
    When STA+SAP concurrency,  sta roaming pcl will remove all channels of SAP
    band except scc channel to avoid mcc.
    
    When SAP stopped, STA roaming pcl isn't updated for wrong vdev id is used,
    so can't roam to all channels of old SAP band except scc channel to avoid
    mcc.
    
    Change-Id: I35adb55ac155e4c84f858f16796dbec12589ab2f
    CRs-Fixed: 3102601

--- components/cmn_services/policy_mgr/src/wlan_policy_mgr_pcl.c
+++ components/cmn_services/policy_mgr/src/wlan_policy_mgr_pcl.c
@@ -1,5 +1,6 @@
 /*
  * Copyright (c) 2012-2021 The Linux Foundation. All rights reserved.
+ * Copyright (c) 2021-2022 Qualcomm Innovation Center, Inc. All rights reserved.
  *
  * Permission to use, copy, modify, and/or distribute this software for
  * any purpose with or without fee is hereby granted, provided that the
@@ -200,15 +201,15 @@ void policy_mgr_decr_session_set_pcl(struct wlan_objmgr_psoc *psoc,
 
 			/* Send RSO stop before sending set pcl command */
 			pm_ctx->sme_cbacks.sme_rso_stop_cb(
-						mac_handle, session_id,
+						mac_handle, vdev_id,
 						REASON_DRIVER_DISABLED,
 						RSO_SET_PCL);
 
 			policy_mgr_set_pcl_for_existing_combo(psoc, PM_STA_MODE,
-							      session_id);
+							      vdev_id);
 
 			pm_ctx->sme_cbacks.sme_rso_start_cb(
-					mac_handle, session_id,
+					mac_handle, vdev_id,
 					REASON_DRIVER_ENABLED,
 					RSO_SET_PCL);
 		}
