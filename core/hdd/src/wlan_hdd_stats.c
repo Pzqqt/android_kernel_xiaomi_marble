ec5a62e19b328bfac49222f5a800eee738555f81
Bapiraju Alla <quic_balla@quicinc.com>
2022-01-05 15:38:11 +0530

    qcacld-3.0: Reject LL stats request for SAP mode
    LL stats request is not supported for SAP mode. But driver is not checking
    for device mode for LL stats request. Instead it is depending on LL stats
    request set flag which won't be set for SAP mode. In case of mode switch
    from STA to SAP, it is possible that LL stats request set flag will be set
    for STA mode and LL stats are requested after switching to SAP mode. This
    will bypass the mode SAP.
    
    To avoid this, reject LL stats request for SAP mode.
    
    Change-Id: I8c680288991126f0b349584f502b22e18af72287
    CRs-Fixed: 3081453

--- core/hdd/src/wlan_hdd_stats.c
+++ core/hdd/src/wlan_hdd_stats.c
@@ -1,6 +1,6 @@
 /*
  * Copyright (c) 2012-2021 The Linux Foundation. All rights reserved.
- * Copyright (c) 2021 Qualcomm Innovation Center, Inc. All rights reserved.
+ * Copyright (c) 2021-2022 Qualcomm Innovation Center, Inc. All rights reserved.
  *
  * Permission to use, copy, modify, and/or distribute this software for
  * any purpose with or without fee is hereby granted, provided that the
@@ -2206,6 +2206,11 @@ __wlan_hdd_cfg80211_ll_stats_get(struct wiphy *wiphy,
 	if (!adapter->is_link_layer_stats_set) {
 		hdd_nofl_debug("is_link_layer_stats_set: %d",
 			       adapter->is_link_layer_stats_set);
+		return -EINVAL;
+	}
+
+	if (adapter->device_mode == QDF_SAP_MODE) {
+		hdd_nofl_debug("LL_STATS get is not supported for SAP mode");
 		return -EINVAL;
 	}
 
