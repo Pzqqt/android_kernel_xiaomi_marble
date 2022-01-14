7eec683b18cdaba68305ce4da19aea1a2d8c3551
Rajeev Kumar <quic_rajekuma@quicinc.com>
2022-01-13 22:59:35 -0800

    qcacmn: Clear num stats over QMI once WoW enter/exit
    Num stats over QMI indicates how many stats requests are fetched
    without triggering RTPM resume. If RTPM resume/suspend happens in
    the middle then num stats over QMI counter should reset. Add logic
    to reset num stats over QMI counter during WoW enter/exit.
    
    Change-Id: I34fe93759bf8a7a2a6c0d4b389ad96b8e1ba4d6c
    CRs-Fixed: 3110590

--- wmi/src/wmi_unified.c
+++ wmi/src/wmi_unified.c
@@ -1,6 +1,6 @@
 /*
  * Copyright (c) 2015-2021 The Linux Foundation. All rights reserved.
- * Copyright (c) 2021 Qualcomm Innovation Center, Inc. All rights reserved.
+ * Copyright (c) 2021-2022 Qualcomm Innovation Center, Inc. All rights reserved.
  *
  * Permission to use, copy, modify, and/or distribute this software for
  * any purpose with or without fee is hereby granted, provided that the
@@ -3657,6 +3657,7 @@ void wmi_set_target_suspend(wmi_unified_t wmi_handle, A_BOOL val)
 void wmi_set_target_suspend_acked(wmi_unified_t wmi_handle, A_BOOL val)
 {
 	qdf_atomic_set(&wmi_handle->is_target_suspend_acked, val);
+	qdf_atomic_set(&wmi_handle->num_stats_over_qmi, 0);
 }
 
 /**
