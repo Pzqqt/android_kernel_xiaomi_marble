107f473e54dfbaf00af286d228ae1e93ba253cc1
Yashwanth <quic_yvulapu@quicinc.com>
2022-01-20 18:49:27 +0530

    disp: msm: update copyright description
    This change updates copyright description with correct
    license marking as per the guidelines.
    
    Change-Id: Ia74b721e78afcc7f8e88bcbccfcf15430111ec37
    Signed-off-by: Yashwanth <quic_yvulapu@quicinc.com>

--- msm/sde/sde_encoder.c
+++ msm/sde/sde_encoder.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021-2022, Qualcomm Innovation Center, Inc. All rights reserved.
+ * Copyright (c) 2021-2022 Qualcomm Innovation Center, Inc. All rights reserved.
  * Copyright (c) 2014-2021, The Linux Foundation. All rights reserved.
  * Copyright (C) 2013 Red Hat
  * Author: Rob Clark <robdclark@gmail.com>
