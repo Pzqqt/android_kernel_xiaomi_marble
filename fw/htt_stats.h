c3efac9930a12533c3e47235f28beffd1b260918
spuligil <quic_spuligil@quicinc.com>
2022-01-20 18:01:26 -0800

    fw-api: CL 17004158 - update fw common interface files
    Update copyright header in htt_stats.h file.
    Update copyright header in htt_stats.h file based on new guideance from LOST team that current ownership should be for Qualcomm Innovation Center , not Linux Foundation.Similar changes will be applied in other wlanfw_cmn .h files in a follow-up change.
    
    Change-Id: I4441b5225bedfab969a97974c7f2f8296b0c13f6
    CRs-Fixed: 2262693

--- fw/htt_stats.h
+++ fw/htt_stats.h
@@ -1,5 +1,6 @@
 /*
- * Copyright (c) 2017-2022 The Linux Foundation. All rights reserved.
+ * Copyright (c) 2017-2021 The Linux Foundation. All rights reserved.
+ * Copyright (c) 2021-2022 Qualcomm Innovation Center, Inc. All rights reserved.
  *
  * Permission to use, copy, modify, and/or distribute this software for
  * any purpose with or without fee is hereby granted, provided that the
