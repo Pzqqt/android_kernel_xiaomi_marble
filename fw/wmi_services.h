5051c33aecd10b3732e20ba447d71872e4a5d71c
spuligil <quic_spuligil@quicinc.com>
2022-01-25 06:00:50 -0800

    fw-api: CL 17040921 - update fw common interface files
    Modify copyright to be Qualcomm Innovation Center in files modified in 2022
    
    Change-Id: Ie04aac9816e43ac664f31a88270083673b5722cf
    CRs-Fixed: 2262693

--- fw/wmi_services.h
+++ fw/wmi_services.h
@@ -1,5 +1,6 @@
 /*
- * Copyright (c) 2011-2022 The Linux Foundation. All rights reserved.
+ * Copyright (c) 2011-2021 The Linux Foundation. All rights reserved.
+ * Copyright (c) 2021-2022 Qualcomm Innovation Center, Inc. All rights reserved.
  *
  * Previously licensed under the ISC license by Qualcomm Atheros, Inc.
  *
