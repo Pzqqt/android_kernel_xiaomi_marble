9a4eb00222ec7fd38666e5819d0da0fb1f6b6ed5
Jingxiang Ge <quic_jge@quicinc.com>
2022-01-28 13:59:11 +0800

    qcacmn: Revise LF copyright back
    There is no need to update LF date if using QuIC account.
    
    Revise I29b998939cfa25f0547a9871721c6daeb0fdd961.
    
    Change-Id: Id27e90487df4023e45482792e726ba82c59f0b84
    CRs-Fixed: 3119512

--- hif/inc/hif.h
+++ hif/inc/hif.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2013-2022 The Linux Foundation. All rights reserved.
+ * Copyright (c) 2013-2021 The Linux Foundation. All rights reserved.
  * Copyright (c) 2021-2022 Qualcomm Innovation Center, Inc. All rights reserved.
  *
  * Permission to use, copy, modify, and/or distribute this software for
