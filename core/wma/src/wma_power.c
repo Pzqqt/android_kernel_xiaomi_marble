508249f9a39a93776d7fe71cf55b92781c1897d7
Vinod Kumar Myadam <quic_vmyadam@quicinc.com>
2022-02-15 20:21:42 +0530

    qcacld-3.0: Fix validate vdev param
    In wma_unified_set_sta_ps_param(), sending vdev param to
    fw with out validate the vdev param, effects crash in fw
    for the invalid vdev.
    
    Add validate vdev param to send proper context data to fw.
    
    Change-Id: I75206cff745a2774f30b36e8e6261cb452d749e8
    CRs-Fixed: 3128593

--- core/wma/src/wma_power.c
+++ core/wma/src/wma_power.c
@@ -1,5 +1,6 @@
 /*
  * Copyright (c) 2013-2021 The Linux Foundation. All rights reserved.
+ * Copyright (c) 2022 Qualcomm Innovation Center, Inc. All rights reserved.
  *
  * Permission to use, copy, modify, and/or distribute this software for
  * any purpose with or without fee is hereby granted, provided that the
@@ -108,6 +109,8 @@ QDF_STATUS wma_unified_set_sta_ps_param(wmi_unified_t wmi_handle,
 	wma = cds_get_context(QDF_MODULE_ID_WMA);
 	if (!wma)
 		return QDF_STATUS_E_FAILURE;
+	if (!wma_is_vdev_valid(vdev_id))
+		return QDF_STATUS_E_INVAL;
 
 	wma_debug("Set Sta Ps param vdevId %d Param %d val %d",
 		 vdev_id, param, value);
