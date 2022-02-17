e2fdbc9c9e1dd5dc3905cab73ecef9137592f43d
Alan Chen <quic_alache@quicinc.com>
2022-02-17 14:59:10 -0800

    qcacmn: Add new cfg80211 wrapper for registering net device
    Add a new wrapper wlan_cfg80211_register_netdevice() that calls
    cfg80211_register_netdevice for Kernel 5.12+.
    
    Change-Id: I17655f1e3123d663d25b9dfc82e7c6f737486391
    CRs-Fixed: 3133310

--- os_if/linux/wlan_cfg80211.h
+++ os_if/linux/wlan_cfg80211.h
@@ -1,5 +1,6 @@
 /*
  * Copyright (c) 2016-2021 The Linux Foundation. All rights reserved.
+ * Copyright (c) 2022 Qualcomm Innovation Center, Inc. All rights reserved.
  *
  * Permission to use, copy, modify, and/or distribute this software for
  * any purpose with or without fee is hereby granted, provided that the
@@ -471,4 +472,29 @@ wlan_cfg80211_nla_strscpy(char *dst, const struct nlattr *nla, size_t dstsize)
 	return nla_strscpy(dst, nla, dstsize);
 }
 #endif
+
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(5, 12, 0)
+static inline int wlan_cfg80211_register_netdevice(struct net_device *dev)
+{
+	return cfg80211_register_netdevice(dev);
+}
+#else
+static inline int wlan_cfg80211_register_netdevice(struct net_device *dev)
+{
+	return register_netdevice(dev);
+}
+#endif
+
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(5, 12, 0)
+static inline void wlan_cfg80211_unregister_netdevice(struct net_device *dev)
+{
+	cfg80211_unregister_netdevice(dev);
+}
+#else
+static inline void wlan_cfg80211_unregister_netdevice(struct net_device *dev)
+{
+	unregister_netdevice(dev);
+}
+#endif
+
 #endif
