b084a28b32a8bc535c4730ae9c335bf808993bcb
Raghavendar rao l <quic_rlomte@quicinc.com>
2022-02-21 15:21:24 +0530

    msm: ipa3: Enable IPA compilation of parrot
    Update change to enable compilation of IPA
    for parrot.
    
    Change-Id: Ibfe1f8708625e4f314ba4bcb2d95ca5163898ac2
    Signed-off-by: Raghavendar rao l <quic_rlomte@quicinc.com>

--- drivers/platform/msm/Android.mk
+++ drivers/platform/msm/Android.mk
@@ -1,5 +1,5 @@
 ifneq ($(TARGET_BOARD_PLATFORM),qssi)
-GSI_DLKM_PLATFORMS_LIST := taro
+GSI_DLKM_PLATFORMS_LIST := taro parrot
 
 ifeq ($(call is-board-platform-in-list, $(GSI_DLKM_PLATFORMS_LIST)),true)
 #Make file to create GSI DLKM
