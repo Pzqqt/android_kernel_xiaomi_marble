bc4adf1732bc31b1c0684f1a437bc7b965e46fac
Surabhi Vishnoi <quic_svishnoi@quicinc.com>
2022-02-21 15:37:11 +0530

    qcacld-3.0: Fix deadlock scenario in packet capture mode
    Ppdu_stats are received from firmware in softirq context.
    While inserting the ppdu_stats into a qdf list, a spinlock
    is held for its access, same qdf list is accessed by mon
    thread using spinlock. There can be a scenario in which
    mon_thread held spinlock for accessing qdf_list and is
    interrupted by softirq in which ppdu_stats are received.
    This leads to softirq spinning for lock forever leading
    to deadlock.
    
    To address this issue, use spin_lock_bh instead of spin_lock.
    Also reduce the time for which lock is taken to access the list
    for insertion and removal of ppdu_stats.
    
    Change-Id: I52171fe3c1d22a1e9d1ab36daac54d8fa2b96020
    CRs-Fixed: 3136901

--- components/pkt_capture/core/src/wlan_pkt_capture_data_txrx.c
+++ components/pkt_capture/core/src/wlan_pkt_capture_data_txrx.c
@@ -300,9 +300,10 @@ pkt_capture_update_tx_status(
 	pktcapture_hdr->nss = vdev_priv->tx_nss;
 
 	/* Remove the ppdu stats from front of list and fill it in tx_status */
-	qdf_spin_lock(&vdev_priv->lock_q);
+	qdf_spin_lock_bh(&vdev_priv->lock_q);
 	if (QDF_STATUS_SUCCESS ==
 	    qdf_list_remove_front(&vdev_priv->ppdu_stats_q, &node)) {
+		qdf_spin_unlock_bh(&vdev_priv->lock_q);
 		q_node = qdf_container_of(
 			node, struct pkt_capture_ppdu_stats_q_node, node);
 		smu = (htt_ppdu_stats_for_smu_tlv *)(q_node->buf);
@@ -318,8 +319,9 @@ pkt_capture_update_tx_status(
 				     2 * sizeof(uint32_t));
 
 		qdf_mem_free(q_node);
+	} else {
+		qdf_spin_unlock_bh(&vdev_priv->lock_q);
 	}
-	qdf_spin_unlock(&vdev_priv->lock_q);
 
 skip_ppdu_stats:
 	pkt_capture_tx_get_phy_info(pktcapture_hdr, tx_status);
