97fb480df8f8417eaa47c0c372657cbc2d9f0a11
Paul Zhang <quic_paulz@quicinc.com>
2022-02-23 15:02:39 +0800

    qcacmn: fetch profile_data from right position
    Since structure wmi_wlan_profile_t includes tlv header, it has
    to consider the tlv header in each iteration.
    
    Change-Id: I2928f89a14646fceb93729f508b817c173eff1a2
    CRs-Fixed: 3136679

--- wmi/src/wmi_unified_tlv.c
+++ wmi/src/wmi_unified_tlv.c
@@ -11766,7 +11766,6 @@ static QDF_STATUS extract_profile_data_tlv(wmi_unified_t wmi_handle,
 {
 	WMI_WLAN_PROFILE_DATA_EVENTID_param_tlvs *param_buf;
 	wmi_wlan_profile_t *ev;
-	uint8_t *buf_ptr;
 
 	param_buf = (WMI_WLAN_PROFILE_DATA_EVENTID_param_tlvs *)evt_buf;
 	if (!param_buf) {
@@ -11774,12 +11773,7 @@ static QDF_STATUS extract_profile_data_tlv(wmi_unified_t wmi_handle,
 		return QDF_STATUS_E_INVAL;
 	}
 
-	buf_ptr = (uint8_t *)param_buf->profile_ctx;
-	buf_ptr = buf_ptr + sizeof(wmi_wlan_profile_ctx_t) + WMI_TLV_HDR_SIZE;
-
-	buf_ptr = buf_ptr + (sizeof(wmi_wlan_profile_t) * idx);
-	ev = (wmi_wlan_profile_t *)buf_ptr;
-
+	ev = &param_buf->profile_data[idx];
 	profile_data->id  = ev->id;
 	profile_data->cnt = ev->cnt;
 	profile_data->tot = ev->tot;
