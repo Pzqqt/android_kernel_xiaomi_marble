bbbfe2d3c08db74d6fd5e767c62294d0f5bb3257
Subash Abhinov Kasiviswanathan <quic_subashab@quicinc.com>
2022-04-06 11:53:30 -0600

    perf: Ic82967aaf3d6652b2302ea438949a79e39cbd7d5
    Update to Ic82967aaf3d6652b2302ea438949a79e39cbd7d5
    
    CRs-Fixed: 3164101
    Change-Id: Ic82967aaf3d6652b2302ea438949a79e39cbd7d5
    Signed-off-by: Subash Abhinov Kasiviswanathan <quic_subashab@quicinc.com>

--- perf/rmnet_perf_main.c
+++ perf/rmnet_perf_main.c
@@ -11,9 +11,10 @@
 #include <linux/ip.h>
 #include <linux/ipv6.h>
 MODULE_LICENSE("\x47\x50\x4c\x20\x76\x32");static char*verinfo[]={
-"\x37\x34\x31\x35\x39\x32\x31\x63","\x34\x64\x65\x34\x39\x64\x62\x35",
-"\x62\x31\x63\x34\x34\x62\x34\x63"};module_param_array(verinfo,charp,NULL,
-(0xcb7+5769-0x221c));MODULE_PARM_DESC(verinfo,
+"\x33\x39\x63\x62\x64\x37\x64\x35","\x37\x34\x31\x35\x39\x32\x31\x63",
+"\x34\x64\x65\x34\x39\x64\x62\x35","\x62\x31\x63\x34\x34\x62\x34\x63"};
+module_param_array(verinfo,charp,NULL,(0xcb7+5769-0x221c));MODULE_PARM_DESC(
+verinfo,
 "\x56\x65\x72\x73\x69\x6f\x6e\x20\x6f\x66\x20\x74\x68\x65\x20\x64\x72\x69\x76\x65\x72"
 );extern int(*rmnet_perf_ingress_hook1)(struct sk_buff*DATARMNET543491eb0f);
 extern void(*rmnet_perf_egress_hook1)(struct sk_buff*DATARMNET543491eb0f);
@@ -47,8 +48,8 @@ DATARMNET543491eb0f->priority=14286874;}void DATARMNET49c17a32bc(void){
 rcu_assign_pointer(rmnet_perf_ingress_hook1,DATARMNETee9214ce9b);
 rcu_assign_pointer(rmnet_perf_egress_hook1,DATARMNET05c4bf8af9);}void 
 DATARMNET41e8cc085c(void){rcu_assign_pointer(rmnet_perf_ingress_hook1,NULL);
-rcu_assign_pointer(rmnet_perf_egress_hook1,NULL);}static int __init 
-DATARMNET63abbdc3d3(void){pr_info("%s(): Loading\n",__func__);
+rcu_assign_pointer(rmnet_perf_egress_hook1,NULL);synchronize_rcu();}static int 
+__init DATARMNET63abbdc3d3(void){pr_info("%s(): Loading\n",__func__);
 DATARMNET49c17a32bc();return(0xd2d+202-0xdf7);}static void __exit 
 DATARMNETa343229e33(void){DATARMNET41e8cc085c();pr_info(
 "\x25\x73\x28\x29\x3a\x20\x65\x78\x69\x74\x69\x6e\x67" "\n",__func__);}
