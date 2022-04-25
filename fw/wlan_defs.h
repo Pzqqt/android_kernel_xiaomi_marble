47329d822a5f1d016e3466ff36b9939b13cf06d0
spuligil <quic_spuligil@quicinc.com>
2022-04-25 00:02:38 -0700

    fw-api: CL 17946727 - update fw common interface files
    Update the copyright header comment on wlan_defs.h to have QUIC as the copyright holder for 2022
    
    Change-Id: I5db48a5db9bf30928be7499f5cb5349cb9f2b208
    CRs-Fixed: 3180263

--- fw/wlan_defs.h
+++ fw/wlan_defs.h
@@ -1,5 +1,6 @@
 /*
- * Copyright (c) 2013-2016, 2018-20222The Linux Foundation. All rights reserved.*
+ * Copyright (c) 2013-2016, 2018-2021 The Linux Foundation. All rights reserved.
+ * Copyright (c) 2021-2022 Qualcomm Innovation Center, Inc. All rights reserved.
  *
  * Previously licensed under the ISC license by Qualcomm Atheros, Inc.
  *
