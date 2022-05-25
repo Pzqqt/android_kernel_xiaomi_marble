c7f4ae1bb8e5b1ed1570e448616ca80a41fdeb78
Surabhi Vishnoi <quic_svishnoi@quicinc.com>
2022-05-25 10:25:19 +0530

    qcacld-3.0: Acquire wakelock during roaming if key installation is pending
    Currently, wakelock is only taken during initial connection if
    pairwise key installation is pending and not during roaming. This
    may lead to EAPOL delay/timeout during roaming as runtime pm and
    suspend is allowed during roaming. To fix this issue, acquire
    wakelock if key installation is pending during roaming.
    
    This change also refactors the code and release the wakelock in
    wma_remove_peer if EAPOL fails during initial connection or roaming.
    
    Change-Id: Id4cac30b3c383ca3d3e963571846f8a30eaa1006
    CRs-Fixed: 3189799

--- core/wma/inc/wma.h
+++ core/wma/inc/wma.h
@@ -1,6 +1,6 @@
 /*
  * Copyright (c) 2013-2021 The Linux Foundation. All rights reserved.
- * Copyright (c) 2021-2022 Qualcomm Innovation Center, Inc. All rights reserved.
+ * Copyright (c) 2021-2023 Qualcomm Innovation Center, Inc. All rights reserved.
  *
  * Permission to use, copy, modify, and/or distribute this software for
  * any purpose with or without fee is hereby granted, provided that the
@@ -681,8 +681,6 @@ struct wma_invalid_peer_params {
  * @ns_offload_req: cached ns offload request
  * @rcpi_req: rcpi request
  * @in_bmps: Whether bmps for this interface has been enabled
- * @vdev_set_key_wakelock: wakelock to protect vdev set key op with firmware
- * @vdev_set_key_runtime_wakelock: runtime pm wakelock for set key
  * @ch_freq: channel frequency
  * @roam_scan_stats_req: cached roam scan stats request
  * @wma_invalid_peer_params: structure storing invalid peer params
@@ -729,8 +727,6 @@ struct wma_txrx_node {
 	bool in_bmps;
 	struct beacon_filter_param beacon_filter;
 	bool beacon_filter_enabled;
-	qdf_wake_lock_t vdev_set_key_wakelock;
-	qdf_runtime_lock_t vdev_set_key_runtime_wakelock;
 	struct roam_synch_frame_ind roam_synch_frame_ind;
 	bool is_waiting_for_key;
 	uint32_t ch_freq;
