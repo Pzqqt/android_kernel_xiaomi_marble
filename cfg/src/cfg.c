e59fa068329aa3011000a215c71651888657d35e
David Oladunjoye <quic_doladunj@quicinc.com>
2022-06-09 11:32:21 -0700

    qcacmn: cfg: Replace fallthrough comment with attribute
    -Wimplicit-fallthrough is being enabled by default. Some compilers
    such as clang require the attribute instead of just a fallthrough comment.
    
    Change-Id: I35cf752c724368243fa954ef982e7a3b426db54f
    CRs-Fixed: 3218238

--- cfg/src/cfg.c
+++ cfg/src/cfg.c
@@ -131,7 +131,7 @@ cfg_int_item_handler(struct cfg_value_store *store,
 	default:
 		QDF_DEBUG_PANIC("Unknown fallback method %d for cfg item '%s'",
 				meta->fallback, meta->name);
-		/* fall through */
+		fallthrough;
 	case CFG_VALUE_OR_DEFAULT:
 		/* store already contains default */
 		break;
@@ -185,7 +185,7 @@ cfg_uint_item_handler(struct cfg_value_store *store,
 	default:
 		QDF_DEBUG_PANIC("Unknown fallback method %d for cfg item '%s'",
 				meta->fallback, meta->name);
-		/* fall through */
+		fallthrough;
 	case CFG_VALUE_OR_DEFAULT:
 		/* store already contains default */
 		break;
