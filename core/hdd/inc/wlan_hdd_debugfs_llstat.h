8ac37bcc7246958c847587558212864bae5f8e66
Divyajyothi Goparaju <quic_dgoparaj@quicinc.com>
2022-06-15 20:48:57 +0530

    qcacld-3.0: Increase the buffer size for LL_STATS
    ll_stats are not fit into current buffer sizeof
    12288 bytes to write into debugfs
    
    Increase the LL_STATS buffer size i.e DEBUGFS_LLSTATS_BUF_SIZE
    from 3 pages to 4 pages for single vdev case also double the
    buffer size for MLO case.
    
    Change-Id: Idcae0816de0a29ae92a821623d3d2e8c4f33bdf8
    CRs-Fixed: 3214694

--- core/hdd/inc/wlan_hdd_debugfs_llstat.h
+++ core/hdd/inc/wlan_hdd_debugfs_llstat.h
@@ -1,5 +1,6 @@
 /*
  * Copyright (c) 2013-2018 The Linux Foundation. All rights reserved.
+ * Copyright (c) 2022 Qualcomm Innovation Center, Inc. All rights reserved.
  *
  * Permission to use, copy, modify, and/or distribute this software for
  * any purpose with or without fee is hereby granted, provided that the
@@ -26,7 +27,12 @@
 #ifndef _WLAN_HDD_DEBUGFS_LLSTAT_H
 #define _WLAN_HDD_DEBUGFS_LLSTAT_H
 
-#define DEBUGFS_LLSTATS_BUF_SIZE 12288
+#if defined(WLAN_FEATURE_11BE_MLO) && defined(CFG80211_11BE_BASIC)
+#define DEBUGFS_LLSTATS_BUF_SIZE 32768
+#else
+#define DEBUGFS_LLSTATS_BUF_SIZE 16384
+#endif
+
 #define DEBUGFS_LLSTATS_REQID   4294967295UL
 #define DEBUGFS_LLSTATS_REQMASK 0x7
 
