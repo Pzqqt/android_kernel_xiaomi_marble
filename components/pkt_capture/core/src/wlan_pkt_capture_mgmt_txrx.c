a545de25e7f191d94d0ebcbefbf35a027b63652a
Vulupala Shashank Reddy <quic_vulupa@quicinc.com>
2022-08-03 15:34:25 +0530

    qcacld-3.0: Fix the channel for M2 packet in pkt capture mode
    Currently in packet capture, channel for data packets is updated
    from pdev. We are updating channel in pdev when we receive auth
    frame in mon thread. But, sometimes we are updating channel in
    radiotap of M2 in pkt_capture_update_tx_status, before mon thread
    process auth packet and update channel in pdev, because of
    which we are updating wrong channel in M2.
    
    So, to avoid this move update of channel from mon thread to
    pkt_capture_mgmtpkt_process.
    
    Change-Id: Icf14c4331849ad7cfdfbd7a3043e4e4dc3d33721
    CRs-Fixed: 3266890

--- components/pkt_capture/core/src/wlan_pkt_capture_mgmt_txrx.c
+++ components/pkt_capture/core/src/wlan_pkt_capture_mgmt_txrx.c
@@ -31,6 +31,7 @@
 #include "wlan_mgmt_txrx_utils_api.h"
 #include "wlan_utility.h"
 #include "cds_ieee80211_common.h"
+#include "cdp_txrx_ctrl.h"
 
 enum pkt_capture_tx_status
 pkt_capture_mgmt_status_map(uint8_t status)
@@ -133,13 +134,107 @@ pkt_capture_mgmtpkt_process(struct wlan_objmgr_psoc *psoc,
 	struct wlan_objmgr_vdev *vdev;
 	struct pkt_capture_mon_pkt *pkt;
 	uint32_t headroom;
+	uint8_t type, sub_type;
+	struct ieee80211_frame *wh;
+	void *soc = cds_get_context(QDF_MODULE_ID_SOC);
+	struct wlan_objmgr_pdev *pdev;
+	cdp_config_param_type val;
+	tSirMacAuthFrameBody *auth;
+	struct pkt_capture_vdev_priv *vdev_priv;
 
 	vdev = wlan_objmgr_get_vdev_by_opmode_from_psoc(psoc,
 							QDF_STA_MODE,
 							WLAN_PKT_CAPTURE_ID);
+
 	if (!vdev) {
 		pkt_capture_err("vdev is NULL");
 		return QDF_STATUS_E_FAILURE;
+	}
+
+	vdev_priv = pkt_capture_vdev_get_priv(vdev);
+	if (!vdev_priv) {
+		pkt_capture_err("packet capture vdev priv is NULL");
+		return QDF_STATUS_E_FAILURE;
+	}
+
+	pdev = wlan_vdev_get_pdev(vdev);
+	if (!pdev) {
+		pkt_capture_err("pdev is NULL");
+		return QDF_STATUS_E_FAILURE;
+	}
+
+	wh = (struct ieee80211_frame *)(qdf_nbuf_data(nbuf));
+	type = (wh)->i_fc[0] & IEEE80211_FC0_TYPE_MASK;
+	sub_type = (wh)->i_fc[0] & IEEE80211_FC0_SUBTYPE_MASK;
+
+	/*
+	 *  Update channel only if successful AUTH Resp is received.
+	 *  This is done so that EAPOL M1 data frame have correct
+	 *  channel
+	 */
+	if ((type == IEEE80211_FC0_TYPE_MGT) &&
+	    (sub_type == MGMT_SUBTYPE_AUTH)) {
+		uint8_t chan = wlan_freq_to_chan(txrx_status->chan_freq);
+
+		auth = (tSirMacAuthFrameBody *)(qdf_nbuf_data(nbuf) +
+			sizeof(tSirMacMgmtHdr));
+
+		if (auth->authTransactionSeqNumber == SIR_MAC_AUTH_FRAME_2 ||
+		    auth->authTransactionSeqNumber == SIR_MAC_AUTH_FRAME_4) {
+			if (auth->authStatusCode == STATUS_SUCCESS) {
+				val.cdp_pdev_param_monitor_chan = chan;
+				cdp_txrx_set_pdev_param(
+					soc, wlan_objmgr_pdev_get_pdev_id(pdev),
+					CDP_MONITOR_CHANNEL, val);
+
+				val.cdp_pdev_param_mon_freq =
+							txrx_status->chan_freq;
+				cdp_txrx_set_pdev_param(
+					soc, wlan_objmgr_pdev_get_pdev_id(pdev),
+					CDP_MONITOR_FREQUENCY, val);
+			}
+		}
+	}
+
+	/*
+	 *  Update channel to last connected channel in case of assoc/reassoc
+	 *  response failure and save current chan in case of success
+	 */
+	if ((type == IEEE80211_FC0_TYPE_MGT) &&
+	    ((sub_type == MGMT_SUBTYPE_ASSOC_RESP) ||
+	    (sub_type == MGMT_SUBTYPE_REASSOC_RESP))) {
+		if (qdf_nbuf_len(nbuf) < (sizeof(tSirMacMgmtHdr) +
+		   SIR_MAC_ASSOC_RSP_STATUS_CODE_OFFSET)) {
+			pkt_capture_err("Packet length is less than expected");
+			qdf_nbuf_free(nbuf);
+			return QDF_STATUS_E_FAILURE;
+		}
+
+		status = (uint16_t)(*(qdf_nbuf_data(nbuf) +
+			 sizeof(tSirMacMgmtHdr) +
+			 SIR_MAC_ASSOC_RSP_STATUS_CODE_OFFSET));
+
+		if (status == STATUS_SUCCESS) {
+			vdev_priv->last_freq = vdev_priv->curr_freq;
+			vdev_priv->curr_freq = txrx_status->chan_freq;
+		} else {
+			uint8_t chan_num;
+
+			chan_num = wlan_reg_freq_to_chan(pdev,
+							 vdev_priv->last_freq);
+
+			val.cdp_pdev_param_monitor_chan = chan_num;
+			cdp_txrx_set_pdev_param(
+				soc, wlan_objmgr_pdev_get_pdev_id(pdev),
+				CDP_MONITOR_CHANNEL, val);
+
+			val.cdp_pdev_param_mon_freq = vdev_priv->last_freq;
+			cdp_txrx_set_pdev_param(
+				soc, wlan_objmgr_pdev_get_pdev_id(pdev),
+				CDP_MONITOR_FREQUENCY, val);
+
+			vdev_priv->curr_freq = vdev_priv->last_freq;
+		}
 	}
 
 	/*
