0baa6ea4811e9965a26d4637ae2c6152d3ba5d31
spuligil <quic_spuligil@quicinc.com>
2022-09-17 06:00:58 -0700

    fw-api: CL 19622099 - update fw common interface files
    Change-Id: I08a30bd58cc334d09cf9469c72110cf720e4f802
    WMI: allow vdev restart to specify bitmap of vdev IDs
    CRs-Fixed: 2262693

--- fw/wmi_services.h
+++ fw/wmi_services.h
@@ -599,6 +599,7 @@ typedef  enum  {
     WMI_SERVICE_BANG_RADAR_320_SUPPORT = 346, /* Host to send frequency offset for bang radar in extended field for 320M support */
     WMI_SERVICE_XGAP_SUPPORT = 347, /* FW support for XGAP */
     WMI_SERVICE_OBSS_PER_PACKET_SR_SUPPORT = 348, /* Spatial Reuse support for per PPDU setting */
+    WMI_SERVICE_MULTIPLE_VDEV_RESTART_BITMAP_SUPPORT = 349, /* Extended Multiple VDEV Restart with Bitmap Support */
 
 
     WMI_MAX_EXT2_SERVICE
