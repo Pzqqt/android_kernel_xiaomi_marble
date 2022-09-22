a57ac69a3b34d3d71acda42740c836e7337afcdb
Srihitha Tangudu <quic_tangudu@quicinc.com>
2022-09-22 19:18:48 +0530

    disp: msm: dsi: clear the panel esd_recovery_pending in power on commit
    Currently the panel esd_recovery_pending flag is cleared for every mode
    set. The ESD recovery completes only after the suspend and resume. Clear
    the flag only during power on commit.
    
    Change-Id: I97e370feba0aad34558e4675168b4bcb7f5901ca
    Signed-off-by: Srihitha Tangudu <quic_tangudu@quicinc.com>

--- msm/dsi/dsi_drm.c
+++ msm/dsi/dsi_drm.c
@@ -204,7 +204,8 @@ static void dsi_bridge_pre_enable(struct drm_bridge *bridge)
 		return;
 	}
 
-	atomic_set(&c_bridge->display->panel->esd_recovery_pending, 0);
+	if (bridge->encoder->crtc->state->active_changed)
+		atomic_set(&c_bridge->display->panel->esd_recovery_pending, 0);
 
 	/* By this point mode should have been validated through mode_fixup */
 	rc = dsi_display_set_mode(c_bridge->display,
