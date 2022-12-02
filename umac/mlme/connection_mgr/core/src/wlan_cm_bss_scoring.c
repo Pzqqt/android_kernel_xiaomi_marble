2b5dd170a5078822042c0414af3938062a7934cf
Surabhi Vishnoi <quic_svishnoi@quicinc.com>
2022-12-02 16:04:56 +0530

    qcacmn: Fix 6 GHz bss HE capable beamformee score
    Currently, BSS beamformee score is calculated from vht_cap or
    eht caps, so for 6 GHz HE capable BSS it will calculated to zero.
    Due to this 5 GHz will always get selected compared to 6 GHz BSS with
    all other capabilities and conditions same in both BSS as 5 GHz
    will have more total candidate score.
    Fix this issue by correct calculation of AP beamformer capability
    in case of 6 GHz HE BSS.
    
    Change-Id: I0f3285ce1c5c1aeeba624c5371ff0f884ae78c58
    CRs-Fixed: 3351991

--- umac/mlme/connection_mgr/core/src/wlan_cm_bss_scoring.c
+++ umac/mlme/connection_mgr/core/src/wlan_cm_bss_scoring.c
@@ -1,6 +1,6 @@
 /*
  * Copyright (c) 2017-2021, The Linux Foundation. All rights reserved.
- * Copyright (c) 2021-2022 Qualcomm Innovation Center, Inc. All rights reserved.
+ * Copyright (c) 2021-2023 Qualcomm Innovation Center, Inc. All rights reserved.
  *
  * Permission to use, copy, modify, and/or distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
@@ -1677,6 +1677,7 @@ static int cm_calculate_bss_score(struct wlan_objmgr_psoc *psoc,
 	bool same_bucket = false;
 	bool ap_su_beam_former = false;
 	struct wlan_ie_vhtcaps *vht_cap;
+	struct wlan_ie_hecaps *he_cap;
 	struct scoring_cfg *score_config;
 	struct weight_cfg *weight_config;
 	uint32_t sta_nss;
@@ -1766,8 +1767,16 @@ static int cm_calculate_bss_score(struct wlan_objmgr_psoc *psoc,
 				score_config->rssi_score.bad_rssi_bucket_size);
 
 	vht_cap = (struct wlan_ie_vhtcaps *)util_scan_entry_vhtcap(entry);
-	if (vht_cap && vht_cap->su_beam_former)
+	he_cap = (struct wlan_ie_hecaps *)util_scan_entry_hecap(entry);
+
+	if (vht_cap && vht_cap->su_beam_former) {
 		ap_su_beam_former = true;
+	} else if (he_cap && QDF_GET_BITS(*(he_cap->he_phy_cap.phy_cap_bytes +
+		   WLAN_HE_PHYCAP_SU_BFER_OFFSET), WLAN_HE_PHYCAP_SU_BFER_IDX,
+		   WLAN_HE_PHYCAP_SU_BFER_BITS)) {
+		ap_su_beam_former = true;
+	}
+
 	if (phy_config->beamformee_cap && is_vht &&
 	    ap_su_beam_former &&
 	    (entry->rssi_raw > rssi_pref_5g_rssi_thresh) && !same_bucket)
