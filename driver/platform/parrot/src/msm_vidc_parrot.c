1a18ea09fd3dc389783281fc88b8386d5a5e3fca
Ankush Mitra <quic_ankumitr@quicinc.com>
2022-12-28 12:49:31 +0530

    video: driver: Adjust MAX MBPF in parrot
    Adjust MAX MBPF to a lower value.
    
    Change-Id: I8006c47547a3bea3e85823464d9b0437bd33d037
    Signed-off-by: Ankush Mitra <quic_ankumitr@quicinc.com>

--- driver/platform/parrot/src/msm_vidc_parrot.c
+++ driver/platform/parrot/src/msm_vidc_parrot.c
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: GPL-2.0-only
 /*
- * Copyright (c) 2022 Qualcomm Innovation Center, Inc. All rights reserved.
+ * Copyright (c) 2022-2023 Qualcomm Innovation Center, Inc. All rights reserved.
  * Copyright (c) 2020-2021, The Linux Foundation. All rights reserved.
  */
 
@@ -52,12 +52,12 @@ static struct msm_platform_core_capability core_data_parrot_v0[] = {
 	{ENC_CODECS, H264|HEVC|HEIC},
 	{DEC_CODECS, H264|HEVC|VP9|HEIC},
 	{MAX_SESSION_COUNT, 16},
-	{MAX_NUM_720P_SESSIONS, 16},
-	{MAX_NUM_1080P_SESSIONS, 8},
+	{MAX_NUM_720P_SESSIONS, 8},
+	{MAX_NUM_1080P_SESSIONS, 4},
 	{MAX_NUM_4K_SESSIONS, 2},
 	{MAX_SECURE_SESSION_COUNT, 3},
-	{MAX_RT_MBPF, 97920}, /* ((3840x2176)/256) x 3 */
-	{MAX_MBPF, 104448}, /* ((4096x2176)/256) x 3 */
+	{MAX_RT_MBPF, 69362}, /* ((4096x2176)/256) x 2 */
+	{MAX_MBPF, 77522}, /* ((4096x2176)/256) x 2 + (1920x1088)/256 */
 	/* Concurrency: UHD@30 decode + 1080p@30 encode */
 	{MAX_MBPS, 2088960}, /* max_load 4096x2176@60fps */
 	{MAX_IMAGE_MBPF, 1048576},  /* (16384x16384)/256 */
@@ -104,12 +104,12 @@ static struct msm_platform_core_capability core_data_parrot_v1[] = {
 	{ENC_CODECS, H264|HEVC|HEIC},
 	{DEC_CODECS, H264|HEVC|VP9|HEIC},
 	{MAX_SESSION_COUNT, 16},
-	{MAX_NUM_720P_SESSIONS, 16},
-	{MAX_NUM_1080P_SESSIONS, 8},
-	{MAX_NUM_4K_SESSIONS, 2},
+	{MAX_NUM_720P_SESSIONS, 4},
+	{MAX_NUM_1080P_SESSIONS, 2},
+	{MAX_NUM_4K_SESSIONS, 1},
 	{MAX_SECURE_SESSION_COUNT, 3},
-	{MAX_RT_MBPF, 97920}, /* ((3840x2176)/256) x 3 */
-	{MAX_MBPF, 104448}, /* ((4096x2176)/256) x 3 */
+	{MAX_RT_MBPF, 40800}, /* ((3840x2176)/256) + (1920x1088)/256 */
+	{MAX_MBPF, 42976}, /* ((4096x2176)/256) + (1920x1088)/256 */
 	/* max_load 4096x2176@30fps */
 	{MAX_MBPS, 1044480}, /* Concurrency: UHD@30 decode + 1080p@30 encode */
 	{MAX_IMAGE_MBPF, 1048576},  /* (16384x16384)/256 */
@@ -156,12 +156,12 @@ static struct msm_platform_core_capability core_data_parrot_v2[] = {
 	{ENC_CODECS, H264|HEVC|HEIC},
 	{DEC_CODECS, H264|HEVC|VP9|HEIC},
 	{MAX_SESSION_COUNT, 16},
-	{MAX_NUM_720P_SESSIONS, 16},
-	{MAX_NUM_1080P_SESSIONS, 8},
-	{MAX_NUM_4K_SESSIONS, 2},
+	{MAX_NUM_720P_SESSIONS, 4},
+	{MAX_NUM_1080P_SESSIONS, 2},
+	{MAX_NUM_4K_SESSIONS, 1},
 	{MAX_SECURE_SESSION_COUNT, 3},
-	{MAX_RT_MBPF, 97920}, /* ((3840x2176)/256) x 3 */
-	{MAX_MBPF, 104448}, /* ((4096x2176)/256) x 3 */
+	{MAX_RT_MBPF, 40800}, /* ((3840x2176)/256) + (1920x1088)/256 */
+	{MAX_MBPF, 42976}, /* ((4096x2176)/256) + (1920x1088)/256 */
 	/* max_load 4096x2176@30fps */
 	{MAX_MBPS, 1044480}, /* Concurrency: UHD@30 decode + 1080p@30 encode */
 	{MAX_IMAGE_MBPF, 1048576},  /* (16384x16384)/256 */
