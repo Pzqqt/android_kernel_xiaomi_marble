22eb2ac7cb0908fed374f96d9b0bb9d751c521e5
Sultan Alsawaf <sultan@kerneltoast.com>
2023-01-15 15:01:35 +0530

    rcu: Run nocb and nocb GP khreads on little clusters
    RCU callbacks are not time-critical and constitute kernel housekeeping.
    Offload the no-callback kthreads onto little CPUs to clear load off of
    the more important, higher-performance CPUs.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>
    Signed-off-by: Panchajanya1999 <kernel@panchajanya.dev>

--- kernel/rcu/tree_plugin.h
+++ kernel/rcu/tree_plugin.h
@@ -2458,7 +2458,7 @@ static void rcu_spawn_one_nocb_kthread(int cpu)
 	/* If we didn't spawn the GP kthread first, reorganize! */
 	rdp_gp = rdp->nocb_gp_rdp;
 	if (!rdp_gp->nocb_gp_kthread) {
-		t = kthread_run(rcu_nocb_gp_kthread, rdp_gp,
+		t = kthread_run_perf_critical(cpu_lp_mask, rcu_nocb_gp_kthread, rdp_gp,
 				"rcuog/%d", rdp_gp->cpu);
 		if (WARN_ONCE(IS_ERR(t), "%s: Could not start rcuo GP kthread, OOM is now expected behavior\n", __func__))
 			return;
@@ -2466,7 +2466,7 @@ static void rcu_spawn_one_nocb_kthread(int cpu)
 	}
 
 	/* Spawn the kthread for this CPU. */
-	t = kthread_run(rcu_nocb_cb_kthread, rdp,
+	t = kthread_run_perf_critical(cpu_lp_mask, rcu_nocb_cb_kthread, rdp,
 			"rcuo%c/%d", rcu_state.abbr, cpu);
 	if (WARN_ONCE(IS_ERR(t), "%s: Could not start rcuo CB kthread, OOM is now expected behavior\n", __func__))
 		return;
