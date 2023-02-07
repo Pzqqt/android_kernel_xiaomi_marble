7fcfb1e2091f8a1cedfe726443e663eeca03d2fe
Madan Koyyalamudi <quic_mkoyyala@quicinc.com>
2023-02-07 02:36:08 -0800

    Release 2.0.9.16C
    Release 2.0.9.16C
    
    Change-Id: I8f0e2d6bee9489dc89a9d7178d7d32a8bd965d90
    CRs-Fixed: 774533

--- core/mac/inc/qwlan_version.h
+++ core/mac/inc/qwlan_version.h
@@ -32,9 +32,9 @@
 #define QWLAN_VERSION_MAJOR            2
 #define QWLAN_VERSION_MINOR            0
 #define QWLAN_VERSION_PATCH            9
-#define QWLAN_VERSION_EXTRA            "B"
+#define QWLAN_VERSION_EXTRA            "C"
 #define QWLAN_VERSION_BUILD            16
 
-#define QWLAN_VERSIONSTR               "2.0.9.16B"
+#define QWLAN_VERSIONSTR               "2.0.9.16C"
 
 #endif /* QWLAN_VERSION_H */
