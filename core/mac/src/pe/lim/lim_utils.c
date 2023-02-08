5a1d5c83302c1c38de8b7a31c41b7ad951802894
Aravind Kishore Sukla <quic_asukla@quicinc.com>
2023-02-08 14:45:03 +0530

    qcacld-3.0: Ignore CSA request for invalid channel
    In present scenario, STA disconnects with AP if it receives
    invalid channel in CSA IE. In this case STA shouldn't
    disconnect with AP as this request may come from a spoof AP.
    
    Ignore this CSA request as it might be from spoof AP and
    if it is from genuine AP heart beat failure happens and
    results in disconnection. After disconnection DUT may
    reconnect to same or other APs.
    
    Change-Id: I840508dd27d8c313a3e8f74c4e1f5aa64eecf6f9
    CRs-Fixed: 3390251

--- core/mac/src/pe/lim/lim_utils.c
+++ core/mac/src/pe/lim/lim_utils.c
@@ -1874,17 +1874,16 @@ static void __lim_process_channel_switch_timeout(struct pe_session *pe_session)
 		}
 
 		/*
-		 * If the channel-list that AP is asking us to switch is invalid
-		 * then we cannot switch the channel. Just disassociate from AP.
-		 * We will find a better AP !!!
+		 * The channel switch request received from AP is carrying
+		 * invalid channel. It's ok to ignore this channel switch
+		 * request as it might be from spoof AP. If it's from genuine
+		 * AP, it may lead to heart beat failure and result in
+		 * disconnection. DUT can go ahead and reconnect to it/any
+		 * other AP once it disconnects.
 		 */
-		if (lim_is_sb_disconnect_allowed(pe_session)) {
-			pe_err("Invalid channel! Disconnect");
-			lim_tear_down_link_with_ap(mac, pe_session->peSessionId,
-					   REASON_UNSUPPORTED_CHANNEL_CSA,
-					   eLIM_LINK_MONITORING_DISASSOC);
-			return;
-		}
+		pe_err("Invalid channel freq %u Ignore CSA request",
+		       channel_freq);
+		return;
 	}
 	switch (pe_session->gLimChannelSwitch.state) {
 	case eLIM_CHANNEL_SWITCH_PRIMARY_ONLY:
