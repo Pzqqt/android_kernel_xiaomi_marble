4a96a406b955764c009d6fc958268073bc758c6d
LibXZR <i@xzr.moe>
2023-03-24 23:54:41 +0800

    kernel: Introduce panic logstore
    Well, this can be an alternative of pstore but you shouldn't use this
    if your pstore works fine. It's not stable and doesn't give as many
    information as pstore does.
    
    It optimistically assumes that filesystems and related storage drivers
    are not to blame for this panic, otherwise the behavior can be
    unpredictable. Also, it can't catch logs before the target partition
    is properly mounted.
    
    The path of the log file is defined in the LOG_FILE_PATH macro.
    
    Test: echo 1 > /sys/module/panic_logstore/parameters/trigger
    
    Signed-off-by: LibXZR <i@xzr.moe>

---
+++ include/linux/panic_logstore.h
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: GPL-2.0
+/*
+ * Copyright (C) 2023 LibXZR <i@xzr.moe>.
+ */
+#ifndef _PANIC_LOGSTORE_H_
+#define _PANIC_LOGSTORE_H_
+
+void do_logstore(void);
+
+#endif /* _PANIC_LOGSTORE_H_ */
