6ec2446c3f85661e5fc6be3b48964b0e92ddf91e
Rahul Choudhary <quic_rahulc@quicinc.com>
2023-05-27 14:16:40 -0700

    Release 2.0.9.18U
    Release 2.0.9.18U
    
    Change-Id: I5fbc6b35886dd9ace1892b11878923805319977a
    CRs-Fixed: 774533

--- core/mac/inc/qwlan_version.h
+++ core/mac/inc/qwlan_version.h
@@ -32,9 +32,9 @@
 #define QWLAN_VERSION_MAJOR            2
 #define QWLAN_VERSION_MINOR            0
 #define QWLAN_VERSION_PATCH            9
-#define QWLAN_VERSION_EXTRA            "T"
+#define QWLAN_VERSION_EXTRA            "U"
 #define QWLAN_VERSION_BUILD            18
 
-#define QWLAN_VERSIONSTR               "2.0.9.18T"
+#define QWLAN_VERSIONSTR               "2.0.9.18U"
 
 #endif /* QWLAN_VERSION_H */
