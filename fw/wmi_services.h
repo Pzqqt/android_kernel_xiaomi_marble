b1c558a8c922c47608cfd54509ac9786cb170aae
spuligil <quic_spuligil@quicinc.com>
2023-06-25 06:01:08 -0700

    fw-api: CL 23581168 - update fw common interface files
    add WMI_SERVICE_BRIDGE_VDEV_SUPPORT def
    
    Change-Id: Ic1a0ca508c0432f76fbce5948498ded97d99fa82
    CRs-Fixed: 2262693

--- fw/wmi_services.h
+++ fw/wmi_services.h
@@ -634,6 +634,7 @@ typedef  enum  {
     WMI_SERVICE_N_LINK_MLO_SUPPORT = 381,  /* Indicate FW supports N MLO link & vdev re-purpose between links */
     WMI_SERVICE_ATF_MAX_CLIENT_512_SUPPORT = 382, /* Indicates FW supports maximum of 512 clients when ATF is enabled */
     WMI_SERVICE_FISA_DYNAMIC_MSDU_AGGR_SIZE_SUPPORT = 383, /* Indicates FW support for FISA aggregation size up to 64 instead of only 16 */
+    WMI_SERVICE_BRIDGE_VDEV_SUPPORT = 384, /* Indicated FW supports Bridge VDEV */
 
     WMI_MAX_EXT2_SERVICE
 
