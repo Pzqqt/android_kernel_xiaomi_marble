7ee54d3260244a6dac8eb83ac06de91991c54ded
Pzqqt <821026875@qq.com>
2023-07-10 18:33:46 +0800

    kernel: module: Custom module blacklist
    - It does not affect behavior of the default module blacklist.
    - Add blacklist of modules removed from last commit to it.

--- kernel/module.c
+++ kernel/module.c
@@ -3554,13 +3554,24 @@ int __weak module_frob_arch_sections(Elf_Ehdr *hdr,
 
 /* module_blacklist is a comma-separated list of module names */
 static char *module_blacklist;
+static char *custom_module_blacklist[] = {
+    /* Not required */
+    "qca6750", "cs35l41_dlkm",
+    /* Already built into the kernel image */
+    "aw882xx_dlkm",
+    /* Coresight */
+    "coresight", "coresight_csr", "coresight_cti", "coresight_dummy", "coresight_funnel",
+    "coresight_hwevent", "coresight_remote_etm", "coresight_replicator", "coresight_stm",
+    "coresight_tgu", "coresight_tmc", "coresight_tpda", "coresight_tpdm"
+};
 static bool blacklisted(const char *module_name)
 {
 	const char *p;
 	size_t len;
+	int i;
 
 	if (!module_blacklist)
-		return false;
+		goto custom_blacklist;
 
 	for (p = module_blacklist; *p; p += len) {
 		len = strcspn(p, ",");
@@ -3569,6 +3580,12 @@ static bool blacklisted(const char *module_name)
 		if (p[len] == ',')
 			len++;
 	}
+
+custom_blacklist:
+	for (i = 0; i < ARRAY_SIZE(custom_module_blacklist); i++)
+		if (!strcmp(module_name, custom_module_blacklist[i]))
+			return true;
+
 	return false;
 }
 core_param(module_blacklist, module_blacklist, charp, 0400);
