6a6b2b3f6cdfd1170c1f1f6199e73ee187fb185a
Pzqqt <821026875@qq.com>
2023-07-12 17:39:17 +0800

    kernel: panic_logstore: Change the log file path

--- kernel/panic_logstore.c
+++ kernel/panic_logstore.c
@@ -12,7 +12,7 @@
 #include <linux/kmsg_dump.h>
 #include <linux/module.h>
 
-#define LOG_FILE_PATH "/mnt/oplus/op2/last_panic.txt"
+#define LOG_FILE_PATH "/data/cache/last_panic.txt"
 
 #if IS_ENABLED(CONFIG_SECURITY_SELINUX_DEVELOP)
 void sel_set_enforce(int);
