4e4fb82793162bdf4d47a4ce42a0c4b80e6857eb
spuligil <quic_spuligil@quicinc.com>
2023-07-19 06:01:22 -0700

    fw-api: CL 23852898 - update fw common interface files
    add WMI_SERVICE_TX_PWR_PER_PPDU_STATS_SUPPORT def
    
    Change-Id: I1f0197e96b365d13200aea55100ead6bd0a33a9e
    CRs-Fixed: 2262693

--- fw/wmi_services.h
+++ fw/wmi_services.h
@@ -636,6 +636,7 @@ typedef  enum  {
     WMI_SERVICE_FISA_DYNAMIC_MSDU_AGGR_SIZE_SUPPORT = 383, /* Indicates FW support for FISA aggregation size up to 64 instead of only 16 */
     WMI_SERVICE_BRIDGE_VDEV_SUPPORT = 384, /* Indicated FW supports Bridge VDEV */
     WMI_SERVICE_MLO_MODE1_RECOVERY_SUPPORTED = 385, /* Indicate fw support for mlo mode1 recovery */
+    WMI_SERVICE_TX_PWR_PER_PPDU_STATS_SUPPORT = 386, /* FW support to check tx power stats per PPDU */
 
     WMI_MAX_EXT2_SERVICE
 
