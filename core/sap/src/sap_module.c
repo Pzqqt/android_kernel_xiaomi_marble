aee6944241f50148bd4af8712c0114c1dafeebef
Krupali Dhanvijay <quic_kdhanvij@quicinc.com>
2023-08-29 14:15:50 +0530

    qcacld-3.0: Update set_disable_channel_list cmd for 6 GHz channels
    Currently SET_DISABLE_CHANNEL_LIST command is used to disable
    legacy channels only.
    
    Update this command to accept frequency for all the bands along with
    the channel numbers from legacy (2.4 GHz/5 GHz).
    
    Change-Id: Id55493acfa115da059404af1cd3901e877e46632
    CRs-Fixed: 3601196

--- core/sap/src/sap_module.c
+++ core/sap/src/sap_module.c
@@ -3304,8 +3304,9 @@ qdf_freq_t wlansap_get_chan_band_restrict(struct sap_context *sap_ctx,
 				return 0;
 			}
 		}
-	} else if (wlan_reg_is_disable_for_freq(mac->pdev,
-						sap_ctx->chan_freq) &&
+	} else if (wlan_reg_is_disable_in_secondary_list_for_freq(
+							mac->pdev,
+							sap_ctx->chan_freq) &&
 		   !utils_dfs_is_freq_in_nol(mac->pdev, sap_ctx->chan_freq)) {
 		sap_debug("channel is disabled");
 		*csa_reason = CSA_REASON_CHAN_DISABLED;
