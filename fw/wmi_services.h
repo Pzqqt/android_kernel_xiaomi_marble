413535a78d07d392d1dabd641b63a199e3070d62
spuligil <quic_spuligil@quicinc.com>
2023-09-01 06:03:27 -0700

    fw-api: CL 24431228 - update fw common interface files
    add WMI_SERVICE_XPAN_SUPPORT def
    
    Change-Id: I3f72d23c8911cca20dcee672e8e09aad0f22b71e
    CRs-Fixed: 2262693

--- fw/wmi_services.h
+++ fw/wmi_services.h
@@ -641,6 +641,7 @@ typedef  enum  {
     WMI_SERVICE_PDEV_WSI_STATS_INFO_SUPPORT = 388, /* Support for WSI Stats Info. */
     WMI_SERVICE_MULTIPLE_RF_PATH_SOC_SUPPORT = 389, /* Indicates FW supports Multiple RF Path on SOC Level */
     WMI_SERVICE_RADAR_FLAGS_SUPPORT = 390, /* Indicates FW supports radar flags, such as full bandwidth need put to NOL */
+    WMI_SERVICE_XPAN_SUPPORT = 391, /* Indicate FW support XPAN configuration */
 
     WMI_MAX_EXT2_SERVICE
 
