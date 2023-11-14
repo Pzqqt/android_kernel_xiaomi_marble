d283a843d8b8f7a9b20e5db66634c4f6dc58ecee
Juhyung Park <qkrwngud825@gmail.com>
2023-11-14 22:39:38 +0900

    Revert "UPSTREAM: usb: dwc3: Issue core soft reset before enabling run/stop"
    This reverts commit 1a73ed4b2e1e4c9bbf18bbb864c9175cf296883b.
    
    This breaks USB tethering.
    
    Signed-off-by: Juhyung Park <qkrwngud825@gmail.com>

--- drivers/usb/dwc3/core.c
+++ drivers/usb/dwc3/core.c
@@ -114,6 +114,8 @@ void dwc3_set_prtcap(struct dwc3 *dwc, u32 mode)
 	dwc->current_dr_role = mode;
 }
 
+static int dwc3_core_soft_reset(struct dwc3 *dwc);
+
 static void __dwc3_set_mode(struct work_struct *work)
 {
 	struct dwc3 *dwc = work_to_dwc(work);
@@ -267,7 +269,7 @@ u32 dwc3_core_fifo_space(struct dwc3_ep *dep, u8 type)
  * dwc3_core_soft_reset - Issues core soft reset and PHY reset
  * @dwc: pointer to our context structure
  */
-int dwc3_core_soft_reset(struct dwc3 *dwc)
+static int dwc3_core_soft_reset(struct dwc3 *dwc)
 {
 	u32		reg;
 	int		retries = 1000;
