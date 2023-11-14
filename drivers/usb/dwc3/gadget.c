e039bc1fa0d5cf78278a0d957114e95787e949fe
Juhyung Park <qkrwngud825@gmail.com>
2023-11-14 22:39:38 +0900

    Revert "UPSTREAM: usb: dwc3: Issue core soft reset before enabling run/stop"
    This reverts commit 1a73ed4b2e1e4c9bbf18bbb864c9175cf296883b.
    
    This breaks USB tethering.
    
    Signed-off-by: Juhyung Park <qkrwngud825@gmail.com>

--- drivers/usb/dwc3/gadget.c
+++ drivers/usb/dwc3/gadget.c
@@ -2646,22 +2646,10 @@ static int dwc3_gadget_pullup(struct usb_gadget *g, int is_on)
 	if (!is_on) {
 		ret = dwc3_gadget_soft_disconnect(dwc);
 	} else {
-		/*
-		 * In the Synopsys DWC_usb31 1.90a programming guide section
-		 * 4.1.9, it specifies that for a reconnect after a
-		 * device-initiated disconnect requires a core soft reset
-		 * (DCTL.CSftRst) before enabling the run/stop bit.
-		 */
-		ret = dwc3_core_soft_reset(dwc);
-		if (ret)
-			goto done;
-
-		dwc3_event_buffers_setup(dwc);
 		__dwc3_gadget_start(dwc);
 		ret = dwc3_gadget_run_stop(dwc, true, false);
 	}
 
-done:
 	pm_runtime_put(dwc->dev);
 
 	return ret;
