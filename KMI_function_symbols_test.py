41c5ed4c891520fd66fe92d9caf8f6e6177249a1
Pzqqt <821026875@qq.com>
2023-11-21 21:15:58 +0800

    Update KMI_function_symbols_test.py

--- KMI_function_symbols_test.py
+++ KMI_function_symbols_test.py
@@ -4,7 +4,7 @@
 import os
 import sys
 import re
-from typing import Optional, Final
+from typing import Final
 
 from lxml import etree
 from rich.console import Console
@@ -15,32 +15,30 @@ __author__: Final = "Pzqqt"
 
 LOCAL_DIR: Final = os.path.dirname(os.path.abspath(__file__))
 
-def main(abi_gki_aarch64_xml_file: Optional[str] = None, module_symvers_file: Optional[str] = None) -> int:
-    if abi_gki_aarch64_xml_file is None:
-        abi_gki_aarch64_xml_file = os.path.join(LOCAL_DIR, 'android', 'abi_gki_aarch64.xml')
-    if module_symvers_file is None:
-        module_symvers_file = os.path.join(LOCAL_DIR, 'out', 'vmlinux.symvers')
 
+def crc_to_int(crc: str) -> int:
+    assert isinstance(crc, str)
+    if re.match(r'0x[0-9a-fA-F]+', crc):
+        return int(crc[2:], 16)
+    raise ValueError(crc)
+
+def int_to_crc(num: int) -> str:
+    assert isinstance(num, int)
+    return '0x' + hex(num)[2:].zfill(8)
+
+def main(abi_gki_aarch64_xml_file: str, vmlinux_symvers_file: str) -> int:
     assert os.path.isfile(abi_gki_aarch64_xml_file)
-    assert os.path.isfile(module_symvers_file)
-
-    def _crc_value_fix(crc_value: str):
-        assert isinstance(crc_value, str)
-        if re.match(r'0x[0-9a-f]{8}', crc_value):
-            return crc_value
-        if re.match(r'0x[0-9a-f]{1,7}', crc_value):
-            return '0x' + crc_value[2:].zfill(8)
-        raise ValueError(crc_value)
+    assert os.path.isfile(vmlinux_symvers_file)
 
     with open(abi_gki_aarch64_xml_file, 'r', encoding="utf-8") as f:
         elf_function_symbols = {
-            elf_symbol.get("name"): _crc_value_fix(elf_symbol.get("crc"))
+            elf_symbol.get("name"): crc_to_int(elf_symbol.get("crc"))
             for elf_symbol in etree.XML(f.read()).findall('.//elf-function-symbols/elf-symbol')
         }
 
-    with open(module_symvers_file, 'r', encoding="utf-8") as f:
+    with open(vmlinux_symvers_file, 'r', encoding="utf-8") as f:
         module_symvers = {
-            line.split()[1]: _crc_value_fix(line.split()[0]) for line in f.readlines()
+            line.split()[1]: crc_to_int(line.split()[0]) for line in f.readlines()
         }
 
     if missing_symbols := elf_function_symbols.keys() - module_symvers.keys():
@@ -65,7 +63,7 @@ def main(abi_gki_aarch64_xml_file: Optional[str] = None, module_symvers_file: Op
     rich_table.add_column("Crc from abi_gki_aarch64.xml")
     rich_table.add_column("Crc from vmlinux.symvers")
     for item in diff_crc_items:
-        rich_table.add_row(*item)
+        rich_table.add_row(item[0], int_to_crc(item[1]), int_to_crc(item[2]))
 
     rich_console.print(rich_table)
 
@@ -74,7 +72,10 @@ def main(abi_gki_aarch64_xml_file: Optional[str] = None, module_symvers_file: Op
 
 if __name__ == "__main__":
     if len(sys.argv) == 1:
-        sys.exit(main())
+        sys.exit(main(
+            os.path.join(LOCAL_DIR, 'android', 'abi_gki_aarch64.xml'),
+            os.path.join(LOCAL_DIR, 'out', 'vmlinux.symvers')
+        ))
     if len(sys.argv) == 3:
         sys.exit(main(sys.argv[1], sys.argv[2]))
     print('Usage: %s <abi_gki_aarch64.xml file> <vmlinux.symvers file>' % sys.argv[0])
