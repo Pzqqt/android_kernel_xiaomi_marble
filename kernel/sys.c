39a05b18f92235b41eebbebb824d97472492d4ec
Juhyung Park <qkrwngud825@gmail.com>
2024-02-01 16:31:53 +0800

    kernel/sys.c: implement custom uname override
    The uname system-call will return CONFIG_UNAME_OVERRIDE_STRING on struct
    new_utsname->release when a process with CONFIG_UNAME_OVERRIDE_TARGET
    included in its cmdline calls it.
    
    Signed-off-by: Juhyung Park <qkrwngud825@gmail.com>

--- kernel/sys.c
+++ kernel/sys.c
@@ -44,6 +44,7 @@
 #include <linux/ctype.h>
 #include <linux/mm.h>
 #include <linux/mempolicy.h>
+#include <linux/string_helpers.h>
 
 #include <linux/compat.h>
 #include <linux/syscalls.h>
@@ -1239,6 +1240,24 @@ DECLARE_RWSEM(uts_sem);
 #define override_architecture(name)	0
 #endif
 
+static void override_custom_release(char __user *release, size_t len)
+{
+#ifdef CONFIG_UNAME_OVERRIDE
+	char *buf;
+
+	buf = kstrdup_quotable_cmdline(current, GFP_KERNEL);
+	if (buf == NULL)
+		return;
+
+	if (strstr(buf, CONFIG_UNAME_OVERRIDE_TARGET)) {
+		copy_to_user(release, CONFIG_UNAME_OVERRIDE_STRING,
+			       strlen(CONFIG_UNAME_OVERRIDE_STRING) + 1);
+	}
+
+	kfree(buf);
+#endif
+}
+
 /*
  * Work around broken programs that cannot handle "Linux 3.0".
  * Instead we map 3.x to 2.6.40+x, so e.g. 3.0 would be 2.6.40
@@ -1281,6 +1300,7 @@ SYSCALL_DEFINE1(newuname, struct new_utsname __user *, name)
 	if (copy_to_user(name, &tmp, sizeof(tmp)))
 		return -EFAULT;
 
+	override_custom_release(name->release, sizeof(name->release));
 	if (override_release(name->release, sizeof(name->release)))
 		return -EFAULT;
 	if (override_architecture(name))
