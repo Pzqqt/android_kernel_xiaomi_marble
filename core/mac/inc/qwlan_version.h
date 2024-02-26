167cc449bfa08ad905081d1f3e22d68648700d4b
Ravindra Konda <quic_konda@quicinc.com>
2024-02-26 08:04:13 -0800

    Release 2.0.9.21W
    Release 2.0.9.21W
    
    Change-Id: Ied0a56596615175ba5c5228fa12372d09e3d613e
    CRs-Fixed: 774533

--- core/mac/inc/qwlan_version.h
+++ core/mac/inc/qwlan_version.h
@@ -32,9 +32,9 @@
 #define QWLAN_VERSION_MAJOR            2
 #define QWLAN_VERSION_MINOR            0
 #define QWLAN_VERSION_PATCH            9
-#define QWLAN_VERSION_EXTRA            "V"
+#define QWLAN_VERSION_EXTRA            "W"
 #define QWLAN_VERSION_BUILD            21
 
-#define QWLAN_VERSIONSTR               "2.0.9.21V"
+#define QWLAN_VERSIONSTR               "2.0.9.21W"
 
 #endif /* QWLAN_VERSION_H */
