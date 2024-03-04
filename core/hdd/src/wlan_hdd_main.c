0c3a5c9a56419b4a38b0ffb735b3e166259b9751
Asutosh Mohapatra <quic_asutmoha@quicinc.com>
2024-03-04 03:58:51 -0800

    qcacld-3.0: Reset global_cl.is_active before starting logging_thread
    wlan_logging_thread starts from the load time and connectivity_logging
    starts in the start_modules. There is a possibility  that before
    connectivity logging started wlan_connectivity_log_dequeue will
    get triggered if hang event is received and as global_cl.is_active
    is not yet set, host reads garbage value while reading this and
    proceeds to take spin lock sometime. This leads to kernel assert
    as spin lock is not yet created.
    
    To address this issue reset global_cl.is_active before
    logging_thread start.
    
    Change-Id: I3649ee82846721d97b2fc938df3ca1488bc31069
    CRs-Fixed: 3763744

--- core/hdd/src/wlan_hdd_main.c
+++ core/hdd/src/wlan_hdd_main.c
@@ -1,6 +1,6 @@
 /*
  * Copyright (c) 2012-2021 The Linux Foundation. All rights reserved.
- * Copyright (c) 2021-2023 Qualcomm Innovation Center, Inc. All rights reserved.
+ * Copyright (c) 2021-2024 Qualcomm Innovation Center, Inc. All rights reserved.
  *
  * Permission to use, copy, modify, and/or distribute this software for
  * any purpose with or without fee is hereby granted, provided that the
@@ -17622,6 +17622,7 @@ int hdd_init(void)
 	wlan_init_bug_report_lock();
 
 #ifdef WLAN_LOGGING_SOCK_SVC_ENABLE
+	wlan_connectivity_logging_init();
 	wlan_logging_sock_init_svc();
 #endif
 
