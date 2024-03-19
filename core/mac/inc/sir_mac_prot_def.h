13dcbb6150ea494ade1c05102f0b96f65832c9a9
Surya Prakash Sivaraj <quic_suryapra@quicinc.com>
2024-03-19 17:22:28 +0530

    qcacld-3.0: Enhance the RSNXE inter-op logic
    Some third-party APs are not able to handle more than 1 octet
    in the RSNXE, even though RSNXE support is present.
    
    Therefore, to prevent this interop issue, send only 1 octet of
    RSNXE if the AP broadcasts only 1 octet.
    
    RSNXE handling logic summary:
    1. Don't modify userspace RSNXE when caps other than
       SAE_H2E, SAE_PK, SECURE_LTF, SECURE_RTT,
       PROT_RANGE_NEGOTIOATION are set.
    2. AP doesn't send RSNXE
       For WPA2 - Strip the RSNXE completely.
       For WPA3 - Retain only SAE capabilities such as H2E and PK.
    3. AP supports RSNXE with length 1
       For WPA2 & WPA3 - Retain only the first octet in RSNXE.
    4. AP supports RSNXE with multiple octet
       For WPA2 & WPA3 - Use the userspace assoc ie RSNXE as it is.
    
    Change-Id: I56d1d5711b067fe5e0ff19117f6a600219cb86a0
    CRs-Fixed: 3490369

--- core/mac/inc/sir_mac_prot_def.h
+++ core/mac/inc/sir_mac_prot_def.h
@@ -1,6 +1,6 @@
 /*
  * Copyright (c) 2011-2021 The Linux Foundation. All rights reserved.
- * Copyright (c) 2022-2023 Qualcomm Innovation Center, Inc. All rights reserved.
+ * Copyright (c) 2022-2024 Qualcomm Innovation Center, Inc. All rights reserved.
  *
  * Permission to use, copy, modify, and/or distribute this software for
  * any purpose with or without fee is hereby granted, provided that the
@@ -1870,6 +1870,9 @@ struct he_6ghz_capability_info {
 
 #define SIR_MAC_TXSTBC                             1
 #define SIR_MAC_RXSTBC                             1
+
+#define SIR_MAC_RSNX_CAP_MIN_LEN                   1
+#define SIR_MAC_RSNX_CAP_MAX_LEN                  16
 
 #define SIR_MAC_IE_TYPE_OFFSET                     0
 #define SIR_MAC_IE_LEN_OFFSET                      1
