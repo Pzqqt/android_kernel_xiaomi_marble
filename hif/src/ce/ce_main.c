4d46fc713e090a1825d1ccfd845c6bf907264fec
Karthik Kantamneni <quic_vkantamn@quicinc.com>
2024-03-21 10:34:56 +0530

    qcacmn: Don't program ctrl register for unused Copy engines
    Currently all the CE channels control registers are getting
    programmed as part of configuring RRI feature. Ideally host
    should not program any CE channel which is not being used.
    
    Fix this by programming only CE channels which are used by host.
    
    Change-Id: I64e662d522abc121cc4086474b20644fbfaf98c5
    CRs-Fixed: 3765898

--- hif/src/ce/ce_main.c
+++ hif/src/ce/ce_main.c
@@ -1,6 +1,6 @@
 /*
  * Copyright (c) 2013-2021 The Linux Foundation. All rights reserved.
- * Copyright (c) 2021-2023 Qualcomm Innovation Center, Inc. All rights reserved.
+ * Copyright (c) 2021-2024 Qualcomm Innovation Center, Inc. All rights reserved.
  *
  * Permission to use, copy, modify, and/or distribute this software for
  * any purpose with or without fee is hereby granted, provided that the
@@ -3643,6 +3643,7 @@ static inline void hif_config_rri_on_ddr(struct hif_softc *scn)
 	unsigned int i;
 	uint32_t high_paddr, low_paddr;
 	struct HIF_CE_state *hif_state = HIF_GET_CE_STATE(scn);
+	struct CE_attr *attr;
 	struct CE_pipe_config *ce_config;
 
 	if (hif_alloc_rri_on_ddr(scn) != QDF_STATUS_SUCCESS)
@@ -3657,7 +3658,11 @@ static inline void hif_config_rri_on_ddr(struct hif_softc *scn)
 	WRITE_CE_DDR_ADDRESS_FOR_RRI_HIGH(scn, high_paddr);
 
 	for (i = 0; i < CE_COUNT; i++) {
+		attr = &hif_state->host_ce_config[i];
 		ce_config = &hif_state->target_ce_config[i];
+
+		if (!attr->src_nentries && !attr->dest_nentries)
+			continue;
 		/*
 		 * For DST channel program both IDX_UPD_EN and
 		 * DMAX length(behalf of F.W) at once to avoid
