800e0208848c8bce18adac8ad4f012079f7977ff
Prateek Patil <quic_pratpati@quicinc.com>
2024-04-09 00:11:47 +0530

    icnss2: Add is_idle_shutdown atomic variable to solve race condition
    Currently, there might be a scenario where wlan host driver calls
    idle_shutdown and at the same time icnss driver receives shutdown
    notification for wpss. In this case Race condition might occur for
    is_ssr variable which is shared among icnss_wpss_notifier_nb() api
    and icnss_idle_shutdown() api.
    
    To resolve this race condition, set is_idle_shutdown atomic variable
    in icnss_idle_shutdown() api and check this variable before posting
    ICNSS_DRIVER_EVENT_UNREGISTER_DRIVER event.
    
    Change-Id: Ic2738a06489718ae7d1f263e67e53d29ca2b36c4
    Signed-off-by: Prateek Patil <quic_pratpati@quicinc.com>

--- drivers/soc/qcom/icnss2/main.h
+++ drivers/soc/qcom/icnss2/main.h
@@ -2,7 +2,7 @@
 /*
  * Copyright (c) 2017-2020, 2021, The Linux Foundation.
  * All rights reserved.
- * Copyright (c) 2023 Qualcomm Innovation Center, Inc. All rights reserved.
+ * Copyright (c) 2023-2024 Qualcomm Innovation Center, Inc. All rights reserved.
  */
 
 #ifndef __MAIN_H__
@@ -471,6 +471,7 @@ struct icnss_priv {
 	struct kobject *icnss_kobject;
 	struct rproc *rproc;
 	atomic_t is_shutdown;
+	atomic_t is_idle_shutdown;
 	u32 qdss_mem_seg_len;
 	struct icnss_fw_mem qdss_mem[QMI_WLFW_MAX_NUM_MEM_SEG_V01];
 	void *get_info_cb_ctx;
