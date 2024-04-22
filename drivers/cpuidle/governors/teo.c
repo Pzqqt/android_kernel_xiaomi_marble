3c09147255c80ee9d98fb6b7217b9e5a9c3216b2
ztc1997 <dev@ztc1997.com>
2024-04-22 09:32:16 +0800

    cpuidle: teo: Increase default rating
    We want the teo priority to be higher than menu but lower than qcom-cpu-lpm

--- drivers/cpuidle/governors/teo.c
+++ drivers/cpuidle/governors/teo.c
@@ -614,7 +614,7 @@ static int teo_enable_device(struct cpuidle_driver *drv,
 
 static struct cpuidle_governor teo_governor = {
 	.name =		"teo",
-	.rating =	19,
+	.rating =	30,
 	.enable =	teo_enable_device,
 	.select =	teo_select,
 	.reflect =	teo_reflect,
