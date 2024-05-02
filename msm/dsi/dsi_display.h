1affd1a1c99862be78843971b482ca4bd1e2cd90
Anand Tarakh <quic_atarakh@quicinc.com>
2024-05-02 16:03:55 +0530

    disp: msm: dsi: Add Qualcomm licence
    Add Qualcomm licence.
    
    Fixes: I829d9dbd ("disp: msm: dsi: enable splash in hibernation
    for neo target")
    
    Change-Id: I7b19db7f8bfb8fd68bf78a240165684537321a28
    Signed-off-by: Anand Tarakh <quic_atarakh@quicinc.com>
    (cherry picked from commit 246a113963e1f906aab69f0bad0379e82c981acd)

--- msm/dsi/dsi_display.h
+++ msm/dsi/dsi_display.h
@@ -1,6 +1,7 @@
 /* SPDX-License-Identifier: GPL-2.0-only */
 /*
- * Copyright (c) 2015-2021,2024 The Linux Foundation. All rights reserved.
+ * Copyright (c) 2024, Qualcomm Innovation Center, Inc. All rights reserved.
+ * Copyright (c) 2015-2021, The Linux Foundation. All rights reserved.
  */
 
 #ifndef _DSI_DISPLAY_H_
