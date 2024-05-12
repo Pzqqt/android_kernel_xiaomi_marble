260cddd3db685f9754d8c5bb4bf18832725b5f02
Pranay Varma Kopanati <quic_pkopanat@quicinc.com>
2024-05-12 14:41:32 +0530

    msm: eva: Adding kref count for cvp_get_inst_from_id
    Adding count for instance
    
    Change-Id: I1de6e3d4371f1b473907390afafa11e41806671b
    Signed-off-by: Pranay Varma Kopanati <quic_pkopanat@quicinc.com>
    (cherry picked from commit 0b08404795196283af230fcf94b9a9a52610e8f1)

--- msm/eva/hfi_response_handler.c
+++ msm/eva/hfi_response_handler.c
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: GPL-2.0-only
 /*
  * Copyright (c) 2018-2021, The Linux Foundation. All rights reserved.
+ * Copyright (c) 2024, Qualcomm Innovation Center, Inc. All rights reserved.
  */
 
 #include <linux/bitops.h>
@@ -406,7 +407,7 @@ retry:
 			}
 		}
 
-		inst = match ? inst : NULL;
+		inst = match && kref_get_unless_zero(&inst->kref) ? inst : NULL;
 		mutex_unlock(&core->lock);
 	} else {
 		if (core->state == CVP_CORE_UNINIT)
@@ -462,6 +463,7 @@ static int hfi_process_session_dump_notify(u32 device_id,
 	info->response_type = HAL_SESSION_DUMP_NOTIFY;
 	info->response.cmd = cmd_done;
 
+	cvp_put_inst(inst);
 	return 0;
 }
 
@@ -500,7 +502,7 @@ static int hfi_process_session_cvp_msg(u32 device_id,
 	sess_msg = kmem_cache_alloc(cvp_driver->msg_cache, GFP_KERNEL);
 	if (sess_msg == NULL) {
 		dprintk(CVP_ERR, "%s runs out msg cache memory\n", __func__);
-		return -ENOMEM;
+		goto error_no_mem;
 	}
 
 	memcpy(&sess_msg->pkt, pkt, get_msg_size(pkt));
@@ -523,11 +525,14 @@ static int hfi_process_session_cvp_msg(u32 device_id,
 
 	info->response_type = HAL_NO_RESP;
 
+	cvp_put_inst(inst);
 	return 0;
 
 error_handle_msg:
 	spin_unlock(&sq->lock);
 	kmem_cache_free(cvp_driver->msg_cache, sess_msg);
+error_no_mem:
+	cvp_put_inst(inst);
 	return -ENOMEM;
 }
 
