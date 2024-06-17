b3d4793de2f3f300adc58829624627db52113a4c
Ravindra Konda <quic_konda@quicinc.com>
2024-06-17 01:47:54 -0700

    Release 2.0.9.22M
    Release 2.0.9.22M
    
    Change-Id: I105df148e77c35fedf7be5d2082c00dc0598a50c
    CRs-Fixed: 774533

--- core/mac/inc/qwlan_version.h
+++ core/mac/inc/qwlan_version.h
@@ -32,9 +32,9 @@
 #define QWLAN_VERSION_MAJOR            2
 #define QWLAN_VERSION_MINOR            0
 #define QWLAN_VERSION_PATCH            9
-#define QWLAN_VERSION_EXTRA            "L"
+#define QWLAN_VERSION_EXTRA            "M"
 #define QWLAN_VERSION_BUILD            22
 
-#define QWLAN_VERSIONSTR               "2.0.9.22L"
+#define QWLAN_VERSIONSTR               "2.0.9.22M"
 
 #endif /* QWLAN_VERSION_H */
