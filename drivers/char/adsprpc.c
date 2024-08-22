9588d6d32630ff9a3822b2e2dc85d6d333ef59f2
Anand Kulkarni <quic_anankulk@quicinc.com>
2024-08-22 19:40:07 +0530

    dsp-kernel: Avoid taking reference for group_info
    Currently, the get_current_groups API accesses group info, which
    increases the usage refcount. If the IOCTL using the
    get_current_groups API is called many times, the usage counter
    overflows. To avoid this, access group info without taking a
    reference. A reference is not required as group info is not
    released during the IOCTL call.
    
    Change-Id: Ibe35d316d23f24f8d60970e4909eeaab512a7bf3
    Acked-by: quic_anane <quic_anane@quicinc.com>
    Signed-off-by: Anand Kulkarni <quic_anankulk@quicinc.com>

--- drivers/char/adsprpc.c
+++ drivers/char/adsprpc.c
@@ -6299,7 +6299,7 @@ static int fastrpc_device_open(struct inode *inode, struct file *filp)
 
 static int fastrpc_get_process_gids(struct gid_list *gidlist)
 {
-	struct group_info *group_info = get_current_groups();
+	struct group_info *group_info = current_cred()->group_info;
 	int i = 0, err = 0, num_gids = group_info->ngroups + 1;
 	unsigned int *gids = NULL;
 
