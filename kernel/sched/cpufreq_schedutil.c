f657b866b5f4a889e73b385966d26bbc760ee6ee
Pzqqt <821026875@qq.com>
2026-09-14 21:32:24 +0800

    Revert "schedutil : cap iowait boost by uclamp_max"
    Conflict with changes in v5.10.270.
    
    This reverts commit 95d0b0f837f5f12ed2bbb4459daeb077b2c107b1.

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -489,10 +489,7 @@ static unsigned long sugov_iowait_apply(struct sugov_cpu *sg_cpu, u64 time,
 	 * into the same scale so we can compare.
 	 */
 	boost = (sg_cpu->iowait_boost * max) >> SCHED_CAPACITY_SHIFT;
-	boost = max(boost, util);
-	boost = uclamp_rq_util_with(cpu_rq(sg_cpu->cpu), boost, NULL);
-
-	return boost;
+	return max(boost, util);
 }
 
 #ifdef CONFIG_NO_HZ_COMMON
