4e88ddbf4c42d3dcbe1e46eed131126d91e3cd10
Pzqqt <821026875@qq.com>
2026-09-14 21:58:52 +0800

    Revert "remoteproc: Introduce function rproc_detach()"
    This reverts commit cab21c9992042e339c0b1a8e47e235ef85a47cf0.

--- include/linux/remoteproc.h
+++ include/linux/remoteproc.h
@@ -404,7 +404,6 @@ rproc_of_resm_mem_entry_init(struct device *dev, u32 of_resm_idx, size_t len,
 
 int rproc_boot(struct rproc *rproc);
 void rproc_shutdown(struct rproc *rproc);
-int rproc_detach(struct rproc *rproc);
 int rproc_set_firmware(struct rproc *rproc, const char *fw_name);
 void rproc_report_crash(struct rproc *rproc, enum rproc_crash_type type);
 
